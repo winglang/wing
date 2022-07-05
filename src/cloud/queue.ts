47a3b53de4d9d09e0f442f58e850debfdd338aee
Elad Ben-Israel <elad.benisrael@gmail.com>
2022-07-05 21:39:58 +0300

    chore: make some updates to queue

--- src/cloud/queue.ts
+++ src/cloud/queue.ts
@@ -1,15 +1,25 @@
 
 import { Construct } from 'constructs';
-import { Binding, ICapturable, ICaptureSource } from '../core';
+import { Binding, Duration, ICapturable, ICaptureSource } from '../core';
 import { Function } from './lambda';
 
+export interface QueueProps {
+  readonly timeout: Duration;
+}
+
 export class Queue extends Construct implements ICapturable {
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, props: QueueProps) {
     super(scope, id);
+
+    console.log(`Timeout is ${props.timeout.seconds} seconds`);
   }
 
   public addWorker(fn: Function) {
     fn;
+  }
+
+  public hello() {
+    console.log('world!');
   }
 
   public capture(_symbol: string, _binding: Binding): ICaptureSource {
