ea1e746694880a25bcff19d16439a024a6341b3f
Mark McCulloh <Mark.McCulloh@gmail.com>
2022-07-27 18:43:19 -0400

    add TypedSymbol

--- wingc/src/type_env.rs
+++ wingc/src/type_env.rs
@@ -1,8 +1,13 @@
 use crate::{ast::Symbol, type_check::Type};
 use std::collections::HashMap;
 
+pub struct TypedSymbol {
+	pub symbol: Symbol,
+	pub _type: Type,
+}
+
 pub struct TypeEnv<'a> {
-	type_map: HashMap<String, Type>,
+	type_map: HashMap<String, TypedSymbol>,
 	parent: Option<&'a TypeEnv<'a>>,
 	pub return_type: Option<Type>,
 }
@@ -18,17 +23,23 @@ impl<'a> TypeEnv<'a> {
 	}
 
 	pub fn define(&mut self, symbol: &Symbol, _type: Type) {
-		if self.type_map.contains_key(&symbol.name) {
-			// TODO span is a byte offset, not a line number
-			panic!("Symbol {} already defined.", symbol);
+		if let Some(typed_symbol) = self.type_map.get(&symbol.name) {
+			// TODO span is a byte offset, not a friendly character one
+			panic!("Symbol {} already defined at {}.", symbol, typed_symbol.symbol.span);
 		}
 
-		self.type_map.insert(symbol.name.clone(), _type);
+		self.type_map.insert(
+			symbol.name.clone(),
+			TypedSymbol {
+				symbol: symbol.clone(),
+				_type,
+			},
+		);
 	}
 
 	pub fn lookup(&self, symbol: &Symbol) -> &Type {
-		if let Some(_type) = self.type_map.get(&symbol.name) {
-			_type
+		if let Some(typed_symbol) = self.type_map.get(&symbol.name) {
+			&typed_symbol._type
 		} else if let Some(parent_env) = self.parent {
 			parent_env.lookup(symbol)
 		} else {
