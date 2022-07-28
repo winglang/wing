506d3df90c8dada400e1ceda3c93df55b2d27fd0
Mark McCulloh <Mark.McCulloh@gmail.com>
2022-07-28 06:55:01 -0400

    Revert "add TypedSymbol"
    This reverts commit ea1e746694880a25bcff19d16439a024a6341b3f.

--- wingc/src/type_env.rs
+++ wingc/src/type_env.rs
@@ -1,13 +1,8 @@
 use crate::{ast::Symbol, type_check::Type};
 use std::collections::HashMap;
 
-pub struct TypedSymbol {
-	pub symbol: Symbol,
-	pub _type: Type,
-}
-
 pub struct TypeEnv<'a> {
-	type_map: HashMap<String, TypedSymbol>,
+	type_map: HashMap<String, Type>,
 	parent: Option<&'a TypeEnv<'a>>,
 	pub return_type: Option<Type>,
 }
@@ -23,23 +18,17 @@ impl<'a> TypeEnv<'a> {
 	}
 
 	pub fn define(&mut self, symbol: &Symbol, _type: Type) {
-		if let Some(typed_symbol) = self.type_map.get(&symbol.name) {
-			// TODO span is a byte offset, not a friendly character one
-			panic!("Symbol {} already defined at {}.", symbol, typed_symbol.symbol.span);
+		if self.type_map.contains_key(&symbol.name) {
+			// TODO span is a byte offset, not a line number
+			panic!("Symbol {} already defined.", symbol);
 		}
 
-		self.type_map.insert(
-			symbol.name.clone(),
-			TypedSymbol {
-				symbol: symbol.clone(),
-				_type,
-			},
-		);
+		self.type_map.insert(symbol.name.clone(), _type);
 	}
 
 	pub fn lookup(&self, symbol: &Symbol) -> &Type {
-		if let Some(typed_symbol) = self.type_map.get(&symbol.name) {
-			&typed_symbol._type
+		if let Some(_type) = self.type_map.get(&symbol.name) {
+			_type
 		} else if let Some(parent_env) = self.parent {
 			parent_env.lookup(symbol)
 		} else {
