3f6040abef5e7e92db1e8e2341f9ea489588d918
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-07-30 15:43:15 -0700

    added "mkwing" as a test CLI to push to npm temporarily

---
+++ mkwing/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+const child_process = require("child_process");
+
+// Add the "bin" directory to LD_LIBRARY_PATH
+process.env.LD_LIBRARY_PATH = path.join(__dirname, "bin");
+
+// Spawn "wingc" binary and forward stdio to it
+child_process.spawnSync(path.join(__dirname, "bin/wingc"), process.argv.slice(2), {
+  stdio: "inherit",
+});
