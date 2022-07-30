213dc2051df08c3f52cb5842c66a761f4c071cdd
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-07-30 15:54:29 -0700

    building and uploading artifacts for each platform

--- mkwing/index.js
+++ mkwing/index.js
@@ -4,6 +4,8 @@ const child_process = require("child_process");
 
 // Add the "bin" directory to LD_LIBRARY_PATH
 process.env.LD_LIBRARY_PATH = path.join(__dirname, "bin");
+process.env.DYLD_LIBRARY_PATH = path.join(__dirname, "bin");
+process.env.DYLD_FALLBACK_LIBRARY_PATH = path.join(__dirname, "bin");
 
 // Spawn "wingc" binary and forward stdio to it
 child_process.spawnSync(path.join(__dirname, "bin/wingc"), process.argv.slice(2), {
