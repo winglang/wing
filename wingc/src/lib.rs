3f5364dda3895ad355b6b9e554e46fbf44bfa1e3
Mark McCulloh <Mark.McCulloh@gmail.com>
2022-08-15 12:09:14 -0400

    feat(wingc): Separate Rust API and C API and extend testing (#37)
    * wip
    
    * we technically allow variable re-declaration right now
    
    * removed yarn by accident
    
    * dumb math

--- wingc/src/lib.rs
+++ wingc/src/lib.rs
@@ -1,3 +1,5 @@
+use ast::Scope;
+
 use crate::parser::Parser;
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
@@ -14,14 +16,7 @@ pub mod parser;
 pub mod type_check;
 pub mod type_env;
 
-#[no_mangle]
-pub extern "C" fn wingc_compile(source: *const c_char, outdir: *const c_char) -> *const c_char {
-	let source_file = unsafe { CStr::from_ptr(source).to_str().unwrap() };
-	let out_dir = if outdir != std::ptr::null() {
-		unsafe { CStr::from_ptr(outdir).to_str().unwrap() }
-	} else {
-		Box::leak(format!("{}.out", source_file).into_boxed_str())
-	};
+pub fn parse(source_file: &str) -> Scope {
 	let language = tree_sitter_winglang::language();
 	let mut parser = tree_sitter::Parser::new();
 	parser.set_language(language).unwrap();
@@ -42,21 +37,42 @@ pub extern "C" fn wingc_compile(source: *const c_char, outdir: *const c_char) ->
 		}
 	};
 
-	let out_dir = PathBuf::from(&out_dir);
-	fs::create_dir_all(&out_dir).expect("create output dir");
-
-	let ast_root = Parser {
+	return Parser {
 		source: &source[..],
 		source_name: source_file.to_string(),
 	}
 	.wingit(&tree.root_node());
+}
 
+pub fn type_check(scope: &Scope) -> TypeEnv {
 	let mut root_env = TypeEnv::new(None, None, false);
-
 	let mut tc = TypeChecker::new();
-	tc.type_check_scope(&ast_root, &mut root_env);
+	tc.type_check_scope(&scope, &mut root_env);
 
-	let output = jsify::jsify(&ast_root, true);
+	return root_env;
+}
+
+pub fn compile(source_file: &str, out_dir: Option<&str>) -> String {
+	let scope = parse(source_file);
+	type_check(&scope);
+
+	// prepare output directory for support inflight code
+	let out_dir = PathBuf::from(&out_dir.unwrap_or(format!("{}.out", source_file).as_str()));
+	fs::create_dir_all(&out_dir).expect("create output dir");
+
+	return jsify::jsify(&scope, true);
+}
+
+#[no_mangle]
+pub extern "C" fn wingc_compile(source: *const c_char, outdir: *const c_char) -> *const c_char {
+	let source_file = unsafe { CStr::from_ptr(source).to_str().unwrap() };
+	let out_dir = if outdir != std::ptr::null() {
+		unsafe { Some(CStr::from_ptr(outdir).to_str().unwrap()) }
+	} else {
+		None
+	};
+
+	let output = compile(source_file, out_dir);
 
 	CString::new(output).unwrap().into_raw()
 }
@@ -68,15 +84,21 @@ pub extern "C" fn wingc_release(s: *const c_char) {
 
 #[cfg(test)]
 mod sanity {
-	use crate::{wingc_compile, wingc_release};
-	use std::ffi::CString;
-	use std::ptr::null;
+	use crate::compile;
+	use std::fs;
 
 	#[test]
-	fn does_not_blow_up() {
-		let source = "../corpus/math.w";
-		let source_raw = CString::new(source).unwrap();
-		let intermediate = wingc_compile(source_raw.as_ptr(), null());
-		wingc_release(intermediate)
+	fn can_compile_simple_files() {
+		let paths = fs::read_dir("../corpus/simple").unwrap();
+
+		for entry in paths {
+			if let Ok(entry) = entry {
+				if let Some(source) = entry.path().to_str() {
+					if source.ends_with(".w") {
+						println!("\n=== {} ===\n{}\n---", source, compile(source, None));
+					}
+				}
+			}
+		}
 	}
 }
