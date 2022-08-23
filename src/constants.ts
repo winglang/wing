8997a941ae5123982a62096f1e46813c1251abd4
Shai Ber <shaib@equitybee.com>
2022-08-23 20:10:03 +0300

    fix paths to SDK clients

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const CLIENTS_PACKAGE = "@monadahq/wingsdk-clients";
-export const LOCAL_CLIENTS_PATH: string = `${CLIENTS_PACKAGE}/lib/local`;
-export const TERRAFORM_AWS_CLIENTS_PATH: string = `${CLIENTS_PACKAGE}/lib/tf-aws`;
+export const LOCAL_CLIENTS_PATH: string = `${CLIENTS_PACKAGE}/local`;
+export const TERRAFORM_AWS_CLIENTS_PATH: string = `${CLIENTS_PACKAGE}/aws`;
