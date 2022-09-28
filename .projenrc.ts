4b3db3bda0bb5be4de40e8a03c671d67d5559fb4
Ainvoner <2538825+ainvoner@users.noreply.github.com>
2022-09-28 17:52:39 +0300

    feat: publish app (#72)

--- .projenrc.ts
+++ .projenrc.ts
@@ -80,7 +80,7 @@ project.addTask("dev").exec("vite");
 project.compileTask.exec("vite build");
 // TODO: track https://github.com/electron-userland/electron-builder/issues/6411
 project.compileTask.exec(
-  "npm exec electron-builder --config=electron-builder.json5",
+  "npm exec electron-builder --publish=always --config=electron-builder.json5",
 );
 project.addTask("storybook").exec("start-storybook -p 6006");
 project.addTask("build-storybook").exec("build-storybook");
