cc29f5ca9d024290310da4fd5ec31a24489110c7
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-10-04 08:56:18 -0700

    compile subcommand needs a source arg too

--- libs/wingc/src/bin.rs
+++ libs/wingc/src/bin.rs
@@ -13,9 +13,12 @@ enum Commands {
 	/// Compile a Wing source file
 	#[command(arg_required_else_help = true)]
 	Compile {
-		/// target to compile to. currently ignored.
+		/// target to compile to (tf-aws, local, etc.)
 		#[arg(required = true)]
 		target: String,
+		/// source file to compile
+		#[arg(required = true)]
+		source: String,
 		/// output directory. defaults to a .out directory
 		outdir: Option<String>,
 	},
@@ -33,11 +36,12 @@ pub fn main() {
 	let args = CliArgs::parse();
 
 	match args.command {
-		Commands::Compile { target, outdir } => {
-			compile(target.as_str(), outdir.as_deref());
-		},
+		Commands::Compile { target, source, outdir } => {
+			println!("Compiling for {}", target);
+			compile(source.as_str(), outdir.as_deref());
+		}
 		Commands::Run { executable } => {
 			println!("Running {}", executable);
-		},
+		}
 	}
 }
