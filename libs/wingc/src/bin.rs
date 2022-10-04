15a404fc05aa1794f156a32db2cd1ad96fdb06f7
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-10-04 10:28:26 -0700

    added string passing mechanic between Rust and WASM without breaking native builds

--- libs/wingc/src/bin.rs
+++ libs/wingc/src/bin.rs
@@ -35,6 +35,24 @@ enum Commands {
 	},
 }
 
+// there are two schools of thought when it comes to passing strings from native
+// to WASM side: 1) a function is declared "extern" and native passes a byte arr
+// to it, WASM side decodes and executes. 2) native side stores the strings into
+// WASM's linear memory and WASM side accesses it after native call returns.
+// Here we use option #2 because declaring an "extern" makes the binary useless
+// in non-WASM environments.
+const PATH_MAX: usize = 4096;
+static mut EXECUTABLE_PATH_BUF: [u8; PATH_MAX] = [0; PATH_MAX];
+static mut EXECUTABLE_PATH_LEN: usize = 0;
+#[no_mangle]
+pub fn get_executable_buf() -> *mut u8 {
+	unsafe { EXECUTABLE_PATH_BUF.as_mut_ptr() }
+}
+#[no_mangle]
+pub fn get_executable_len() -> usize {
+	unsafe { EXECUTABLE_PATH_LEN }
+}
+
 pub fn main() {
 	let args = CliArgs::parse();
 
@@ -47,8 +65,13 @@ pub fn main() {
 			println!("Compiling for {}", target);
 			compile(source.as_str(), outdir.as_deref());
 		}
-		Commands::Run { executable } => {
+		Commands::Run { executable } => unsafe {
 			println!("Running {}", executable);
-		}
+			EXECUTABLE_PATH_LEN = executable.len();
+			EXECUTABLE_PATH_BUF[..EXECUTABLE_PATH_LEN].copy_from_slice(executable.as_bytes());
+			// when running in WASM mode, the host will call "get_executable_buf" and
+			// "get_executable_len" to get the executable buffer and its size out of
+			// WASM's linear memory. Then it will execute the executable.
+		},
 	}
 }
