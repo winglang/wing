0ed41995681bb161c39c841f119cf25d652e3845
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-10-04 08:51:30 -0700

    swapped the built-in arg parser with clap, hooked up to compile()

--- libs/wingc/src/bin.rs
+++ libs/wingc/src/bin.rs
@@ -1,15 +1,43 @@
-use std::env;
+use clap::{Parser, Subcommand};
 use wingc::compile;
 
+#[derive(Parser, Debug)]
+#[command(about = "Wing Programming Language CLI", long_about = None)]
+struct CliArgs {
+	#[command(subcommand)]
+	command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+	/// Compile a Wing source file
+	#[command(arg_required_else_help = true)]
+	Compile {
+		/// target to compile to. currently ignored.
+		#[arg(required = true)]
+		target: String,
+		/// output directory. defaults to a .out directory
+		outdir: Option<String>,
+	},
+
+	/// Runs a Wing compiled executable
+	#[command(arg_required_else_help = true)]
+	Run {
+		/// a wing executable to run
+		#[arg(required = true)]
+		executable: String,
+	},
+}
+
 pub fn main() {
-	let args: Vec<String> = env::args().collect();
+	let args = CliArgs::parse();
 
-	if args.len() < 2 {
-		panic!("Usage: wingc <source_file> [output_dir]");
+	match args.command {
+		Commands::Compile { target, outdir } => {
+			compile(target.as_str(), outdir.as_deref());
+		},
+		Commands::Run { executable } => {
+			println!("Running {}", executable);
+		},
 	}
-
-	let source = &args[1];
-	let outdir = args.get(2).map(|s| s.as_str());
-
-	println!("{}", compile(source, outdir));
 }
