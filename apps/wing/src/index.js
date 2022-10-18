ea07e29767c800a5f977ad9d59b33780385d582a
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-10-18 11:21:11 -0700

    verbose debug logging in wing CLI + some cleanup in the compile command

--- apps/wing/src/index.js
+++ apps/wing/src/index.js
@@ -3,6 +3,8 @@ const { bootstrap } = require("./bootstrap");
 const { Command } = require("commander");
 const { argv } = require("process");
 const { readFile, mkdir } = require("fs/promises");
+const { version } = require("../package.json");
+const debug = require("debug")("wing:index");
 
 // Note: it is important that these variables are defined as exactly
 // they are defined here. These are in the form of "hints" for "pkg"
@@ -11,16 +13,15 @@ const WINGC_WASM_PATH = resolve(__dirname, "../wingc.wasm");
 const WINGC_CURR_WDIR = resolve(process.cwd(), ".wing");
 
 async function main() {
-  const { WASI } = require("wasi");
-
   const program = new Command();
 
   program.name("wing");
+  program.version(version);
 
   program
     .command("bootstrap")
     .description("Bootstraps the given directory for Wing")
-    .argument("<directory>", "The directory to bootstrap (e.g. .wing)")
+    .argument("<context>", "The context directory to bootstrap (e.g. .wing)")
     .action(async (directory) => {
       await bootstrap(directory);
     });
@@ -29,23 +30,24 @@ async function main() {
     .command("compile")
     .description("Compile a wing file")
     .argument("<input-file>", "input file")
-    .option("-o, --out-dir <out-dir>", "Output directory", WINGC_CURR_WDIR)
-    .option("-t, --target <target>", "Target platform", "aws-tfe")
+    .option("-c, --context <context>", "Context directory", WINGC_CURR_WDIR)
     .option("-s, --skip-bootstrap", "Skip automatic bootstrapping")
     .action(async (inputFile, options) => {
-      if (!options.skipBootstrap) {
-        await bootstrap(WINGC_CURR_WDIR);
-      }
       const wingFile = inputFile;
+      debug("Wing file: %s", wingFile);
       const wingDir = dirname(wingFile);
+      debug("Wing directory: %s", wingDir);
+      const workDir = options.context;
+      debug("Work directory: %s", workDir);
+      const args = [argv[0], wingFile, workDir];
+      debug("Arguments: %s", args);
 
-      const args = [argv[0], wingFile];
+      if (!options.skipBootstrap) {
+        await bootstrap(workDir);
+      }
 
-      // create all intermediate files in a .wing directory
-      const workdir = ".wing/";
-      await mkdir(workdir, { recursive: true });
-      args.push(workdir);
-
+      debug("Loading wingc.wasm");
+      const { WASI } = require("wasi");
       const wasi = new WASI({
         args,
         env: {
@@ -54,15 +56,17 @@ async function main() {
         },
         preopens: {
           [wingDir]: wingDir,
-          [workdir]: workdir,
+          [workDir]: workDir,
         },
       });
 
-      // Some WASI binaries require:
+      debug("wingc.wasm loaded, preparing the WASI importObject");
       const importObject = { wasi_snapshot_preview1: wasi.wasiImport };
-
+      debug("Compiling wingc.wasm");
       const wasm = await WebAssembly.compile(await readFile(WINGC_WASM_PATH));
+      debug("wingc.wasm compiled, instantiating");
       const instance = await WebAssembly.instantiate(wasm, importObject);
+      debug("wingc.wasm instantiated, starting");
       wasi.start(instance);
     });
 
