3e22781f8d030b435cee82d42024f13dfb1ec263
Christopher Rybicki <chrisr@monada.co>
2022-10-24 22:15:48 -0400

    add exports

--- libs/wingsdk/src/sim/exports.ts
+++ libs/wingsdk/src/sim/exports.ts
@@ -2,4 +2,5 @@ export * from "./index";
 export * from "./queue.inflight";
 export * from "./bucket.inflight";
 export * from "./function.inflight";
+export * from "./logger.inflight";
 export * from "./schema-resources";
