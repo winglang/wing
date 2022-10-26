61db4f882abfe06869ab339ce17aaae2d08aa53a
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-10-26 10:43:33 -0700

    fail if this key doesn't exist already (when we rename the package)

--- apps/wing/version.js
+++ apps/wing/version.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
+const assert = require("assert");
 const pJson = require("./package.json");
 // assign the same version as CLI to the wingsdk
+assert(
+  typeof pJson.dependencies["@monadahq/wingsdk"] === "string",
+  "wingsdk dependency key not found"
+);
 pJson.dependencies["@monadahq/wingsdk"] = pJson.version;
 fs.writeFileSync("./package.json", JSON.stringify(pJson, undefined, 2));
