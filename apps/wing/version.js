c2f970f579edba3ac9b5b8fcf0d229b2d18e1402
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2022-10-26 09:35:21 -0700

    fix the version script so it's not dependent on env vars

--- apps/wing/version.js
+++ apps/wing/version.js
@@ -1,9 +1,5 @@
 const fs = require("fs");
-const assert = require("assert");
-const VERSION = process.env.PROJEN_BUMP_VERSION;
-assert(VERSION, "PROJEN_BUMP_VERSION is not set");
-
 const pJson = require("./package.json");
-pJson.version = VERSION;
-pJson.dependencies["@monadahq/wingsdk"] = VERSION;
+// assign the same version as CLI to the wingsdk
+pJson.dependencies["@monadahq/wingsdk"] = pJson.version;
 fs.writeFileSync("./package.json", JSON.stringify(pJson, undefined, 2));
