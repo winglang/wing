63a8f1bbc4a8f2d6fa0708271d8219af9033e89b
Ainvoner <2538825+ainvoner@users.noreply.github.com>
2022-11-22 13:02:09 +0200

    fix: header banner design (#193)
    worked with @amielpb and adjusted styles
    
    Signed-off-by: monabot <monabot@monada.co>
    Co-authored-by: monabot <monabot@monada.co>

--- src/components/HeaderBanner.tsx
+++ src/components/HeaderBanner.tsx
@@ -14,23 +14,23 @@ export const HeaderBanner = ({
   onClose,
 }: HeaderBannerProps) => {
   return (
-    <div className="bg-indigo-600">
+    <div className="bg-monada-light">
       <div className="mx-auto max-w-7xl py-3 px-3 sm:px-6 lg:px-8">
         <div className="flex flex-wrap items-center justify-between">
           <div className="flex w-0 flex-1 items-center">
-            <span className="flex rounded-lg bg-indigo-800 p-2">
+            <span className="flex rounded-lg bg-transparent p-2">
               <MegaphoneIcon
-                className="h-6 w-6 text-white"
+                className="h-6 w-6 text-monada-black"
                 aria-hidden="true"
               />
             </span>
-            <p className="ml-3 truncate font-medium text-white">
+            <p className="ml-3 truncate font-medium text-monada-black">
               <span className="hidden md:inline">{title}</span>
             </p>
           </div>
           <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
             <button
-              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50"
+              className="flex items-center justify-center rounded-md border border-transparent bg-monada-dark px-4 py-2 text-sm font-medium text-monada-black shadow-sm"
               onClick={onClick}
             >
               {buttonLabel}
@@ -39,11 +39,14 @@ export const HeaderBanner = ({
           <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
             <button
               type="button"
-              className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+              className="-mr-1 flex rounded-md p-2 sm:-mr-2"
               onClick={onClose}
             >
               <span className="sr-only">Dismiss</span>
-              <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+              <XMarkIcon
+                className="h-6 w-6 text-monada-dark"
+                aria-hidden="true"
+              />
             </button>
           </div>
         </div>
