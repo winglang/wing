fbbdb74180d32d3d59841764924247be5d54fef6
Christopher Rybicki <chrisr@monada.co>
2022-11-21 23:48:33 -0500

    pin SDK dependencies

--- libs/wingsdk/.projenrc.ts
+++ libs/wingsdk/.projenrc.ts
@@ -18,18 +18,20 @@ const project = new cdk.JsiiProject({
   peerDeps: [...JSII_DEPS],
   deps: [...JSII_DEPS],
   bundledDeps: [
-    "safe-stable-stringify",
     // preflight dependencies
     "esbuild-wasm",
+    "safe-stable-stringify",
     // aws client dependencies
-    "@aws-sdk/client-s3",
-    "@aws-sdk/client-lambda",
-    "@aws-sdk/client-sqs",
-    "@aws-sdk/client-dynamodb",
-    "@aws-sdk/client-cloudwatch-logs",
-    "@aws-sdk/types",
-    "@aws-sdk/util-utf8-node",
-    "@aws-sdk/util-stream-node",
+    // (note: these should always be updated together, otherwise they will
+    // conflict with each other)
+    "@aws-sdk/client-cloudwatch-logs@3.215.0",
+    "@aws-sdk/client-dynamodb@3.215.0",
+    "@aws-sdk/client-lambda@3.215.0",
+    "@aws-sdk/client-s3@3.215.0",
+    "@aws-sdk/client-sqs@3.215.0",
+    "@aws-sdk/types@3.215.0",
+    "@aws-sdk/util-stream-node@3.215.0",
+    "@aws-sdk/util-utf8-node@3.208.0",
     // simulator dependencies
     "tar",
   ],
