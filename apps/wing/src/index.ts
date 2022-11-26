7cdee6515a70aa00c0aca033b4e393585f60aab4
Joao Pedro Zanutto <jpberno@gmail.com>
2022-11-26 09:06:39 -0300

    Update apps/wing/src/index.ts
    Co-authored-by: Elad Ben-Israel <eladb@monada.co>

--- apps/wing/src/index.ts
+++ apps/wing/src/index.ts
@@ -60,7 +60,7 @@ async function main() {
 function checkNodeVersion(){
   const supportedVersion = "^v18.0.0"
 
-  if(!satisfies(process.version, supportedVersion)){
+  if (!satisfies(process.version, supportedVersion)) {
     console.log("WARN: You are running node " + process.version + ", please change to " + supportedVersion)
   }
 }
