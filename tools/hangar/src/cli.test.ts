3153c0b365dfdafc94e3c99d557ef28733eb8e68
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2023-01-03 21:44:48 -0800

    of course this api needs to be different from mocha,jest, and jasmin. of course.

--- tools/hangar/src/cli.test.ts
+++ tools/hangar/src/cli.test.ts
@@ -102,9 +102,9 @@ async function enterTestDir(testDir: string) {
 async function runWingCompile(wingBinPrefix: string, wingFile: string) {
 	const isError = path.dirname(wingFile).endsWith("error");
 	if (isError) {
-		await expect(() => $`${wingBinPrefix}/wing compile ${wingFile}`).rejects.toThrow();
+		await expect($`${wingBinPrefix}/wing compile ${wingFile}`).rejects.toThrow();
 	} else {
-		await expect(() => $`${wingBinPrefix}/wing compile ${wingFile}`).resolves.toBeDefined();
+		await expect($`${wingBinPrefix}/wing compile ${wingFile}`).resolves.toBeDefined();
 	}
 }
 
