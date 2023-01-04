a9f00ba31ecc8daed116e92c3a9b12c0b0049ae2
Sepehr Laal <5657848+3p3r@users.noreply.github.com>
2023-01-04 06:23:09 -0800

    trying to move around nx' quoting behavior

--- tools/hangar/src/cli.test.ts
+++ tools/hangar/src/cli.test.ts
@@ -99,12 +99,19 @@ async function enterTestDir(testDir: string) {
   $.cwd = testDir;
 }
 
-async function runWingCompile(wingBinPrefix: string, wingFile: string) {
+enum InvocationType {
+	Direct,
+	NPX,
+}
+
+async function runWingCompile(type: InvocationType, wingFile: string) {
 	const isError = path.dirname(wingFile).endsWith("error");
-  const work = async () => {
-    const out = await $`${path.join(wingBinPrefix, 'wing')} compile ${wingFile}`
-    return out.exitCode;
-  };
+	const work = async () => {
+		const out = await (type === InvocationType.Direct
+			? $`${npxBin} @winglang/wing compile ${wingFile}`
+			: $`../node_modules/.bin/wing compile ${wingFile}`);
+		return out.exitCode;
+	};
 	if (isError) {
 		await expect(work()).rejects.toThrow();
 	} else {
@@ -123,7 +130,7 @@ test.each(validWingFiles)(
 
       await enterTestDir(test_dir);
 
-      await runWingCompile(`${npxBin} @winglang/`, path.join(validTestDir, wingFile));
+      await runWingCompile(InvocationType.NPX, path.join(validTestDir, wingFile));
       const npx_tfManifest = sanitize_json_paths(tf_manifest);
       const npx_tfJson = sanitize_json_paths(tf_json);
 
@@ -142,7 +149,7 @@ test.each(validWingFiles)(
         }
       }
 
-      await runWingCompile('../node_modules/.bin/', path.join(validTestDir, wingFile));
+      await runWingCompile(InvocationType.Direct, path.join(validTestDir, wingFile));
 
       expect(sanitize_json_paths(tf_manifest)).toStrictEqual(npx_tfManifest);
       expect(sanitize_json_paths(tf_json)).toStrictEqual(npx_tfJson);
@@ -160,9 +167,9 @@ test.each(validWingFiles)(
       const test_dir = path.join(tmpDir, `${wingFile}_sim`);
       await enterTestDir(test_dir);
 
-      await runWingCompile(`${npxBin} @winglang/`, path.join(validTestDir, wingFile));
+      await runWingCompile(InvocationType.NPX, path.join(validTestDir, wingFile));
 
-      await runWingCompile('../node_modules/.bin/', path.join(validTestDir, wingFile));
+      await runWingCompile(InvocationType.Direct, path.join(validTestDir, wingFile));
 
       // TODO snapshot .wsim contents
     });
