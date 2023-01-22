winglang/wing

commit b1242ab3fc1b7c9c8f84f204e374ad41088db2fc

fix: playground crashes due to memory leaks (#1199)
wingc is no longer a binary, now only a library. The cli and playground use it via WASM and pointer fun.
So, far this seems to increase the speed of `wing compile ...` by ~80%! It unfortunately slows down the playground a little bit, but now it no longer has a huge memory leak and won't just break after a while.

Right now the logic for `wingcInvoke` is duplicated for the cli and playground. `wingc` should eventually have a wrapper npm package that includes this logic. 

Added an "example" for wingc, which allows a similar workflow to the previous "cargo run". No doc updates needed, as it can be accessed via `nx dev` like before.

*By submitting this pull request, I confirm that my contribution is made under the terms of the 
[Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

apps/wing-playground/index.html
@@ -122,18 +122,12 @@
     #wing-side {
       display: inline-flex;
       flex-grow: 1;
-      padding: 2rem;
-      height: 80vh;
+      padding: 0.5rem;
     }
 
     #js-side {
       width: 60%;
-      height: 80vh;
       font-size: 12;
-    }
-
-    #intermediateJS {
-      height: 98%;
     }
 
     #lineCounter {
@@ -143,7 +137,12 @@
       overflow-y: hidden;
       text-align: right;
       box-shadow: none;
-      width: auto;
+      width: 2.2em;
+    }
+
+    #js-side pre {
+      /* counters the adjustments made by highlight.js to line up with the wing side */
+      margin-top: -0.5em;
     }
   </style>
 </html>

apps/wing-playground/worker.js
@@ -1,62 +1,72 @@
-import wingcURL from './wingc.wasm?url'
-import { init, WASI } from '@wasmer/wasi';
-import { env } from 'process';
+import wingcURL from "./wingc.wasm?url";
+import { init, WASI } from "@wasmer/wasi";
+import { env } from "process";
 
-const wingsdkJSIIContent = await import('@winglang/sdk/.jsii?raw').then(i => i.default);
-const wingsdkPackageJsonContent = await import('@winglang/sdk/package.json?raw').then(i => i.default);
+const WINGC_COMPILE = "wingc_compile";
+
+const wingsdkJSIIContent = await import("@winglang/sdk/.jsii?raw").then(
+  (i) => i.default
+);
+const wingsdkPackageJsonContent = await import(
+  "@winglang/sdk/package.json?raw"
+).then((i) => i.default);
 
 await init();
 
 const wasm = await WebAssembly.compileStreaming(fetch(wingcURL));
 
-self.onmessage = async event => {
-  if(event.data === '') {
+let wasi = new WASI({
+  env: {
+    ...env,
+    WINGSDK_MANIFEST_ROOT: "/wingsdk",
+    RUST_BACKTRACE: "full",
+  },
+});
+
+const instance = await wasi.instantiate(wasm, {});
+
+const defaultFilePerms = { read: true, write: true, create: true };
+wasi.fs.createDir("/wingsdk");
+let wingsdk_packagejson_file = wasi.fs.open(
+  "/wingsdk/package.json",
+  defaultFilePerms
+);
+wingsdk_packagejson_file.writeString(wingsdkPackageJsonContent);
+let wingsdk_jsii_file = wasi.fs.open("/wingsdk/.jsii", defaultFilePerms);
+wingsdk_jsii_file.writeString(wingsdkJSIIContent);
+
+self.onmessage = async (event) => {
+  if (event.data === "") {
     self.postMessage(undefined);
     return;
   }
 
-  let wasi = new WASI({
-    args: ['wingc', 'code.w'],
-    env: {
-      ...env,
-      WINGSDK_MANIFEST_ROOT: '/wingsdk',
-      RUST_BACKTRACE: "full",
-    },
-  });
-
-  const instance = await wasi.instantiate(wasm, {});
-
   try {
-    const defaultFilePerms = {read: true, write: true, create: true}
-
     let file = wasi.fs.open("/code.w", defaultFilePerms);
     file.writeString(event.data);
 
-    wasi.fs.createDir("/wingsdk");
-    let wingsdk_packagejson_file = wasi.fs.open("/wingsdk/package.json", defaultFilePerms);
-    wingsdk_packagejson_file.writeString(wingsdkPackageJsonContent);
-    let wingsdk_jsii_file = wasi.fs.open("/wingsdk/.jsii", defaultFilePerms);
-    wingsdk_jsii_file.writeString(wingsdkJSIIContent);
-
-    wasi.start(instance);
+    await wingcInvoke(instance, WINGC_COMPILE, "code.w");
     const stdout = wasi.getStdoutString();
     let intermediateJS = "";
 
-    const intermediateFile = wasi.fs.open("/code.w.out/preflight.js", defaultFilePerms);
+    const intermediatePath = "/code.w.out/preflight.js";
+    const intermediateFile = wasi.fs.open(intermediatePath, defaultFilePerms);
     intermediateJS += intermediateFile.readString();
+    wasi.fs.removeFile(intermediatePath);
 
     let procRegex = /fromFile\(.+"(.+index\.js)"/g;
     let procMatch;
-    while (procMatch = procRegex.exec(intermediateJS)) {
+    while ((procMatch = procRegex.exec(intermediateJS))) {
       const proc = procMatch[1];
       const procPath = `/code.w.out/${proc}`;
       let procFile = wasi.fs.open(procPath, defaultFilePerms);
-      intermediateJS += `\n\n// ${proc}\n// START\n${procFile.readString()}\n// END`
+      intermediateJS += `\n\n// ${proc}\n// START\n${procFile.readString()}\n// END`;
+      wasi.fs.removeFile(procPath);
     }
-    
+
     self.postMessage({
       stdout,
-      intermediateJS
+      intermediateJS,
     });
   } catch (error) {
     console.error(error);
@@ -64,7 +74,38 @@ self.onmessage = async event => {
       stdout: wasi.getStdoutString(),
       intermediateJS: wasi.getStderrString(),
     });
+  } finally {
+    try {
+      wasi.fs.removeFile("/code.w");
+    } catch (error) {}
   }
 };
 
 self.postMessage("WORKER_READY");
+
+/**
+ * Assumptions:
+ * 1. The called WASM function is expecting a pointer and a length representing a string
+ * 2. The string will be UTF-8 encoded
+ * 3. The string will be less than 2^32 bytes long  (4GB)
+ * 4. the WASI instance has already been started
+ */
+async function wingcInvoke(instance, func, arg) {
+  const exports = instance.exports;
+
+  const bytes = new TextEncoder().encode(arg);
+  const argPointer = exports.wingc_malloc(bytes.byteLength);
+
+  try {
+    const argMemoryBuffer = new Uint8Array(
+      exports.memory.buffer,
+      argPointer,
+      bytes.byteLength
+    );
+    argMemoryBuffer.set(bytes);
+  
+    exports[func](argPointer, bytes.byteLength);
+  } finally {
+    exports.wingc_free(argPointer, bytes.byteLength);
+  }
+}

.github/workflows/pull-request-lint.yml
@@ -27,6 +27,8 @@ jobs:
             examples
             build
             repo
+            playground
+            vscode
           types: |-
             feat
             fix

apps/wing/src/commands/compile.ts
@@ -7,11 +7,11 @@ import { dirname, resolve } from "path";
 import { mkdir, readFile } from "fs/promises";
 
 import { WASI } from "wasi";
-import { argv } from "process";
 import debug from "debug";
 import * as chalk from "chalk";
 
 const log = debug("wing:compile");
+const WINGC_COMPILE = "wingc_compile";
 
 const WINGC_WASM_PATH = resolve(__dirname, "../../wingc.wasm");
 log("wasm path: %s", WINGC_WASM_PATH);
@@ -60,10 +60,7 @@ export async function compile(entrypoint: string, options: ICompileOptions) {
     mkdir(outDir, { recursive: true }),
   ]);
 
-  const args = [argv[0], wingFile, workDir];
-
   const wasi = new WASI({
-    args,
     env: {
       ...process.env,
       RUST_BACKTRACE: "full",
@@ -85,7 +82,11 @@ export async function compile(entrypoint: string, options: ICompileOptions) {
   log("instantiating wingc WASM module");
   const instance = await WebAssembly.instantiate(wasm, importObject);
   log("invoking wingc with importObject: %o", importObject);
-  wasi.start(instance);
+  wasi.initialize(instance);
+
+  const arg = `${wingFile};${workDir}`;
+  log(`invoking %s with: "%s"`, WINGC_COMPILE, arg);
+  await wingcInvoke(instance, WINGC_COMPILE, arg);
 
   const artifactPath = resolve(workDir, WINGC_PREFLIGHT);
   log("reading artifact from %s", artifactPath);
@@ -152,5 +153,36 @@ export async function compile(entrypoint: string, options: ICompileOptions) {
     } else {
       console.log("  " + chalk.bold.white("note:") + " " + chalk.white("run with `NODE_STACKTRACE=1` environment variable to display a stack trace"));
     }
+  }
+}
+
+/**
+ * Assumptions:
+ * 1. The called WASM function is expecting a pointer and a length representing a string
+ * 2. The string will be UTF-8 encoded
+ * 3. The string will be less than 2^32 bytes long  (4GB)
+ * 4. the WASI instance has already been started
+ */
+async function wingcInvoke(
+  instance: WebAssembly.Instance,
+  func: string,
+  arg: string
+) {
+  const exports = instance.exports as any;
+
+  const bytes = new TextEncoder().encode(arg);
+  const argPointer = exports.wingc_malloc(bytes.byteLength);
+
+  try {
+    const argMemoryBuffer = new Uint8Array(
+      exports.memory.buffer,
+      argPointer,
+      bytes.byteLength
+    );
+    argMemoryBuffer.set(bytes);
+  
+    exports[func](argPointer, bytes.byteLength);
+  } finally {
+    exports.wingc_free(argPointer, bytes.byteLength);
   }
 }

libs/wingc/Cargo.toml
@@ -22,11 +22,4 @@ indexmap = "1.9.1"
 
 [lib]
 path = "src/lib.rs"
-
-[[bin]]
-name = "wingc"
-path = "src/bin.rs"
-
-[package.metadata]
-# Currently wasm-opt is not changing the size of the wasm file, so it's just adding unnecessary time to the build.
-wasm-opt = false
+crate-type = ["cdylib", "rlib"]

libs/wingc/examples/compile.rs
@@ -1,3 +1,6 @@
+// Compiles a source file and prints any errors to stderr.
+// This should only be used for testing wingc directly.
+
 use std::{env, process};
 use wingc::compile;
 
@@ -5,13 +8,12 @@ pub fn main() {
 	let args: Vec<String> = env::args().collect();
 
 	if args.len() < 2 {
-		panic!("Usage: wingc <source_file> [output_dir]");
+		panic!("Usage: cargo run --example compile <source_file>");
 	}
 
 	let source = &args[1];
-	let outdir = args.get(2).map(|s| s.as_str());
 
-	let results = compile(source, outdir);
+	let results = compile(source, None);
 	if let Err(mut err) = results {
 		// Sort error messages by line number (ascending)
 		err.sort_by(|a, b| a.cmp(&b));

libs/wingc/src/lib.rs
@@ -48,6 +48,43 @@ pub struct CompilerOutput {
 	pub diagnostics: Diagnostics,
 }
 
+unsafe fn ptr_to_string(ptr: u32, len: u32) -> String {
+	let slice = std::slice::from_raw_parts(ptr as *const u8, len as usize);
+	String::from_utf8_unchecked(slice.to_vec())
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn wingc_malloc(size: u32) -> *mut u8 {
+	let layout = core::alloc::Layout::from_size_align_unchecked(size as usize, 0);
+	std::alloc::alloc(layout)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn wingc_free(ptr: u32, size: u32) {
+	let layout = core::alloc::Layout::from_size_align_unchecked(size as usize, 0);
+	std::alloc::dealloc(ptr as *mut u8, layout);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn wingc_compile(ptr: u32, len: u32) {
+	let args = ptr_to_string(ptr, len);
+
+	let split = args.split(";").collect::<Vec<&str>>();
+	let source_file = split[0];
+	let output_dir = split.get(1).map(|s| *s);
+
+	let results = compile(source_file, output_dir);
+	if let Err(mut err) = results {
+		// Sort error messages by line number (ascending)
+		err.sort_by(|a, b| a.cmp(&b));
+		eprintln!(
+			"Compilation failed with {} errors\n{}",
+			err.len(),
+			err.iter().map(|d| format!("{}", d)).collect::<Vec<_>>().join("\n")
+		);
+	}
+}
+
 pub fn parse(source_file: &str) -> (Scope, Diagnostics) {
 	let language = tree_sitter_wing::language();
 	let mut parser = tree_sitter::Parser::new();

commit d9972c1e17482e1df9ec3d3c7cafddbd182c7706

fix(playground): compiler errors missing (#1207)
regressions from https://github.com/winglang/wing/pull/1199

Writing to stderr from wingc was no longer causing an exception in the playground, which was hiding it. This does not affect the CLI, which uses the node WASI system rather than wasmer-js.

I also went ahead and made stderr more obvious, instead of showing it in where the user was expecting JS.

*By submitting this pull request, I confirm that my contribution is made under the terms of the 
[Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

apps/wing-playground/index.html
@@ -21,6 +21,7 @@
       const code = document.getElementById("code");
       const intermediateJS = document.getElementById("intermediateJS");
       const stdout = document.getElementById("stdout");
+      const stderr = document.getElementById("stderr");
       const example = document.getElementById("example");
       const lineCounter = document.getElementById("lineCounter")
 
@@ -65,11 +66,20 @@
           sendCodeToWorker(code.value);
         } else if(e.data) {
           stdout.textContent = e.data.stdout;
+          stderr.textContent = e.data.stderr;
           intermediateJS.textContent = e.data.intermediateJS;
           hljs.highlightAll();
         } else {
           stdout.textContent = "";
+          stderr.textContent = "";
           intermediateJS.textContent = "";
+        }
+
+        // hide intermediateJS if it's empty
+        if (intermediateJS.textContent === "") {
+          intermediateJS.style.display = "none";
+        } else {
+          intermediateJS.style.display = "block";
         }
       });
 
@@ -88,9 +98,8 @@
         <pre>
           <code class="language-javascript" id="intermediateJS"></code>
         </pre>
-        <pre>
-          <code id="stdout" class="no-highlight"></code>
-        </pre>
+          <pre id="stderr" class="stderr"></pre>
+          <pre id="stdout" class="stdout"></pre>
       </section>
     </main>
   </body>
@@ -143,6 +152,16 @@
     #js-side pre {
       /* counters the adjustments made by highlight.js to line up with the wing side */
       margin-top: -0.5em;
+    }
+
+    .stdout {
+      color: white;
+      background: black;
+    }
+
+    .stderr {
+      color: red;
+      background: black;
     }
   </style>
 </html>

apps/wing-playground/worker.js
@@ -46,6 +46,11 @@ self.onmessage = async (event) => {
     file.writeString(event.data);
 
     await wingcInvoke(instance, WINGC_COMPILE, "code.w");
+    const stderr = wasi.getStderrString();
+    if (stderr) {
+      throw stderr;
+    }
+
     const stdout = wasi.getStdoutString();
     let intermediateJS = "";
 
@@ -66,13 +71,15 @@ self.onmessage = async (event) => {
 
     self.postMessage({
       stdout,
+      stderr: wasi.getStderrString(),
       intermediateJS,
     });
   } catch (error) {
     console.error(error);
     self.postMessage({
+      stderr: error,
       stdout: wasi.getStdoutString(),
-      intermediateJS: wasi.getStderrString(),
+      intermediateJS: null,
     });
   } finally {
     try {