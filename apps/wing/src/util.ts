9f89099f2c497341f9ff792db89a80a0038de109
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-02-06 13:39:54 -0500

    comment

--- apps/wing/src/util.ts
+++ apps/wing/src/util.ts
@@ -3,7 +3,12 @@
  */
 export function normalPath(path: string) {
   if (process.platform === "win32") {
-    return path.replace(/\\/g, "/").replace(/^[a-zA-Z]:/, "");
+    return path
+    // Replace backslashes with forward slashes
+    .replace(/\\/g, "/")
+    // Remove drive letter
+    // Note: This uses the assumption that starting a path with `/` in windows resolves to the default drive
+    .replace(/^[a-zA-Z]:/, "");
   } else {
     return path;
   }
