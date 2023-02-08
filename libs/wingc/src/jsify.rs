winglang/wing

commit efe18ac61042444ea53d78841bd942e5b3aba4eb

feat(compiler): `string.contains()` and allowing small js macros in jsii metadata (#1395)
Notes:
- Changed Calls to have expressions instead of references
  - Ended up adding support for IIFEs, so I added a test for that as well
- Added a way to denote the JS that gets emitted when invoking a function definition
  - The builtin globals now uses this for their custom justification
  - This string has two builtin placeholders to use when emitting, `$self$` and `$args$`
    - To handle the string replacements properly (avoid over-eager replacement), added a nice crate https://crates.io/crates/aho-corasick
- Added `@macro` for docstring jsii metadata, which can be set for methods of a class to set the JS emit customization above (I'm open for name suggestions)

Fixes #1243

*By submitting this pull request, I confirm that my contribution is made under the terms of the 
[Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

libs/wingc/src/jsify.rs
@@ -1,3 +1,4 @@
+use aho_corasick::AhoCorasick;
 use itertools::Itertools;
 use std::{cell::RefCell, cmp::Ordering, fs, path::PathBuf};
 
@@ -6,10 +7,11 @@ use sha2::{Digest, Sha256};
 use crate::{
 	ast::{
 		ArgList, BinaryOperator, ClassMember, Expr, ExprKind, FunctionDefinition, InterpolatedStringPart, Literal, Phase,
-		Reference, Scope, Stmt, StmtKind, Symbol, Type, UnaryOperator, UtilityFunctions,
+		Reference, Scope, Stmt, StmtKind, Symbol, Type, UnaryOperator,
 	},
 	capture::CaptureKind,
 	utilities::snake_case_to_camel_case,
+	MACRO_REPLACE_ARGS, MACRO_REPLACE_SELF,
 };
 
 const STDLIB: &str = "$stdlib";
@@ -149,36 +151,6 @@ impl JSifier {
 			Reference::Identifier(identifier) => symbolize(self, identifier),
 			Reference::NestedIdentifier { object, property } => {
 				self.jsify_expression(object, phase) + "." + &symbolize(self, property)
-			}
-		}
-	}
-	// note: Globals are emitted here and wrapped in "{{ ... }}" blocks. Wrapping makes these emissions, actual
-	// statements and not expressions. this makes the runtime panic if these are used in place of expressions.
-	fn jsify_global_utility_function(&self, args: &ArgList, function_type: UtilityFunctions, phase: Phase) -> String {
-		match function_type {
-			UtilityFunctions::Assert => {
-				return format!(
-					"{{((cond) => {{if (!cond) throw new Error(`assertion failed: '{0}'`)}})({0})}}",
-					self.jsify_arg_list(args, None, None, false, phase)
-				);
-			}
-			UtilityFunctions::Print => {
-				return format!(
-					"{{console.log({})}}",
-					self.jsify_arg_list(args, None, None, false, phase)
-				);
-			}
-			UtilityFunctions::Throw => {
-				return format!(
-					"{{((msg) => {{throw new Error(msg)}})({})}}",
-					self.jsify_arg_list(args, None, None, false, phase)
-				);
-			}
-			UtilityFunctions::Panic => {
-				return format!(
-					"{{((msg) => {{console.error(msg, (new Error()).stack);process.exit(1)}})({})}}",
-					self.jsify_arg_list(args, None, None, false, phase)
-				);
 			}
 		}
 	}
@@ -327,23 +299,8 @@ impl JSifier {
 			},
 			ExprKind::Reference(_ref) => self.jsify_reference(&_ref, None, phase),
 			ExprKind::Call { function, args } => {
-				let mut needs_case_conversion = false;
-				if matches!(&function, Reference::Identifier(Symbol { name, .. }) if name == UtilityFunctions::Print.to_string().as_str())
-				{
-					return self.jsify_global_utility_function(&args, UtilityFunctions::Print, phase);
-				} else if matches!(&function, Reference::Identifier(Symbol { name, .. }) if name == UtilityFunctions::Assert.to_string().as_str())
-				{
-					return self.jsify_global_utility_function(&args, UtilityFunctions::Assert, phase);
-				} else if matches!(&function, Reference::Identifier(Symbol { name, .. }) if name == UtilityFunctions::Panic.to_string().as_str())
-				{
-					return self.jsify_global_utility_function(&args, UtilityFunctions::Panic, phase);
-				} else if matches!(&function, Reference::Identifier(Symbol { name, .. }) if name == UtilityFunctions::Throw.to_string().as_str())
-				{
-					return self.jsify_global_utility_function(&args, UtilityFunctions::Throw, phase);
-				} else if let Reference::NestedIdentifier { object, .. } = function {
-					let object_type = object.evaluated_type.borrow().unwrap();
-
-					needs_case_conversion = if let Some(obj) = object_type.as_class_or_resource() {
+				if let Some(function_type) = *function.evaluated_type.borrow() {
+					let needs_case_conversion = if let Some(obj) = function_type.as_class_or_resource() {
 						obj.should_case_convert_jsii
 					} else {
 						// There are two reasons this could happen:
@@ -352,13 +309,37 @@ impl JSifier {
 						// In either case, it's probably safe to assume that case conversion is required.
 						true
 					};
+
+					let arg_string = self.jsify_arg_list(&args, None, None, needs_case_conversion, phase);
+					let expr_string = match &function.kind {
+						ExprKind::Reference(reference) => self.jsify_reference(reference, Some(needs_case_conversion), phase),
+						_ => format!("({})", self.jsify_expression(function, phase)),
+					};
+
+					if let Some(function_sig) = function_type.as_function_sig() {
+						if let Some(js_override) = &function_sig.js_override {
+							let self_string = &match &function.kind {
+								// for "loose" macros, e.g. `print()`, $self$ is the global object
+								ExprKind::Reference(Reference::Identifier(_)) => "global".to_string(),
+								ExprKind::Reference(Reference::NestedIdentifier { object, .. }) => {
+									self.jsify_expression(object, phase).clone()
+								}
+
+								_ => expr_string,
+							};
+							let patterns = &[MACRO_REPLACE_SELF, MACRO_REPLACE_ARGS];
+							let replace_with = &[self_string, &arg_string];
+							let ac = AhoCorasick::new(patterns);
+							return ac.replace_all(js_override, replace_with);
+						}
+					} else {
+						panic!("Expressions at {} is not callable", function.span);
+					}
+
+					format!("({}{}({}))", auto_await, expr_string, arg_string)
+				} else {
+					panic!("Expressions at {} does not have type information", function.span);
 				}
-				format!(
-					"({}{}({}))",
-					auto_await,
-					self.jsify_reference(&function, Some(needs_case_conversion), phase),
-					self.jsify_arg_list(&args, None, None, needs_case_conversion, phase)
-				)
 			}
 			ExprKind::Unary { op, exp } => {
 				let op = match op {

examples/tests/valid/anon_function.w
@@ -11,3 +11,8 @@ let myfunc = (var x: num) => {
 };
 
 myfunc(1);
+
+// Immediately invoked function expression
+((var x: num) => {
+  assert(x == 1);
+})(1);

examples/tests/valid/primitive_methods.w
@@ -2,6 +2,6 @@ let dur = 1m;
 let stringy = "${dur.minutes}:${dur.seconds}";
 print(stringy);
 
-if stringy.includes("60") && stringy.split(":").at(0) == "60" {
+if stringy.contains("60") && stringy.split(":").at(0) == "60" {
   print("${stringy.length}!");
 }

libs/tree-sitter-wing/grammar.js
@@ -12,6 +12,7 @@ const PREC = {
   UNARY: 11,
   NIL_COALESCING: 12,
   MEMBER: 13,
+  CALL: 14,
 };
 
 module.exports = grammar({
@@ -285,7 +286,7 @@ module.exports = grammar({
       ),
 
     call: ($) =>
-      seq(field("caller", $.reference), field("args", $.argument_list)),
+      prec.left(PREC.CALL, seq(field("caller", $.expression), field("args", $.argument_list))),
 
     argument_list: ($) =>
       seq(

libs/wingc/Cargo.toml
@@ -19,6 +19,7 @@ itertools = "0.10"
 inflections = "1.1.1"
 phf = { version = "0.11", features = ["macros"] }
 indexmap = "1.9.1"
+aho-corasick = "0.7.20"
 
 [lib]
 path = "src/lib.rs"

libs/wingc/src/ast.rs
@@ -256,7 +256,7 @@ pub enum ExprKind {
 	Literal(Literal),
 	Reference(Reference),
 	Call {
-		function: Reference,
+		function: Box<Expr>,
 		args: ArgList,
 	},
 	Unary {

libs/wingc/src/capture.rs
@@ -131,9 +131,7 @@ pub fn scan_for_inflights_in_expression(expr: &Expr, diagnostics: &mut Diagnosti
 			scan_for_inflights_in_expression(object, diagnostics);
 		}
 		ExprKind::Call { function, args } => {
-			if let Reference::NestedIdentifier { object, property: _ } = function {
-				scan_for_inflights_in_expression(object, diagnostics);
-			}
+			scan_for_inflights_in_expression(function, diagnostics);
 			scan_for_inflights_in_arglist(args, diagnostics);
 		}
 		ExprKind::Unary { op: _, exp } => {
@@ -178,40 +176,15 @@ fn scan_for_inflights_in_arglist(args: &ArgList, diagnostics: &mut Diagnostics) 
 }
 
 fn scan_captures_in_call(
-	reference: &Reference,
+	callee: &Expr,
 	args: &ArgList,
 	env: &SymbolEnv,
 	statement_idx: usize,
 	diagnostics: &mut Diagnostics,
 ) -> Vec<Capture> {
 	let mut res = vec![];
-	if let Reference::NestedIdentifier { object, property } = reference {
-		res.extend(scan_captures_in_expression(&object, env, statement_idx, diagnostics));
-
-		// If the expression evaluates to a resource we should check what method of the resource we're accessing
-		if let Type::Resource(ref resource) = **object.evaluated_type.borrow().as_ref().unwrap() {
-			let (prop_type, _flight) = match resource.env.lookup_ext(property, None) {
-				Ok((prop_type, phase)) => (
-					prop_type
-						.as_variable()
-						.expect("Expected resource property to be a variable")
-						._type,
-					phase,
-				),
-				Err(type_error) => {
-					panic!("{}", type_error);
-				}
-			};
-
-			let func = prop_type.as_function_sig().unwrap();
-			if matches!(func.flight, Phase::Preflight) {
-				panic!("Can't access preflight method {} inflight", property);
-			}
-			debug!(
-				"We seem to be accessing the preflight method {}.{} {} inflight!",
-				resource.name.name, property.name, property.span
-			);
-		}
+	if !matches!(callee.kind, ExprKind::Reference(Reference::Identifier(_))) {
+		res.extend(scan_captures_in_expression(callee, env, statement_idx, diagnostics));
 	}
 
 	for arg in args.pos_args.iter() {
@@ -312,22 +285,35 @@ fn scan_captures_in_expression(
 					}
 				}
 			}
-			Reference::NestedIdentifier { object, property: _ } => {
+			Reference::NestedIdentifier { object, property } => {
 				res.extend(scan_captures_in_expression(object, env, statement_idx, diagnostics));
 
-				// If the expression evaluates to a resource we should check if we need to capture the property as well
-				// TODO: do we really need this? I think we capture `object` above recursively and therefore don't need special handling of `property`.
-				// if let &Type::ResourceObject(resource) = object.evaluated_type.borrow().unwrap().into() {
-				// 	let resource = resource.as_resource().unwrap();
-				// 	let (prop_type, flight) = resource.env.lookup_ext(property);
-				// 	if prop_type.as_resource_object().is_some() && matches!(flight, Flight::Pre) {
-				// 		debug!(
-				// 			"We seem to be accessing the preflight resource {} {} inflight!",
-				// 			r, property.span
-				// 		);
-				// 		res.push(r);
-				// 	}
-				// }
+				// If the expression evaluates to a resource we should check what method of the resource we're accessing
+				if let Type::Resource(ref resource) = **object.evaluated_type.borrow().as_ref().unwrap() {
+					let (prop_type, _flight) = match resource.env.lookup_ext(property, None) {
+						Ok((prop_type, phase)) => (
+							prop_type
+								.as_variable()
+								.expect("Expected resource property to be a variable")
+								._type,
+							phase,
+						),
+						Err(type_error) => {
+							panic!("{}", type_error);
+						}
+					};
+
+					// TODO: handle accessing things other than function_sigs while recursively accessing Reference?
+					if let Some(func) = prop_type.as_function_sig() {
+						if matches!(func.flight, Phase::Preflight) {
+							panic!("Can't access preflight method {} inflight", property);
+						}
+						debug!(
+							"We seem to be accessing the preflight method {}.{} {} inflight!",
+							resource.name.name, property.name, property.span
+						);
+					}
+				}
 			}
 		},
 		ExprKind::Call { function, args } => {

libs/wingc/src/lib.rs
@@ -42,6 +42,9 @@ const WINGSDK_INFLIGHT: &'static str = "core.Inflight";
 
 const CONSTRUCT_BASE: &'static str = "constructs.Construct";
 
+const MACRO_REPLACE_SELF: &'static str = "$self$";
+const MACRO_REPLACE_ARGS: &'static str = "$args$";
+
 pub struct CompilerOutput {
 	pub preflight: String,
 	// pub inflights: BTreeMap<String, String>,
@@ -133,12 +136,15 @@ pub fn type_check(scope: &mut Scope, types: &mut Types) -> Diagnostics {
 	let env = SymbolEnv::new(None, types.void(), false, false, Phase::Preflight, 0);
 	scope.set_env(env);
 
+	// note: Globals are emitted here and wrapped in "{ ... }" blocks. Wrapping makes these emissions, actual
+	// statements and not expressions. this makes the runtime panic if these are used in place of expressions.
 	add_builtin(
 		UtilityFunctions::Print.to_string().as_str(),
 		Type::Function(FunctionSignature {
 			args: vec![types.string()],
 			return_type: types.void(),
 			flight: Phase::Independent,
+			js_override: Some("{console.log($args$)}".to_string()),
 		}),
 		scope,
 		types,
@@ -149,6 +155,7 @@ pub fn type_check(scope: &mut Scope, types: &mut Types) -> Diagnostics {
 			args: vec![types.bool()],
 			return_type: types.void(),
 			flight: Phase::Independent,
+			js_override: Some("{((cond) => {if (!cond) throw new Error(`assertion failed: '$args$'`)})($args$)}".to_string()),
 		}),
 		scope,
 		types,
@@ -159,6 +166,7 @@ pub fn type_check(scope: &mut Scope, types: &mut Types) -> Diagnostics {
 			args: vec![types.string()],
 			return_type: types.void(),
 			flight: Phase::Independent,
+			js_override: Some("{((msg) => {throw new Error(msg)})($args$)}".to_string()),
 		}),
 		scope,
 		types,
@@ -169,6 +177,7 @@ pub fn type_check(scope: &mut Scope, types: &mut Types) -> Diagnostics {
 			args: vec![types.string()],
 			return_type: types.void(),
 			flight: Phase::Independent,
+			js_override: Some("{((msg) => {console.error(msg, (new Error()).stack);process.exit(1)})($args$)}".to_string()),
 		}),
 		scope,
 		types,

commit 138e8a4610a8c1d02cd38bfbc22903029774c5cd

Case conversion bug introduced in #1395 since "calls" are expressions.

libs/wingc/src/jsify.rs
@@ -311,47 +311,48 @@ impl JSifier {
 			},
 			ExprKind::Reference(_ref) => self.jsify_reference(&_ref, None, phase),
 			ExprKind::Call { function, args } => {
-				if let Some(function_type) = *function.evaluated_type.borrow() {
-					let needs_case_conversion = if let Some(obj) = function_type.as_class_or_resource() {
-						obj.should_case_convert_jsii
-					} else {
-						// There are two reasons this could happen:
-						// 1. The object is a `any` type, which is either something unimplemented or an explicit `any` from JSII.
-						// 2. The object is a builtin type (e.g. Array) but the call reference intentionally resolves to a JSII class.
-						// In either case, it's probably safe to assume that case conversion is required.
-						true
-					};
+				let function_type = function.evaluated_type.borrow().unwrap();
+				let function_sig = function_type
+					.as_function_sig()
+					.expect("Expected expression to be callable");
+				let mut needs_case_conversion = false;
 
-					let arg_string = self.jsify_arg_list(&args, None, None, needs_case_conversion, phase);
-					let expr_string = match &function.kind {
-						ExprKind::Reference(reference) => self.jsify_reference(reference, Some(needs_case_conversion), phase),
-						_ => format!("({})", self.jsify_expression(function, phase)),
-					};
-
-					if let Some(function_sig) = function_type.as_function_sig() {
-						if let Some(js_override) = &function_sig.js_override {
-							let self_string = &match &function.kind {
-								// for "loose" macros, e.g. `print()`, $self$ is the global object
-								ExprKind::Reference(Reference::Identifier(_)) => "global".to_string(),
-								ExprKind::Reference(Reference::NestedIdentifier { object, .. }) => {
-									self.jsify_expression(object, phase).clone()
-								}
-
-								_ => expr_string,
-							};
-							let patterns = &[MACRO_REPLACE_SELF, MACRO_REPLACE_ARGS];
-							let replace_with = &[self_string, &arg_string];
-							let ac = AhoCorasick::new(patterns);
-							return ac.replace_all(js_override, replace_with);
+				let expr_string = match &function.kind {
+					ExprKind::Reference(reference) => {
+						if let Reference::NestedIdentifier { object, property } = reference {
+							let object_type = object.evaluated_type.borrow().unwrap();
+							if let Some(class) = object_type.as_class_or_resource() {
+								println!("{} Needs case conversion: {}", property, class.should_case_convert_jsii);
+								needs_case_conversion = class.should_case_convert_jsii;
+							} else {
+								// TODO I think in this case we shouldn't convert tye case but originally we had code that
+								// set `needs_case_conversion` to true for `any` and builtin types, and I'm not sure why..
+								needs_case_conversion = false;
+							}
 						}
-					} else {
-						panic!("Expressions at {} is not callable", function.span);
+						self.jsify_reference(reference, Some(needs_case_conversion), phase)
 					}
+					_ => format!("({})", self.jsify_expression(function, phase)),
+				};
+				let arg_string = self.jsify_arg_list(&args, None, None, needs_case_conversion, phase);
 
-					format!("({}{}({}))", auto_await, expr_string, arg_string)
-				} else {
-					panic!("Expressions at {} does not have type information", function.span);
+				if let Some(js_override) = &function_sig.js_override {
+					let self_string = &match &function.kind {
+						// for "loose" macros, e.g. `print()`, $self$ is the global object
+						ExprKind::Reference(Reference::Identifier(_)) => "global".to_string(),
+						ExprKind::Reference(Reference::NestedIdentifier { object, .. }) => {
+							self.jsify_expression(object, phase).clone()
+						}
+
+						_ => expr_string,
+					};
+					let patterns = &[MACRO_REPLACE_SELF, MACRO_REPLACE_ARGS];
+					let replace_with = &[self_string, &arg_string];
+					let ac = AhoCorasick::new(patterns);
+					return ac.replace_all(js_override, replace_with);
 				}
+
+				format!("({}{}({}))", auto_await, expr_string, arg_string)
 			}
 			ExprKind::Unary { op, exp } => {
 				let op = match op {