ab7729ebaf5f91e7c2cce37112c551ba62bf4d80
mutator <mutator@winglang.io>
2023-02-09 13:17:13 +0000

    chore: self mutation
    Signed-off-by: mutator <mutator@winglang.io>

--- libs/wingsdk/src/target-tf-aws/schedule.ts
+++ libs/wingsdk/src/target-tf-aws/schedule.ts
@@ -35,7 +35,7 @@ export class Schedule extends cloud.ScheduleBase {
      * The schedule cron format consists of 5 values (minutes, hour, day of month,
      * month, and day of week), but the AWS EventBridge Schedule uses a 6-string
      * format adding the year at the bottom of the string.
-     * 
+     *
      * That way, we add * at the bottom.
      */
     this.scheduleExpression = rate
