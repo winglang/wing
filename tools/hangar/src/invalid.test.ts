fdc63bb65a31dfb5c0d8030d0baa6b91f1973604
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-02-10 14:49:23 -0500

    force lf

--- tools/hangar/src/invalid.test.ts
+++ tools/hangar/src/invalid.test.ts
@@ -24,11 +24,7 @@ test.each(invalidWingFiles)(
       // Remove absolute paths
       .replaceAll(relativeWingFile, relativeWingFile.replaceAll("\\", "/"))
       // Normalize line endings
-      .replaceAll("\r\n", "\n")
-      // For the error about a missing semicolon, remove the char number
-      // because it's usually the last char of the line which is
-      // different due to windows line endings (`\r\n`)
-      .replace(/:\d+ \| ';' expected./g, ":0 | ';' expected.");
+      .replaceAll("\r\n", "\n");
 
     expect(stderrSanitized).toMatchSnapshot("stderr");
   },
