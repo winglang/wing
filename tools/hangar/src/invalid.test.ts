cb2a4dc8a8daf20d1f9fe05611f31b371dcd51f0
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-02-12 21:49:57 -0500

    fix: unclear compiler errors and lack of error snapshot testing (#1499)
    Ever since wingc stopped being a binary, calling `compile` doesn't cause an error unless there's a panic. This means that execution isn't stopped in the CLI for regular errors but the rest of the cli expects the preflight js to exist, causing it to throw trying to read the file.
    
    To help verify this, added snapshot testing for the `invalid` tests to hangar.
    
    Fixes #926
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

---
+++ tools/hangar/src/invalid.test.ts
@@ -0,0 +1,36 @@
+import * as fs from "fs-extra";
+import * as path from "path";
+import { describe, test } from "vitest";
+import { invalidTestDir, invalidWingFiles, tmpDir } from "./paths";
+import { runWingCommand } from "./utils";
+
+describe.each(invalidWingFiles)("%s", (wingFile) => {
+  test.concurrent(
+    "wing test (--target sim) - invalid",
+    async ({ expect }) => {
+      const args = ["test"];
+      const testDir = path.join(tmpDir, `invalid_${wingFile}_sim`);
+      fs.mkdirpSync(testDir);
+
+      const relativeWingFile = path.relative(
+        testDir,
+        path.join(invalidTestDir, wingFile)
+      );
+
+      const out = await runWingCommand(testDir, relativeWingFile, args, false);
+
+      const stderr = out.stderr;
+
+      const stderrSanitized = stderr
+        // Remove absolute paths
+        .replaceAll(relativeWingFile, relativeWingFile.replaceAll("\\", "/"))
+        // Normalize line endings
+        .replaceAll("\r\n", "\n");
+
+      expect(stderrSanitized).toMatchSnapshot("stderr");
+    },
+    {
+      timeout: 1000 * 30,
+    }
+  );
+});
---
+++ tools/hangar/src/utils.ts
@@ -0,0 +1,40 @@
+import { execa } from "execa";
+import * as fs from "fs-extra";
+import { expect } from "vitest";
+import { wingBin } from "./paths";
+
+export async function runWingCommand(
+  cwd: string,
+  wingFile: string,
+  args: string[],
+  shouldSucceed: boolean
+) {
+  const out = await execa(wingBin, [...args, wingFile], {
+    cwd,
+    reject: false,
+    stdin: "ignore",
+  });
+  if (shouldSucceed) {
+    expect(out.exitCode).toBe(0);
+  } else {
+    expect(out.stderr).not.toBe("");
+    expect(out.exitCode).not.toBe(0);
+  }
+  return out;
+}
+
+export function sanitize_json_paths(path: string) {
+  const assetKeyRegex = /"asset\..+"/g;
+  const assetSourceRegex = /"assets\/.+"/g;
+  const json = fs.readJsonSync(path);
+
+  const jsonText = JSON.stringify(json);
+  const sanitizedJsonText = jsonText
+    .replace(assetKeyRegex, '"<ASSET_KEY>"')
+    .replace(assetSourceRegex, '"<ASSET_SOURCE>"');
+
+  const finalObj = JSON.parse(sanitizedJsonText);
+  delete finalObj.terraform;
+
+  return finalObj;
+}
