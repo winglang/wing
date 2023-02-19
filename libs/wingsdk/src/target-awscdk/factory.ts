f7eab305bc3c4f9721cbfc2329e174c7ddaa36f1
Marcio Cruz de Almeida <marciocadev@gmail.com>
2023-02-19 08:55:52 -0300

    removing bucket name from cdk implementation and some comments

--- libs/wingsdk/src/target-awscdk/factory.ts
+++ libs/wingsdk/src/target-awscdk/factory.ts
@@ -21,16 +21,8 @@ export class PolyconFactory implements IPolyconFactory {
     switch (type) {
       case BUCKET_TYPE:
         return new Bucket(scope, id, args[0]);
-      // case FUNCTION_TYPE:
-      //   return new Function(scope, id, args[0], args[1]);
-      // case QUEUE_TYPE:
-      //   return new Queue(scope, id, args[0]);
       case LOGGER_TYPE:
         return new Logger(scope, id);
-      // case COUNTER_TYPE:
-      //   return new Counter(scope, id, args[0]);
-      // case TOPIC_TYPE:
-      //   return new Topic(scope, id, args[0]);
       default:
         throw new Error(`Type ${type} not implemented for tf-aws target.`);
     }
