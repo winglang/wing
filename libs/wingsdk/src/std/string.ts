4bccdef84341743ec5741c05277d3b020ba3b090
Uri Bar <urib@monada.co>
2023-02-21 18:51:02 +0200

    feat(sdk): str standard object api

--- libs/wingsdk/src/std/string.ts
+++ libs/wingsdk/src/std/string.ts
@@ -7,17 +7,76 @@
  */
 export class String {
   /**
-   * The length of the string
+   * The length of the string.
    */
   public get length(): number {
     throw new Error("Abstract");
   }
 
   /**
-   * Split string by separator
+   * Returns the character at the specified index.
    *
-   * @param separator separator to split by
-   * @returns array of strings
+   * @param index position of the character.
+   * @returns string at the specified index.
+   */
+  public at(index: number): string {
+    index;
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Combines the text of two (or more) strings and returns a new string.
+   *
+   * @param strN one or more strings to concatenate to this string.
+   * @returns a new combined string.
+   */
+  public concat(...strN: string[]): string {
+    strN;
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Checks if string includes substring.
+   *
+   * @macro $self$.includes($args$)
+   *
+   * @param searchString substring to search for.
+   * @returns true if string includes substring.
+   */
+  public contains(searchString: string): boolean {
+    searchString;
+    throw new Error("Macro");
+  }
+
+  /**
+   * Does this string end with the given searchString?
+   *
+   * @macro $self$.endsWith($args$)
+   *
+   * @param searchString substring to search for.
+   * @returns true if string ends with searchString.
+   */
+  public ends(searchString: string): boolean {
+    searchString;
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Returns the index of the first occurrence of searchString found.
+   *
+   * @param searchString substring to search for.
+   * @returns the index of the first occurrence of searchString found, or -1 if not found.
+   */
+  public indexOf(searchString: string): number {
+    searchString;
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Splits string by separator.
+   *
+   * @param separator separator to split by.
+   * @returns array of strings.
    */
   public split(separator: string): string[] {
     separator;
@@ -25,15 +84,59 @@ export class String {
   }
 
   /**
-   * Check if string includes substring
+   * Does this string start with the given searchString?
    *
-   * @macro $self$.includes($args$)
+   * @macro $self$.startsWith($args$)
    *
-   * @param searchString substring to search for
-   * @returns true if string includes substring
+   * @param searchString substring to search for.
+   * @returns true if string starts with searchString.
    */
-  public contains(searchString: string): boolean {
+  public starts(searchString: string): boolean {
     searchString;
-    throw new Error("Macro");
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Returns a string between indexStart, indexEnd.
+   *
+   * @param indexStart index of the character we slice at.
+   * @param indexEnd optional - index of the character we end slicing at.
+   * @returns the string contained from indexStart to indexEnd.
+   */
+  public substring(indexStart: number, indexEnd?: number): string {
+    indexStart;
+    indexEnd;
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Returns this string in lower case.
+   *
+   * @macro $self$.toLowerCase()
+   *
+   * @returns a new lower case string.
+   */
+  public toLowCase(): string {
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Returns this string in upper case.
+   *
+   * @macro $self$.toUpperCase()
+   *
+   * @returns a new upper case string.
+   */
+  public toUpCase(): string {
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Removes white spaces from start and end of this string.
+   *
+   * @returns a new string with white spaces removed from start and end.
+   */
+  public trim(): string {
+    throw new Error("Abstract");
   }
 }
