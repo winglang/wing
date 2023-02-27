8faf4fa8fc3e7ea9585e0d1947a7ea798dbc29f9
Hasan Aburayyan <hasanaburayyan21@gmail.com>
2023-02-27 15:57:10 -0500

    snapshot update

--- libs/wingsdk/src/std/json.ts
+++ libs/wingsdk/src/std/json.ts
@@ -1,9 +1,12 @@
+/**
+ * Immutable Json
+ */
 export class Json {
   /**
    * Returns a specified element from the Json.
-   * 
+   *
    * @macro ($self$)[$args$]
-   * 
+   *
    * @param key The key of the element to return
    * @returns The element associated with the specified key, or undefined if the key can't be found
    */
@@ -13,12 +16,15 @@ export class Json {
   }
 }
 
+/**
+ * Mutable Json
+ */
 export class MutJson {
   /**
    * Returns a specified element from the Json.
-   * 
+   *
    * @macro ($self$)[$args$]
-   * 
+   *
    * @param key The key of the element to return
    * @returns The element associated with the specified key, or undefined if the key can't be found
    */
@@ -29,15 +35,15 @@ export class MutJson {
 
   /**
    * Adds or updates an element in MutJson with a specific key and value
-   * 
+   *
    * @macro ((obj, args) => { obj[args[0]] = args[1]; })($self$, [$args$])
-   * 
+   *
    * @param key The key of the element to add
    * @param value The value of the element to add
    */
   public set(key: string, value: any): void {
     key;
     value;
-    throw new Error("Macro")
+    throw new Error("Macro");
   }
 }
