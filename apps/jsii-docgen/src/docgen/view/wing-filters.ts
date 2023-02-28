1fbc26bb28bbd854963fc70f565ef7cfc0d33761
Elad Ben-Israel <elad.benisrael@gmail.com>
2023-02-28 10:01:49 +0200

    fix API docs

---
+++ apps/jsii-docgen/src/docgen/view/wing-filters.ts
@@ -0,0 +1,4 @@
+// this file includes some special cases for wing documentation
+export const VISIBLE_SUBMODULES = ["cloud", "fs", "std"];
+export const HIDDEN_METHODS = ["toString", "toJSON", "addConnection"];
+export const HIDDEN_STATIC = ["of", "isConstruct"];
