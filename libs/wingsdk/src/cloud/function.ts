winglang/wing

commit fd353b2fc8a1cc60f3d48ceaac1ad7c0fe7a5003

fix: compiling cloud function mangles terminal output (#1721)
Adds a workaround for an issue where compiling cloud functions (which calls esbuild) results in mangled terminal output.

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

libs/wingsdk/src/cloud/function.ts
@@ -1,7 +1,7 @@
+import { spawnSync } from "child_process";
 import { readFileSync, writeFileSync } from "fs";
 import { join } from "path";
 import { Construct } from "constructs";
-import * as esbuild from "esbuild-wasm";
 import { Logger } from "./logger";
 import { fqnForType } from "../constants";
 import { IInflightHost, IResource, Inflight, Resource, App } from "../core";
@@ -110,25 +110,20 @@ export abstract class Function extends Resource implements IInflightHost {
     });
 
     const tempdir = mkdtemp();
+    const infile = join(tempdir, "prebundle.js");
     const outfile = join(tempdir, "index.js");
+    writeFileSync(infile, lines.join("\n"));
 
+    // We would invoke esbuild directly here, but there is a bug where esbuild
+    // mangles the stdout/stderr of the process that invokes it.
+    // https://github.com/evanw/esbuild/issues/2927
+    // To workaround the issue, spawn a new process and invoke esbuild inside it.
     try {
-      esbuild.buildSync({
-        bundle: true,
-        stdin: {
-          contents: lines.join("\n"),
-          resolveDir: tempdir,
-          sourcefile: "inflight.js",
-        },
-        nodePaths: [join(__dirname, "..", "..", "node_modules")],
-        target: "node16",
-        platform: "node",
-        absWorkingDir: tempdir,
-        outfile,
-        minify: false,
-        logLevel: "silent",
-        external: ["aws-sdk"],
-      });
+      let esbuildScript = [
+        `const esbuild = require("${require.resolve("esbuild-wasm")}");`,
+        `esbuild.buildSync({ bundle: true, entryPoints: ["${infile}"], outfile: "${outfile}", minify: false, platform: "node", target: "node16", external: ["aws-sdk"] });`,
+      ].join("\n");
+      spawnSync(process.argv[0], ["-e", esbuildScript]);
     } catch (e) {
       throw new Error(`Failed to bundle function: ${e}`);
     }

libs/wingc/src/jsify.rs
@@ -325,15 +325,14 @@ impl<'a> JSifier<'a> {
 					None
 				};
 
-				if !is_resource || fqn.is_none() {
-					format!("new {}({})", ctor, args)
-				} else {
-					let fqn = fqn.expect("expecting fqn to be defined");
+				if let (true, Some(fqn)) = (is_resource, fqn) {
 					if is_abstract {
 						format!("this.node.root.newAbstract(\"{}\",{})", fqn, args)
 					} else {
 						format!("this.node.root.new(\"{}\",{},{})", fqn, ctor, args)
 					}
+				} else {
+					format!("new {}({})", ctor, args)
 				}
 			}
 			ExprKind::Literal(lit) => match lit {

libs/wingsdk/src/core/app.ts
@@ -1,11 +1,4 @@
-import {
-  mkdirSync,
-  readdirSync,
-  renameSync,
-  rmSync,
-  rmdirSync,
-  existsSync,
-} from "fs";
+import { mkdirSync, readdirSync, renameSync, rmSync, existsSync } from "fs";
 import { join } from "path";
 import * as cdktf from "cdktf";
 import { Construct } from "constructs";
@@ -272,7 +265,7 @@ export abstract class CdktfApp extends App {
         // If the file is a directory we need to delete contents of previous synthesis
         // or rename will fail
         if (existsSync(destination)) {
-          rmdirSync(destination, { recursive: true });
+          rmSync(destination, { recursive: true });
         }
 
         renameSync(source, destination);

libs/wingsdk/test/sandbox/main.ts
@@ -13,15 +13,19 @@ class HelloWorld extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const bucket = new cloud.Bucket(this, "Bucket", { public: false });
-    bucket.addObject("test.txt", "yoyoyo");
+    const handler = testing.Testing.makeHandler(
+      this,
+      "Handler",
+      `async handle() { console.log("Hello, world!"); }`
+    );
+    // cloud.Function._newFunction(this, "Function", handler);
+    const queue = cloud.Queue._newQueue(this, "Queue");
+    queue.onMessage(handler);
   }
 }
 
-const app = new tfgcp.App({
+const app = new tfaws.App({
   outdir: join(__dirname, "target"),
-  projectId: "my-project",
-  storageLocation: "US",
 });
 new HelloWorld(app, "HelloWorld");
 app.synth();

libs/wingsdk/test/target-sim/__snapshots__/file-counter.test.ts.snap
@@ -296,7 +296,7 @@ __export(queue_onmessage_inflight_exports, {
 });
 var QueueOnMessageHandlerClient;
 var init_queue_onmessage_inflight = __esm({
-  \\"[REDACTED]/wingsdk/src/target-sim/queue.onmessage.inflight.ts\\"() {
+  \\"src/target-sim/queue.onmessage.inflight.ts\\"() {
     \\"use strict\\";
     QueueOnMessageHandlerClient = class {
       constructor({ handler }) {

libs/wingsdk/test/target-sim/__snapshots__/queue.test.ts.snap
@@ -345,7 +345,7 @@ __export(queue_onmessage_inflight_exports, {
 });
 var QueueOnMessageHandlerClient;
 var init_queue_onmessage_inflight = __esm({
-  \\"[REDACTED]/wingsdk/src/target-sim/queue.onmessage.inflight.ts\\"() {
+  \\"src/target-sim/queue.onmessage.inflight.ts\\"() {
     \\"use strict\\";
     QueueOnMessageHandlerClient = class {
       constructor({ handler }) {
@@ -895,7 +895,7 @@ __export(queue_onmessage_inflight_exports, {
 });
 var QueueOnMessageHandlerClient;
 var init_queue_onmessage_inflight = __esm({
-  \\"[REDACTED]/wingsdk/src/target-sim/queue.onmessage.inflight.ts\\"() {
+  \\"src/target-sim/queue.onmessage.inflight.ts\\"() {
     \\"use strict\\";
     QueueOnMessageHandlerClient = class {
       constructor({ handler }) {
@@ -1308,7 +1308,7 @@ __export(queue_onmessage_inflight_exports, {
 });
 var QueueOnMessageHandlerClient;
 var init_queue_onmessage_inflight = __esm({
-  \\"[REDACTED]/wingsdk/src/target-sim/queue.onmessage.inflight.ts\\"() {
+  \\"src/target-sim/queue.onmessage.inflight.ts\\"() {
     \\"use strict\\";
     QueueOnMessageHandlerClient = class {
       constructor({ handler }) {

commit 4d52345f982747030c416d71a05672c30825c290

fix(sdk): esbuild fails silently (#1736)
Fixes an issue where esbuild fails silently (regression from #1721) and handles a potential issue where a path may not be created correctly on Windows.

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

libs/wingsdk/src/cloud/function.ts
@@ -6,7 +6,7 @@ import { Logger } from "./logger";
 import { fqnForType } from "../constants";
 import { IInflightHost, IResource, Inflight, Resource, App } from "../core";
 import { Duration } from "../std";
-import { mkdtemp } from "../util";
+import { mkdtemp, normalPath } from "../util";
 
 /**
  * Global identifier for `Function`.
@@ -118,14 +118,18 @@ export abstract class Function extends Resource implements IInflightHost {
     // mangles the stdout/stderr of the process that invokes it.
     // https://github.com/evanw/esbuild/issues/2927
     // To workaround the issue, spawn a new process and invoke esbuild inside it.
-    try {
-      let esbuildScript = [
-        `const esbuild = require("${require.resolve("esbuild-wasm")}");`,
-        `esbuild.buildSync({ bundle: true, entryPoints: ["${infile}"], outfile: "${outfile}", minify: false, platform: "node", target: "node16", external: ["aws-sdk"] });`,
-      ].join("\n");
-      spawnSync(process.argv[0], ["-e", esbuildScript]);
-    } catch (e) {
-      throw new Error(`Failed to bundle function: ${e}`);
+
+    let esbuildScript = [
+      `const esbuild = require("${normalPath(
+        require.resolve("esbuild-wasm")
+      )}");`,
+      `esbuild.buildSync({ bundle: true, entryPoints: ["${infile}"], outfile: "${outfile}", minify: false, platform: "node", target: "node16", external: ["aws-sdk"] });`,
+    ].join("\n");
+    let result = spawnSync(process.argv[0], ["-e", esbuildScript]);
+    if (result.status !== 0) {
+      throw new Error(
+        `Failed to bundle function: ${result.stderr.toString("utf-8")}`
+      );
     }
 
     // the bundled contains line comments with file paths, which are not useful for us, especially