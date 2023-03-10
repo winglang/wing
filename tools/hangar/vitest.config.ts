69e6c90f7c8b903e38410fb31f786aaa864e966e
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-03-10 11:07:59 -0500

    fix windows maybe

--- tools/hangar/vitest.config.ts
+++ tools/hangar/vitest.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from "vitest/config";
-import { join, sep } from "path";
+import { join, relative } from "path";
 
 export default defineConfig({
   test: {
-    testTimeout: 150_000,
+    testTimeout: 120_000,
     globalSetup: "src/package.setup.ts",
     resolveSnapshotPath(path, extension) {
       const baseSnapshotPath = join(__dirname, "__snapshots__");
-      const relativePath = path
-        .replace(join(__dirname, "src") + sep, "")
-        .replace(/\.test\.ts$/, `.ts${extension}`);
+      const srcPath = join(__dirname, "src");
+      const relativePath = relative(srcPath, path).replace(/\.test\.ts$/, `.ts${extension}`);
 
       return join(baseSnapshotPath, relativePath);
     },
