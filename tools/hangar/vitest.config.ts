201e2a4523a1cd3f80861ad11a1fdcf7dc42cc18
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-03-10 09:14:19 -0500

    tweak

--- tools/hangar/vitest.config.ts
+++ tools/hangar/vitest.config.ts
@@ -7,9 +7,11 @@ export default defineConfig({
     globalSetup: "src/package.setup.ts",
     resolveSnapshotPath(path, extension) {
       const baseSnapshotPath = join(__dirname, "__snapshots__");
-      const relativePath = path.replace(join(__dirname, "src") + sep, "");
+      const relativePath = path
+        .replace(join(__dirname, "src") + sep, "")
+        .replace(/\.test\.ts$/, `.ts${extension}`);
 
-      return join(baseSnapshotPath, relativePath + extension);
+      return join(baseSnapshotPath, relativePath);
     },
   },
 });
