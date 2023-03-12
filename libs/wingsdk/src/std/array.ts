29a1ce8e1e16d240a28e8db8535c6a54ef942ce3
Uri Bar <urib@monada.co>
2023-03-12 18:57:01 +0200

    break dependency of mut and immut container types

--- libs/wingsdk/src/std/array.ts
+++ libs/wingsdk/src/std/array.ts
@@ -64,6 +64,94 @@ export class ImmutableArray {
   }
 
   /**
+   * Returns the index of the first occurrence of searchElement found.
+   *
+   * @macro $self$.indexOf($args$)
+   *
+   * @param searchElement to search for.
+   * @returns the index of the first occurrence of searchElement found, or -1 if not found.
+   */
+  public indexOf(searchElement: T1): number {
+    searchElement;
+    throw new Error("Macro");
+  }
+
+  /**
+   * Returns a new string containing the concatenated values in this array,
+   * separated by commas or a specified separator string. If the array has only
+   * one item, then that item will be returned without using the separator.
+   *
+   * @returns a string containing the concatenated values in this array,
+   * separated by commas or a specified separator string.
+   */
+  public join(separator?: string): string {
+    separator;
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Returns the index of the last occurrence of searchElement found.
+   *
+   * @macro $self$.lastIndexOf($args$)
+   *
+   * @param searchElement to search for.
+   * @returns the index of the last occurrence of searchElement found, or -1 if not found.
+   */
+  public lastIndexOf(searchElement: T1): number {
+    searchElement;
+    throw new Error("Macro");
+  }
+}
+
+/**
+ * Mutable Array
+ *
+ * @typeparam T1
+ */
+export class MutableArray {
+  /**
+   * The length of the array
+   * @returns the length of the array
+   */
+  public get length(): number {
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Get the value at the given index
+   * @param index index of the value to get
+   * @returns the value at the given index
+   */
+  public at(index: number): T1 {
+    index;
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Merge arr to the end of this array
+   * @param arr array to merge
+   *
+   * @returns a new MutableArray with the values of this array followed by the values of arr
+   */
+  public concat(arr: MutableArray): MutableArray {
+    arr;
+    throw new Error("Abstract");
+  }
+
+  /**
+   * Checks if this array includes searchElement.
+   *
+   * @macro $self$.includes($args$)
+   *
+   * @param searchElement to search for.
+   * @returns true if this array includes searchElement.
+   */
+  public contains(searchElement: T1): boolean {
+    searchElement;
+    throw new Error("Macro");
+  }
+
+  /**
    * Create an immutable shallow copy of this array
    *
    * @macro Object.freeze([...($self$)])
@@ -112,14 +200,7 @@ export class ImmutableArray {
     searchElement;
     throw new Error("Macro");
   }
-}
 
-/**
- * Mutable Array
- *
- * @typeparam T1
- */
-export class MutableArray extends ImmutableArray {
   /**
    * Add value to end of array
    * @param value value to add
