9ca5c193ae281f06b1c1e20ad87b40fcfc312a4e
EJ Wang <ej.wang.devs@gmail.com>
2023-03-13 08:59:38 -0700

    feat(compiler): add continue statement support (#1770)
    Implements Continue statement in https://github.com/winglang/wing/issues/114
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

--- libs/wingc/src/type_check.rs
+++ libs/wingc/src/type_check.rs
@@ -1592,7 +1592,7 @@ impl<'a> TypeChecker<'a> {
 
 				self.inner_scopes.push(statements);
 			}
-			StmtKind::Break => {}
+			StmtKind::Break | StmtKind::Continue => {}
 			StmtKind::If {
 				condition,
 				statements,
