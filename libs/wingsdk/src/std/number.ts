7199b4f0418cb425cabdb06d4f7cf32b6dcf5dfc
Hasan <hasanaburayyan21@gmail.com>
2023-03-14 09:59:20 -0400

    fix(sdk): use from_json methods in asserts

--- libs/wingsdk/src/std/number.ts
+++ libs/wingsdk/src/std/number.ts
@@ -7,7 +7,7 @@ export class Number {
   /**
    * Parse a number from Json.
    *
-   * @macro ((args) => { if (typeof args !== "number") {throw new Error(`unable to parse ${typeof args} "${args}" as a number`)}; return JSON.parse(JSON.stringify(args)) })($args$)
+   * @macro ((args) => { if (typeof args !== "number") {throw new Error("unable to parse" + " " + typeof args + " " + args + " as a number")}; return JSON.parse(JSON.stringify(args)) })($args$)
    *
    * @param json to parse number from.
    * @returns a number.
