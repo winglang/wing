66c048d4e64109689cc71349df7a4b502265accc
mutator <mutator@winglang.io>
2023-03-16 04:29:27 +0000

    chore: self mutation
    Signed-off-by: mutator <mutator@winglang.io>

--- libs/wingsdk/src/std/index.ts
+++ libs/wingsdk/src/std/index.ts
@@ -4,7 +4,7 @@ export * from "./duration";
 export * from "./json";
 export * from "./map";
 export * from "./number";
+export * from "./range";
 export * from "./set";
 export * from "./string";
 export * from "./util";
-export * from "./range";
--- libs/wingsdk/src/std/range.ts
+++ libs/wingsdk/src/std/range.ts
@@ -4,16 +4,20 @@
 
 export class Range {
   /**
-   * Generate a range of numbers. 
-   * 
+   * Generate a range of numbers.
+   *
    * @macro ((start, end, inclusive) => { function* iterator(start, end, inclusive) { let i = start; let limit = inclusive ? ((end < start) ? end - 1 : end + 1) : end; while (i < limit) yield i++; while (i > limit) yield i--; }; return iterator(start, end, inclusive);})($args$)
-   * 
+   *
    * @param start lower bound of the range.
    * @param end upper bound of the range.
    * @param inclusive if true, the upper bound value is included.
    * @returns a iterator.
    */
-  public static of(start: number, end: number, inclusive: boolean = false): Array<number> {
+  public static of(
+    start: number,
+    end: number,
+    inclusive: boolean = false
+  ): Array<number> {
     start;
     end;
     inclusive;
