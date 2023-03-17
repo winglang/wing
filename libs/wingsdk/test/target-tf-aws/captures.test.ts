06c7a99d3600d659e592193e3077a971164e0072
Hasan <hasanaburayyan21@gmail.com>
2023-03-17 13:28:27 -0400

    added capture tests

--- libs/wingsdk/test/target-tf-aws/captures.test.ts
+++ libs/wingsdk/test/target-tf-aws/captures.test.ts
@@ -1,43 +1,112 @@
-import { test, expect } from "vitest";
+import { test, expect, describe } from "vitest";
 import * as cloud from "../../src/cloud";
 import * as tfaws from "../../src/target-tf-aws";
 import { Testing } from "../../src/testing";
 import { mkdtemp, sanitizeCode } from "../../src/util";
 import { tfResourcesOf, tfSanitize } from "../util";
 
-test("function with a bucket binding", () => {
-  // GIVEN
-  const app = new tfaws.App({ outdir: mkdtemp() });
-  const bucket = cloud.Bucket._newBucket(app, "Bucket");
-  const inflight = Testing.makeHandler(
-    app,
-    "Handler",
-    `async handle(event) { await this.bucket.put("hello.txt", event); }`,
-    {
-      bucket: {
-        obj: bucket,
-        ops: [cloud.BucketInflightMethods.PUT],
-      },
+describe("function with bucket binding", () => {
+  // Dirty little helper to check if a config contains a set of actions
+  const statementsContain = (config: any, actions: string[], effect: string): boolean => {
+    const policies = config.resource.aws_iam_role_policy;
+
+    // This is dangerous because it checks for any policy in the entire config to contain the actions
+    // its safe for the tests here because we are really only checking 1 policy.
+    for (const policy of Object.keys(policies)) {
+      for (const statement of JSON.parse(policies[policy].policy).Statement) {
+        if (statement.Effect !== effect) {
+          continue;
+        }
+        if (actions.every(action => statement.Action.includes(action))) {
+          return true;
+        }
+      }
     }
-  );
-  cloud.Function._newFunction(app, "Function", inflight);
-  const output = app.synth();
+    return false;
+  };
 
-  // THEN
-  expect(sanitizeCode(inflight._toInflight())).toMatchSnapshot();
+  test("put operation", () => {
+    // GIVEN
+    const app = new tfaws.App({ outdir: mkdtemp() });
+    const bucket = cloud.Bucket._newBucket(app, "Bucket");
+    const inflight = Testing.makeHandler(
+      app,
+      "Handler",
+      `async handle(event) { await this.bucket.put("hello.txt", event); }`,
+      {
+        bucket: {
+          obj: bucket,
+          ops: [cloud.BucketInflightMethods.PUT],
+        },
+      }
+    );
+    cloud.Function._newFunction(app, "Function", inflight);
+    const output = app.synth();
+  
+    // THEN
+    expect(sanitizeCode(inflight._toInflight())).toMatchSnapshot();
+  
+    expect(tfResourcesOf(output)).toEqual([
+      "aws_iam_role",
+      "aws_iam_role_policy",
+      "aws_iam_role_policy_attachment",
+      "aws_lambda_function",
+      "aws_s3_bucket",
+      "aws_s3_bucket_public_access_block",
+      "aws_s3_bucket_server_side_encryption_configuration",
+      "aws_s3_object",
+    ]);
+    expect(tfSanitize(output)).toMatchSnapshot();
+  });
 
-  expect(tfResourcesOf(output)).toEqual([
-    "aws_iam_role",
-    "aws_iam_role_policy",
-    "aws_iam_role_policy_attachment",
-    "aws_lambda_function",
-    "aws_s3_bucket",
-    "aws_s3_bucket_public_access_block",
-    "aws_s3_bucket_server_side_encryption_configuration",
-    "aws_s3_object",
-  ]);
-  expect(tfSanitize(output)).toMatchSnapshot();
-});
+  test("put json operation has correct permissions", () => {
+    // GIVEN
+    const app = new tfaws.App({ outdir: mkdtemp() });
+    const bucket = cloud.Bucket._newBucket(app, "Bucket");
+    const inflight = Testing.makeHandler(
+      app,
+      "Handler",
+      `async handle(event) { await this.bucket.put("hello.txt", event); }`,
+      {
+        bucket: {
+          obj: bucket,
+          ops: [cloud.BucketInflightMethods.PUT_JSON],
+        },
+      }
+    );
+    cloud.Function._newFunction(app, "Function", inflight);
+    const output = JSON.parse(app.synth());
+    const hasActions = statementsContain(output, ["s3:PutObject*"], "Allow");
+      
+    // THEN
+    expect(hasActions).toEqual(true);
+  });
+
+  test("get json operation has correct permissions", () => {
+    // GIVEN
+    const app = new tfaws.App({ outdir: mkdtemp() });
+    const bucket = cloud.Bucket._newBucket(app, "Bucket");
+    const inflight = Testing.makeHandler(
+      app,
+      "Handler",
+      `async handle(event) { await this.bucket.put("hello.txt", event); }`,
+      {
+        bucket: {
+          obj: bucket,
+          ops: [cloud.BucketInflightMethods.GET_JSON],
+        },
+      }
+    );
+    cloud.Function._newFunction(app, "Function", inflight);
+    const output = JSON.parse(app.synth());
+    const hasActions = statementsContain(output, ["s3:GetObject*", "s3:GetBucket*", "s3:List*"], "Allow");
+      
+    // THEN
+    expect(hasActions).toEqual(true);
+  });
+
+})
+
 
 test("function with a function binding", () => {
   // GIVEN
