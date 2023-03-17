029640d26b7fc0f9a41416ce5614a62c48e9bb8a
Chris Rybicki <chrisr@monada.co>
2023-03-17 12:45:34 -0400

    update snapshots

--- tools/hangar/src/test_corpus/valid/optionals.w.test.ts
+++ tools/hangar/src/test_corpus/valid/optionals.w.test.ts
@@ -4,9 +4,9 @@ import { test } from "vitest";
 import { compileTest, testTest } from "../../generated_test_targets";
 
 test("wing compile -t tf-aws", async ({ expect }) => {
-  await compileTest(expect, "resource_as_inflight_literal.w");
+  await compileTest(expect, "optionals.w");
 });
 
 test("wing test", async ({ expect }) => {
-  await testTest(expect, "resource_as_inflight_literal.w");
+  await testTest(expect, "optionals.w");
 });
