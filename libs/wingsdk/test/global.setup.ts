c881003c70a857e2de864f298bbdb1e6138f73d2
Elad Ben-Israel <eladb@monada.co>
2023-03-21 02:40:24 +0200

    Update libs/wingsdk/test/global.setup.ts

--- libs/wingsdk/test/global.setup.ts
+++ libs/wingsdk/test/global.setup.ts
@@ -1,6 +1,8 @@
 import { exec, execSync } from "child_process";
 
 export default async function () {
+  // compile src/**/*.on*.inflight.ts to .js because these are going to be
+  // injected into our javascript vm and cannot be resolved via vitest
   execSync("npx tsc -p tsconfig.test.json");
   return () => {};
 }
