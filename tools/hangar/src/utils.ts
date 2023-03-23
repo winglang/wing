1b194e069da9f9f3d4279030f28d6bcc984476d3
Hasan <hasanaburayyan21@gmail.com>
2023-03-23 08:33:39 -0400

    chore(plugin): move example plugin tests to hangar

--- tools/hangar/src/utils.ts
+++ tools/hangar/src/utils.ts
@@ -3,20 +3,26 @@ import * as fs from "fs-extra";
 import { expect } from "vitest";
 import { wingBin } from "./paths";
 
-export async function runWingCommand(
-  cwd: string,
-  wingFile: string,
-  args: string[],
-  shouldSucceed: boolean,
-  env?: Record<string, string>,
-) {
-  const out = await execa(wingBin, [...args, wingFile], {
-    cwd,
+export interface RunWingCommandOptions {
+  cwd: string;
+  wingFile: string;
+  args: string[];
+  shouldSucceed: boolean;
+  plugins?: string[];
+  env?: Record<string, string>;
+}
+
+export async function runWingCommand(options: RunWingCommandOptions) {
+  // TODO: plugins cant easily be combined with args because the plugin option is a variadic option which will consume
+  // the name of the wing file. So they must be passed in as the last option. 
+  const plugins = options.plugins? ['--plugins', ...options.plugins] : '';
+  const out = await execa(wingBin, [...options.args, options.wingFile, ...plugins], {
+    cwd: options.cwd,
     reject: false,
     stdin: "ignore",
-    env: env,
+    env: options.env,
   });
-  if (shouldSucceed) {
+  if (options.shouldSucceed) {
     if (out.exitCode !== 0) {
       expect.fail(out.stderr);
     }
@@ -41,4 +47,15 @@ export function sanitize_json_paths(path: string) {
   delete finalObj.terraform;
 
   return finalObj;
+}
+
+export function tfResourcesOf(templateStr: string): string[] {
+  return Object.keys(JSON.parse(templateStr).resource).sort();
+}
+
+export function tfResourcesOfCount(
+  templateStr: string,
+  resourceId: string
+): number {
+  return Object.values(JSON.parse(templateStr).resource[resourceId]).length;
 }
