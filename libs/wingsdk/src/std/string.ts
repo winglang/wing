bbaf29581f479860ec060d6d6d69e76132077e44
Uri Bar <106860404+staycoolcall911@users.noreply.github.com>
2023-03-23 13:10:25 +0200

    chore(sdk): rename str method ends to endswith (#1895)
    Following our session writing a wingsdk resource in wing, we decided to rename `str.starts()` and `str.ends()` to `str.starts_with()` and `str.ends_with()` respectively.
    
    *This PR also includes some minor reference documentation update that were missing in #1883 
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

--- libs/wingsdk/src/std/string.ts
+++ libs/wingsdk/src/std/string.ts
@@ -66,14 +66,12 @@ export class String {
   /**
    * Does this string end with the given searchString?
    *
-   * @macro $self$.endsWith($args$)
-   *
    * @param searchString substring to search for.
    * @returns true if string ends with searchString.
    */
-  public ends(searchString: string): boolean {
+  public endsWith(searchString: string): boolean {
     searchString;
-    throw new Error("Macro");
+    throw new Error("Abstract");
   }
 
   /**
@@ -114,14 +112,12 @@ export class String {
   /**
    * Does this string start with the given searchString?
    *
-   * @macro $self$.startsWith($args$)
-   *
    * @param searchString substring to search for.
    * @returns true if string starts with searchString.
    */
-  public starts(searchString: string): boolean {
+  public startsWith(searchString: string): boolean {
     searchString;
-    throw new Error("Macro");
+    throw new Error("Abstract");
   }
 
   /**
