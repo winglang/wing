winglang/wing

commit fbf535245393f01e9aed32205201acd555644d24

chore(repo): generate actual release notes (#1902)
The current release notes suck for various reasons, let's improve/simplify them.

- Properly filters out bots from the contributor list
- Handles breaking changes without dealing with labels (since we have a 0 major version number, it bumps minor)
- Show proper types for changes
- Simplifies the workflow (removed the `prepare` job), opening up options to improve our github actions even more
- Provides a skeleton to create completely custom changelogs

Uses https://github.com/unjs/changelogen which is a really nice lib in a great ecosystem. See [here](https://github.com/unjs/changelogen/releases) for example changelogs.

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

scripts/changelog.mjs
@@ -1,0 +1,89 @@
+#!/usr/bin/env node
+
+import * as changelogen from "changelogen";
+import * as semver from "semver";
+import { appendFileSync } from "fs";
+
+const inAction = process.env.GITHUB_ACTIONS === "true";
+
+async function getData() {
+  // If we want to generate a version, skip changelogen logic
+  if (process.env.GENERATE_VERSION !== "false") {
+    let version = "0.0.0";
+    if (inAction) {
+      const runId = process.env.GITHUB_RUN_ID;
+      const runAttempt = process.env.GITHUB_RUN_ATTEMPT;
+
+      version += `-${runId}.${runAttempt}`;
+    }
+
+    return {
+      lastVersion: version,
+      newVersion: version,
+      bumpType: "patch",
+      sameVersion: true,
+      changelog: "Development Version",
+    };
+  }
+
+  const config = await changelogen.loadChangelogConfig(process.cwd(), {
+    types: {
+      rfc: {
+        title: "👓 RFC",
+        semver: "patch",
+      },
+    },
+  });
+
+  const lastTag = await changelogen.getLastGitTag();
+  const lastVersion = lastTag.replace("v", "");
+  const commits = await changelogen.getGitDiff(
+    lastTag,
+    await changelogen.getCurrentGitRef()
+  );
+
+  const parsed = changelogen.parseCommits(commits, config);
+
+  let bumpType = changelogen.determineSemverChange(parsed, config) ?? "patch";
+  if (bumpType === "major" && semver.major(lastVersion) === 0) {
+    bumpType = "minor";
+  }
+
+  let newVersion =
+    commits.length > 0
+      ? semver.inc(lastVersion, bumpType) ?? lastVersion
+      : lastVersion;
+  newVersion = process.env.WING_VERSION ?? newVersion;
+
+  const md = await changelogen.generateMarkDown(parsed, config);
+
+  return {
+    lastVersion,
+    newVersion,
+    bumpType,
+    sameVersion: lastVersion === newVersion,
+    // Remove the first line of the changelog (the title)
+    changelog: md.split("\n").slice(1).join("\n").trimStart() + "\n",
+  };
+}
+
+const resultObj = await getData();
+
+console.log(resultObj);
+
+if (inAction) {
+  // we are running in a github action and we should output some useful stuff
+  const githubOutputFile = process.env.GITHUB_OUTPUT;
+  const githubStepSummaryFile = process.env.GITHUB_STEP_SUMMARY;
+  const githubEnvFile = process.env.GITHUB_ENV;
+  if (!githubOutputFile || !githubStepSummaryFile || !githubEnvFile) {
+    throw new Error("Missing github action environment variables");
+  }
+
+  appendFileSync(githubOutputFile, `data=${JSON.stringify(resultObj)}\n`);
+  appendFileSync(githubStepSummaryFile, `${resultObj.changelog}\n`);
+  appendFileSync(
+    githubEnvFile,
+    `PROJEN_BUMP_VERSION=${resultObj.newVersion}\n`
+  );
+}

.autorc
@@ -1,30 +1,0 @@
-{
-  "plugins": [
-    ["conventional-commits", { "preset": "angular" }],
-    "git-tag"
-  ],
-  "labels": [
-    {
-      "name": "documentation",
-      "releaseType": "release"
-    },
-    {
-      "name": "internal",
-      "releaseType": "release"
-    },
-    {
-      "name": "tests",
-      "releaseType": "release"
-    },
-    {
-      "name": "dependencies",
-      "releaseType": "release"
-    }
-  ],
-  "onlyPublishWithReleaseLabel": false,
-  "baseBranch": "main",
-  "owner": "winglang",
-  "repo": "wing",
-  "name": "MonadaBot",
-  "email": "monadabot@monada.co"
-}

.github/workflows/build.yml
@@ -14,93 +14,26 @@ concurrency:
   group: ${{ github.workflow }}-${{ github.event.pull_request.head.ref || github.ref }}
   cancel-in-progress: true
 
-env:
-  # Testing runs out of memory without this
-  NODE_OPTIONS: "--max-old-space-size=4096"
-  PROJEN_BUMP_VERSION: "0.0.0-dev.${{ github.run_id }}.${{ github.run_attempt }}"
-  RUST_VERSION: "1.67.1"
-  CARGO_TERM_COLOR: always
-
 jobs:
-  prepare:
-    name: "Prepare"
-    runs-on: ubuntu-latest
-    outputs:
-      last-version: ${{ steps.tags.outputs.last-version || steps.pr_info.outputs.version }}
-      version: ${{ steps.info.outputs.version || steps.pr_info.outputs.version }}
-    steps:
-      - name: Checkout
-        if: ${{ github.event_name == 'push' && github.repository == 'winglang/wing' }}
-        uses: actions/checkout@v3
-
-      - name: Get tags
-        id: tags
-        if: ${{ github.event_name == 'push' && github.repository == 'winglang/wing' }}
-        run: |
-          git fetch --unshallow --tags
-          LAST_VERSION="$(git describe --tags `git rev-list --tags --max-count=1`)"
-          echo "last-version=${LAST_VERSION#?}" >> $GITHUB_OUTPUT
-
-      - name: Install Dependencies
-        if: ${{ github.event_name == 'push' && github.repository == 'winglang/wing' }}
-        uses: bahmutov/npm-install@v1
-        with:
-          install-command: npm ci --ignore-scripts
-
-      - name: Get Version Info
-        if: ${{ github.event_name == 'push' && github.repository == 'winglang/wing' }}
-        id: info
-        env:
-          # Needed for `auto` cli
-          GH_TOKEN: ${{ secrets.GITHUB_TOKEN }}
-        run: |
-          set -e
-          echo "version=$(npx auto release -d | grep 'Would have created a release on GitHub for version: ' | sed 's/.*version: //')" >> $GITHUB_OUTPUT
-          npx auto changelog -d | tail -n +2 > CHANGELOG.md
-          cat CHANGELOG.md >> $GITHUB_STEP_SUMMARY
-
-      # newlines are easier to handle in single-line yaml
-      - run: "echo '\n' >> CHANGELOG.md"
-        if: ${{ github.event_name == 'push' && github.repository == 'winglang/wing' }}
-
-      - name: PR Version Info
-        id: pr_info
-        if: github.event_name != 'push' || github.repository != 'winglang/wing'
-        run: |
-          echo "last-version=0.0.0" >> $GITHUB_OUTPUT
-          echo "version=0.0.0-dev.${{ github.run_id }}.${{ github.run_attempt }}" >> $GITHUB_OUTPUT
-          echo "Pull Request" > CHANGELOG.md
-          cat CHANGELOG.md >> $GITHUB_STEP_SUMMARY
-
-      - name: Upload Changelog
-        uses: actions/upload-artifact@v3
-        with:
-          name: changelog
-          path: CHANGELOG.md
-
-      - name: Compress Docs
-        if: ${{ github.event_name == 'push' && github.repository == 'winglang/wing' }}
-        run: tar -czvf docs.tgz docs/*
-
-      - name: Upload Docs
-        if: ${{ github.event_name == 'push' && github.repository == 'winglang/wing' }}
-        uses: actions/upload-artifact@v3
-        with:
-          name: docs
-          path: docs.tgz
-
   build:
     name: "Build"
     timeout-minutes: 25
     runs-on: ubuntu-latest
-    needs: prepare
     env:
-      PROJEN_BUMP_VERSION: ${{ needs.prepare.outputs.version }}
+      # Testing runs out of memory without this
+      NODE_OPTIONS: "--max-old-space-size=4096"
+      RUST_VERSION: "1.67.1"
+      CARGO_TERM_COLOR: always
     outputs:
       labels: ${{ steps.pr-labels.outputs.labels }}
+      version: ${{ fromJson(steps.changelog.outputs.data).newVersion }}
+      last-version: ${{ fromJson(steps.changelog.outputs.data).lastVersion }}
+      changelog: ${{ fromJson(steps.changelog.outputs.data).changelog }}
     steps:
       - name: Checkout
         uses: actions/checkout@v3
+        with:
+          fetch-depth: 0
 
       # Reads versions from root package.json "volta" section
       - name: Setup Node/NPM with pinned Volta version
@@ -123,6 +56,12 @@ jobs:
 
       - name: Lint Nx Configuration
         run: npx nx workspace-lint
+
+      - name: Changelog Generation
+        id: changelog
+        env:
+          GENERATE_VERSION: ${{ github.event_name != 'push' || github.repository != 'winglang/wing' }}
+        run: npm run changelog
 
       - name: Build
         run: npm run build:ci
@@ -157,6 +96,15 @@ jobs:
           name: vscode-wing
           path: apps/vscode-wing/vscode-wing.vsix
 
+      - name: Compress Docs
+        run: tar -czvf docs.tgz docs/*
+
+      - name: Upload Docs
+        uses: actions/upload-artifact@v3
+        with:
+          name: docs
+          path: docs.tgz
+
       - name: Publish Playground
         if: ${{ github.event_name == 'push' && github.repository == 'winglang/wing' }}
         uses: amondnet/vercel-action@v25.1.0
@@ -177,7 +125,6 @@ jobs:
     name: "E2E / ${{ matrix.runner }} + Node${{ matrix.node }} [${{ matrix.shard }}]"
     needs:
       - build
-      - prepare
     strategy:
       fail-fast: true
       matrix:
@@ -199,8 +146,8 @@ jobs:
             full_run: false
     runs-on: "${{ matrix.runner }}-latest"
     env:
-      HANGAR_WING_SPEC: "file:${{ github.workspace }}/wing/winglang-${{ needs.prepare.outputs.version }}.tgz"
-      HANGAR_WINGSDK_SPEC: "file:${{ github.workspace }}/wingsdk/winglang-sdk-${{ needs.prepare.outputs.version }}.tgz"
+      HANGAR_WING_SPEC: "file:${{ github.workspace }}/wing/winglang-${{ needs.build.outputs.version }}.tgz"
+      HANGAR_WINGSDK_SPEC: "file:${{ github.workspace }}/wingsdk/winglang-sdk-${{ needs.build.outputs.version }}.tgz"
     steps:
       - name: Checkout
         uses: actions/checkout@v3
@@ -241,7 +188,7 @@ jobs:
     if: ${{ github.event_name == 'push' && github.repository == 'winglang/wing' }}
     needs:
       - quality-gate
-      - prepare
+      - build
     runs-on: ubuntu-latest
     steps:
       - name: Download Build Artifacts
@@ -249,17 +196,17 @@ jobs:
 
       - name: Tag commit
         uses: tvdias/github-tagger@v0.0.1
-        if: ${{ needs.prepare.outputs.last-version != needs.prepare.outputs.version }}
+        if: ${{ needs.build.outputs.last-version != needs.build.outputs.version }}
         with:
           repo-token: "${{ secrets.PROJEN_GITHUB_TOKEN }}"
-          tag: "v${{ needs.prepare.outputs.version }}"
+          tag: "v${{ needs.build.outputs.version }}"
 
       - name: Check published VSCode Version
         id: vscode-version
         run: echo "version=$(npx -y vsce show Monada.vscode-wing --json | jq '.versions[0].version' -r)" >> $GITHUB_OUTPUT
 
       - name: Publish Extension to Visual Studio Marketplace
-        if: ${{ steps.vscode-version.outputs.version != needs.prepare.outputs.version }}
+        if: ${{ steps.vscode-version.outputs.version != needs.build.outputs.version }}
         uses: "HaaLeo/publish-vscode-extension@v1"
         with:
           pat: ${{ secrets.VS_MARKETPLACE_TOKEN }}
@@ -274,7 +221,7 @@ jobs:
         run: echo "version=$(npm view winglang version)" >> $GITHUB_OUTPUT
 
       - name: Publish Wing
-        if: ${{ steps.wing-version.outputs.version != needs.prepare.outputs.version }}
+        if: ${{ steps.wing-version.outputs.version != needs.build.outputs.version }}
         working-directory: wing
         run: npm publish *.tgz --access public
 
@@ -283,12 +230,12 @@ jobs:
         run: echo "version=$(npm view @winglang/sdk version)" >> $GITHUB_OUTPUT
 
       - name: Publish Wing SDK
-        if: ${{ steps.wingsdk-version.outputs.version != needs.prepare.outputs.version }}
+        if: ${{ steps.wingsdk-version.outputs.version != needs.build.outputs.version }}
         working-directory: wingsdk
         run: npm publish *.tgz --access public
 
       - name: Rename Docs
-        run: mv docs "docs-${{ needs.prepare.outputs.version }}.tgz"
+        run: mv docs "docs-${{ needs.build.outputs.version }}.tgz"
 
       - name: Compute Checksums
         run: |
@@ -298,8 +245,8 @@ jobs:
           mv ./*/*.wasm ./dist
           cd dist
 
-          cp ../changelog/CHANGELOG.md ../CHANGELOG.md
-          echo '## SHA-1 Checksums' >> ../CHANGELOG.md
+          echo '${{ needs.build.outputs.changelog }}'' > ../CHANGELOG.md
+          echo '### SHA-1 Checksums' >> ../CHANGELOG.md
           echo '```' >> ../CHANGELOG.md
           sha1sum --binary * >> ../CHANGELOG.md
           echo '```' >> ../CHANGELOG.md
@@ -308,8 +255,8 @@ jobs:
       - name: Cut Development Release
         uses: softprops/action-gh-release@v1
         with:
-          name: "Wing ${{ needs.prepare.outputs.version }}"
-          tag_name: "v${{ needs.prepare.outputs.version }}"
+          name: "Wing ${{ needs.build.outputs.version }}"
+          tag_name: "v${{ needs.build.outputs.version }}"
           body_path: CHANGELOG.md
           files: dist/*
           token: ${{ secrets.PROJEN_GITHUB_TOKEN }}

.github/workflows/pull-request-lint.yml
@@ -3,11 +3,9 @@ name: Pull Request Lint
 on:
   pull_request_target:
     types:
-      - labeled
       - opened
       - synchronize
       - reopened
-      - ready_for_review
       - edited
 
 jobs:
@@ -15,7 +13,7 @@ jobs:
     name: Validate PR title
     runs-on: ubuntu-latest
     steps:
-      - uses: amannn/action-semantic-pull-request@v5.0.2
+      - uses: amannn/action-semantic-pull-request@v5.2.0
         env:
           GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
         with:

.github/workflows/wingsdk-mutation.yml
@@ -66,7 +66,7 @@ jobs:
       - name: Push changes
         if: steps.token-check.outputs.HAS_TOKEN == 'true' && steps.self_mutation.outputs.self_mutation_happened
         run: |-
-          git config user.name "mutator"
-          git config user.email "mutator@winglang.io"
+          git config user.name "monada-bot[bot]"
+          git config user.email "monabot@monada.co"
           git commit -s -m "chore: self mutation"
           git push origin HEAD:${{ github.event.pull_request.head.ref }}

commit c539b2b3269de6837e2d2991ae6c98512ea12340

chore: feats should only increment patch version by default (#1906)
Regression from https://github.com/winglang/wing/pull/1902 (thanks @Chriscbr)

If it's a breaking change, then it'll increment minor

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

scripts/changelog.mjs
@@ -32,6 +32,14 @@ async function getData() {
         title: "👓 RFC",
         semver: "patch",
       },
+      feat: {
+        title: "🚀 Features",
+        semver: "patch",
+      },
+      revert: {
+        title: "⏪ Reverts",
+        semver: "patch",
+      },
     },
   });
 