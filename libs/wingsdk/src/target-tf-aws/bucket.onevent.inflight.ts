8efd928e0fb8c468bf7a5b7e0b1233f3369569a8
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2023-03-27 20:06:46 +0300

    aws bucket events work!!!!!!!

--- libs/wingsdk/src/target-tf-aws/bucket.onevent.inflight.ts
+++ libs/wingsdk/src/target-tf-aws/bucket.onevent.inflight.ts
@@ -5,7 +5,18 @@ export class BucketEventHandlerClient implements IBucketEventHandlerClient {
   constructor({ handler }: { handler: IFunctionHandlerClient }) {
     this.handler = handler;
   }
-  public async handle(event: string) {
-    return this.handler.handle(event);
+  public async handle(event: any) {
+    try {
+      const message = JSON.parse(event?.Message);
+      if (message?.Event === "s3:TestEvent") {
+        // aws sends a test event to the topic before of the actual one, we're ignoring it for now
+        return;
+      }
+      return this.handler.handle(message.Records[0].s3.object.key);
+    } catch (error) {
+      //TODO: change to some sort of warning- console.warn doesn't seems to work
+      console.log("Error parsing the notification event message: ", error);
+      console.log("Event: ", event);
+    }
   }
 }
