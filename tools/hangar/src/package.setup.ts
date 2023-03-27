f4362688808893559265974830f535f252af4d62
Hasan <hasanaburayyan21@gmail.com>
2023-03-27 17:19:05 -0400

    one more thing

--- tools/hangar/src/package.setup.ts
+++ tools/hangar/src/package.setup.ts
@@ -54,6 +54,7 @@ export default async function () {
 
 
   console.log('STDOUT:', installResult.stdout);
+  console.log('STDOUT:', installResult.stdout);
 
   assert.equal(
     installResult.exitCode,
