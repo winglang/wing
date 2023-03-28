winglang/wing

commit 809d50a423c10d3db3f0a90676d6cf8eba477f99

fix(cli): compile command does not write to target directory in an atomic way (#1924)
Fixes #1919

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

apps/wing/src/commands/compile.ts
@@ -1,6 +1,6 @@
 import * as vm from "vm";
 
-import { mkdir, readFile, rm } from "fs/promises";
+import { mkdir, readFile, mkdtemp, rename, rm } from "fs/promises";
 import { basename, dirname, join, resolve } from "path";
 
 import * as chalk from "chalk";
@@ -8,6 +8,7 @@ import debug from "debug";
 import * as wingCompiler from "../wingc";
 import { normalPath } from "../util";
 import { CHARS_ASCII, emitDiagnostic, Severity, File, Label } from "codespan-wasm";
+import * as os from "os";
 
 // increase the stack trace limit to 50, useful for debugging Rust panics
 // (not setting the limit too high in case of infinite recursion)
@@ -67,39 +68,40 @@ function resolveSynthDir(outDir: string, entrypoint: string, target: Target) {
  * @returns the output directory
  */
 export async function compile(entrypoint: string, options: ICompileOptions): Promise<string> {
+  // create a unique temporary directory for the compilation
+  const tmpTargetdir = await mkdtemp(join(os.tmpdir(), "wing-"));
   const targetdir = join(dirname(entrypoint), "target");
   const wingFile = entrypoint;
   log("wing file: %s", wingFile);
   const wingDir = dirname(wingFile);
   log("wing dir: %s", wingDir);
+  const tmpSynthDir = resolveSynthDir(tmpTargetdir, wingFile, options.target);
+  log("temp synth dir: %s", tmpSynthDir);
   const synthDir = resolveSynthDir(targetdir, wingFile, options.target);
   log("synth dir: %s", synthDir);
-  const workDir = resolve(synthDir, ".wing");
+  const workDir = resolve(tmpSynthDir, ".wing");
   log("work dir: %s", workDir);
 
-  // clean up before
-  await rm(synthDir, { recursive: true, force: true });
-
-  process.env["WING_SYNTH_DIR"] = synthDir;
+  process.env["WING_SYNTH_DIR"] = tmpSynthDir;
   process.env["WING_NODE_MODULES"] = resolve(join(wingDir, "node_modules"));
   process.env["WING_TARGET"] = options.target;
 
   await Promise.all([
     mkdir(workDir, { recursive: true }),
-    mkdir(synthDir, { recursive: true }),
+    mkdir(tmpSynthDir, { recursive: true }),
   ]);
 
   const wingc = await wingCompiler.load({
     env: {
       RUST_BACKTRACE: "full",
-      WING_SYNTH_DIR: normalPath(synthDir),
+      WING_SYNTH_DIR: normalPath(tmpSynthDir),
       WINGC_PREFLIGHT,
       CLICOLOR_FORCE: chalk.supportsColor ? "1" : "0",
     },
     preopens: {
       [wingDir]: wingDir, // for Rust's access to the source file
       [workDir]: workDir, // for Rust's access to the work directory
-      [synthDir]: synthDir, // for Rust's access to the synth directory
+      [tmpSynthDir]: tmpSynthDir, // for Rust's access to the synth directory
     },
   });
 
@@ -248,6 +250,12 @@ export async function compile(entrypoint: string, options: ICompileOptions): Pro
 
     return process.exit(1);
   }
+
+  // clean up before
+  await rm(synthDir, { recursive: true, force: true });
+  await mkdir(synthDir, { recursive: true });
+  // move the temporary directory to the final target location in an atomic operation
+  await rename(tmpSynthDir, synthDir);
 
   return synthDir;
 }

commit 6594c1f7c175ccafc847f09bc0a021446c382cc8

fix: unable to compile on windows when compiling on non-system drive (#1929)
Regression from https://github.com/winglang/wing/pull/1924. Instead implementing Elad's suggesting to keep the tmp dir within the target folder.

also undo the changes from https://github.com/winglang/wing/pull/1846 since it broke the functionality

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Monada Contribution License](https://docs.winglang.io/terms-and-policies/contribution-license.html)*.

.github/workflows/build.yml
@@ -25,7 +25,6 @@ jobs:
       RUST_VERSION: "1.67.1"
       CARGO_TERM_COLOR: always
     outputs:
-      labels: ${{ steps.pr-labels.outputs.labels }}
       version: ${{ fromJson(steps.changelog.outputs.data).newVersion }}
       last-version: ${{ fromJson(steps.changelog.outputs.data).lastVersion }}
       changelog: ${{ fromJson(steps.changelog.outputs.data).changelog }}
@@ -117,10 +116,6 @@ jobs:
           github-comment: false
           vercel-args: "--prod "
 
-      - name: Get PR labels
-        id: pr-labels
-        uses: joerick/pr-labels-action@v1.0.7
-
   e2e:
     name: "E2E / ${{ matrix.runner }} + Node${{ matrix.node }} [${{ matrix.shard }}]"
     needs:
@@ -135,7 +130,7 @@ jobs:
         shard: ["1/2", "2/2"]
         full_run:
           # Do a full run on push or when the PR is labeled "pr/e2e-full"
-          - ${{ github.event_name == 'push' || contains(needs.build.labels, 'pr/e2e-full') }}
+          - ${{ github.event_name == 'push' || contains(github.event.pull_request.labels.*.name, 'pr/e2e-full') }}
         exclude:
           - runner: macos
             full_run: false

apps/wing/src/commands/compile.ts
@@ -1,14 +1,14 @@
 import * as vm from "vm";
 
-import { mkdir, readFile, mkdtemp, rename, rm } from "fs/promises";
+import { readFile, rmSync, mkdirp, move, mkdirpSync, copySync } from "fs-extra";
 import { basename, dirname, join, resolve } from "path";
+import * as os from "os";
 
 import * as chalk from "chalk";
 import debug from "debug";
 import * as wingCompiler from "../wingc";
 import { normalPath } from "../util";
 import { CHARS_ASCII, emitDiagnostic, Severity, File, Label } from "codespan-wasm";
-import * as os from "os";
 
 // increase the stack trace limit to 50, useful for debugging Rust panics
 // (not setting the limit too high in case of infinite recursion)
@@ -52,13 +52,14 @@ export interface ICompileOptions {
  * within the output directory where the SDK app will synthesize its artifacts
  * for the given target.
  */
-function resolveSynthDir(outDir: string, entrypoint: string, target: Target) {
+function resolveSynthDir(outDir: string, entrypoint: string, target: Target, tmp: boolean = false) {
   const targetDirSuffix = DEFAULT_SYNTH_DIR_SUFFIX[target];
   if (!targetDirSuffix) {
     throw new Error(`unsupported target ${target}`);
   }
   const entrypointName = basename(entrypoint, ".w");
-  return join(outDir, `${entrypointName}.${targetDirSuffix}`);
+  const tmpSuffix = tmp ? `.${Date.now().toString().slice(-6)}.tmp` : "";
+  return join(outDir, `${entrypointName}.${targetDirSuffix}${tmpSuffix}`);
 }
 
 /**
@@ -69,13 +70,12 @@ function resolveSynthDir(outDir: string, entrypoint: string, target: Target) {
  */
 export async function compile(entrypoint: string, options: ICompileOptions): Promise<string> {
   // create a unique temporary directory for the compilation
-  const tmpTargetdir = await mkdtemp(join(os.tmpdir(), "wing-"));
   const targetdir = join(dirname(entrypoint), "target");
   const wingFile = entrypoint;
   log("wing file: %s", wingFile);
   const wingDir = dirname(wingFile);
   log("wing dir: %s", wingDir);
-  const tmpSynthDir = resolveSynthDir(tmpTargetdir, wingFile, options.target);
+  const tmpSynthDir = resolveSynthDir(targetdir, wingFile, options.target, true);
   log("temp synth dir: %s", tmpSynthDir);
   const synthDir = resolveSynthDir(targetdir, wingFile, options.target);
   log("synth dir: %s", synthDir);
@@ -87,8 +87,8 @@ export async function compile(entrypoint: string, options: ICompileOptions): Pro
   process.env["WING_TARGET"] = options.target;
 
   await Promise.all([
-    mkdir(workDir, { recursive: true }),
-    mkdir(tmpSynthDir, { recursive: true }),
+    mkdirp(workDir),
+    mkdirp(tmpSynthDir),
   ]);
 
   const wingc = await wingCompiler.load({
@@ -251,11 +251,18 @@ export async function compile(entrypoint: string, options: ICompileOptions): Pro
     return process.exit(1);
   }
 
-  // clean up before
-  await rm(synthDir, { recursive: true, force: true });
-  await mkdir(synthDir, { recursive: true });
-  // move the temporary directory to the final target location in an atomic operation
-  await rename(tmpSynthDir, synthDir);
+  if(os.platform() === "win32") {
+    // Windows doesn't really support fully atomic moves.
+    // So we just copy the directory instead.
+    // Also only using sync methods to avoid possible async fs issues.
+    rmSync(synthDir, { recursive: true, force: true });
+    mkdirpSync(synthDir);
+    copySync(tmpSynthDir, synthDir);
+    rmSync(tmpSynthDir, { recursive: true, force: true });
+  } else {
+    // Move the temporary directory to the final target location in an atomic operation
+    await move(tmpSynthDir, synthDir, { overwrite: true } );
+  }
 
   return synthDir;
 }