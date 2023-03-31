a22896e5fe0973c0ab1371fee5c488cbb10bcff7
Marcio Cruz de Almeida <marciocadev@gmail.com>
2023-03-31 15:00:41 -0300

    refactoring

--- libs/wingc/src/type_check.rs
+++ libs/wingc/src/type_check.rs
@@ -1714,7 +1714,7 @@ impl<'a> TypeChecker<'a> {
 
 	fn check_class_field_initialization(&mut self, statements: &Scope, fields: &Vec<ClassField>) {
 		let init_fields = self.list_fields_on_init(statements);
-		for field in fields.clone() {
+		for field in fields {
 			if !init_fields.contains(&field.name.name) {
 				self.type_error(TypeError {
 					message: format!("\"{}\" is not initialized", field.name.name),
