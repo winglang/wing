46a03cb3fd6e3a7c5e1c271dbfb0eb8d36586b06
Chris Rybicki <chrisr@monada.co>
2023-04-10 18:09:44 -0400

    write free variable finding code

--- libs/wingc/src/inflight_transform.rs
+++ libs/wingc/src/inflight_transform.rs
@@ -1,9 +1,11 @@
 use std::cell::RefCell;
 
+use indexmap::IndexSet;
+
 use crate::{
 	ast::{
-		ArgList, Class, Constructor, Expr, ExprKind, FunctionDefinition, FunctionSignature, Phase, Scope, Stmt, StmtKind,
-		Symbol, TypeAnnotation, UserDefinedType,
+		ArgList, CatchBlock, Class, Constructor, Expr, ExprKind, FunctionBody, FunctionDefinition, FunctionSignature,
+		Phase, Reference, Scope, Stmt, StmtKind, Symbol, TypeAnnotation, UserDefinedType, UtilityFunctions,
 	},
 	fold::{self, Fold},
 };
@@ -48,12 +50,6 @@ impl Fold for InflightTransformer {
 
 		match expr.kind {
 			ExprKind::FunctionClosure(def) => {
-				// resource_def = resource {
-				//   init() {}
-				//   inflight handle() {
-				//     expr.def
-				//   }
-				// }
 				let resource_name = Symbol {
 					name: "$Resource1".to_string(),
 					span: expr.span.clone(),
@@ -68,6 +64,20 @@ impl Fold for InflightTransformer {
 					fields: vec![],
 				});
 
+				let mut free_var_transformer = FreeVariableTransformer::new(vec![
+					Symbol::global(UtilityFunctions::Log.to_string()),
+					Symbol::global(UtilityFunctions::Assert.to_string()),
+					Symbol::global(UtilityFunctions::Throw.to_string()),
+					Symbol::global(UtilityFunctions::Panic.to_string()),
+				]);
+				let (transformed_def, _free_vars) = free_var_transformer.transform_def(def);
+
+				// resource_def = resource {
+				//   init() {}
+				//   inflight handle() {
+				//     <transformed_def>
+				//   }
+				// }
 				let resource_def = Stmt {
 					kind: StmtKind::Class(Class {
 						name: resource_name.clone(),
@@ -83,13 +93,13 @@ impl Fold for InflightTransformer {
 						fields: vec![],
 						implements: vec![],
 						parent: None,
-						methods: vec![(handle_name.clone(), def)],
+						methods: vec![(handle_name.clone(), transformed_def)],
 					}),
 					idx: 0,
 					span: expr.span.clone(),
 				};
 
-				// return_resource_instance = return new $Resource1();
+				// return_resource_instance = return new <resource_name>();
 				let return_resource_instance = Stmt {
 					kind: StmtKind::Return(Some(Expr::new(
 						ExprKind::New {
@@ -105,12 +115,12 @@ impl Fold for InflightTransformer {
 				};
 
 				// make_resource_body = {
-				//   resource_def
-				//   return_resource_instance
+				//   <resource_def>
+				//   <return_resource_instance>
 				// }
 				let make_resource_body = Scope::new(vec![resource_def, return_resource_instance], expr.span.clone());
 
-				// make_resource_closure = (): resource => { ...make_resource_body }
+				// make_resource_closure = (): resource => { ...<make_resource_body> }
 				let make_resource_closure = Expr::new(
 					ExprKind::FunctionClosure(FunctionDefinition {
 						signature: FunctionSignature {
@@ -126,7 +136,7 @@ impl Fold for InflightTransformer {
 					expr.span.clone(),
 				);
 
-				// call_expr = make_resource_closure()
+				// call_expr = <make_resource_closure>()
 				let call_expr = Expr::new(
 					ExprKind::Call {
 						arg_list: ArgList::new(),
@@ -137,6 +147,192 @@ impl Fold for InflightTransformer {
 				call_expr
 			}
 			_ => fold::fold_expr(self, expr),
+		}
+	}
+}
+
+// AST transform that rewrites all free variables in a scope to be
+// prefixed with "this.".
+struct FreeVariableTransformer {
+	bound_vars: Vec<Symbol>,
+	free_vars: IndexSet<String>,
+}
+
+impl FreeVariableTransformer {
+	pub fn new(globals: Vec<Symbol>) -> Self {
+		Self {
+			bound_vars: globals,
+			free_vars: IndexSet::new(),
+		}
+	}
+
+	pub fn transform_def(&mut self, def: FunctionDefinition) -> (FunctionDefinition, Vec<String>) {
+		let new_def = self.fold_function_definition(def);
+		let free_vars = self.free_vars.iter().cloned().collect();
+		(new_def, free_vars)
+	}
+}
+
+impl Fold for FreeVariableTransformer {
+	fn fold_reference(&mut self, node: Reference) -> Reference {
+		if let Reference::Identifier(ref x) = node {
+			if self.bound_vars.contains(x) || x.name == "this" {
+				return node;
+			} else {
+				self.free_vars.insert(x.name.clone());
+				return Reference::Identifier(Symbol {
+					name: format!("this.{}", x.name),
+					span: x.span.clone(),
+				});
+			}
+		};
+
+		return fold::fold_reference(self, node);
+	}
+
+	// invariant: adds net zero bound variables
+	fn fold_scope(&mut self, scope: Scope) -> Scope {
+		let old_bound_vars = self.bound_vars.clone();
+		let new_scope = fold::fold_scope(self, scope);
+		self.bound_vars = old_bound_vars;
+		new_scope
+	}
+
+	fn fold_stmt(&mut self, stmt: Stmt) -> Stmt {
+		let kind = match stmt.kind {
+			// this statement introduces bound variables!
+			StmtKind::Bring {
+				module_name,
+				identifier,
+			} => {
+				let new_module_name = self.fold_symbol(module_name);
+				// bring cloud;
+				if !(new_module_name.name.starts_with('"') && new_module_name.name.ends_with('"')) {
+					// add `cloud` to the list of bound variables
+					self.bound_vars.push(new_module_name.clone());
+				}
+
+				let new_identifier = identifier.map(|id| self.fold_symbol(id));
+				// bring "foo" as bar;
+				if let Some(ref id) = new_identifier {
+					// add `bar` to the list of bound variables
+					self.bound_vars.push(id.clone());
+				}
+
+				StmtKind::Bring {
+					module_name: new_module_name,
+					identifier: new_identifier,
+				}
+			}
+			// this statement introduces bound variables!
+			StmtKind::VariableDef {
+				reassignable,
+				var_name,
+				initial_value,
+				type_,
+			} => {
+				let new_var_name = self.fold_symbol(var_name);
+				let new_initial_value = self.fold_expr(initial_value);
+				let new_type = type_.map(|type_| self.fold_type_annotation(type_));
+				self.bound_vars.push(new_var_name.clone());
+				StmtKind::VariableDef {
+					reassignable,
+					var_name: new_var_name,
+					initial_value: new_initial_value,
+					type_: new_type,
+				}
+			}
+			// invariant: adds net zero bound variables
+			StmtKind::ForLoop {
+				iterator,
+				iterable,
+				statements,
+			} => {
+				let new_iterator = self.fold_symbol(iterator);
+				let new_iterable = self.fold_expr(iterable);
+				self.bound_vars.push(new_iterator.clone());
+				let new_statements = self.fold_scope(statements);
+				self.bound_vars.pop();
+				StmtKind::ForLoop {
+					iterator: new_iterator,
+					iterable: new_iterable,
+					statements: new_statements,
+				}
+			}
+			// invariant: adds net zero bound variables
+			StmtKind::TryCatch {
+				try_statements,
+				catch_block,
+				finally_statements,
+			} => {
+				let new_try_statements = self.fold_scope(try_statements);
+				let new_catch_block = catch_block.map(|cb| {
+					let new_exception_var = cb.exception_var.map(|var| self.fold_symbol(var));
+					if let Some(ref var) = new_exception_var {
+						self.bound_vars.push(var.clone());
+					}
+					let new_catch_statements = self.fold_scope(cb.statements);
+					if new_exception_var.is_some() {
+						self.bound_vars.pop();
+					}
+					CatchBlock {
+						exception_var: new_exception_var,
+						statements: new_catch_statements,
+					}
+				});
+				let new_finally_statements = finally_statements.map(|stmts| self.fold_scope(stmts));
+				StmtKind::TryCatch {
+					try_statements: new_try_statements,
+					catch_block: new_catch_block,
+					finally_statements: new_finally_statements,
+				}
+			}
+			_ => return fold::fold_stmt(self, stmt),
+		};
+
+		Stmt {
+			kind,
+			idx: stmt.idx,
+			span: stmt.span,
+		}
+	}
+
+	// invariant: adds net zero bound variables
+	fn fold_function_definition(&mut self, node: FunctionDefinition) -> FunctionDefinition {
+		let FunctionDefinition {
+			signature,
+			body,
+			captures,
+			is_static,
+			span,
+		} = node;
+
+		let new_function_signature = self.fold_function_signature(signature);
+
+		// TODO: what to do about "this"?
+
+		for param in &new_function_signature.parameters {
+			self.bound_vars.push(param.name.clone());
+		}
+
+		let new_body = match body {
+			FunctionBody::Statements(statements) => {
+				let new_statements = self.fold_scope(statements);
+				FunctionBody::Statements(new_statements)
+			}
+			FunctionBody::External(s) => FunctionBody::External(s),
+		};
+
+		for _ in &new_function_signature.parameters {
+			self.bound_vars.pop();
+		}
+
+		FunctionDefinition {
+			signature: new_function_signature,
+			body: new_body,
+			captures,
+			is_static,
+			span,
 		}
 	}
 }
