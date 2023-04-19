05bb148719306ae7bd641e4781e3b4f5f93d7330
Jonathan Goldwasser <jonathan.goldwasser@gmail.com>
2023-04-19 18:29:04 +0200

    test get secret value

--- libs/wingsdk/test/target-sim/secret.test.ts
+++ libs/wingsdk/test/target-sim/secret.test.ts
@@ -5,14 +5,16 @@ import { test, expect } from "vitest";
 import * as cloud from "../../src/cloud";
 import { SimApp } from "../sim-app";
 
+const secretsFile = path.join(os.homedir(), ".wing", "secrets.json");
+
 test("create a secret", async () => {
   // GIVEN
   const app = new SimApp();
   cloud.Secret._newSecret(app, "my_secret", {
-    name: "my-secret",
+    name: "wing-sim-test-my-secret",
   });
 
-  fs.ensureFileSync(path.join(os.homedir(), ".wing", "secrets.json"));
+  await fs.ensureFile(secretsFile);
 
   const s = await app.startSimulator();
 
@@ -31,3 +33,43 @@ test("create a secret", async () => {
 
   expect(app.snapshot()).toMatchSnapshot();
 });
+
+test("can get the secret value", async () => {
+  // GIVEN
+  const app = new SimApp();
+  cloud.Secret._newSecret(app, "my_secret", {
+    name: "wing-sim-test-my-secret",
+  });
+
+  await fs.ensureFile(path.join(os.homedir(), ".wing", "secrets.json"));
+  const secretsContent = await fs.readFile(
+    path.join(os.homedir(), ".wing", "secrets.json"),
+    "utf-8"
+  );
+
+  const secrets = tryParse(secretsContent) ?? {};
+  await fs.writeFile(
+    secretsFile,
+    JSON.stringify({
+      ...secrets,
+      "wing-sim-test-my-secret": "secret-value",
+    })
+  );
+
+  const s = await app.startSimulator();
+  const secretClient = s.getResource("/my_secret") as cloud.ISecretClient;
+
+  // THEN
+  const secretValue = await secretClient.value();
+  expect(secretValue).toBe("secret-value");
+  await s.stop();
+});
+
+function tryParse(content: string): string | undefined {
+  try {
+    const parsed = JSON.parse(content);
+    return parsed;
+  } catch (err) {
+    return undefined;
+  }
+}
