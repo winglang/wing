c7e9b6794c376ec40cf1385b056423a0b29b58d3
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-04-19 08:07:38 -0400

    add new test

--- tools/hangar/__snapshots__/test_corpus/resource.w/compile/tf-aws/preflight.js
+++ tools/hangar/__snapshots__/test_corpus/resource.w/compile/tf-aws/preflight.js
@@ -27,7 +27,6 @@ class $Root extends $stdlib.std.Resource {
         `);
       }
     }
-    Foo._annotateInflight("$inflight_init", {"this.c": { ops: [] },"this.stateful": { ops: [] }});
     Foo._annotateInflight("$inflight_init", {"this.c": { ops: ["dec","inc"] },"this.stateful": { ops: [] }});
     Foo._annotateInflight("foo_get", {"this.c": { ops: ["peek"] }});
     Foo._annotateInflight("foo_inc", {"this.c": { ops: ["inc"] }});
