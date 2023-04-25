63c4284ba903b38d208711acb51d8e3bf3624c67
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2023-04-25 11:58:22 +0300

    Update libs/wingsdk/src/target-tf-aws/website.ts
    Co-authored-by: Chris Rybicki <chrisr@monada.co>

--- libs/wingsdk/src/target-tf-aws/website.ts
+++ libs/wingsdk/src/target-tf-aws/website.ts
@@ -25,7 +25,7 @@ export class Website extends cloud.Website {
   constructor(scope: Construct, id: string, props: cloud.WebsiteProps) {
     super(scope, id, props);
 
-    this.bucket = createEncryptedBucket(this, true, "website-bucket");
+    this.bucket = createEncryptedBucket(this, true, "WebsiteBucket");
 
     new S3BucketWebsiteConfiguration(this, "bucket-website-configuration", {
       bucket: this.bucket.bucket,
