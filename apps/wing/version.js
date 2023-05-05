7bc7f3864efc7243f85fd2cda483868e0ce56281
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-05-05 10:10:29 -0400

    fix

--- apps/wing/version.js
+++ apps/wing/version.js
@@ -2,12 +2,15 @@ const fs = require("fs");
 const assert = require("assert");
 const pJson = require("./package.json");
 
-// assign the same version as CLI to the wingsdk
-const newVersion = process.env.PROJEN_BUMP_VERSION ?? "0.0.0";
+const defaultVersion = "0.0.0";
+const newVersion = process.env.PROJEN_BUMP_VERSION ?? defaultVersion;
+
+const newSdkVersion = newVersion === defaultVersion ? `file:winglang-sdk-${newVersion}.tgz` : newVersion;
+
 assert(
   typeof pJson.dependencies["@winglang/sdk"] === "string",
   "wingsdk dependency key not found"
 );
 pJson.version = newVersion;
-pJson.dependencies["@winglang/sdk"] = newVersion;
+pJson.dependencies["@winglang/sdk"] = newSdkVersion;
 fs.writeFileSync("./package.json", JSON.stringify(pJson, undefined, 2));
