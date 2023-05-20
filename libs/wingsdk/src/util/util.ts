7a7f6b3c658647c050144c6e334c4db253396df0
GuyDev1 <30812132+GuyDev1@users.noreply.github.com>
2023-05-20 12:08:42 +0300

    Update libs/wingsdk/src/util/util.ts
    Co-authored-by: Elad Ben-Israel <eladb@monada.co>

--- libs/wingsdk/src/util/util.ts
+++ libs/wingsdk/src/util/util.ts
@@ -29,6 +29,7 @@ export class Util {
    * Suspends execution for a given duration.
    *
    * @param duration The duration that has to pass before the Promise is resolved.
+   * @inflight
    */
   public static async sleep(duration: Duration): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, duration.milliseconds));
