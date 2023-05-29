1fe5971447e217747eff4d14a83cefba84373a5c
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2023-05-29 18:11:21 +0300

    fix

--- tools/hangar/src/error.test.ts
+++ tools/hangar/src/error.test.ts
@@ -16,7 +16,7 @@ errorWingFiles.forEach((wingFile) => {
       cwd: tmpDir,
       wingFile: relativeWingFile,
       args,
-      shouldSucceed: false,
+      shouldSucceed: true,
     });
 
     const stdout = out.stdout;
