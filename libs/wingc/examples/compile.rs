90b7def7d68d8b522aa4844576010db2c843a913
Elad Ben-Israel <elad.benisrael@gmail.com>
2023-05-31 14:37:19 +0300

    revert

--- libs/wingc/examples/compile.rs
+++ libs/wingc/examples/compile.rs
@@ -1,12 +1,18 @@
 // Compiles a source file and prints any errors to stderr.
 // This should only be used for testing wingc directly.
 
-use std::{path::Path, process};
+use std::{env, path::Path, process};
 use wingc::compile;
 
 pub fn main() {
-	let source_path = Path::new("../../examples/tests/valid/print.w");
-	std::env::set_current_dir("/Users/eladb/code/wing/libs/wingc").unwrap();
+	let args: Vec<String> = env::args().collect();
+
+	if args.len() < 2 {
+		panic!("Usage: cargo run --example compile <source_file>");
+	}
+
+	let source_path = &args[1];
+	let source_path = Path::new(source_path);
 
 	let results = compile(
 		source_path,
