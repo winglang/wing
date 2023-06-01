529bba633fbf33ce4220d03685b81ca704fe6f5e
Chris Rybicki <chrisr@monada.co>
2023-06-01 19:50:52 +0000

    feat(compiler): error message when class extends itself

--- libs/wingc/src/type_check.rs
+++ libs/wingc/src/type_check.rs
@@ -3492,8 +3492,13 @@ impl<'a> TypeChecker<'a> {
 				return (None, None);
 			}
 		}
-		// Safety: we return early if parent_udt is None
+		// Safety: we return from the function above so parent_udt cannot be None
 		let parent_udt = parent_udt.unwrap();
+
+		if &parent_udt.root == name && parent_udt.fields.is_empty() {
+			self.spanned_error(parent_udt, format!("Class cannot extend itself"));
+			return (None, None);
+		}
 
 		let parent_type = self.resolve_user_defined_type(parent_udt, env, stmt.idx);
 		let parent_type = match parent_type {
