63a7851e7c43e3be03ae04b91ac43779a393dfcc
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2023-06-06 19:04:34 +0300

    running build and adding many docs...

--- libs/wingsdk/src/util/fetch.ts
+++ libs/wingsdk/src/util/fetch.ts
@@ -1,43 +1,141 @@
 import { Code, InflightClient } from "../core";
 
+/**
+ * RequestCache- 
+ * The cache read-only property that contains the cache mode of the request.
+ * It controls how the request will interact with the browser's HTTP cache.
+
+ */
 export enum RequestCache {
+  /**
+   * The browser looks for a matching request in its HTTP cache.
+   * If there is a match and it is fresh, it will be returned from the cache.
+   * If there is a match but it is stale, the browser will make a conditional request to the remote server.
+   * If the server indicates that the resource has not changed, it will be returned from the cache.
+   * Otherwise the resource will be downloaded from the server and the cache will be updated.
+   * If there is no match, the browser will make a normal request, and will update the cache with the downloaded resource.
+   */
   DEFAULT = "default",
+  /**
+   * The browser fetches the resource from the remote server without first looking in the cache,
+   * and will not update the cache with the downloaded resource.
+   */
   NO_STORE = "no-store",
+  /**
+   * The browser fetches the resource from the remote server without first looking in the cache,
+   * but then will update the cache with the downloaded resource.
+   */
   RELOAD = "reload",
+  /**
+   * The browser looks for a matching request in its HTTP cache.
+   * If there is a match, fresh or stale, the browser will make a conditional request to the remote server.
+   * If the server indicates that the resource has not changed, it will be returned from the cache. Otherwise the resource will be downloaded from the server and the cache will be updated.
+   * If there is no match, the browser will make a normal request, and will update the cache with the downloaded resource.
+   */
   NO_CACHE = "no-cache",
+  /**
+   * The browser looks for a matching request in its HTTP cache.
+   * If there is a match, fresh or stale, it will be returned from the cache.
+   * If there is no match, the browser will make a normal request, and will update the cache with the downloaded resource.
+   */
   FORCE_CACHE = "force-cache",
 }
-
+/**
+ * The redirect read-only property that contains the mode for how redirects are handled.
+ */
 export enum RequestRedirect {
+  /**
+   * Follow all redirects incurred when fetching a resource.
+   */
   FOLLOW = "follow",
+  /**
+   * Return a network error when a request is met with a redirect.
+   */
   ERROR = "error",
 }
-
+/**
+ * The request's method
+ */
 export enum HttpMethods {
+  /**
+   * GET
+   */
   GET = "GET",
+  /**
+   * PUT
+   */
   PUT = "PUT",
+  /**
+   * DELETE
+   */
   DELETE = "DELETE",
+  /**
+   * PATCH
+   */
   PATCH = "PATCH",
+  /**
+   * POST
+   */
   POST = "POST",
 }
-
+/**
+ * An object containing any custom settings that you want to apply to the request.
+ */
 export interface RequestOptions {
+  /**
+   * The request method, e.g., GET, POST. The default is GET.
+   */
   readonly method: HttpMethods;
+  /**
+   * Any headers you want to add to your request.
+   */
   readonly headers: Record<string, string>;
+  /**
+   * Any body that you want to add to your request.
+   * Note that a request using the GET or HEAD method cannot have a body.
+   */
   readonly body?: string;
+  /**
+   * The cache mode you want to use for the request.
+   */
   readonly cache: RequestCache;
+  /**
+   * he redirect mode to use: follow, error. The default is follow.
+   */
   readonly redirect: RequestRedirect;
+  /**
+   * A string specifying "no-referrer", client, or a URL. The default is "about:client".
+   */
   readonly referrer?: string;
 }
-
+/**
+ * Represents the response to a request.
+ */
 export interface Response {
+  /**
+   * The status code of the response. (This will be 200 for a success).
+   */
   readonly status: number;
+  /**
+   * The URL of the response.
+   */
   readonly url: string;
+  /**
+   * A boolean indicating whether the response was successful (status in the range 200 – 299) or not.
+   */
   readonly ok: boolean;
+  /**
+   * The Headers object associated with the response.
+   */
   readonly headers: Record<string, string>;
+  /**
+   * A string represents the body contents.
+   */
   readonly body: string;
 }
-
+/**
+ * default options to attach to any request
+ */
 const defaultOptions: RequestOptions = {
   method: HttpMethods.GET,
   headers: {},
@@ -45,19 +143,28 @@ const defaultOptions: RequestOptions = {
   redirect: RequestRedirect.FOLLOW,
 };
 /**
+ * the Http class is used for calling different HTTP methods and requesting and sending information online,
+ *  as well as testing public accessible resources
  * @inflight
  */
 export class Http {
+  /**
+   * Sends an http request of any method.
+   * @param url
+   * @param options
+   * @inflight
+   * @returns the formatted response of the call
+   */
   static async fetch(url: string, options?: RequestOptions): Promise<Response> {
     const res = await fetch(url, { ...defaultOptions, ...options });
     return this._formatResponse(res);
   }
   /**
-   *
+   * Sends a GET request.
    * @param url
    * @param options
-   * @returns Response
    * @inflight
+   * @returns the formatted response of the call
    */
   static async get(url: string, options?: RequestOptions): Promise<Response> {
     return this.fetch(url, {
@@ -67,11 +174,11 @@ export class Http {
     });
   }
   /**
-   *
+   * Sends a POST request.
    * @param url
    * @param options
-   * @returns Response
    * @inflight
+   * @returns the formatted response of the call
    */
   static async post(url: string, options?: RequestOptions): Promise<Response> {
     return this.fetch(url, {
@@ -81,11 +188,11 @@ export class Http {
     });
   }
   /**
-   *
+   * Sends a PUT request.
    * @param url
    * @param options
-   * @returns Response
    * @inflight
+   * @returns the formatted response of the call
    */
   static async put(url: string, options?: RequestOptions): Promise<Response> {
     return this.fetch(url, {
@@ -95,11 +202,11 @@ export class Http {
     });
   }
   /**
-   *
+   * Sends a PATCH request.
    * @param url
    * @param options
-   * @returns Response
    * @inflight
+   * @returns the formatted response of the call
    */
   static async patch(url: string, options?: RequestOptions): Promise<Response> {
     return this.fetch(url, {
@@ -110,11 +217,11 @@ export class Http {
   }
 
   /**
-   *
+   * Sends a DELETE request.
    * @param url
    * @param options
-   * @returns Response
    * @inflight
+   * @returns the formatted response of the call
    */
   static async delete(
     url: string,
--- tools/hangar/__snapshots__/test_corpus/valid/api_path_vars.w_compile_tf-aws.md
+++ tools/hangar/__snapshots__/test_corpus/valid/api_path_vars.w_compile_tf-aws.md
@@ -25,7 +25,7 @@ module.exports = function({  }) {
 
 ## inflight.$Closure2.js
 ```js
-module.exports = function({ f, api }) {
+module.exports = function({ api, util_Http, std_Json }) {
   class $Closure2 {
     constructor({  }) {
       const $obj = (...args) => this.handle(...args);
@@ -36,30 +36,12 @@ module.exports = function({ f, api }) {
     }
     async handle()  {
       const username = "tsuf";
-      const res = (await f.get(`${api.url}/users/${username}`));
-      {((cond) => {if (!cond) throw new Error(`assertion failed: '((res)["status"] === 200)'`)})(((res)["status"] === 200))};
-      {((cond) => {if (!cond) throw new Error(`assertion failed: '(((res)["body"])["user"] === username)'`)})((((res)["body"])["user"] === username))};
+      const res = (await util_Http.get(`${api.url}/users/${username}`));
+      {((cond) => {if (!cond) throw new Error(`assertion failed: '(res.status === 200)'`)})((res.status === 200))};
+      {((cond) => {if (!cond) throw new Error(`assertion failed: '(((JSON.parse(res.body)))["user"] === username)'`)})((((JSON.parse(res.body)))["user"] === username))};
     }
   }
   return $Closure2;
-}
-
-```
-
-## inflight.Fetch.js
-```js
-module.exports = function({  }) {
-  class Fetch {
-    constructor({  }) {
-    }
-    async $inflight_init()  {
-      const __parent_this = this;
-    }
-    async get(url)  {
-      return (require("<ABSOLUTE_PATH>/api_path_vars.js")["get"])(url)
-    }
-  }
-  return Fetch;
 }
 
 ```
@@ -329,41 +311,10 @@ const std = $stdlib.std;
 const $wing_is_test = process.env.WING_IS_TEST === "true";
 const $AppBase = $stdlib.core.App.for(process.env.WING_TARGET);
 const cloud = require('@winglang/sdk').cloud;
+const util = require('@winglang/sdk').util;
 class $Root extends $stdlib.std.Resource {
   constructor(scope, id) {
     super(scope, id);
-    class Fetch extends $stdlib.std.Resource {
-      constructor(scope, id, ) {
-        super(scope, id);
-        this._addInflightOps("get");
-        const __parent_this = this;
-      }
-      static _toInflightType(context) {
-        const self_client_path = "././inflight.Fetch.js";
-        return $stdlib.core.NodeJsCode.fromInline(`
-          require("${self_client_path}")({
-          })
-        `);
-      }
-      _toInflight() {
-        return $stdlib.core.NodeJsCode.fromInline(`
-          (await (async () => {
-            const FetchClient = ${Fetch._toInflightType(this).text};
-            const client = new FetchClient({
-            });
-            if (client.$inflight_init) { await client.$inflight_init(); }
-            return client;
-          })())
-        `);
-      }
-      _registerBind(host, ops) {
-        if (ops.includes("$inflight_init")) {
-        }
-        if (ops.includes("get")) {
-        }
-        super._registerBind(host, ops);
-      }
-    }
     class $Closure1 extends $stdlib.std.Resource {
       constructor(scope, id, ) {
         super(scope, id);
@@ -404,12 +355,14 @@ class $Root extends $stdlib.std.Resource {
       }
       static _toInflightType(context) {
         const self_client_path = "././inflight.$Closure2.js";
-        const f_client = context._lift(f);
         const api_client = context._lift(api);
+        const util_HttpClient = util.Http._toInflightType(context);
+        const std_JsonClient = std.Json._toInflightType(context);
         return $stdlib.core.NodeJsCode.fromInline(`
           require("${self_client_path}")({
-            f: ${f_client},
             api: ${api_client},
+            util_Http: ${util_HttpClient.text},
+            std_Json: ${std_JsonClient.text},
           })
         `);
       }
@@ -427,11 +380,9 @@ class $Root extends $stdlib.std.Resource {
       _registerBind(host, ops) {
         if (ops.includes("$inflight_init")) {
           $Closure2._registerBindObject(api, host, []);
-          $Closure2._registerBindObject(f, host, []);
         }
         if (ops.includes("handle")) {
           $Closure2._registerBindObject(api.url, host, []);
-          $Closure2._registerBindObject(f, host, ["get"]);
         }
         super._registerBind(host, ops);
       }
@@ -439,7 +390,6 @@ class $Root extends $stdlib.std.Resource {
     const api = this.node.root.newAbstract("@winglang/sdk.cloud.Api",this,"cloud.Api");
     const handler = new $Closure1(this,"$Closure1");
     (api.get("/users/{name}",handler));
-    const f = new Fetch(this,"Fetch");
     this.node.root.new("@winglang/sdk.std.Test",std.Test,this,"test:test",new $Closure2(this,"$Closure2"));
   }
 }
