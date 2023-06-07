fee75de672669c561d7ec03c5a96492273103155
Cristian Pallarés <cristianp@monada.co>
2023-06-07 16:42:05 +0200

    wip

--- libs/wingcompiler/vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    // Some tests use process.chdir which is not supported in vitest's multi-thread mode
-    threads: false,
-  },
-});
