96f2f0aab5e84e72dc980dfce45ad022eeedd8e2
Cristian Pallarés <cristianp@monada.co>
2023-06-07 20:45:32 +0200

    chore: separate wing cli and wing compiler (#2762)
    Extract a new package `@winglang/compiler` from the `winglang` package.
    
    The `@winglang/compiler` package:
    
    - Exports a `compile` function that can be reused by the Console.
    - Adds three error classes to denote the different kind of errors that might occur:
      - `InternalError`
      - `CompileError`
      - `PreflightError`
    - Doesn't know anything about terminals and terminal colors. That knowledge belongs to the `winglang` package
    
    Important things to note:
    
    - Makes `nx` test task depend on `^build` since we can't test `winglang` until `@winglang/compiler` has been compiled

--- libs/wingcompiler/version.js
+++ libs/wingcompiler/version.js
@@ -6,15 +6,13 @@ const newVersion = process.env.PROJEN_BUMP_VERSION ?? defaultVersion;
 
 pJson.version = newVersion;
 
-// for dev builds, assume the user will provide an installation of the sdk & compiler
+// for dev builds, assume the user will provide an installation of the sdk
 if (newVersion === defaultVersion) {
-  // remove the dependency on the sdk & compiler
+  // remove the dependency on the sdk
   // @ts-ignore
   delete pJson.dependencies["@winglang/sdk"];
-  delete pJson.dependencies["@winglang/compiler"];
 } else {
   pJson.dependencies["@winglang/sdk"] = newVersion;
-  pJson.dependencies["@winglang/compiler"] = newVersion;
 }
 
 fs.writeFileSync("./package.json", JSON.stringify(pJson, undefined, 2));
