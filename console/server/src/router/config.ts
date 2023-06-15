2734726ecdf4fd251b8897b4821ed1a6acf84510
polamoros <info@polamoros.com>
2023-06-15 17:16:50 +0200

    feat: add handler to update theme using post messages (#617)

--- console/server/src/router/config.ts
+++ console/server/src/router/config.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 import { createProcedure, createRouter } from "../utils/createRouter.js";
 
 export const createConfigRouter = () => {
@@ -5,5 +7,14 @@ export const createConfigRouter = () => {
     "config.getThemeMode": createProcedure.query(async ({ ctx }) => {
       return { mode: ctx?.config?.get("themeMode") };
     }),
+    "config.setThemeMode": createProcedure
+      .input(
+        z.object({
+          mode: z.string(),
+        }),
+      )
+      .mutation(async ({ input, ctx }) => {
+        ctx?.config?.set("themeMode", input.mode);
+      }),
   });
 };
--- console/ui/src/App.tsx
+++ console/ui/src/App.tsx
@@ -2,7 +2,9 @@ import {
   DefaultTheme,
   ThemeProvider,
   NotificationsProvider,
+  Mode,
 } from "@wingconsole/design-system";
+import { useCallback, useEffect } from "react";
 
 import { LayoutProvider, LayoutType } from "./layout/layout-provider.js";
 import { trpc } from "./services/trpc.js";
@@ -30,6 +32,23 @@ export const App = ({ layout }: AppProps) => {
   const appDetails = trpc["app.details"].useQuery();
   const appState = trpc["app.state"].useQuery();
   const themeMode = trpc["config.getThemeMode"].useQuery();
+
+  // TODO: Needed to be able to change the theme mode from the Learn / playground sites
+  //       This should be removed once we have a better way to handle this
+  const setThemeMode = trpc["config.setThemeMode"].useMutation();
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.data?.type === "theme") {
+        setThemeMode.mutate({
+          mode: event.data.theme,
+        });
+      }
+    };
+    window.addEventListener("message", handleMessage);
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, [setThemeMode]);
 
   return (
     <ThemeProvider mode={themeMode?.data?.mode} theme={DefaultTheme}>
