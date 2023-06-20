1bd04300d11c1d2e6311ff474b2ea0bc890d2c0c
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-06-20 13:41:26 -0400

    undo other changes

--- examples/tests/sdk_tests/external/fs.js
+++ examples/tests/sdk_tests/external/fs.js
@@ -2,9 +2,6 @@ const fs = require("fs");
 const path = require("path");
 
 exports.readFile = function (filePath) {
-  const normalizedDirname = __dirname.replace(/\\/g, "/");
-  const finalPath = path.join(path.resolve(normalizedDirname), "../", filePath);
-  console.log("finalPath", finalPath);
   // reading file from sdk_tests root
-  return fs.readFileSync(finalPath, "utf-8");
+  return fs.readFileSync(path.join(path.resolve(__dirname), "../", filePath), "utf-8");
 };
