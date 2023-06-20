68aecd4b1a0f66815b3f87562b983bc101b2eb84
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-06-20 13:37:00 -0400

    dummy commit

--- examples/tests/sdk_tests/external/fs.js
+++ examples/tests/sdk_tests/external/fs.js
@@ -5,7 +5,6 @@ exports.readFile = function (filePath) {
   const normalizedDirname = __dirname.replace(/\\/g, "/");
   const finalPath = path.join(path.resolve(normalizedDirname), "../", filePath);
   console.log("finalPath", finalPath);
-
   // reading file from sdk_tests root
   return fs.readFileSync(finalPath, "utf-8");
 };
