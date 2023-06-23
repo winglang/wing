3523e62006ecc634700f8892541ffb0401121d93
Chris Rybicki <chrisr@monada.co>
2023-06-22 20:34:43 -0400

    export redis docs

--- apps/jsii-docgen/src/docgen/view/wing-filters.ts
+++ apps/jsii-docgen/src/docgen/view/wing-filters.ts
@@ -1,5 +1,5 @@
 // this file includes some special cases for wing documentation
-export const VISIBLE_SUBMODULES = ["cloud", "fs", "std", "util"];
+export const VISIBLE_SUBMODULES = ["cloud", "fs", "std", "util", "redis"];
 export const HIDDEN_METHODS = ["toString", "toJSON"];
 export const HIDDEN_PROPS = ["node", "display"];
 export const HIDDEN_STATIC = ["of", "isConstruct", "addConnection"];
