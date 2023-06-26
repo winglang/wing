cca474679ae247d6bb877757fc5aec0de961bce9
Cristian Pallarés <cristianp@monada.co>
2023-06-26 14:47:51 +0200

    refactor vite config

--- apps/wing-console/console/app/scripts/dev.mjs
+++ apps/wing-console/console/app/scripts/dev.mjs
@@ -1,20 +1,13 @@
 import { createConsoleServer } from "@wingconsole/server";
 import { fileURLToPath } from "node:url";
 import open from "open";
-import react from "@vitejs/plugin-react";
 
 import { createServer as createViteServer } from "vite";
+import { viteConfig } from "./config.mjs";
 
 const vite = await createViteServer({
+  ...viteConfig,
   server: { middlewareMode: true },
-
-  configFile: false,
-  root: fileURLToPath(new URL("../web", import.meta.url)),
-  plugins: [react()],
-  build: {
-    outDir: fileURLToPath(new URL("../dist/vite", import.meta.url)),
-  },
-  base: "./",
 });
 
 const { port } = await createConsoleServer({
