b3d6a4a803ab6f8bbffa47d5ebbb2910bebc7916
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-06-29 12:42:01 -0400

    update cdktf snaps

--- tools/bump-pack/src/pack.ts
+++ tools/bump-pack/src/pack.ts
@@ -15,16 +15,16 @@ export interface PackOptions {
 }
 
 /**
- * Runs `npm pack` in the given package directory.
+ * Runs `pnpm pack` in the given package directory.
  */
 export async function pack(options: PackOptions) {
   const { packageDir, dryRun } = options;
 
   if (dryRun) {
-    console.log(`Would have run "npm pack" in ${packageDir}`);
+    console.log(`Would have run "pnpm pack" in ${packageDir}`);
   } else {
     preparePackageJsonAndRun(packageDir, () => {
-      execSync("npm pack", { cwd: packageDir, stdio: "inherit" });
+      execSync("pnpm pack", { cwd: packageDir, stdio: "inherit" });
     });
   }
 }
