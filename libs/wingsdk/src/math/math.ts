be8f03fa3a7dc069111f0889a099f15a046dce68
Marcio Cruz de Almeida <marciocadev@gmail.com>
2023-06-28 22:25:47 -0300

    adding median, mode, mean (arithmetic, geometric, harmonic)

--- libs/wingsdk/src/math/math.ts
+++ libs/wingsdk/src/math/math.ts
@@ -20,6 +20,70 @@ export class Util {
   }
 
   /**
+   * Calculates the median value of an array of numbers.
+   * @param arr - The array of numbers.
+   */
+  public static median(arr: number[]): number {
+    const mid = Math.floor(arr.length / 2);
+    const nums = [...arr].sort((a, b) => a - b);
+    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+  }
+
+  /**
+   * Calculates the mode values of an array of numbers.
+   * @param arr - The array of numbers.
+   */
+  public static mode(arr: number[]): number[] {
+    const frequencyMap: Record<number, number> = {};
+    let maxFrequency = 0;
+
+    arr.forEach((num) => {
+      frequencyMap[num] = (frequencyMap[num] || 0) + 1;
+      maxFrequency = Math.max(maxFrequency, frequencyMap[num]);
+    });
+
+    const modeValues: number[] = [];
+
+    for (const num in frequencyMap) {
+      if (frequencyMap[num] === maxFrequency) {
+        modeValues.push(+num);
+      }
+    }
+
+    return modeValues;
+  }
+
+  /**
+   * Calculates the mean value of an array of numbers.
+   * @param arr - The array of numbers.
+   */
+  public static arithmeticMean(arr: number[]): number {
+    const sum = arr.reduce((acc, num) => acc + num, 0);
+    const meanValue = sum / arr.length;
+    return meanValue;
+  }
+
+  /**
+   * Calculates the geometric mean of an array of numbers.
+   * @param arr - The array of numbers.
+   */
+  public static geometricMean(arr: number[]): number {
+    const product = arr.reduce((acc, num) => acc * num, 1);
+    const meanValue = Number(Math.pow(product, 1 / arr.length).toPrecision(6));
+    return meanValue;
+  }
+
+  /**
+   * Calculates the harmonic mean of an array of numbers.
+   * @param arr - The array of numbers.
+   */
+  public static harmonicMean(arr: number[]): number {
+    const reciprocalSum = arr.reduce((acc, num) => acc + 1 / num, 0);
+    const meanValue = arr.length / reciprocalSum;
+    return meanValue;
+  }
+
+  /**
    * Returns the largest integer less than or equal to a given number.
    * @param value The input number.
    */
@@ -67,5 +131,5 @@ export class Util {
     throw new Error("The argument is not a Set");
   }
 
-  private constructor() {}
+  private constructor() { }
 }
