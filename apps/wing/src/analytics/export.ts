9a177dea63bd0b2346bab8e115f36619e597fb4f
Hasan <hasanaburayyan21@gmail.com>
2023-06-30 14:06:29 -0400

    added todo

--- apps/wing/src/analytics/export.ts
+++ apps/wing/src/analytics/export.ts
@@ -2,6 +2,7 @@ import { Analytics } from '@segment/analytics-node';
 import { AnalyticsStorage } from './storage';
 
 
+// TODO: How do we manage this writeKey? Right now its just my personal free-tier writeKey
 let analytics = new Analytics({ writeKey: 'Rfx0id3AfQCZ5hfSDVy97EoMQB1wDLzF'});
 let report = AnalyticsStorage.loadAnalyticsReport();
 
