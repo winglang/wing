76bc73971a72d9a3bb72351e9520064cc18758bc
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-07-02 11:48:06 -0400

    cleanup again

--- tools/bump-pack/src/bundle-link.ts
+++ tools/bump-pack/src/bundle-link.ts
@@ -13,15 +13,24 @@ import {
 import { findWorkspacePackages, Project } from "@pnpm/workspace.find-packages";
 import { findWorkspaceDir } from "@pnpm/find-workspace-dir";
 
+/**
+ * Given a pnpm projen in a workspace, ensure all of its
+ * bundled deps (and their transitive deps) are symlinked in the project's node_modules.
+ *
+ * This is necessary because pnpm intentionally does not make transitive deps of
+ * bundled deps available when publishing packages. When packing the tarball for the project, it
+ * will be able to include all the symlinked transitive deps.
+ * @param workspaceDir pnpm workspace root, must contain a lockfile
+ * @param project pnpm project
+ * @returns
+ */
 export async function linkBundledTransitiveDeps(
   workspaceDir: string,
   project: Project
 ) {
-  const bundledDeps: string[] =
-    project.manifest.bundledDependencies ??
-    project.manifest.bundleDependencies ??
-    [];
-  if (!bundledDeps.length) {
+  const bundledDeps =
+    project.manifest.bundledDependencies ?? project.manifest.bundleDependencies;
+  if (!bundledDeps || bundledDeps.length === 0) {
     // No bundled deps
     return;
   }
@@ -41,7 +50,7 @@ export async function linkBundledTransitiveDeps(
   const transitiveDeps: Record<string, PackageNode> = {};
 
   function visitDependencies(deps?: PackageNode[]) {
-    if (deps == null || !deps.length) {
+    if (!deps || deps.length === 0) {
       return;
     }
 
@@ -67,10 +76,10 @@ export async function linkBundledTransitiveDeps(
   }
 
   for (const bundledDepName of bundledDeps) {
-    const bundledDep = (dependencyHierarchy.dependencies ?? []).find(
+    const bundledDep = dependencyHierarchy.dependencies?.find(
       (v) => v.alias === bundledDepName
     );
-    if (bundledDep == null) {
+    if (!bundledDep) {
       throw new Error(
         `Package ${project.manifest.name} bundled dependency "${bundledDepName}" is missing dependency declaration.`
       );
@@ -81,13 +90,13 @@ export async function linkBundledTransitiveDeps(
 
   // create symlink for each transitive dep in package node_modules
   for (const [name, dep] of Object.entries(transitiveDeps)) {
-    const _dest = path.join(project.dir, "node_modules", name);
-    if (!(await fs.pathExists(_dest))) {
-      if (!(await fs.pathExists(dep.path))) {
+    const destModule = path.join(project.dir, "node_modules", name);
+    if (!await fs.pathExists(destModule)) {
+      if (!await fs.pathExists(dep.path)) {
         throw new Error(`Pnpm dependency path not found: ${dep.path}`);
       }
 
-      await fs.ensureSymlink(dep.path, _dest, "dir");
+      await fs.ensureSymlink(dep.path, destModule, "dir");
     }
   }
 
@@ -98,17 +107,13 @@ export async function linkBundledTransitiveDeps(
   );
 }
 
-// walk up until we find a pnpm-lock.yaml
+// Run for all packages in the workspace
 let workspaceDir = await findWorkspaceDir(process.cwd());
 if (!workspaceDir) {
   throw new Error("No workspace found");
 }
-
-const workspacePackages = await findWorkspacePackages(workspaceDir);
-
-for (const pkg of workspacePackages) {
+for (const pkg of await findWorkspacePackages(workspaceDir)) {
   if (!pkg.manifest.private) {
-    // console.log(`Linking bundled deps for ${pkg.dir}`);
     await linkBundledTransitiveDeps(workspaceDir, pkg);
   }
 }
