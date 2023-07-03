5131952203f48b923fd93f52253e2bfbad0fc00b
polamoros <pola@monada.co>
2023-07-03 10:38:47 +0200

    wip

--- apps/wing-console/console/ui/src/layout/status-bar.tsx
+++ apps/wing-console/console/ui/src/layout/status-bar.tsx
@@ -6,6 +6,29 @@ import { AutoUpdater } from "../features/auto-updater.js";
 import { useCallback } from "react";
 import { ArrowPathIcon, MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 
+const AutoThemeIcon = () => {
+  const { theme } = useTheme();
+  return (
+    <div className={"relative h-4 w-4"}>
+      <ArrowPathIcon className="h-4 absolute -rotate-[5deg]" />
+      <SunIcon
+        className={classNames(
+          "h-[7.5px] absolute z-10 top-0 -left-[1px]",
+          theme.bg3,
+          "rounded-xl",
+        )}
+      />
+      <MoonIcon
+        className={classNames(
+          "h-[7.5px] absolute z-10 bottom-0 -right-[1px]",
+          theme.bg3,
+          "rounded-xl",
+        )}
+      />
+    </div>
+  );
+};
+
 export interface StatusBarProps {
   wingVersion?: string;
   cloudAppState: State;
@@ -56,25 +79,7 @@ export const StatusBar = ({
           >
             {mode === "light" && <SunIcon className="h-4" />}
             {mode === "dark" && <MoonIcon className="h-4" />}
-            {mode === "auto" && (
-              <div className="relative h-4 w-4">
-                <ArrowPathIcon className="h-4 absolute -rotate-[5deg]" />
-                <SunIcon
-                  className={classNames(
-                    "h-[7.5px] absolute z-10 top-0 -left-[1px]",
-                    theme.bg3,
-                    "rounded-xl",
-                  )}
-                />
-                <MoonIcon
-                  className={classNames(
-                    "h-[7.5px] absolute z-10 bottom-0 -right-[1px]",
-                    theme.bg3,
-                    "rounded-xl",
-                  )}
-                />
-              </div>
-            )}
+            {mode === "auto" && <AutoThemeIcon />}
           </button>
         </div>
 
