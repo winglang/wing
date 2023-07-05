8f2c9dbf95cfed2e750e76e0c678a8cdd229e2b1
Cristian Pallarés <cristianp@monada.co>
2023-07-05 11:22:12 +0200

    wip

--- apps/wing-console/console/app/web/index.tsx
+++ apps/wing-console/console/app/web/index.tsx
@@ -2,13 +2,6 @@ import { Console } from "@wingconsole/ui";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
-import "@ibm/plex/IBM-Plex-Sans/fonts/split/woff2/IBMPlexSans-Regular.css";
-import "@ibm/plex/IBM-Plex-Sans/fonts/split/woff2/IBMPlexSans-Medium.css";
-import "@ibm/plex/IBM-Plex-Sans/fonts/split/woff2/IBMPlexSans-Italic.css";
-import "@ibm/plex/IBM-Plex-Mono/fonts/split/woff2/IBMPlexMono-Regular.css";
-import "@ibm/plex/IBM-Plex-Mono/fonts/split/woff2/IBMPlexMono-Medium.css";
-
 const query = new URLSearchParams(location.search);
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
