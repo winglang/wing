bd56a017d174385e09f2ff3d09f7e3efeb5e4cde
polamoros <pola@monada.co>
2023-07-06 09:52:19 +0200

    wip

---
+++ apps/wing-console/console/app/tests/cloud.bucket/index.test.ts
@@ -0,0 +1,18 @@
+import { expect, test } from "@playwright/test";
+import { describe } from "../describe.js";
+import { getResourceNode } from "../helpers.js";
+
+describe(`${__dirname}/index.w`, () => {
+  test("publishes message", async ({ page }) => {
+    await getResourceNode(page, "root/Default/cloud.Topic").click();
+
+    await page.getByTestId("cloud.topic:message").fill("Hello world!");
+
+    await page.getByTestId("cloud.topic:send-message").click();
+
+    await page.waitForLoadState("networkidle");
+
+    const logs = await page.getByTestId("logs").allTextContents();
+    expect(logs.includes("Message received: Hello world!"));
+  });
+});
---
+++ apps/wing-console/console/app/tests/cloud.bucket/index.w
@@ -0,0 +1,3 @@
+bring cloud;
+
+let bucket = new cloud.Bucket();
