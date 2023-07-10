e7a792f3fd7e817a0481c21bf02bdddea57dd5c6
polamoros <pola@monada.co>
2023-07-10 09:43:51 +0200

    wip

--- apps/wing-console/console/ui/src/layout/terms-and-conditions.tsx
+++ apps/wing-console/console/ui/src/layout/terms-and-conditions.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@wingconsole/design-system";
+
 export interface TermsAndConditionsProps {
   visible: boolean;
   onAccept: () => void;
@@ -12,7 +14,7 @@ export const TermsAndConditions = ({
       <div className="fixed inset-0 flex justify-center items-center z-50 bg-black bg-opacity-40">
         <div className="bg-white p-4 rounded-lg shadow-lg space-y-2">
           <h1 className="text-lg font-bold">Terms and Conditions</h1>
-          <div className="text-sm space-y-2">
+          <div className="text-sm space-y-2 py-2">
             <p className="text-sm">
               Please take a moment to review our <b>Terms and Conditions</b> by
               visiting{" "}
@@ -32,12 +34,7 @@ export const TermsAndConditions = ({
             </p>
           </div>
           <div className="flex justify-end">
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-              onClick={onAccept}
-            >
-              Accept
-            </button>
+            <Button onClick={onAccept}>Accept</Button>
           </div>
         </div>
       </div>
