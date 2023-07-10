a7c16e28fa6ca2f771bd0557af336ea42038570b
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-07-10 12:01:11 -0400

    better idea

--- apps/wing-console/console/app/playwright.config.ts
+++ apps/wing-console/console/app/playwright.config.ts
@@ -14,8 +14,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // only open if in interactive shell
-  reporter: [["html", { open: process.stdout.isTTY ? "on-failure" : "never" }]],
+  reporter: process.stdout.isTTY ? "html" : "list",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
