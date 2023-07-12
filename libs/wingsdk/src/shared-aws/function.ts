b81c75f23d2c4203eb487d433e953ebe62189844
Chris Rybicki <chrisr@monada.co>
2023-07-12 15:49:50 -0400

    feat(sdk): add utilities for custom bind methods (#3368)
    This PR introduces the capability to write Wing classes with `_bind` methods that specify custom permissions for resources. This addresses a key limitation that has prevented classes like those found in the standard library (cloud.Bucket, cloud.Counter, etc.) from being written in Wing.
    
    Writing `_bind` methods that add permissions hasn't been possible before because the `_bind` method only provides a `std.IInflightHost` as an argument, while in practice it will be provided a concrete class, like the `sim` target's `cloud.Function` implementation or the `tf-aws` target's `cloud.Function` implementation. To narrow the type, the SDK source code currently uses the JS `instanceof` operator (see [here](https://github.com/winglang/wing/blob/d7769ada65a6f3a917fd54843d4690280d421e11/libs/wingsdk/src/target-tf-aws/bucket.ts#L113-L116)) but this isn't supported in Wing today.
    
    To support this use case, I added a module called `aws` that exposes a bare minimum class `AwsFunction` that can safely cast a `std.IInflightHost` into a `aws.IAwsFunction`, which has methods like `addEnvironment` and `addIamPolicy` for customizing permissions. I've added an example to our test suite that demonstrates how it's used.
    
    ```js
    class DynamoTable {
      // ...
      _bind(host: std.IInflightHost, ops: Array<str>) {
        if let host = aws.AwsFunction.from(host) {
          if ops.contains("putItem") {
            host.addIamPolicy(aws.PolicyStatement {
              actions: ["dynamodb:PutItem"],
              resources: [this.table.arn],
              effect: aws.Effect.ALLOW,
            });
          }
        }
      }
    }
    ```
    
    ## Checklist
    
    - [ ] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
    - [ ] Description explains motivation and solution
    - [ ] Tests added (always)
    - [ ] Docs updated (only required for features)
    - [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Monada Contribution License](https://www.winglang.io/terms-and-policies/contribution-license.html)*.

---
+++ libs/wingsdk/src/shared-aws/function.ts
@@ -0,0 +1,44 @@
+import { PolicyStatement } from "./types";
+import { IInflightHost } from "../std";
+import { Function as AwsCdkFunction } from "../target-awscdk";
+import { Function as TfAwsFunction } from "../target-tf-aws";
+
+/**
+ * A shared interface for AWS functions.
+ */
+export interface IAwsFunction {
+  /**
+   * Add an environment variable to the function.
+   */
+  addEnvironment(key: string, value: string): void;
+
+  /**
+   * Add policy statements to the function's IAM role.
+   *
+   * TODO: update this to accept a variadic parameter (...policies)
+   * https://github.com/winglang/wing/issues/397
+   */
+  addPolicyStatements(policies: PolicyStatement[]): void;
+}
+
+/**
+ * A helper class for working with AWS functions.
+ */
+export class Function {
+  /**
+   * If the inflight host is an AWS function, return a helper interface for
+   * working with it.
+   * @param host The inflight host.
+   */
+  public static from(host: IInflightHost): IAwsFunction | undefined {
+    if (host instanceof TfAwsFunction) {
+      return host;
+    }
+
+    if (host instanceof AwsCdkFunction) {
+      return host;
+    }
+
+    return undefined;
+  }
+}
