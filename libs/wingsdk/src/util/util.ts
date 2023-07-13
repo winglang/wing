046aecd4670f4a7b745d4f695a4f3426fb9b7122
Marcio Cruz de Almeida <marciocadev@gmail.com>
2023-07-13 13:35:39 -0300

    feat: adding nanoid, sha256 and uuidv4 to Utils

--- libs/wingsdk/src/util/util.ts
+++ libs/wingsdk/src/util/util.ts
@@ -25,7 +25,7 @@ export interface WaitUntilProps {
  * `util.busyWait`.
  * @inflight `@winglang/sdk.util.IPredicateHandlerClient`
  */
-export interface IPredicateHandler extends IResource { }
+export interface IPredicateHandler extends IResource {}
 
 /**
  * Inflight client for `IPredicateHandler`.
@@ -148,5 +148,5 @@ export class Util {
   public static _toInflightType(): Code {
     return InflightClient.forType(__filename, this.name);
   }
-  private constructor() { }
+  private constructor() {}
 }
