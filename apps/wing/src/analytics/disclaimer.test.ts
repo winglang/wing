81b5a16cd366147a763aef89540a10d27fb56dfd
Hasan <45375125+hasanaburayyan@users.noreply.github.com>
2023-07-13 14:42:49 -0400

    fix(cli): display analytics disclaimer only in tty (#3439)
    This PR introduces 1 fix and 1 baby feature
    
    1. The fix is to ensure we only display disclaimer when there is a stdin terminal. This stops things like vs-code extension from altering the `displayedDisclaimer` value.
    2. The feature is to add a version to our config file for displayed disclaimer. This way if we need to update the disclaimer we can just tweak the `WING_DISCLAIMER_VERSION` variable and it will force display on the next run for the user.
    
    Closes: https://github.com/winglang/wing/issues/3405
    
    ## Checklist
    
    - [x] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
    - [x] Description explains motivation and solution
    - [x] Tests added (always)
    - [x] Docs updated (only required for features)
    - [x] Added `pr/e2e-full` label if this feature requires end-to-end testing
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Monada Contribution License](https://www.winglang.io/terms-and-policies/contribution-license.html)*.
    
    (cherry picked from commit ac57c0c80bcf74773bcf9f144b29bb3092f9c309)

--- apps/wing/src/analytics/disclaimer.test.ts
+++ apps/wing/src/analytics/disclaimer.test.ts
@@ -2,48 +2,135 @@ import {test, expect, describe, beforeEach, afterEach, vi} from "vitest";
 import { AnalyticsStorage } from "./storage";
 import path from "path";
 import * as os from "os";
-import { WING_DISCLAIMER, optionallyDisplayDisclaimer } from "./disclaimer";
+import { WING_DISCLAIMER, WING_DISCLAIMER_VERSION, optionallyDisplayDisclaimer, shouldDisplayDisclaimer } from "./disclaimer";
 import { existsSync, unlinkSync } from "fs";
 
 
 describe("disclaimer", () => {
+  const originalTTY = process.stdin.isTTY;
   const nonExistentFile = path.join(os.tmpdir(), "_)(*noWayThis_file_exists%$#@.json");
-
-  let log: any;
-
+  
   beforeEach(() => {
-    log = console.log;
-    console.log = vi.fn();
-
     if (existsSync(nonExistentFile)) {
       unlinkSync(nonExistentFile);
     }
   });
 
-  afterEach(() => {
-    console.log = log;
-  });
+  describe("behavior when in TTY", () => {
+    let log: any;
 
-  test("appears on first run", () => {
-    // GIVEN
-    const storage = new AnalyticsStorage({configFile: nonExistentFile});
+    beforeEach(() => {
+      process.stdin.isTTY = true;
+      log = console.log;
+      console.log = vi.fn();
+    });
 
-    // WHEN
-    optionallyDisplayDisclaimer(storage);
+    afterEach(() => {
+      process.stdin.isTTY = originalTTY;
+      console.log = log;
+    });
 
-    // THEN
-    expect(console.log).toHaveBeenCalledWith(expect.stringContaining(WING_DISCLAIMER.split("\n")[0]));
-  });
+    test("appears on first run", () => {
+      // GIVEN
+      const storage = new AnalyticsStorage({configFile: nonExistentFile});
+  
+      // WHEN
+      optionallyDisplayDisclaimer(storage);
+  
+      // THEN
+      expect(console.log).toHaveBeenCalledWith(expect.stringContaining(WING_DISCLAIMER.split("\n")[0]));
+    });
+  
+    test("alters config value for displayed", () => {
+      // GIVEN
+      const storage = new AnalyticsStorage({configFile: nonExistentFile});
+      storage.saveConfig({anonymousId: "fake-id", disclaimerDisplayed: false});
+  
+      // WHEN
+      optionallyDisplayDisclaimer(storage);
+  
+      // THEN
+      expect(storage.loadConfig().disclaimerDisplayed).toBe(true);
+    });
+  
+    test("alters displayed version", () => {
+      // GIVEN 
+      const storage = new AnalyticsStorage({configFile: nonExistentFile});
+      storage.saveConfig({anonymousId: "fake-id", disclaimerDisplayed: false});
+  
+      // WHEN 
+      optionallyDisplayDisclaimer(storage);
+  
+      // THEN
+      expect(storage.loadConfig().disclaimerVersion).toBe(WING_DISCLAIMER_VERSION);
+    })
+  
+    test("does not appear on second run", () => {
+      // GIVEN
+      const storage = new AnalyticsStorage({configFile: nonExistentFile});
+  
+      // WHEN
+      optionallyDisplayDisclaimer(storage);
+      optionallyDisplayDisclaimer(storage);
+  
+      // THEN
+      expect(console.log).toHaveBeenCalledTimes(1);
+    });
 
-  test("does not appear on second run", () => {
-    // GIVEN
-    const storage = new AnalyticsStorage({configFile: nonExistentFile});
+    describe("shouldDisplayDisclaimer", () => {
+      test("returns true if config is empty", () => {
+        expect(shouldDisplayDisclaimer({} as any)).toBe(true);
+      });
+  
+      test("returns true if version is not set", () => {
+        expect(shouldDisplayDisclaimer({
+          anonymousId: "fake-id",
+          disclaimerDisplayed: true
+        })).toBe(true);
+      });
+  
+      test("returns true if version is not current", () => {
+        expect(shouldDisplayDisclaimer({
+          anonymousId: "fake-id",
+          disclaimerDisplayed: true,
+          disclaimerVersion: "-100.0.0"
+        })).toBe(true);
+      });
+  
+      test("return false if displayed version is current", () => {
+        expect(shouldDisplayDisclaimer({
+          anonymousId: "fake-id",
+          disclaimerDisplayed: true,
+          disclaimerVersion: WING_DISCLAIMER_VERSION
+        })).toBe(false);
+      })
+    });
+  })
 
-    // WHEN
-    optionallyDisplayDisclaimer(storage);
-    optionallyDisplayDisclaimer(storage);
+  describe("behavior when not in TTY", () => {
+    
+    beforeEach(() => {
+      process.stdin.isTTY = false;
+    })
 
-    // THEN
-    expect(console.log).toHaveBeenCalledTimes(1);
+    afterEach(() => {
+      process.stdin.isTTY = originalTTY;
+    });
+
+    test("does not alter config value for displayed", () => {
+      // GIVEN
+      const storage = new AnalyticsStorage({configFile: nonExistentFile});
+      storage.saveConfig({anonymousId: "fake-id", disclaimerDisplayed: false});
+
+      // WHEN
+      optionallyDisplayDisclaimer(storage);
+
+      // THEN
+      expect(storage.loadConfig().disclaimerDisplayed).toBe(false);
+    });
+
+    test("returns false if no matter the config", () => {
+      expect(shouldDisplayDisclaimer({} as any )).toBe(false);
+    });
   });
 })
