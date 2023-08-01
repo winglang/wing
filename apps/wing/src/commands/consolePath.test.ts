e89256b70c3e910a44e49bbaae18776f918054ba
polamoros <info@polamoros.com>
2023-08-01 11:27:51 +0200

    wip

--- apps/wing/src/commands/consolePath.test.ts
+++ apps/wing/src/commands/consolePath.test.ts
@@ -1,10 +1,9 @@
 import { vi, test, expect } from "vitest";
 import { consolePath } from "./consolePath";
 
-// mock console.log
-vi.mock("console.log");
+vi.spyOn(console, "log");
 
 test("wing console-path prints the path to the Wing Console", async () => {
   await consolePath();
-  expect(console.log).toBeCalledWith(expect.stringContaining("@wingconsole/app"));
+  expect(console.log).toBeCalledWith(expect.stringContaining("wing-console"));
 });
