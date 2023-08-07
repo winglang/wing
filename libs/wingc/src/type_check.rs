5422bfb174462d512c2d628d70135c2dfb4ffea8
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-08-06 22:52:26 -0400

    fix

--- libs/wingc/src/type_check.rs
+++ libs/wingc/src/type_check.rs
@@ -2419,7 +2419,7 @@ impl<'a> TypeChecker<'a> {
 		if expected_types.len() == 1 {
 			// First check if the actual type is an inference that can be replaced with the expected type
 			if self.add_new_inference(&actual_type, &expected_types[0]) {
-				return_type = expected_types[0];
+				return_type = self.types.maybe_unwrap_inference(return_type);
 			} else {
 				// otherwise, check if the expected type is an inference that can be replaced with the actual type
 				self.add_new_inference(&expected_types[0], &actual_type);
