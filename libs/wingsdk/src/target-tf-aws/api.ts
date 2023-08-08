f0228b77c1e12991c11af41a1b4a2aedf8b31189
Pol Amorós <info@polamoros.com>
2023-08-08 16:51:42 +0200

    wip

--- libs/wingsdk/src/target-tf-aws/api.ts
+++ libs/wingsdk/src/target-tf-aws/api.ts
@@ -317,7 +317,7 @@ export class Api extends cloud.Api {
     const inflightNodeHash = inflight.node.addr.slice(-8);
 
     const functionHandler = convertBetweenHandlers(
-      this.node.scope!, // ok since we're not a tree root
+      this,
       `${this.node.id}-OnRequestHandler-${inflightNodeHash}`,
       inflight,
       join(
@@ -327,7 +327,7 @@ export class Api extends cloud.Api {
       "ApiOnRequestHandlerClient"
     );
     const fn = Function._newFunction(
-      this.node.scope!, // ok since we're not a tree root
+      this,
       `${this.node.id}-OnRequest-${inflightNodeHash}`,
       functionHandler
     );
