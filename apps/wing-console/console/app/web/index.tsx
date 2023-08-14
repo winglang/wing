29cd1cf9a057dce9b7f434418931d04bd6a04603
Elad Cohen <eladc@monada.co>
2023-08-14 16:12:37 +0300

    wip

--- apps/wing-console/console/app/web/index.tsx
+++ apps/wing-console/console/app/web/index.tsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <Console
       trpcUrl="/trpc"
-      wsUrl={`ws://${location.host}/trpc`}
+      wsUrl={`${location.protocol === "http:" ? "ws://" : "wss://"}${location.host}/trpc`}
       layout={Number(query.get("layout")) || 1} // default to 1 = vscode (2 = playground, 3 = tutorial)
       theme={query.get("theme") as any}
       onTrace={(trace) => {
