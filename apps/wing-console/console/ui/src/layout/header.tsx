cf2b0cf8bf9d4b1b0b25393f37e5c1218ed0b5cf
polamoros <info@polamoros.com>
2023-08-14 16:27:06 +0200

    wip

--- apps/wing-console/console/ui/src/layout/header.tsx
+++ apps/wing-console/console/ui/src/layout/header.tsx
@@ -1,4 +1,4 @@
-import { CUSTOMIZABLE_COLOR, useTheme } from "@wingconsole/design-system";
+import { useTheme } from "@wingconsole/design-system";
 import classNames from "classnames";
 
 import { ThemeToggle } from "./theme-toggle.js";
