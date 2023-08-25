winglang/wing

commit 8efecc27b58d3266e3946ea5c8fb8b3ef853914d

feat(sdk)!: std.Node (#3884)
As part of the effort to fix #3717, I'm trying to refactor some parts of the SDK so that we do not need to rely on the `std.Resource` class.  The issue this PR addresses is that the `Resource` class has an extra property named `display` which is not available if you extend `cdk8s.Chart`:

```js
bring cdk8s;

class Bar {
  init() {
    this.display.title = "hi"; // OK
  }
}

class MyChart extends cdk8s.Chart {
  init() {
    this.display.title = "hello"; // error: Unknown symbol "display"
  }
}
```

The solution implemented by this PR is to standardize display property access through `std.Node`:

```js
bring cdk8s;

class Bar {
  init() {
    std.Node.of(this).title = "hi"; // OK
  }
}

// once #3717 is fixed
class MyChart extends cdk8s.Chart {
  init() {
    std.Node.of(this).title = "hello"; // OK
  }
}
```

`std.Node.of(this)` also allows you toaccess all other fields and methods normally available through the `constructs.Node` class.

Other changes:
- I've replaced the `_addInflightOps` method from `std.Resource` with an API contract where a class can (optionally) implement a method named `_getInflightOps()`. This removes one more of the places where the compiler assumes behavior that only exists on `std.Resource` and not on `constructs.Construct`. 
- I've removed the `Code` class from the SDK. I looked through our codebase and 100% of the usages have just been to access its `text` field, so the class isn't really doing anything important. Just using plain `string`'s seems like a nice simplification.

BREAKING CHANGE: `.display` property is no longer available in Wing classes. To change how classes are displayed in the Wing Console, use `std.Node.of(this)` and modify fields like `.title` and `.description`.

## Checklist

- [x] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
- [x] Description explains motivation and solution

libs/wingsdk/src/core/tree.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 import { IConstruct } from "constructs";
 import { App } from "./app";
-import { IResource, Resource } from "../std/resource";
+import { IResource, Node, Resource } from "../std";
 
 export const TREE_FILE_PATH = "tree.json";
 
@@ -153,9 +153,13 @@ function synthDisplay(construct: IConstruct): DisplayInfo | undefined {
   if (!isIResource(construct)) {
     return;
   }
-  const { display } = construct;
+  const display = Node.of(construct);
   if (display.description || display.title || display.hidden) {
-    return display;
+    return {
+      title: display.title,
+      description: display.description,
+      hidden: display.hidden,
+    };
   }
   return;
 }

libs/wingsdk/test/target-sim/__snapshots__/file-counter.test.ts.snap
@@ -18,19 +18,19 @@ return class Handler {
 };
 })())({
 counter: (function(env) {
-        let handle = process.env[env];
-        if (!handle) {
-          throw new Error(\\"Missing environment variable: \\" + env);
-        }
-        return $simulator.findInstance(handle);
-      })(\\"COUNTER_HANDLE_4ecd8d46\\"),
+  let handle = process.env[env];
+  if (!handle) {
+    throw new Error(\\"Missing environment variable: \\" + env);
+  }
+  return $simulator.findInstance(handle);
+})(\\"COUNTER_HANDLE_4ecd8d46\\"),
 bucket: (function(env) {
-        let handle = process.env[env];
-        if (!handle) {
-          throw new Error(\\"Missing environment variable: \\" + env);
-        }
-        return $simulator.findInstance(handle);
-      })(\\"BUCKET_HANDLE_5f2a41c8\\")
+  let handle = process.env[env];
+  if (!handle) {
+    throw new Error(\\"Missing environment variable: \\" + env);
+  }
+  return $simulator.findInstance(handle);
+})(\\"BUCKET_HANDLE_5f2a41c8\\")
 }), args: {} })).handle(event);
 };",
   "connections.json": {
@@ -147,7 +147,7 @@ bucket: (function(env) {
             "Bucket": {
               "constructInfo": {
                 "fqn": "constructs.Construct",
-                "version": "10.1.314",
+                "version": "10.2.69",
               },
               "display": {
                 "description": "A cloud object store",
@@ -159,7 +159,7 @@ bucket: (function(env) {
             "Counter": {
               "constructInfo": {
                 "fqn": "constructs.Construct",
-                "version": "10.1.314",
+                "version": "10.2.69",
               },
               "display": {
                 "description": "A distributed atomic counter",
@@ -171,7 +171,7 @@ bucket: (function(env) {
             "Processor": {
               "constructInfo": {
                 "fqn": "constructs.Construct",
-                "version": "10.1.314",
+                "version": "10.2.69",
               },
               "display": {
                 "description": "An inflight resource",
@@ -186,7 +186,7 @@ bucket: (function(env) {
                 "Queue-QueueEventMapping-401ee792": {
                   "constructInfo": {
                     "fqn": "constructs.Construct",
-                    "version": "10.1.314",
+                    "version": "10.2.69",
                   },
                   "display": {
                     "hidden": true,
@@ -197,11 +197,10 @@ bucket: (function(env) {
                 "Queue-SetConsumer-401ee792": {
                   "constructInfo": {
                     "fqn": "constructs.Construct",
-                    "version": "10.1.314",
+                    "version": "10.2.69",
                   },
                   "display": {
                     "description": "A cloud function (FaaS)",
-                    "sourceModule": "@winglang/sdk",
                     "title": "setConsumer()",
                   },
                   "id": "Queue-SetConsumer-401ee792",
@@ -210,7 +209,7 @@ bucket: (function(env) {
                 "Queue-SetConsumerHandler-401ee792": {
                   "constructInfo": {
                     "fqn": "constructs.Construct",
-                    "version": "10.1.314",
+                    "version": "10.2.69",
                   },
                   "display": {
                     "hidden": true,
@@ -221,7 +220,7 @@ bucket: (function(env) {
               },
               "constructInfo": {
                 "fqn": "constructs.Construct",
-                "version": "10.1.314",
+                "version": "10.2.69",
               },
               "display": {
                 "description": "A distributed message queue",
@@ -233,7 +232,7 @@ bucket: (function(env) {
           },
           "constructInfo": {
             "fqn": "constructs.Construct",
-            "version": "10.1.314",
+            "version": "10.2.69",
           },
           "id": "HelloWorld",
           "path": "root/HelloWorld",
@@ -241,7 +240,7 @@ bucket: (function(env) {
         "cloud.TestRunner": {
           "constructInfo": {
             "fqn": "constructs.Construct",
-            "version": "10.1.314",
+            "version": "10.2.69",
           },
           "display": {
             "description": "A suite of APIs for running tests and collecting results.",
@@ -254,7 +253,7 @@ bucket: (function(env) {
       },
       "constructInfo": {
         "fqn": "constructs.Construct",
-        "version": "10.1.314",
+        "version": "10.2.69",
       },
       "id": "root",
       "path": "root",

libs/wingsdk/test/target-sim/__snapshots__/on-deploy.test.ts.snap
@@ -58,7 +58,7 @@ return class Handler {
         "Handler": {
           "constructInfo": {
             "fqn": "constructs.Construct",
-            "version": "10.1.314",
+            "version": "10.2.69",
           },
           "display": {
             "description": "An inflight resource",
@@ -71,7 +71,7 @@ return class Handler {
         "cloud.TestRunner": {
           "constructInfo": {
             "fqn": "constructs.Construct",
-            "version": "10.1.314",
+            "version": "10.2.69",
           },
           "display": {
             "description": "A suite of APIs for running tests and collecting results.",
@@ -86,11 +86,10 @@ return class Handler {
             "Function": {
               "constructInfo": {
                 "fqn": "constructs.Construct",
-                "version": "10.1.314",
+                "version": "10.2.69",
               },
               "display": {
                 "description": "A cloud function (FaaS)",
-                "sourceModule": "@winglang/sdk",
                 "title": "Function",
               },
               "id": "Function",
@@ -99,7 +98,7 @@ return class Handler {
           },
           "constructInfo": {
             "fqn": "constructs.Construct",
-            "version": "10.1.314",
+            "version": "10.2.69",
           },
           "display": {
             "description": "Run code during the app's deployment.",
@@ -111,7 +110,7 @@ return class Handler {
       },
       "constructInfo": {
         "fqn": "constructs.Construct",
-        "version": "10.1.314",
+        "version": "10.2.69",
       },
       "id": "root",
       "path": "root",

commit 554920dab53aa421d951346be1dac7f54a5ffb31

fix(sdk): missing sourceModule info (#3968)
Fixes a regression from https://github.com/winglang/wing/pull/3884 - tree.json was no longer being emitted with `sourceModule` information, causing some issues in the Wing Console. Thanks @polamoros for catching this 🙏 

## Checklist

- [x] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
- [x] Description explains motivation and solution
- [x] Tests added (always)
- [ ] Docs updated (only required for features)
- [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

libs/wingsdk/src/core/tree.ts
@@ -62,6 +62,12 @@ export interface DisplayInfo {
    * @default false (visible)
    */
   readonly hidden?: boolean;
+
+  /**
+   * The source file or library where the construct was defined.
+   * @default - no source information
+   */
+  readonly sourceModule?: string;
 }
 
 /**
@@ -159,6 +165,7 @@ function synthDisplay(construct: IConstruct): DisplayInfo | undefined {
       title: display.title,
       description: display.description,
       hidden: display.hidden,
+      sourceModule: display.sourceModule,
     };
   }
   return;

libs/wingsdk/test/target-sim/__snapshots__/api.test.ts.snap
@@ -158,6 +158,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-e645076f",
@@ -346,6 +347,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-e645076f",
@@ -534,6 +536,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-e645076f",
@@ -722,6 +725,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-e645076f",
@@ -910,6 +914,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-e645076f",
@@ -1230,6 +1235,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-e645076f",
@@ -1416,6 +1422,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-e645076f",
@@ -1692,6 +1699,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-7c48a9f0",
@@ -1704,6 +1712,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "post()",
               },
               "id": "OnRequestHandler-f6d90a7f",
@@ -1982,6 +1991,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-7c48a9f0",
@@ -1994,6 +2004,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-f6d90a7f",
@@ -2182,6 +2193,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-e645076f",
@@ -2379,6 +2391,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "get()",
               },
               "id": "OnRequestHandler-e645076f",
@@ -2567,6 +2580,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "post()",
               },
               "id": "OnRequestHandler-e645076f",

libs/wingsdk/test/target-sim/__snapshots__/file-counter.test.ts.snap
@@ -201,6 +201,7 @@ bucket: (function(env) {
                   },
                   "display": {
                     "description": "A cloud function (FaaS)",
+                    "sourceModule": "@winglang/sdk",
                     "title": "setConsumer()",
                   },
                   "id": "Queue-SetConsumer-401ee792",

libs/wingsdk/test/target-sim/__snapshots__/on-deploy.test.ts.snap
@@ -90,6 +90,7 @@ return class Handler {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "Function",
               },
               "id": "Function",

libs/wingsdk/test/target-sim/__snapshots__/queue.test.ts.snap
@@ -221,6 +221,7 @@ async handle(message) {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "setConsumer()",
               },
               "id": "my_queue-SetConsumer-e645076f",
@@ -414,6 +415,7 @@ async handle(message) {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "setConsumer()",
               },
               "id": "my_queue-SetConsumer-e645076f",
@@ -608,6 +610,7 @@ async handle(message) {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "setConsumer()",
               },
               "id": "my_queue-SetConsumer-e645076f",
@@ -938,6 +941,7 @@ async handle(message) {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "setConsumer()",
               },
               "id": "my_queue-SetConsumer-e645076f",
@@ -975,6 +979,7 @@ async handle(message) {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "Function",
               },
               "id": "Function",
@@ -1156,6 +1161,7 @@ async handle(message) {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "setConsumer()",
               },
               "id": "my_queue-SetConsumer-e645076f",

libs/wingsdk/test/target-sim/__snapshots__/schedule.test.ts.snap
@@ -187,6 +187,7 @@ console.log(\\"Hello from schedule!\\");
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "onTick()",
               },
               "id": "my_schedule-OnTick-e645076f",
@@ -358,6 +359,7 @@ console.log(\\"Hello from schedule!\\");
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "onTick()",
               },
               "id": "my_schedule-OnTick-e645076f",
@@ -529,6 +531,7 @@ console.log(\\"Hello from schedule!\\");
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "onTick()",
               },
               "id": "my_schedule-OnTick-e645076f",

libs/wingsdk/test/target-sim/__snapshots__/service.test.ts.snap
@@ -121,6 +121,7 @@ async handle(message) {
               },
               "display": {
                 "description": "A cloud function (FaaS)",
+                "sourceModule": "@winglang/sdk",
                 "title": "onStart()",
               },
               "id": "my_service-ServiceOnStartef2b13b9",