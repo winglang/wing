078903e6e061d23bfa82200f15d2e2f90a650b9f
polamoros <info@polamoros.com>
2023-09-04 08:25:47 +0200

    wip

---
+++ apps/wing-console/console/ui/src/shared/use-file-link.tsx
@@ -0,0 +1,45 @@
+import { PropsWithChildren } from "react";
+
+import { trpc } from "../services/trpc.js";
+
+export const createHtmlLink = (
+  error: string,
+  className: string,
+  expanded: boolean = false,
+) => {
+  return error
+    .replaceAll(
+      /\B((?:[a-z]:)?[/\\]\S+):(\d+):(\d+)/gi,
+      (match, path, line, column) => {
+        const link = `vscode://file/${path}:${line}:${column}`;
+        return `<a class="${className}" href="${link}">${match}</a>`;
+      },
+    )
+    .replaceAll(/(\r\n|\n|\r)/gm, expanded ? "<br />" : "\n");
+};
+
+export const OpenFileInEditorButton = ({ children }: PropsWithChildren) => {
+  const openFileInEditor = trpc["app.openFileInEditor"].useMutation();
+
+  return (
+    <button
+      className="appearance-none text-left"
+      onClick={(event) => {
+        const target = event.target as HTMLElement;
+        if (target.tagName === "A") {
+          const [path, row, column] = target
+            .getAttribute("href")!
+            .replace("vscode://file/", "")
+            .split(":");
+          openFileInEditor.mutate({
+            path,
+            row: row && Number.parseInt(row),
+            column: column && Number.parseInt(column),
+          });
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+};
