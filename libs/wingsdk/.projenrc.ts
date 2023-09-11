d06145c0e975429efb32a11a30a4785ecb5c1120
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-09-11 11:53:29 -0400

    wip

--- libs/wingsdk/.projenrc.ts
+++ libs/wingsdk/.projenrc.ts
@@ -27,10 +27,10 @@ const JSII_DEPS = ["constructs@~10.2.69"];
 const CDKTF_VERSION = "0.17.0";
 
 const CDKTF_PROVIDERS = [
-  "aws@~>4.65.0",
   "random@~>3.5.1",
-  "azurerm@~>3.54.0",
   "google@~>4.63.1",
+  "aws@~>4.65.0",
+  "azurerm@~>3.54.0",
 ];
 
 const PUBLIC_MODULES = ["std", "http", "util", "aws", "ex"];
