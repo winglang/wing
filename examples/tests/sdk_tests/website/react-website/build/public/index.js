165b2d9e58e2daf0e58c90082c000a0ecdf3db6b
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2023-09-19 12:28:33 +0300

    try

--- examples/tests/sdk_tests/website/react-website/build/public/index.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("par1").innerText = `apiUrl is: ${window.wingEnv?.apiUrl}`;
-document.getElementById("par2").innerText = `anotherEnvVar is: ${window.wingEnv?.anotherEnvVar}`;
