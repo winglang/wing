bdb1967fabd9ee7ff68fe221b4be49717ac549ab
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-10-04 11:14:12 -0400

    cleanup

--- examples/tests/sdk_tests/website/react-website/build.js
+++ examples/tests/sdk_tests/website/react-website/build.js
@@ -5,8 +5,6 @@ const publicDir = path.join(__dirname, "public");
 const buildDir = path.join(__dirname, "build");
 const buildPublicDir =path.join(buildDir, "public");
 
-if (!fs.existsSync(publicDir)) {
-  fs.mkdirSync(publicDir, { recursive: true });
-}
-
+fs.rmSync(buildDir, { recursive: true, force: true });
+fs.mkdirSync(buildDir, { recursive: true });
 fs.cpSync(publicDir, buildPublicDir, { recursive: true });
