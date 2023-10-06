2ae397275b7848b16161a7fc0bc253c1b4a168ad
Wiktor Zając <wiktor.zajac888@gmail.com>
2023-10-06 22:23:55 +0200

    feat(sdk): gcp implementation of ex.table - preflight (#4393)
    Partially solves #1316 
    
    Video showing deployment to GCP: https://streamable.com/dty9yd
    
    It basically works, but I'd appreciate some guidance on how to make it production-ready.
    
    ## Checklist
    
    - [x] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
    - [x] Description explains motivation and solution
    - [x] Tests added (always)
    - [x] Docs updated (only required for features)
    - [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

--- libs/wingsdk/src/target-tf-gcp/app.ts
+++ libs/wingsdk/src/target-tf-gcp/app.ts
@@ -1,10 +1,12 @@
 import { Construct } from "constructs";
 import { Bucket } from "./bucket";
 import { Function } from "./function";
+import { Table } from "./table";
 import { GoogleProvider } from "../.gen/providers/google/provider";
 import { RandomProvider } from "../.gen/providers/random/provider";
 import { BUCKET_FQN, FUNCTION_FQN } from "../cloud";
 import { AppProps as CdktfAppProps } from "../core";
+import { TABLE_FQN } from "../ex";
 import { CdktfApp } from "../shared-tf/app";
 
 /**
@@ -21,6 +23,12 @@ export interface AppProps extends CdktfAppProps {
    * @see https://cloud.google.com/functions/docs/locations
    */
   readonly region: string;
+
+  /**
+   * The Google Cloud zone, used for all resources.
+   * @see https://cloud.google.com/functions/docs/locations
+   */
+  readonly zone: string;
 }
 
 /**
@@ -38,14 +46,17 @@ export class App extends CdktfApp {
    */
   public readonly region: string;
 
+  /**
+   * The Google Cloud zone.
+   */
+  public readonly zone: string;
+
   public readonly _target = "tf-gcp";
 
   constructor(props: AppProps) {
     super(props);
 
     this.projectId = props.projectId ?? process.env.GOOGLE_PROJECT_ID;
-    // Using env variable for location is work around until we are
-    // able to implement https://github.com/winglang/wing/issues/493 (policy as infrastructure)
     if (this.projectId === undefined) {
       throw new Error(
         "A Google Cloud project ID must be specified through the GOOGLE_PROJECT_ID environment variable."
@@ -58,6 +69,8 @@ export class App extends CdktfApp {
         "A Google Cloud region must be specified through the GOOGLE_REGION environment variable."
       );
     }
+
+    this.zone = props.zone ?? `${this.region}-a`;
 
     new GoogleProvider(this, "google", {
       project: this.projectId,
@@ -86,6 +99,8 @@ export class App extends CdktfApp {
         return new Bucket(scope, id, args[0]);
       case FUNCTION_FQN:
         return new Function(scope, id, args[0], args[1]);
+      case TABLE_FQN:
+        return new Table(scope, id, args[0]);
     }
 
     return undefined;
