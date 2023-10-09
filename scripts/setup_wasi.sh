ee4c81bd5811688a47b5fcc45b648e8dfa34c4a1
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-10-08 22:18:01 -0400

    clean up script

--- scripts/setup_wasi.sh
+++ scripts/setup_wasi.sh
@@ -3,12 +3,9 @@ set -eo pipefail
 
 TOOL_INSTALL_DIR="./.cargo"
 
+BINARYEN_VERSION="version_116"
 WASI_SDK_VERSION="20"
 WASI_SDK_VERSION_FULL="$WASI_SDK_VERSION.0"
-WASI_SDK_INSTALL_DIR="$TOOL_INSTALL_DIR/wasi-sdk-$WASI_SDK_VERSION_FULL"
-
-BINARYEN_VERSION="version_116"
-BINARYEN_INSTALL_DIR="$TOOL_INSTALL_DIR/binaryen-$BINARYEN_VERSION"
 
 SYS_OS=$OSTYPE
 SYS_ARCH=$(uname -m)
@@ -30,34 +27,37 @@ if [[ "$SYS_ARCH" != "x86_64" && "$SYS_ARCH" != "arm64" ]]; then
 fi
 
 # Download binaryen tools
+BINARYEN_INSTALL_DIR="$TOOL_INSTALL_DIR/binaryen-$BINARYEN_VERSION"
 if [ ! -d $BINARYEN_INSTALL_DIR ]; then
-    BINARYEN_TARBALL="binaryen-$BINARYEN_VERSION-$SYS_ARCH-$SYS_OS.tar.gz"
-    BINARYEN_INSTALL_URL="https://github.com/WebAssembly/binaryen/releases/download/$BINARYEN_VERSION/$BINARYEN_TARBALL"
-    OUTFILE="/tmp/$BINARYEN_TARBALL"
+    TARBALL="binaryen-$BINARYEN_VERSION-$SYS_ARCH-$SYS_OS.tar.gz"
+    INSTALL_URL="https://github.com/WebAssembly/binaryen/releases/download/$BINARYEN_VERSION/$TARBALL"
+    OUTFILE="/tmp/$TARBALL"
 
     echo "Installing Binaryen $BINARYEN_VERSION to $BINARYEN_INSTALL_DIR..."
 
     if [ ! -f $OUTFILE ]; then
         echo "Downloading Binaryen $BINARYEN_VERSION to $OUTFILE..."
-        curl --retry 2 -L $BINARYEN_INSTALL_URL -o $OUTFILE
+        curl --retry 2 -L $INSTALL_URL -o $OUTFILE
     fi
 
     echo "Extracting to $BINARYEN_INSTALL_DIR..."
     tar zxf $OUTFILE -C $TOOL_INSTALL_DIR
 fi
 
+# Download wasi-sdk
+WASI_SDK_INSTALL_DIR="$TOOL_INSTALL_DIR/wasi-sdk-$WASI_SDK_VERSION_FULL"
 if [ ! -d $WASI_SDK_INSTALL_DIR ]; then
-    WASI_TARBALL="wasi-sdk-$WASI_SDK_VERSION_FULL-$SYS_OS.tar.gz"
-    WASI_INSTALL_URL="https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-$WASI_SDK_VERSION/$WASI_TARBALL"
-    OUTFILE="/tmp/$WASI_TARBALL"
+    WASI_SDK_TARBALL="wasi-sdk-$WASI_SDK_VERSION_FULL-$SYS_OS.tar.gz"
+    WASI_SDK_INSTALL_URL="https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-$WASI_SDK_VERSION/$WASI_SDK_TARBALL"
+    OUTFILE="/tmp/$WASI_SDK_TARBALL"
 
     echo "Installing WASI SDK $WASI_SDK_VERSION_FULL to $WASI_SDK_INSTALL_DIR..."
 
     if [ ! -f $OUTFILE ]; then
         echo "Downloading WASI SDK $WASI_SDK_VERSION_FULL to $OUTFILE..."
-        curl --retry 2 -L $WASI_INSTALL_URL -o $OUTFILE
+        curl --retry 2 -L $WASI_SDK_INSTALL_URL -o $OUTFILE
     fi
-    
-    echo "Extracting to $TOOL_INSTALL_DIR..."
+
+    echo "Extracting to $WASI_SDK_INSTALL_DIR..."
     tar zxf $OUTFILE -C $TOOL_INSTALL_DIR
 fi
