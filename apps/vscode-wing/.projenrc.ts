7027d51c502bbe8368b704ecfd888fb0085db0fd
polamoros <info@polamoros.com>
2023-10-09 15:09:11 +0200

    wip

--- apps/vscode-wing/.projenrc.ts
+++ apps/vscode-wing/.projenrc.ts
@@ -218,7 +218,7 @@ project.addFields({
   preview: true,
   private: true,
   displayName: "Wing",
-  icon: "resources/winglang-symbol-dark.png",
+  icon: "resources/winglang-symbol-dark.svg",
   engines: {
     vscode: `^${VSCODE_BASE_VERSION}`,
   },
