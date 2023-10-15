d996874e3b26b252d17830538fa4c4f526d0e249
Yoav Steinberg <yoavs@monada.co>
2023-10-15 12:10:13 +0300

    remove generated files

--- examples/jsii-fixture/lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class JsiiClass {
-    /** @internal */
-    _field: number;
-    constructor(value: number);
-    field(): number;
-    publicMethod(arg: string): string;
-    protected protectedMethod(arg: string): string;
-}
