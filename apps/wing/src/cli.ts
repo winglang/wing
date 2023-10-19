4487b119adb6276040e4b22a71a1c51ab9f0e255
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2023-10-19 15:39:56 +0200

    feat(cli): add `--test-filter <regex>` to `wing test` (#4207)
    Closes #3086
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

--- apps/wing/src/cli.ts
+++ apps/wing/src/cli.ts
@@ -164,6 +164,10 @@ async function main() {
     )
     .option("-p, --plugins [plugin...]", "Compiler plugins")
     .option("-r, --rootId <rootId>", "App root id")
+    .option(
+      "-f, --test-filter <regex>",
+      "Run tests that match the provided regex pattern within the selected entrypoint files"
+    )
     .option("--no-clean", "Keep build output")
     .hook("preAction", progressHook)
     .hook("preAction", collectAnalyticsHook)
