79d4d71a46a9ebfcfecd76cd56108a24f6189c68
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2023-10-24 15:23:23 +0200

    fix vitest config

--- libs/wingcompiler/vitest.config.ts
+++ libs/wingcompiler/vitest.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    globalSetup: "test/global.setup.ts",
     testTimeout: 200_000,
-    include: ["test/**/*.test.ts"],
+    include: ["src/**/*.test.ts"],
     // https://vitest.dev/guide/features.html#chai-and-jest-expect-compatibility
     // Allows to use the matchers added by "aws-sdk-client-mock-jest"
     globals: true,
