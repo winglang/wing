9b7942a466fbe31871d2253afce6da08d7afd414
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2023-10-25 14:00:49 +0300

    fix

--- libs/wingsdk/test/target-tf-azure/bucket.test.ts
+++ libs/wingsdk/test/target-tf-azure/bucket.test.ts
@@ -163,6 +163,6 @@ test("bucket onEvent is not implemented yet", () => {
 
   // THEN
   expect(error).toBe(
-    "onEvent method isn't implemented yet on the current target."
+    "onEvent method isn't implemented yet on the current target.\nFor more information see: https://github.com/winglang/wing/issues/1954.\nContributions welcome ❤️"
   );
 });
