da44de06358a1466b921c0e80ccd16506c9015dc
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2023-10-27 22:01:14 +0200

    add `invokeAsync()` to supported ops

--- libs/wingsdk/src/target-awscdk/function.ts
+++ libs/wingsdk/src/target-awscdk/function.ts
@@ -61,7 +61,10 @@ export class Function extends cloud.Function implements IAwsFunction {
 
   /** @internal */
   public _supportedOps(): string[] {
-    return [cloud.FunctionInflightMethods.INVOKE];
+    return [
+      cloud.FunctionInflightMethods.INVOKE,
+      cloud.FunctionInflightMethods.INVOKE_ASYNC,
+    ];
   }
 
   public onLift(host: IInflightHost, ops: string[]): void {
