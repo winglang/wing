6361babc31a15aef2330b4a853009db19e00a476
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2023-10-31 16:30:46 +0100

    replace azure with aws for supported ops

--- libs/wingsdk/src/target-tf-azure/bucket.ts
+++ libs/wingsdk/src/target-tf-azure/bucket.ts
@@ -112,7 +112,6 @@ export class Bucket extends cloud.Bucket {
       cloud.BucketInflightMethods.TRY_GET,
       cloud.BucketInflightMethods.TRY_GET_JSON,
       cloud.BucketInflightMethods.TRY_DELETE,
-      cloud.BucketInflightMethods.COPY,
     ];
   }
 
