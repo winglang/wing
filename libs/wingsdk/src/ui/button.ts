eacf704338661b981fcf4fdb5ee44d898f038144
Chris Rybicki <chrisr@monada.co>
2023-11-14 17:21:50 -0500

    feat(sdk): bring ui (#4605)
    Introducing the `ui` module to the SDK, a collection of APIs for adding visual interactions for your classes in the Wing Console! Here's an example of the API in action:
    
    ```js
    bring cloud;
    bring ui;
    
    class WidgetService {
      data: cloud.Bucket;
      counter: cloud.Counter;
      init() {
        this.data = new cloud.Bucket();
        this.counter = new cloud.Counter();
        
        // a button lets you invoke any inflight function
        new ui.Button("Add widget", inflight () => { this.addWidget(); });
        
        // a field displays a labeled value, with optional refreshing
        new ui.Field(
          "Total widgets",
          inflight () => { return this.countWidgets(); },
          refreshRate: 5s,
        );
      }
    
      inflight addWidget() {
        let id = this.counter.inc();
        this.data.put("widget-${id}", "my data");
      }
    
      inflight countWidgets(): str {
        return "${this.data.list().length}";
      }
    }
    
    new WidgetService();
    ```
    
    UI components aren't displayed in the Wing Console quite yet, but they can be accessed and called through the simulator API like so:
    
    ```js
    const sdk = require("@winglang/sdk");
    
    async function main() {
        const simulator = new sdk.simulator.Simulator({ simfile: ... });
        await simulator.start();
    
        const uiComponents = simulator.getResourceUIComponents("root/Default/WidgetService");
        console.log("uiComponents:", JSON.stringify(uiComponents, null, 2));
        const buttonHandlerPath = uiComponents[0].handlerPath;
        const fieldHandlerPath = uiComponents[1].handlerPath;
    
        const buttonClient = simulator.getResource(buttonHandlerPath);
        await buttonClient.invoke();
        await buttonClient.invoke();
    
        const fieldClient = simulator.getResource(fieldHandlerPath);
        const result = await fieldClient.invoke();
        console.log("total widgets:", result);
    
        await simulator.stop();
    }
    
    main();
    ```
    
    <details>
    <summary>Console output</summary>
    
    ```
    uiComponents: [
      {
        "kind": "button",
        "label": "Add widget",
        "handlerPath": "root/Default/WidgetService/ui.Button/Handler"
      },
      {
        "kind": "field",
        "label": "Total widgets",
        "handlerPath": "root/Default/WidgetService/ui.Field/Handler",
        "refreshRate": 5
      }
    ]
    total widgets: 2
    ```
    
    </details>
    
    In addition to `ui.Field` and `ui.Button` mentioned above, you can also use `ui.Section` to group components together:
    
    ```js
    let section = new ui.Section();
    section.add(button1, button2);
    ```
    
    TODO:
    - [x] tests
    - [x] docs
    
    Future work:
    - Allow inflight methods to be passed directly, i.e. `new ui.Button("label", this.myInflightMethod)` (requires compiler support)
    
    ## Checklist
    
    - [ ] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
    - [ ] Description explains motivation and solution
    - [ ] Tests added (always)
    - [ ] Docs updated (only required for features)
    - [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

---
+++ libs/wingsdk/src/ui/button.ts
@@ -0,0 +1,81 @@
+import { Construct } from "constructs";
+import { VisualComponent } from "./base";
+import { Function } from "../cloud";
+import { fqnForType } from "../constants";
+import { App, UIComponent } from "../core";
+import { IResource } from "../std";
+
+/**
+ * Global identifier for `Button`.
+ */
+export const BUTTON_FQN = fqnForType("ui.Button");
+
+/**
+ * A button can be used to perform an action.
+ */
+export class Button extends VisualComponent {
+  /**
+   * Creates a new ui.Button instance through the app.
+   * @internal
+   */
+  public static _newButton(
+    scope: Construct,
+    id: string,
+    label: string,
+    handler: IButtonHandler
+  ): Button {
+    return App.of(scope).newAbstract(BUTTON_FQN, scope, id, label, handler);
+  }
+
+  private readonly fn: Function;
+  private readonly label: string;
+
+  constructor(
+    scope: Construct,
+    id: string,
+    label: string,
+    handler: IButtonHandler
+  ) {
+    super(scope, id);
+    this.label = label;
+    this.fn = new Function(this, "Handler", handler);
+  }
+
+  /** @internal */
+  public _toUIComponent(): UIComponent {
+    return {
+      kind: "button",
+      label: this.label,
+      handler: this.fn.node.path,
+    };
+  }
+
+  /** @internal */
+  public _supportedOps(): string[] {
+    return [];
+  }
+
+  /** @internal */
+  public _toInflight(): string {
+    throw new Error("Method not implemented.");
+  }
+}
+
+/**
+ * A resource with an inflight "handle" method that can be passed to
+ * `Button`.
+ *
+ * @inflight `@winglang/sdk.ui.IButtonHandlerClient`
+ */
+export interface IButtonHandler extends IResource {}
+
+/**
+ * Inflight client for `IButtonHandler`.
+ */
+export interface IButtonHandlerClient {
+  /**
+   * Function that peforms an action.
+   * @inflight
+   */
+  handle(): Promise<void>;
+}
---
+++ libs/wingsdk/test/ui/ui.test.ts
@@ -0,0 +1,78 @@
+import { Construct } from "constructs";
+import { expect, test } from "vitest";
+import * as cloud from "../../src/cloud";
+import { Testing } from "../../src/simulator";
+import { Resource } from "../../src/std";
+import * as ui from "../../src/ui";
+import { SimApp } from "../sim-app";
+import { listMessages } from "../target-sim/util";
+
+test("can obtain ui components", async () => {
+  // GIVEN
+  const app = new SimApp();
+
+  class MyClass extends Resource {
+    constructor(scope: Construct, id: string) {
+      super(scope, id);
+
+      const section = new ui.Section(this, "Section", { label: "Overview" });
+      const buttonHandler = Testing.makeHandler(
+        this,
+        "button_handler",
+        `async handle() { return "button worked"; }`
+      );
+      const button = new ui.Button(
+        this,
+        "Button",
+        "Button Label",
+        buttonHandler
+      );
+      const fieldHandler = Testing.makeHandler(
+        this,
+        "field_handler",
+        `async handle() { return "field worked"; }`
+      );
+      const field = new ui.Field(this, "Field", "Field Label", fieldHandler);
+      section.add(button, field);
+    }
+  }
+  new MyClass(app, "MyClass");
+
+  // WHEN
+  const s = await app.startSimulator();
+  const buttonClient = s.getResource(
+    "/MyClass/Button/Handler"
+  ) as cloud.IFunctionClient;
+  const fieldClient = s.getResource(
+    "/MyClass/Field/Handler"
+  ) as cloud.IFunctionClient;
+
+  // WHEN
+  const response1 = await buttonClient.invoke("");
+  expect(response1).toEqual("button worked");
+  const response2 = await fieldClient.invoke("");
+  expect(response2).toEqual("field worked");
+
+  expect(s.getResourceUI("/MyClass")).toEqual([
+    {
+      kind: "section",
+      label: "Overview",
+      children: [
+        {
+          kind: "button",
+          label: "Button Label",
+          handler: "root/MyClass/Button/Handler",
+        },
+        {
+          kind: "field",
+          label: "Field Label",
+          handler: "root/MyClass/Field/Handler",
+        },
+      ],
+    },
+  ]);
+
+  // THEN
+  await s.stop();
+  expect(listMessages(s)).toMatchSnapshot();
+});
