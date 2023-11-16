0b4aa75f5300991e2c80d7a9c79159e539a0ed72
Cristian Pallarés <cristianp@wing.cloud>
2023-11-16 10:56:55 +0100

    ignore git directory

--- apps/wing-console/console/server/src/utils/compiler.ts
+++ apps/wing-console/console/server/src/utils/compiler.ts
@@ -65,7 +65,11 @@ export const createCompiler = (wingfile: string): Compiler => {
 
   const dirname = path.dirname(wingfile);
   //TODO: infer React App resource folders from source files https://github.com/winglang/wing/issues/3956
-  const ignoreList = [`${dirname}/target/**`, "**/node_modules/**"];
+  const ignoreList = [
+    `${dirname}/target/**`,
+    "**/node_modules/**",
+    "**/.git/**",
+  ];
   const watcher = chokidar.watch(dirname, {
     ignored: ignoreList,
     ignoreInitial: true,
