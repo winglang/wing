794b21ff77d7722174298656532e420b692dc60b
Chris Rybicki <chrisr@monada.co>
2023-12-04 17:01:33 -0500

    fix(sdk): awaiting client object does not no-op

--- libs/wingsdk/src/simulator/client.ts
+++ libs/wingsdk/src/simulator/client.ts
@@ -48,7 +48,14 @@ export function deserializeValue(input: string): any {
 }
 
 export function makeSimulatorClient(url: string, handle: string) {
+  let proxy: any;
+  let hasThenMethod = true; // assume that the object has a "then" method until proven otherwise
+
   const get = (_target: any, method: string, _receiver: any) => {
+    if (method === "then" && !hasThenMethod) {
+      return undefined;
+    }
+
     return async function (...args: any[]) {
       const body: SimulatorServerRequest = { handle, method, args };
       let resp;
@@ -75,15 +82,35 @@ export function makeSimulatorClient(url: string, handle: string) {
       let parsed: SimulatorServerResponse = deserializeValue(await resp.text());
 
       if (parsed.error) {
+        // objects with "then" methods are special-cased by the JS runtime
+        // because they are assumed to be promises, and can be awaited.
+        // however, we don't know ahead of time what methods will be on the
+        // object, so we have to assume that it has a "then" method until
+        // we get an error back from the server saying that it doesn't.
+        if (
+          method === "then" &&
+          parsed.error &&
+          parsed.error.message &&
+          parsed.error.message.startsWith("Method then not found on resource")
+        ) {
+          hasThenMethod = false;
+          // args[0] is the onFulfilled callback passed to the then method.
+          // we call it with the proxy object so that that `await client`
+          // returns the proxy object back, as callers might expect.
+          return args[0](proxy);
+        }
+
         let err = new Error();
         err.message = parsed.error?.message;
         err.stack = parsed.error?.stack;
         err.name = parsed.error?.name;
         throw err;
       }
+
       return parsed.result;
     };
   };
 
-  return new Proxy({}, { get });
+  proxy = new Proxy({}, { get });
+  return proxy;
 }
