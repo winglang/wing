a26ebb220aa2813c54c9208532d74b4f39c82801
Tsuf Cohen <39455181+tsuf239@users.noreply.github.com>
2023-12-06 20:01:08 +0200

    feat(sdk): enabling tf-gcp test command (#4905)
    ## Description
    closes: https://github.com/winglang/wing/issues/4403
    - Adding inflight function, and enabling test command to tf-GCP
    - Refactoring tf-gcp permissions (now each function has its service account, and the permissions granted to it)
    - chore: minor code formatting in the HTTP module
    
    <img width="872" alt="image" src="https://github.com/winglang/wing/assets/39455181/19027ce0-572a-44e9-8260-9dd662bcb592">
    ## Checklist
    
    - [ ] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
    - [ ] Description explains motivation and solution
    - [ ] Tests added (always)
    - [ ] Docs updated (only required for features)
    - [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

---
+++ libs/wingsdk/src/target-tf-gcp/test-runner.ts
@@ -0,0 +1,99 @@
+import { TerraformOutput } from "cdktf/lib/terraform-output";
+import { Lazy } from "cdktf/lib/tokens";
+import { Construct } from "constructs";
+import { App as GCPApp } from "./app";
+import { Function as GcpFunction } from "./function";
+import * as core from "../core";
+import * as std from "../std";
+
+const OUTPUT_TEST_RUNNER_FUNCTION_IDENTIFIERS =
+  "WING_TEST_RUNNER_FUNCTION_IDENTIFIERS";
+
+/**
+ * Tf-Azure implementation of `cloud.TestRunner`.
+ *
+ * @inflight `@winglang/sdk.cloud.ITestRunnerClient`
+ */
+export class TestRunner extends std.TestRunner {
+  constructor(scope: Construct, id: string, props: std.TestRunnerProps = {}) {
+    super(scope, id, props);
+    // This output is created so the CLI's `wing test` command can obtain a list
+    // of all names of test functions by running `terraform output`.
+    const output = new TerraformOutput(this, "TestFunctionIdentifiers", {
+      value: Lazy.stringValue({
+        produce: () => {
+          return JSON.stringify([
+            ...this.getTestFunctionIdentifiers().entries(),
+          ]);
+        },
+      }),
+    });
+
+    output.overrideLogicalId(OUTPUT_TEST_RUNNER_FUNCTION_IDENTIFIERS);
+  }
+
+  public onLift(host: std.IInflightHost, ops: string[]): void {
+    if (!(host instanceof GcpFunction)) {
+      throw new Error("TestRunner can only be bound by tfgcp.Function for now");
+    }
+    const { region, projectId } = core.App.of(this) as GCPApp;
+    const testFunctions = this.getTestFunctionIdentifiers();
+    host.addEnvironment(
+      this.envTestFunctionIdentifiers(),
+      JSON.stringify([...testFunctions.entries()])
+    );
+
+    host.addEnvironment(this.projectEnv(), projectId);
+    host.addEnvironment(this.regionEnv(), region);
+
+    super.onLift(host, ops);
+  }
+
+  /** @internal */
+  public _preSynthesize(): void {
+    // add a dependency on each test function
+    for (const test of this.findTests()) {
+      if (test._fn) {
+        this.node.addDependency(test._fn);
+      }
+    }
+
+    super._preSynthesize();
+  }
+
+  private getTestFunctionIdentifiers(): Map<string, string> {
+    const arns = new Map<string, string>();
+    for (const test of this.findTests()) {
+      if (test._fn) {
+        if (!(test._fn instanceof GcpFunction)) {
+          throw new Error(
+            `Unsupported test function type, ${test._fn.node.path} was not a tfgcp.Function`
+          );
+        }
+        arns.set(test.node.path, (test._fn as GcpFunction).functionName);
+      }
+    }
+    return arns;
+  }
+
+  /** @internal */
+  public _toInflight(): string {
+    return core.InflightClient.for(__dirname, __filename, "TestRunnerClient", [
+      `process.env["${this.envTestFunctionIdentifiers()}"]`,
+      `process.env["${this.projectEnv()}"]`,
+      `process.env["${this.regionEnv()}"]`,
+    ]);
+  }
+
+  private envTestFunctionIdentifiers(): string {
+    return `TEST_RUNNER_FUNCTIONS_${this.node.addr.slice(-8)}`;
+  }
+
+  private regionEnv(): string {
+    return `REGION_${this.node.addr.slice(-8)}`;
+  }
+
+  private projectEnv(): string {
+    return `PROJECT_${this.node.addr.slice(-8)}`;
+  }
+}
