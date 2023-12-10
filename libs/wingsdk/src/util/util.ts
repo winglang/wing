54aa8fa629136f13241bf0fec0465e4021632047
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2023-12-10 08:26:20 +0100

    add `shell()`, `exec()` and `spawn()` to util module

--- libs/wingsdk/src/util/util.ts
+++ libs/wingsdk/src/util/util.ts
@@ -1,8 +1,147 @@
+import { exec as nodeExec, spawn as nodeSpawn } from "child_process";
 import { createHash } from "crypto";
+import { promisify } from "util";
 import { nanoid, customAlphabet } from "nanoid";
 import { v4 } from "uuid";
 import { InflightClient } from "../core";
 import { Duration, IInflight } from "../std";
+
+const execPromise = promisify(nodeExec);
+
+/**
+ * Describes what to do with a standard I/O stream for a child process.
+ */
+export enum Stdio {
+  /**
+   * The child inherits from the corresponding parent descriptor.
+   */
+  INHERIT = "inherit",
+  /**
+   * A new pipe should be arranged to connect the parent and child processes.
+   */
+  PIPED = "pipe",
+  /**
+   * This stream will be ignored. This is the equivalent of attaching the stream to /dev/null.
+   */
+  NULL = "ignore",
+}
+
+/**
+ * Base command options.
+ */
+export interface CommandOptions {
+  /**
+   * Path to a directory to run the command in.
+   * @default - the default working directory of the host
+   */
+  readonly cwd?: string;
+  /**
+   * Environment variables.
+   * @default - no environment variables
+   */
+  readonly env?: { [key: string]: string };
+  /**
+   * Whether to inherit environment variables from the host's environment.
+   * @default false
+   */
+  readonly inheritEnv?: boolean;
+}
+
+/**
+ * Additional options for `util.shell()`
+ */
+export interface ShellOptions extends CommandOptions {}
+
+/**
+ * Additional options for `util.exec()`
+ */
+export interface ExecOptions extends CommandOptions {}
+
+/**
+ * Additional options for `util.spawn()`
+ */
+export interface SpawnOptions extends CommandOptions {
+  /**
+   * Configuration for the process's standard input stream.
+   * @default - Stdio.INHERIT
+   */
+  readonly stdin?: Stdio;
+  /**
+   * Configuration for the process's standard output stream.
+   * @default - Stdio.INHERIT
+   */
+  readonly stdout?: Stdio;
+  /**
+   * Configuration for the process's standard error stream.
+   * @default - Stdio.INHERIT
+   */
+  readonly stderr?: Stdio;
+}
+
+export interface Output {
+  /**
+   * The standard output of a finished process.
+   */
+  readonly stdout: string;
+  /**
+   * The standard error of a finished process.
+   */
+  readonly stderr: string;
+  /**
+   * A process's exit status.
+   */
+  readonly status: number;
+}
+
+/**
+ * Handle to a running child process.
+ */
+export class ChildProcess {
+  /**
+   * The child's OS-assigned process ID.
+   */
+  public readonly id: number;
+
+  private childProcess: ReturnType<typeof nodeSpawn>;
+
+  constructor(program: string, args: string[], opts?: SpawnOptions) {
+    this.childProcess = nodeSpawn(program, args, {
+      cwd: opts?.cwd,
+      env: opts?.env,
+    });
+    this.id = this.childProcess.pid ?? -1;
+  }
+
+  /**
+   * Kill the process.
+   * @param signal - the signal to send to the process (defaults to SIGTERM)
+   */
+  public kill(signal: number = 15): void {
+    this.childProcess.kill(signal);
+  }
+
+  /**
+   * Wait for the process to finish and return its output.
+   * Calling this method multiple times will return the same output.
+   */
+  public async wait(): Promise<Output> {
+    return new Promise((resolve, reject) => {
+      let stdout = "";
+      let stderr = "";
+
+      this.childProcess.stdout?.on("data", (data) => (stdout += data));
+      this.childProcess.stderr?.on("data", (data) => (stderr += data));
+
+      this.childProcess.on("close", (status) => {
+        resolve({ stdout, stderr, status: status !== null ? status : -1 });
+      });
+
+      this.childProcess.on("error", (error) => {
+        reject(error);
+      });
+    });
+  }
+}
 
 /**
  * Properties for `util.waitUntil`.
@@ -57,6 +196,68 @@ export interface NanoidOptions {
  * Utility functions.
  */
 export class Util {
+  /**
+   * Execute a command in the shell and return its standard output.
+   * @param command - The shell command to execute.
+   * @param opts - The shell options including working directory, environment variables, etc.
+   * @returns The standard output of the shell command.
+   * @throws An error if the command returns a non-zero exit code.
+   */
+  public static async shell(
+    command: string,
+    opts?: ShellOptions
+  ): Promise<string> {
+    const output = await Util.exec("/bin/sh", ["-c", command], opts);
+    if (output.status !== 0) {
+      throw new Error(
+        `Command exited with status ${output.status}: ${command}`
+      );
+    }
+    return output.stdout;
+  }
+
+  /**
+   * Execute a program with the given arguments, wait for it to finish, and
+   * return its outputs.
+   * @param program - The program to execute.
+   * @param args - An array of arguments to pass to the program.
+   * @param opts - Execution options including working directory, environment variables, etc.
+   * @returns A promise that resolves to the output of the executed program.
+   * @throws An error if the execution fails.
+   */
+  public static async exec(
+    program: string,
+    args: Array<string>,
+    opts?: ExecOptions
+  ): Promise<Output> {
+    const command = `${program} ${args.join(" ")}`;
+
+    const { stdout, stderr } = await execPromise(command, opts);
+
+    const status = 0;
+
+    const stdoutString = stdout.toString();
+    const stderrString = stderr.toString();
+
+    return { stdout: stdoutString, stderr: stderrString, status };
+  }
+
+  /**
+   * Execute a program with the given arguments, and return a `ChildProcess`
+   * object that can be used to interact with the process while it is running.
+   * @param program - The program to execute.
+   * @param args - An array of arguments to pass to the program.
+   * @param opts - Spawn options including working directory, environment variables, and stdio configurations.
+   * @returns The `ChildProcess` instance associated with the spawned process.
+   */
+  public static spawn(
+    program: string,
+    args: Array<string>,
+    opts?: SpawnOptions
+  ): ChildProcess {
+    return new ChildProcess(program, args, opts);
+  }
+
   /**
    * Returns the value of an environment variable. Throws if not found or empty.
    * @param name The name of the environment variable.
