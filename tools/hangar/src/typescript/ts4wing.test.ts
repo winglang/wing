6bb1d5c14d7ce97c42ff3106f528c480ac1628b6
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-12-18 14:53:43 -0500

    might work now, but not happy about it

--- tools/hangar/src/typescript/ts4wing.test.ts
+++ tools/hangar/src/typescript/ts4wing.test.ts
@@ -1,11 +1,23 @@
 import { describe, test } from "vitest";
 import * as path from "path";
-import { tmpDir, examplesDir } from "../paths";
+import { tmpDir, examplesDir, tmpNodeModulesDir } from "../paths";
 import { runWingCommand } from "../utils";
+import { cp, symlink } from "fs/promises"
 
-describe("ts4wing", () => {
-  const fixtureDir = path.join(examplesDir, "ts-fixture");
-  const tsFile = path.join(fixtureDir, "src", "main.ts");
+describe("ts-fixture", async () => {
+  const originalFixtureDir = path.join(examplesDir, "ts-fixture");
+  const newFixtureDir = path.join(tmpDir, "ts-fixture");
+
+  // make a temp copy of ts-fixture in tmpDir, excluding node_modules
+  await cp(originalFixtureDir, newFixtureDir, { 
+    recursive: true,
+    filter: (src) => !src.endsWith("node_modules")
+  });
+
+  // symlink the tmp node_modules to tmpNodeModulesDir (ensures we have the same node_modules as the rest of the tests)
+  await symlink(tmpNodeModulesDir, path.join(newFixtureDir, "node_modules"), "dir");
+
+  const tsFile = path.join(newFixtureDir, "src", "main.ts");
 
   test("compile", async () => {
     await runWingCommand({
