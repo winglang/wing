50099a46c691347724e6d780aa94abe069fe214d
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2023-12-26 19:03:47 +0100

    restore `spawn()` from PR

--- libs/wingsdk/src/util/util.ts
+++ libs/wingsdk/src/util/util.ts
@@ -1,4 +1,4 @@
-import { exec, execFile } from "child_process";
+import { exec, execFile, spawn } from "child_process";
 import { createHash } from "crypto";
 import { promisify } from "util";
 import { nanoid, customAlphabet } from "nanoid";
@@ -9,6 +9,24 @@ import { Duration, IInflight } from "../std";
 
 const execPromise = promisify(exec);
 const execFilePromise = promisify(execFile);
+
+/**
+ * Describes what to do with a standard I/O stream for a child process.
+ */
+export enum Stdio {
+  /**
+   * The child inherits from the corresponding parent descriptor.
+   */
+  INHERIT = "inherit",
+  /**
+   * A new pipe should be arranged to connect the parent and child processes.
+   */
+  PIPED = "pipe",
+  /**
+   * This stream will be ignored. This is the equivalent of attaching the stream to /dev/null.
+   */
+  NULL = "ignore",
+}
 
 /**
  * Base command options.
@@ -48,6 +66,27 @@ export interface ShellOptions extends CommandOptions {
 export interface ExecOptions extends CommandOptions {}
 
 /**
+ * Additional options for `util.spawn()`
+ */
+export interface SpawnOptions extends CommandOptions {
+  /**
+   * Configuration for the process's standard input stream.
+   * @default - Stdio.INHERIT
+   */
+  readonly stdin?: Stdio;
+  /**
+   * Configuration for the process's standard output stream.
+   * @default - Stdio.INHERIT
+   */
+  readonly stdout?: Stdio;
+  /**
+   * Configuration for the process's standard error stream.
+   * @default - Stdio.INHERIT
+   */
+  readonly stderr?: Stdio;
+}
+
+/**
  * Output of a finished process.
  */
 export interface Output {
@@ -63,6 +102,127 @@ export interface Output {
    * A process's exit status.
    */
   readonly status: number;
+}
+
+/**
+ * Handle to a running child process.
+ */
+export class ChildProcess {
+  /**
+   * @internal
+   */
+  public static _toInflightType(): string {
+    return InflightClient.forType(__filename, this.name);
+  }
+
+  /**
+   * The underlying native child process object representing the spawned process.
+   */
+  private childProcess: ReturnType<typeof spawn>;
+  /**
+   * The child's OS-assigned process ID.
+   */
+  public readonly pid: number | undefined;
+  /**
+   * The accumulated standard output from the child process.
+   */
+  private stdout: string = "";
+  /**
+   * The accumulated standard error from the child process.
+   */
+  private stderr: string = "";
+  /**
+   * The exit status of the child process. Null if the process has not yet finished.
+   */
+  private exitStatus: number | null = null;
+
+  constructor(program: string, args: string[], opts?: SpawnOptions) {
+    // Prepare stdio options based on `SpawnOptions`
+    const stdio = [
+      opts?.stdin === Stdio.PIPED
+        ? "pipe"
+        : opts?.stdin === Stdio.NULL
+        ? "ignore"
+        : "inherit",
+      opts?.stdout === Stdio.PIPED
+        ? "pipe"
+        : opts?.stdout === Stdio.NULL
+        ? "ignore"
+        : "inherit",
+      opts?.stderr === Stdio.PIPED
+        ? "pipe"
+        : opts?.stderr === Stdio.NULL
+        ? "ignore"
+        : "inherit",
+    ];
+
+    // Spawn the child process with the provided options
+    this.childProcess = spawn(program, args, {
+      cwd: opts?.cwd,
+      env: opts?.inheritEnv ? { ...process.env, ...opts.env } : opts?.env,
+      stdio: stdio as [
+        "pipe" | "ignore" | "inherit",
+        "pipe" | "ignore" | "inherit",
+        "pipe" | "ignore" | "inherit"
+      ],
+    });
+
+    this.pid = this.childProcess.pid;
+
+    // If stdio for stdout is set to `pipe`, listen for data events on stdout.
+    if (this.childProcess.stdout) {
+      this.childProcess.stdout.on("data", (data: Buffer) => {
+        this.stdout += data.toString();
+      });
+    }
+
+    // If stdio for stderr is set to `pipe`, listen for data events on stderr.
+    if (this.childProcess.stderr) {
+      this.childProcess.stderr.on("data", (data: Buffer) => {
+        this.stderr += data.toString();
+      });
+    }
+
+    this.childProcess.on("exit", (code) => {
+      this.exitStatus = code;
+    });
+  }
+
+  /**
+   * Kill the process.
+   * @param signal - the signal to send to the process (defaults to SIGTERM)
+   */
+  public kill(signal: number = 15): void {
+    this.childProcess.kill(signal);
+  }
+
+  /**
+   * Wait for the process to finish and return its output.
+   * Calling this method multiple times will return the same output.
+   */
+  public async wait(): Promise<Output> {
+    if (this.exitStatus !== null) {
+      return {
+        stdout: this.stdout,
+        stderr: this.stderr,
+        status: this.exitStatus,
+      };
+    }
+
+    return new Promise((resolve, reject) => {
+      this.childProcess.on("exit", (code, signal) => {
+        if (code !== null) {
+          resolve({ stdout: this.stdout, stderr: this.stderr, status: code });
+        } else {
+          reject(new Error(`Process terminated by signal ${signal}`));
+        }
+      });
+
+      this.childProcess.on("error", (error) => {
+        reject(error);
+      });
+    });
+  }
 }
 
 /**
@@ -208,6 +368,22 @@ export class Util {
         };
       }
     }
+  }
+
+  /**
+   * Execute a program with the given arguments, and return a `ChildProcess`
+   * object that can be used to interact with the process while it is running.
+   * @param program - The program to execute.
+   * @param args - An array of arguments to pass to the program.
+   * @param opts - Spawn options including working directory, environment variables, and stdio configurations.
+   * @returns The `ChildProcess` instance associated with the spawned process.
+   */
+  public static spawn(
+    program: string,
+    args: Array<string>,
+    opts?: SpawnOptions
+  ): ChildProcess {
+    return new ChildProcess(program, args, opts);
   }
 
   /**
