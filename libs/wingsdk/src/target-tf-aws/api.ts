fbec42a4258ffca7e4513f5e8f2e0d78ff390e06
Mark McCulloh <Mark.McCulloh@gmail.com>
2023-12-30 18:51:48 -0500

    revert(sdk): remove /{proxy+} from path and add OPTIONS (#5371)
    Reverts winglang/wing#5358
    
    See failure in AWS here https://github.com/winglang/wing/actions/runs/7366149282/job/20048148050

--- libs/wingsdk/src/target-tf-aws/api.ts
+++ libs/wingsdk/src/target-tf-aws/api.ts
@@ -19,22 +19,8 @@ import {
   ResourceNames,
 } from "../shared/resource-names";
 import { IAwsApi, STAGE_NAME } from "../shared-aws";
-import { corsOptionsMethod } from "../shared-aws/api.cors";
+import { API_CORS_DEFAULT_RESPONSE } from "../shared-aws/api.cors";
 import { IInflightHost, Node } from "../std";
-
-/**
- * Custom error when the resource doesn't exist
- */
-const GATEWAY_404_RESPONSE = {
-  "x-amazon-apigateway-gateway-responses": {
-    MISSING_AUTHENTICATION_TOKEN: {
-      statusCode: "404",
-      responseTemplates: {
-        "application/json": '{"message": $context.error.messageString }',
-      },
-    },
-  },
-};
 
 /**
  * RestApi names are alphanumeric characters, hyphens (-) and underscores (_).
@@ -335,24 +321,21 @@ class WingRestApi extends Construct {
     super(scope, id);
     this.region = (App.of(this) as App).region;
 
+    const defaultResponse = API_CORS_DEFAULT_RESPONSE(props.cors);
+
     this.api = new ApiGatewayRestApi(this, `${id}`, {
       name: ResourceNames.generateName(this, NAME_OPTS),
       // Lazy generation of the api spec because routes can be added after the API is created
       body: Lazy.stringValue({
         produce: () => {
-          const injectOptionsMethod = (openApiSpec: OpenApiSpec) => {
-            Object.keys(openApiSpec.paths).forEach(function (key) {
-              if (!("options" in openApiSpec.paths[key]) && props.cors) {
-                openApiSpec.paths[key].options = corsOptionsMethod(props.cors);
-              }
-            });
-
-            return {
-              ...openApiSpec,
-              ...GATEWAY_404_RESPONSE,
+          const injectGreedy404Handler = (openApiSpec: OpenApiSpec) => {
+            openApiSpec.paths = {
+              ...openApiSpec.paths,
+              ...defaultResponse,
             };
+            return openApiSpec;
           };
-          return JSON.stringify(injectOptionsMethod(props.getApiSpec()));
+          return JSON.stringify(injectGreedy404Handler(props.getApiSpec()));
         },
       }),
       lifecycle: {
