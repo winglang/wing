31c11a11720f5797f7c46c6d17112af12e198e89
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2023-12-31 04:11:54 +0100

    comment lines

--- libs/wingsdk/src/util/child-process.ts
+++ libs/wingsdk/src/util/child-process.ts
@@ -53,20 +53,20 @@ export class ChildProcess {
     console.log(`Spawned child process with PID: ${this.child.pid}`);
 
     // If stdio for stdout is set to `pipe`, listen for data events on stdout.
-    if (this.child.stdout) {
-      this.child.stdout.on("data", (data: Buffer) => {
-        console.log("Received stdout data:", data.toString());
-        this.stdout += data.toString();
-      });
-    }
+    // if (this.child.stdout) {
+    //   this.child.stdout.on("data", (data: Buffer) => {
+    //     console.log("Received stdout data:", data.toString());
+    //     this.stdout += data.toString();
+    //   });
+    // }
 
-    // If stdio for stderr is set to `pipe`, listen for data events on stderr.
-    if (this.child.stderr) {
-      this.child.stderr.on("data", (data: Buffer) => {
-        console.log("Received stderr data:", data.toString());
-        this.stderr += data.toString();
-      });
-    }
+    // // If stdio for stderr is set to `pipe`, listen for data events on stderr.
+    // if (this.child.stderr) {
+    //   this.child.stderr.on("data", (data: Buffer) => {
+    //     console.log("Received stderr data:", data.toString());
+    //     this.stderr += data.toString();
+    //   });
+    // }
 
     this.child.on("exit", (code) => {
       console.log(`Child process exited with code: ${code}`);
