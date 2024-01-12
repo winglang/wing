ebb3c60c096a4dd7f99b55433300a3c9182bd109
Chris Rybicki <chrisr@monada.co>
2024-01-12 16:53:04 -0500

    fix test

--- libs/wingsdk/test/target-sim/binding.test.ts
+++ libs/wingsdk/test/target-sim/binding.test.ts
@@ -1,13 +1,14 @@
 import { test, expect } from "vitest";
-import { Function } from "../../src/cloud";
+import { Bucket, Function } from "../../src/cloud";
 import { Testing } from "../../src/simulator";
 import { SimApp } from "../sim-app";
 
 test("binding throws if a method is unsupported", () => {
   const app = new SimApp();
+  const bucket = new Bucket(app, "Bucket");
   const handler = Testing.makeHandler("async handle() {}");
   const host = new Function(app, "Function", handler);
-  expect(() => handler.onLift(host, ["foo", "bar"])).toThrow(
-    /Resource root\/Handler does not support inflight operation foo \(requested by root\/Function\)/
+  expect(() => bucket.onLift(host, ["foo", "bar"])).toThrow(
+    /Resource root\/Bucket does not support inflight operation foo \(requested by root\/Function\)/
   );
 });
