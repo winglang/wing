282702f029b8b180f9a9f5d484b3d88b93056e6a
Tsuf Cohen <39455181+tsuf239@users.noreply.github.com>
2024-01-21 11:25:09 +0200

    feat(cli): add more data to the out file (#5443)
    adding more data to the out file (for later be consumed by the compatibility matrix in one way or another)
    - unsupportedResource
    - unsupportedOperation (for unsupported method or property)
    - args.methods (for supported methods)
    <img width="371" alt="image" src="https://github.com/winglang/wing/assets/39455181/6a6160b5-3280-49df-a14e-e6cf1a862891">
    <img width="354" alt="image" src="https://github.com/winglang/wing/assets/39455181/c60108e0-23b0-4425-b41b-6c4919d1c48e">
    
    I couldn't think of a way to do it without reading the usage context file - opened an issue #5442 
    
    
    
    
    
    ## Checklist
    
    - [ ] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
    - [ ] Description explains motivation and solution
    - [ ] Tests added (always)
    - [ ] Docs updated (only required for features)
    - [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

--- apps/wing/src/commands/test/results.ts
+++ apps/wing/src/commands/test/results.ts
@@ -99,15 +99,17 @@ export function printResults(
 
 interface TestResultsJson {
   duration: number;
+  platforms: string[];
   results: Record<string, Record<string, std.TestResult>>;
 }
 
 export async function writeResultsToFile(
   testResults: { testName: string; results: std.TestResult[] }[],
   duration: number,
-  filePath: string
+  filePath: string,
+  platforms: string[]
 ) {
-  const output: TestResultsJson = { duration, results: {} };
+  const output: TestResultsJson = { duration, platforms, results: {} };
   for (const result of testResults) {
     output.results[result.testName] = result.results.reduce(
       (acc, item) => ({ ...acc, [item.path.replace(/[\w+\/\\.-]+test:/, "")]: item }),
