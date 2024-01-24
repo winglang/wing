ce6109855f86835f5907903418f8609c52843060
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2024-01-24 10:55:20 +0200

    fix

--- scripts/compatibility-matrix-automation/src/constants.ts
+++ scripts/compatibility-matrix-automation/src/constants.ts
@@ -3,7 +3,7 @@ import { join } from "path";
 export const SKIPPED_RESOURCES = "TestRunner";
 export const MATRIX_PATH = join(
   __dirname,
-  "../../../../docs/docs/04-standard-library/compatibility/compatibility.json"
+  "../../../docs/docs/04-standard-library/compatibility/compatibility.json"
 );
 export const OUT_PATH = join(__dirname, "../../../out.json");
 
