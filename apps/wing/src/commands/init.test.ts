6c454c09c37d0fe4e4870c9a0d157e49a90edf75
Hasan <45375125+hasanaburayyan@users.noreply.github.com>
2024-01-30 23:21:48 -0500

    fix(cli): wing new showing duplicate template names (#5581)
    Duplicate template names were being listed if we supported more than 1 language
    
    ## Checklist
    
    - [ ] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
    - [ ] Description explains motivation and solution
    - [ ] Tests added (always)
    - [ ] Docs updated (only required for features)
    - [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

--- apps/wing/src/commands/init.test.ts
+++ apps/wing/src/commands/init.test.ts
@@ -34,6 +34,30 @@ describe.each(templates)("new %s --language=wing", (template) => {
   });
 });
 
+describe("new --list-templates", () => {
+  let log: any;
+  beforeEach(() => {
+    log = console.log;
+    console.log = vi.fn();
+  });
+
+  afterEach(() => {
+    console.log = log;
+  });
+
+  test("does not contain duplicate template names", async () => {
+    await init("", { listTemplates: true });
+    expect(console.log).toHaveBeenCalledWith(templates.join("\n"));
+
+    const outputLines = (console.log as any).mock.calls[0][0].split("\n");
+    const outputLinesAsSet = new Set(outputLines);
+
+    expect(outputLines.length).toBeGreaterThan(0);
+    // Check there are no duplicate lines
+    expect(outputLinesAsSet.size).toBe(outputLines.length);
+  });
+});
+
 describe("new empty --language=ts", () => {
   let log: any;
   beforeEach(() => {
