2c06d10f80e25f0be9b0bd5a36756b13f4898419
Cristian Pallarés <cristianp@wing.cloud>
2024-02-22 15:51:38 +0100

    feat: add support for signing in with Google in the login page
    The changes in this commit add support for signing in with Google in the login page. 
    
    - Added a new component `GoogleIcon` to display the Google logo.
    - Updated the `useSignIn` hook to include a new function `signInWithGoogle` that handles signing in with Google.
    - Updated the login page to display a button for signing in with Google.
    - Updated the `AppContext` interface to include the `googleSignInURL` property.
    - Added a new prop `googleSignInURL` to the `AppContext` provider.
    - Updated the `index.tsx` file to provide the `googleSignInURL` prop to the `AppContext` provider.

--- apps/wing-console/console/ui/src/layout/sign-in.tsx
+++ apps/wing-console/console/ui/src/layout/sign-in.tsx
@@ -13,6 +13,7 @@ import { AppContext } from "../AppContext.js";
 import { trpc } from "../services/trpc.js";
 
 import { GithubIcon } from "./github-icon.js";
+import { GoogleIcon } from "./google-icon.js";
 
 /**
  * The name of the query parameter that is passed from
@@ -66,17 +67,31 @@ const useNotifyAfterSigningIn = () => {
  * Returns a function that can be used to sign in the user.
  */
 const useSignIn = () => {
-  const { wingCloudSignInUrl } = useContext(AppContext);
+  const { githubSignInURL, googleSignInURL } = useContext(AppContext);
   const { mutateAsync: reportSignInClicked } =
     trpc["app.analytics.signInClicked"].useMutation();
   const analytics = trpc["app.analytics"].useQuery();
-  return useCallback(async () => {
+
+  const signInWithGithub = useCallback(async () => {
     await reportSignInClicked();
     const url = new URL(wingCloudSignInUrl!);
     url.searchParams.append("port", location.port);
     url.searchParams.append("anonymousId", `${analytics.data?.anonymousId}`);
     location.href = url.toString();
-  }, [reportSignInClicked, wingCloudSignInUrl, analytics.data?.anonymousId]);
+  }, [reportSignInClicked, githubSignInURL, analytics.data?.anonymousId]);
+
+  const signInWithGoogle = useCallback(async () => {
+    await reportSignInClicked();
+    const url = new URL(googleSignInURL!);
+    url.searchParams.append("port", location.port);
+    url.searchParams.append("anonymousId", `${analytics.data?.anonymousId}`);
+    location.href = url.toString();
+  }, [reportSignInClicked, googleSignInURL, analytics.data?.anonymousId]);
+
+  return {
+    signInWithGithub,
+    signInWithGoogle,
+  };
 };
 
 /**
@@ -123,25 +138,42 @@ export const SignInModal = (props: SignInModalProps) => {
         </h3>
 
         <p className={classNames(theme.text2, "text-sm text-center")}>
-          Please sign up with your GitHub credentials to help us improve your
-          experience in Wing Console and Wing CLI.
+          Please sign in to help us improve your experience in Wing Console and
+          Wing CLI.
         </p>
 
-        <div className="flex justify-around gap-2">
-          <Button
-            disabled={isLoading}
-            onClick={() => {
-              setIsLoading(true);
-              void signIn();
-            }}
-          >
-            {isLoading ? (
-              <Loader size="xs" className={"mr-1"} />
-            ) : (
-              <GithubIcon className="w-4 h-4" />
-            )}
-            <span className="text-sm">Sign In</span>
-          </Button>
+        <div className="flex justify-around py-3">
+          <div className="flex flex-col gap-2.5">
+            <Button
+              disabled={isLoading}
+              onClick={() => {
+                setIsLoading(true);
+                void signInWithGithub();
+              }}
+            >
+              {isLoading ? (
+                <Loader size="xs" className={"mr-1"} />
+              ) : (
+                <GithubIcon className="w-4 h-4" />
+              )}
+              <span className="text-sm">Continue with GitHub</span>
+            </Button>
+
+            <Button
+              disabled={isLoading}
+              onClick={() => {
+                setIsLoading(true);
+                void signInWithGoogle();
+              }}
+            >
+              {isLoading ? (
+                <Loader size="xs" className={"mr-1"} />
+              ) : (
+                <GoogleIcon className="w-4 h-4" />
+              )}
+              <span className="text-sm">Continue with Google</span>
+            </Button>
+          </div>
         </div>
 
         <div className="flex justify-around">
