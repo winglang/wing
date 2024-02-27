5247fc6f02d4001ad077df69bc17d05294af8fa3
Mark McCulloh <Mark.McCulloh@gmail.com>
2024-02-27 07:56:18 -0500

    remove console log

--- tools/bump-pack/src/bundle-link.ts
+++ tools/bump-pack/src/bundle-link.ts
@@ -95,8 +95,6 @@ export async function linkBundledTransitiveDeps(
         throw new Error(`pnpm dependency path not found: ${dep.path}`);
       }
 
-      console.log(destModule);
-
       // if the symlink exists but is broken, attempt to remove it
       await fs
         .stat(destModule)
