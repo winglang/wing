25a1c2e361db83eb0afec4fd10076125b08697d2
polamoros <info@polamoros.com>
2024-03-13 10:35:56 +0100

    wip

--- apps/wing-console/console/app/test/health/health.test.ts
+++ apps/wing-console/console/app/test/health/health.test.ts
@@ -4,7 +4,6 @@ import { describe } from "../describe.js";
 
 describe(`${__dirname}/main.w`, () => {
   test("Health check", async ({ page }) => {
-    const appState = await page.getByTestId("app-state").textContent();
-    expect(appState).toBe("success");
+    expect(page.getByTestId("app-state")).toContainText("success");
   });
 });
