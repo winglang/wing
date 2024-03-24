8255f6aaec7cf815f6679a584fa28805e04b2ef4
Elad Ben-Israel <eladb@monada.co>
2024-03-24 21:36:10 +0200

    Delete libs/wingsdk/src/std/bundler.ts

--- libs/wingsdk/src/std/bundler.ts
+++
@@ -1,8 +0,0 @@
-import { Construct } from "constructs";
-import { Resource } from "./resource";
-
-export class Bundler extends Resource {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-  }
-}
