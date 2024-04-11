60e6bf90f890d7e1e4410eba002669265277ef56
Marcio Cruz de Almeida <marciocadev@gmail.com>
2024-04-11 09:55:45 -0300

    refactoring

---
+++ examples/tests/sdk_tests/website/react-website/build/public/index.js
@@ -0,0 +1,2 @@
+document.getElementById("par1").innerText = `apiUrl is: ${window.wingEnv?.apiUrl}`;
+document.getElementById("par2").innerText = `anotherEnvVar is: ${window.wingEnv?.anotherEnvVar}`;
