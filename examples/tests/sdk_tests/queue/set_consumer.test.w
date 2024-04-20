winglang/wing

commit 2dea83565214fbceb6d8619f339ec249dcfbeae7

feat(sdk): dead letter queue support for queues (#6060)
⛔ As the dead-letter queue linked to the function is a specific case of AWS, I will remove it from the scope of this PR.

- [x] dlq (with retries) for queue (tf-aws / awscdk)
- [x] dlq (with retries) for queue (sim)
~dlq for function (tf-aws / awscdk)~
~dlq for function (sim)~

Closes #6033

## Checklist

- [x] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
- [x] Description explains motivation and solution
- [x] Tests added (always)
- [ ] Docs updated (only required for features)
- [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

libs/wingsdk/src/target-sim/queue.inflight.ts
@@ -5,9 +5,15 @@ import {
   QueueSchema,
   QueueSubscriber,
   EventSubscription,
+  DeadLetterQueueSchema,
   ResourceHandle,
 } from "./schema-resources";
-import { IFunctionClient, IQueueClient, QUEUE_FQN } from "../cloud";
+import {
+  DEFAULT_DELIVERY_ATTEMPTS,
+  IFunctionClient,
+  IQueueClient,
+  QUEUE_FQN,
+} from "../cloud";
 import {
   ISimulatorContext,
   ISimulatorResourceInstance,
@@ -24,10 +30,12 @@ export class Queue
   private _context: ISimulatorContext | undefined;
   private readonly timeoutSeconds: number;
   private readonly retentionPeriod: number;
+  private readonly dlq?: DeadLetterQueueSchema;
 
   constructor(props: QueueSchema) {
     this.timeoutSeconds = props.timeout;
     this.retentionPeriod = props.retentionPeriod;
+    this.dlq = props.dlq;
     this.processLoop = runEvery(100, async () => this.processMessages()); // every 0.1 seconds
   }
 
@@ -85,7 +93,13 @@ export class Queue
           throw new Error("Empty messages are not allowed");
         }
         for (const message of messages) {
-          this.messages.push(new QueueMessage(this.retentionPeriod, message));
+          this.messages.push(
+            new QueueMessage(
+              this.retentionPeriod,
+              DEFAULT_DELIVERY_ATTEMPTS,
+              message
+            )
+          );
         }
       },
     });
@@ -187,8 +201,38 @@ export class Queue
 
         // we don't use invokeAsync here because we want to wait for the function to finish
         // and requeue the messages if it fails
-        void fnClient
-          .invoke(JSON.stringify({ messages: messagesPayload }))
+        await fnClient
+          .invoke(JSON.stringify({ messages: messages }))
+          .then((result) => {
+            if (this.dlq && result) {
+              const errorList = JSON.parse(result);
+              let retriesMessages = [];
+              for (const msg of errorList) {
+                if (
+                  msg.remainingDeliveryAttempts < this.dlq.maxDeliveryAttempts
+                ) {
+                  msg.remainingDeliveryAttempts++;
+                  retriesMessages.push(msg);
+                } else {
+                  let dlq = this.context.getClient(
+                    this.dlq.dlqHandler
+                  ) as IQueueClient;
+                  void dlq.push(msg.payload).catch((err) => {
+                    this.context.addTrace({
+                      type: TraceType.RESOURCE,
+                      data: {
+                        message: `Pushing messages to the dead-letter queue generates an error -> ${err}`,
+                      },
+                      sourcePath: this.context.resourcePath,
+                      sourceType: QUEUE_FQN,
+                      timestamp: new Date().toISOString(),
+                    });
+                  });
+                }
+              }
+              this.messages.push(...retriesMessages);
+            }
+          })
           .catch((err) => {
             // If the function is at a concurrency limit, pretend we just didn't call it
             if (
@@ -238,12 +282,18 @@ export class Queue
 class QueueMessage {
   public readonly retentionTimeout: Date;
   public readonly payload: string;
+  public remainingDeliveryAttempts: number;
 
-  constructor(retentionPeriod: number, message: string) {
+  constructor(
+    retentionPeriod: number,
+    remainingDeliveryAttempts: number,
+    message: string
+  ) {
     const currentTime = new Date();
     currentTime.setSeconds(retentionPeriod + currentTime.getSeconds());
     this.retentionTimeout = currentTime;
     this.payload = message;
+    this.remainingDeliveryAttempts = remainingDeliveryAttempts;
   }
 }
 

examples/tests/sdk_tests/queue/dead-letter-queue.test.w
@@ -1,0 +1,63 @@
+bring cloud;
+bring util;
+
+let counter_received_messages = new cloud.Counter();
+
+let dlq_without_retries = new cloud.Queue() as "dlq without retries";
+let queue_without_retries = new cloud.Queue(
+  dlq: { queue: dlq_without_retries }
+) as "queue without retries";
+queue_without_retries.setConsumer(inflight (msg: str) => {
+  counter_received_messages.inc(1, msg);
+  if msg == "fail" {
+    throw "error";
+  }
+});
+
+
+new std.Test(
+  inflight () => {
+    queue_without_retries.push("Hello");
+    queue_without_retries.push("fail");
+    queue_without_retries.push("World!");
+
+    // wait until it executes once.
+    assert(util.waitUntil(inflight () => { return counter_received_messages.peek("Hello") == 1; }));
+    assert(util.waitUntil(inflight () => { return counter_received_messages.peek("World!") == 1; }));
+    assert(util.waitUntil(inflight () => { return counter_received_messages.peek("fail") == 1; }));
+
+    // check if the "fail" message has arrived at the dead-letter queue
+    assert(util.waitUntil(inflight () => { return dlq_without_retries.pop() == "fail"; }));
+  }, 
+  // To make this test work on AWS, it's necessary to set a high timeout 
+  // because if the message fails, we have to wait for the visibility timeout 
+  // to expire in order to retrieve the same message from the queue again.
+  timeout: 5m) as "one execution and send fail message to dead-letter queue";
+
+let dlq_with_retries = new cloud.Queue() as "dlq with retries";
+let queue_with_retries = new cloud.Queue(
+  dlq: {
+    queue: dlq_with_retries,
+    maxDeliveryAttempts: 2
+  }
+) as "queue with retries";
+queue_with_retries.setConsumer(inflight (msg: str) => {
+  counter_received_messages.inc(1, msg);
+  if msg == "fail" {
+    throw "error";
+  }
+});
+
+new std.Test(inflight () => {
+  queue_with_retries.push("Hello");
+  queue_with_retries.push("fail");
+  queue_with_retries.push("World!");
+
+  // wait until it executes once and retry one more times.
+  assert(util.waitUntil(inflight () => { return counter_received_messages.peek("Hello") == 1; }));
+  assert(util.waitUntil(inflight () => { return counter_received_messages.peek("World!") == 1; }));
+  assert(util.waitUntil(inflight () => { return counter_received_messages.peek("fail") == 2; }));
+
+  // check if the "fail" message has arrived at the dead-letter queue
+  assert(util.waitUntil(inflight () => { return dlq_with_retries.pop() == "fail"; }));
+}, timeout: 5m) as "one execution, two retries and send the fail message to dead-letter queue";

libs/awscdk/src/queue.ts
@@ -7,7 +7,7 @@ import { App } from "./app";
 import { std, core, cloud } from "@winglang/sdk";
 import { convertBetweenHandlers } from "@winglang/sdk/lib/shared/convert";
 import { calculateQueuePermissions } from "@winglang/sdk/lib/shared-aws/permissions";
-import { IAwsQueue } from "@winglang/sdk/lib/shared-aws/queue";
+import { IAwsQueue, Queue as AwsQueue } from "@winglang/sdk/lib/shared-aws/queue";
 import { addPolicyStatements, isAwsCdkFunction } from "./function";
 
 /**
@@ -23,14 +23,27 @@ export class Queue extends cloud.Queue implements IAwsQueue {
     super(scope, id, props);
     this.timeout = props.timeout ?? std.Duration.fromSeconds(30);
 
-    this.queue = new SQSQueue(this, "Default", {
+    const queueOpt = props.dlq ? {
       visibilityTimeout: props.timeout
         ? Duration.seconds(props.timeout?.seconds)
         : Duration.seconds(30),
       retentionPeriod: props.retentionPeriod
         ? Duration.seconds(props.retentionPeriod?.seconds)
         : Duration.hours(1),
-    });
+      deadLetterQueue: {
+        queue: SQSQueue.fromQueueArn(this, "DeadLetterQueue", AwsQueue.from(props.dlq.queue)?.queueArn!),
+        maxReceiveCount: props.dlq.maxDeliveryAttempts ?? cloud.DEFAULT_DELIVERY_ATTEMPTS,
+      }
+    } : {
+      visibilityTimeout: props.timeout
+        ? Duration.seconds(props.timeout?.seconds)
+        : Duration.seconds(30),
+      retentionPeriod: props.retentionPeriod
+        ? Duration.seconds(props.retentionPeriod?.seconds)
+        : Duration.hours(1),
+    }
+
+    this.queue = new SQSQueue(this, "Default", queueOpt);
   }
 
   public setConsumer(
@@ -63,6 +76,7 @@ export class Queue extends cloud.Queue implements IAwsQueue {
 
     const eventSource = new SqsEventSource(this.queue, {
       batchSize: props.batchSize ?? 1,
+      reportBatchItemFailures: true,
     });
 
     fn.awscdkFunction.addEventSource(eventSource);

libs/awscdk/test/__snapshots__/queue.test.ts.snap
@@ -192,6 +192,9 @@ exports[`queue with a consumer function 2`] = `
         "FunctionName": {
           "Ref": "QueueSetConsumer06749388A",
         },
+        "FunctionResponseTypes": [
+          "ReportBatchItemFailures",
+        ],
       },
       "Type": "AWS::Lambda::EventSourceMapping",
     },

libs/wingc/src/lsp/snapshots/completions/incomplete_inflight_namespace.snap
@@ -47,7 +47,7 @@ source: libs/wingc/src/lsp/completions.rs
   kind: 7
   documentation:
     kind: markdown
-    value: "```wing\nclass Queue\n```\n---\nA queue.\n\n### Initializer\n- `...props` — `QueueProps?`\n  \n  - `retentionPeriod?` — `duration?` — How long a queue retains a message.\n  - `timeout?` — `duration?` — How long a queue's consumers have to process a message.\n### Fields\n- `node` — `Node` — The tree node.\n### Methods\n- `approxSize` — `inflight (): num` — Retrieve the approximate number of messages in the queue.\n- `isConstruct` — `preflight (x: any): bool` — Checks if `x` is a construct.\n- `onLift` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this resource inflight.\n- `onLiftType` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this type inflight.\n- `pop` — `inflight (): str?` — Pop a message from the queue.\n- `purge` — `inflight (): void` — Purge all of the messages in the queue.\n- `push` — `inflight (...messages: Array<str>?): void` — Push one or more messages to the queue.\n- `setConsumer` — `preflight (handler: inflight (message: str): void, props: QueueSetConsumerOptions?): Function` — Create a function to consume messages from this queue.\n- `toString` — `preflight (): str` — Returns a string representation of this construct."
+    value: "```wing\nclass Queue\n```\n---\nA queue.\n\n### Initializer\n- `...props` — `QueueProps?`\n  \n  - `dlq?` — `DeadLetterQueueProps?` — A dead-letter queue.\n  - `retentionPeriod?` — `duration?` — How long a queue retains a message.\n  - `timeout?` — `duration?` — How long a queue's consumers have to process a message.\n### Fields\n- `node` — `Node` — The tree node.\n### Methods\n- `approxSize` — `inflight (): num` — Retrieve the approximate number of messages in the queue.\n- `isConstruct` — `preflight (x: any): bool` — Checks if `x` is a construct.\n- `onLift` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this resource inflight.\n- `onLiftType` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this type inflight.\n- `pop` — `inflight (): str?` — Pop a message from the queue.\n- `purge` — `inflight (): void` — Purge all of the messages in the queue.\n- `push` — `inflight (...messages: Array<str>?): void` — Push one or more messages to the queue.\n- `setConsumer` — `preflight (handler: inflight (message: str): void, props: QueueSetConsumerOptions?): Function` — Create a function to consume messages from this queue.\n- `toString` — `preflight (): str` — Returns a string representation of this construct."
   sortText: gg|Queue
 - label: Schedule
   kind: 7
@@ -229,6 +229,12 @@ source: libs/wingc/src/lsp/completions.rs
     kind: markdown
     value: "```wing\nstruct CounterProps\n```\n---\nOptions for `Counter`.\n### Fields\n- `initial?` — `num?` — The initial value of the counter."
   sortText: hh|CounterProps
+- label: DeadLetterQueueProps
+  kind: 22
+  documentation:
+    kind: markdown
+    value: "```wing\nstruct DeadLetterQueueProps\n```\n---\nDead letter queue options.\n### Fields\n- `queue` — `Queue` — Queue to receive messages that failed processing.\n- `maxDeliveryAttempts?` — `num?` — Number of times a message will be processed before being sent to the dead-letter queue."
+  sortText: hh|DeadLetterQueueProps
 - label: DomainProps
   kind: 22
   documentation:
@@ -269,7 +275,7 @@ source: libs/wingc/src/lsp/completions.rs
   kind: 22
   documentation:
     kind: markdown
-    value: "```wing\nstruct QueueProps\n```\n---\nOptions for `Queue`.\n### Fields\n- `retentionPeriod?` — `duration?` — How long a queue retains a message.\n- `timeout?` — `duration?` — How long a queue's consumers have to process a message."
+    value: "```wing\nstruct QueueProps\n```\n---\nOptions for `Queue`.\n### Fields\n- `dlq?` — `DeadLetterQueueProps?` — A dead-letter queue.\n- `retentionPeriod?` — `duration?` — How long a queue retains a message.\n- `timeout?` — `duration?` — How long a queue's consumers have to process a message."
   sortText: hh|QueueProps
 - label: QueueSetConsumerOptions
   kind: 22
@@ -323,7 +329,7 @@ source: libs/wingc/src/lsp/completions.rs
   kind: 22
   documentation:
     kind: markdown
-    value: "```wing\nstruct TopicSubscribeQueueOptions extends QueueProps\n```\n---\nOptions for `Topic.subscribeQueue`.\n### Fields\n- `retentionPeriod?` — `duration?`\n- `timeout?` — `duration?`"
+    value: "```wing\nstruct TopicSubscribeQueueOptions extends QueueProps\n```\n---\nOptions for `Topic.subscribeQueue`.\n### Fields\n- `dlq?` — `DeadLetterQueueProps?`\n- `retentionPeriod?` — `duration?`\n- `timeout?` — `duration?`"
   sortText: hh|TopicSubscribeQueueOptions
 - label: WebsiteDomainOptions
   kind: 22

libs/wingc/src/lsp/snapshots/completions/namespace_middle_dot.snap
@@ -47,7 +47,7 @@ source: libs/wingc/src/lsp/completions.rs
   kind: 7
   documentation:
     kind: markdown
-    value: "```wing\nclass Queue\n```\n---\nA queue.\n\n### Initializer\n- `...props` — `QueueProps?`\n  \n  - `retentionPeriod?` — `duration?` — How long a queue retains a message.\n  - `timeout?` — `duration?` — How long a queue's consumers have to process a message.\n### Fields\n- `node` — `Node` — The tree node.\n### Methods\n- `approxSize` — `inflight (): num` — Retrieve the approximate number of messages in the queue.\n- `isConstruct` — `preflight (x: any): bool` — Checks if `x` is a construct.\n- `onLift` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this resource inflight.\n- `onLiftType` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this type inflight.\n- `pop` — `inflight (): str?` — Pop a message from the queue.\n- `purge` — `inflight (): void` — Purge all of the messages in the queue.\n- `push` — `inflight (...messages: Array<str>?): void` — Push one or more messages to the queue.\n- `setConsumer` — `preflight (handler: inflight (message: str): void, props: QueueSetConsumerOptions?): Function` — Create a function to consume messages from this queue.\n- `toString` — `preflight (): str` — Returns a string representation of this construct."
+    value: "```wing\nclass Queue\n```\n---\nA queue.\n\n### Initializer\n- `...props` — `QueueProps?`\n  \n  - `dlq?` — `DeadLetterQueueProps?` — A dead-letter queue.\n  - `retentionPeriod?` — `duration?` — How long a queue retains a message.\n  - `timeout?` — `duration?` — How long a queue's consumers have to process a message.\n### Fields\n- `node` — `Node` — The tree node.\n### Methods\n- `approxSize` — `inflight (): num` — Retrieve the approximate number of messages in the queue.\n- `isConstruct` — `preflight (x: any): bool` — Checks if `x` is a construct.\n- `onLift` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this resource inflight.\n- `onLiftType` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this type inflight.\n- `pop` — `inflight (): str?` — Pop a message from the queue.\n- `purge` — `inflight (): void` — Purge all of the messages in the queue.\n- `push` — `inflight (...messages: Array<str>?): void` — Push one or more messages to the queue.\n- `setConsumer` — `preflight (handler: inflight (message: str): void, props: QueueSetConsumerOptions?): Function` — Create a function to consume messages from this queue.\n- `toString` — `preflight (): str` — Returns a string representation of this construct."
   sortText: gg|Queue
 - label: Schedule
   kind: 7
@@ -229,6 +229,12 @@ source: libs/wingc/src/lsp/completions.rs
     kind: markdown
     value: "```wing\nstruct CounterProps\n```\n---\nOptions for `Counter`.\n### Fields\n- `initial?` — `num?` — The initial value of the counter."
   sortText: hh|CounterProps
+- label: DeadLetterQueueProps
+  kind: 22
+  documentation:
+    kind: markdown
+    value: "```wing\nstruct DeadLetterQueueProps\n```\n---\nDead letter queue options.\n### Fields\n- `queue` — `Queue` — Queue to receive messages that failed processing.\n- `maxDeliveryAttempts?` — `num?` — Number of times a message will be processed before being sent to the dead-letter queue."
+  sortText: hh|DeadLetterQueueProps
 - label: DomainProps
   kind: 22
   documentation:
@@ -269,7 +275,7 @@ source: libs/wingc/src/lsp/completions.rs
   kind: 22
   documentation:
     kind: markdown
-    value: "```wing\nstruct QueueProps\n```\n---\nOptions for `Queue`.\n### Fields\n- `retentionPeriod?` — `duration?` — How long a queue retains a message.\n- `timeout?` — `duration?` — How long a queue's consumers have to process a message."
+    value: "```wing\nstruct QueueProps\n```\n---\nOptions for `Queue`.\n### Fields\n- `dlq?` — `DeadLetterQueueProps?` — A dead-letter queue.\n- `retentionPeriod?` — `duration?` — How long a queue retains a message.\n- `timeout?` — `duration?` — How long a queue's consumers have to process a message."
   sortText: hh|QueueProps
 - label: QueueSetConsumerOptions
   kind: 22
@@ -323,7 +329,7 @@ source: libs/wingc/src/lsp/completions.rs
   kind: 22
   documentation:
     kind: markdown
-    value: "```wing\nstruct TopicSubscribeQueueOptions extends QueueProps\n```\n---\nOptions for `Topic.subscribeQueue`.\n### Fields\n- `retentionPeriod?` — `duration?`\n- `timeout?` — `duration?`"
+    value: "```wing\nstruct TopicSubscribeQueueOptions extends QueueProps\n```\n---\nOptions for `Topic.subscribeQueue`.\n### Fields\n- `dlq?` — `DeadLetterQueueProps?`\n- `retentionPeriod?` — `duration?`\n- `timeout?` — `duration?`"
   sortText: hh|TopicSubscribeQueueOptions
 - label: WebsiteDomainOptions
   kind: 22

libs/wingc/src/lsp/snapshots/completions/new_expression_nested.snap
@@ -82,7 +82,7 @@ source: libs/wingc/src/lsp/completions.rs
   kind: 7
   documentation:
     kind: markdown
-    value: "```wing\nclass Queue\n```\n---\nA queue.\n\n### Initializer\n- `...props` — `QueueProps?`\n  \n  - `retentionPeriod?` — `duration?` — How long a queue retains a message.\n  - `timeout?` — `duration?` — How long a queue's consumers have to process a message.\n### Fields\n- `node` — `Node` — The tree node.\n### Methods\n- `approxSize` — `inflight (): num` — Retrieve the approximate number of messages in the queue.\n- `isConstruct` — `preflight (x: any): bool` — Checks if `x` is a construct.\n- `onLift` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this resource inflight.\n- `onLiftType` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this type inflight.\n- `pop` — `inflight (): str?` — Pop a message from the queue.\n- `purge` — `inflight (): void` — Purge all of the messages in the queue.\n- `push` — `inflight (...messages: Array<str>?): void` — Push one or more messages to the queue.\n- `setConsumer` — `preflight (handler: inflight (message: str): void, props: QueueSetConsumerOptions?): Function` — Create a function to consume messages from this queue.\n- `toString` — `preflight (): str` — Returns a string representation of this construct."
+    value: "```wing\nclass Queue\n```\n---\nA queue.\n\n### Initializer\n- `...props` — `QueueProps?`\n  \n  - `dlq?` — `DeadLetterQueueProps?` — A dead-letter queue.\n  - `retentionPeriod?` — `duration?` — How long a queue retains a message.\n  - `timeout?` — `duration?` — How long a queue's consumers have to process a message.\n### Fields\n- `node` — `Node` — The tree node.\n### Methods\n- `approxSize` — `inflight (): num` — Retrieve the approximate number of messages in the queue.\n- `isConstruct` — `preflight (x: any): bool` — Checks if `x` is a construct.\n- `onLift` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this resource inflight.\n- `onLiftType` — `preflight (host: IInflightHost, ops: Array<str>): void` — A hook called by the Wing compiler once for each inflight host that needs to use this type inflight.\n- `pop` — `inflight (): str?` — Pop a message from the queue.\n- `purge` — `inflight (): void` — Purge all of the messages in the queue.\n- `push` — `inflight (...messages: Array<str>?): void` — Push one or more messages to the queue.\n- `setConsumer` — `preflight (handler: inflight (message: str): void, props: QueueSetConsumerOptions?): Function` — Create a function to consume messages from this queue.\n- `toString` — `preflight (): str` — Returns a string representation of this construct."
   sortText: gg|Queue
   insertText: Queue($1)
   insertTextFormat: 2

commit 65d66c22a82d305b44f436aa3f809b3ede06147c

fix(sdk): cloud.Queue does not send messages in parallel (#6280)
Fixes a regression from #6060 that prevented messages from being processed in parallel (we noticed this bug when it caused test failures in one of our winglibs: https://github.com/winglang/winglibs/actions/runs/8767429765/job/24060674878).

I confirmed the updated test (set_consumer.test.w) passes when ran on my AWS account using the `tf-aws` target.

## Checklist

- [x] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
- [x] Description explains motivation and solution
- [x] Tests added (always)
- [ ] Docs updated (only required for features)
- [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

libs/wingsdk/src/target-sim/queue.inflight.ts
@@ -201,7 +201,7 @@ export class Queue
 
         // we don't use invokeAsync here because we want to wait for the function to finish
         // and requeue the messages if it fails
-        await fnClient
+        void fnClient
           .invoke(JSON.stringify({ messages: messages }))
           .then((result) => {
             if (this.dlq && result) {

examples/tests/sdk_tests/queue/set_consumer.test.w
@@ -31,3 +31,27 @@ test "function can push back to the queue" {
   q2.push("hello");
   util.waitUntil(inflight () => { return c2.peek() >= 2; });
 }
+
+let q3 = new cloud.Queue() as "q3";
+let c3 = new cloud.Counter() as "c3";
+
+q3.setConsumer(inflight (message) => {
+  util.sleep(10s);
+  c3.inc();
+});
+
+test "messages pushed to queue can be processed concurrently" {
+  let t1 = datetime.utcNow();
+  q3.push("message1");
+  q3.push("message2");
+  q3.push("message3");
+
+  util.waitUntil(inflight () => { return c3.peek() == 3; });
+
+  let t2 = datetime.utcNow();
+  let elapsed = duration.fromMilliseconds(t2.timestampMs - t1.timestampMs);
+
+  // If the messages were processed concurrently, the elapsed time should be less than 20s.
+  // Note: even though there is only one consumer, the consumer's default concurrency is more than 1.
+  assert(elapsed.seconds < 20, "Messages were likely not processed concurrently");
+}