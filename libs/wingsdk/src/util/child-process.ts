89bf7b17c490bd5ea2890b5e76e4f0293cbfb4ec
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2024-04-21 17:35:31 +0200

    remove debugging logs and comments

--- libs/wingsdk/src/util/child-process.ts
+++ libs/wingsdk/src/util/child-process.ts
@@ -49,34 +49,9 @@ export class ChildProcess {
 
     // Spawn the child process with the provided options
     this.child = spawn(program, args, spawnOpts);
-
     this.pid = this.child.pid;
-    console.log(`Spawned child process with PID: ${this.pid}`);
-
-    // // If stdio for stdout is set to `pipe`, listen for data events on stdout.
-    // if (this.child.stdout) {
-    //   this.child.stdout.on("data", (data: Buffer) => {
-    //     console.log("Received stdout data:", data.toString());
-    //     this.stdout += data.toString();
-    //   });
-    // }
-
-    // // // If stdio for stderr is set to `pipe`, listen for data events on stderr.
-    // if (this.child.stderr) {
-    //   this.child.stderr.on("data", (data: Buffer) => {
-    //     console.log("Received stderr data:", data.toString());
-    //     this.stderr += data.toString();
-    //   });
-    // }
-
-    // this.child.on("error", (error: Error) => {
-    //   console.error(
-    //     `Failed to start child process for ${program}: ${error.message}`
-    //   );
-    // });
 
     this.child.on("exit", (code) => {
-      console.log(`Child process exited with code: ${code}`);
       this.exitStatus = code;
     });
   }
