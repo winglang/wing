d61654606c97018a2436bde7149212510e2c0678
Marcio Cruz de Almeida <marciocadev@gmail.com>
2024-04-21 08:40:12 -0300

    refactoring

--- libs/wingsdk/src/target-sim/topic.inflight.ts
+++ libs/wingsdk/src/target-sim/topic.inflight.ts
@@ -15,12 +15,11 @@ import {
 import { TraceType } from "../std";
 
 export class Topic
-  implements ITopicClient, ISimulatorResourceInstance, IEventPublisher
-{
+  implements ITopicClient, ISimulatorResourceInstance, IEventPublisher {
   private readonly subscribers = new Array<TopicSubscriber>();
   private _context: ISimulatorContext | undefined;
 
-  constructor(_props: TopicSchema) {}
+  constructor(_props: TopicSchema) { }
 
   private get context(): ISimulatorContext {
     if (!this._context) {
@@ -34,9 +33,9 @@ export class Topic
     return {};
   }
 
-  public async cleanup(): Promise<void> {}
+  public async cleanup(): Promise<void> { }
 
-  public async save(): Promise<void> {}
+  public async save(): Promise<void> { }
 
   public async plan() {
     return UpdatePlan.AUTO;
@@ -88,12 +87,12 @@ export class Topic
         if (messages.includes("")) {
           throw new Error("Empty messages are not allowed");
         }
-        let publishMsgList: Array<Promise<void>> = [];
+        let publishAll: Array<Promise<void>> = [];
         for (const message of messages) {
-          publishMsgList.push(this.publishMessage(message));
+          publishAll.push(this.publishMessage(message));
         }
 
-        return Promise.all(publishMsgList);
+        return Promise.all(publishAll);
       },
     });
   }
