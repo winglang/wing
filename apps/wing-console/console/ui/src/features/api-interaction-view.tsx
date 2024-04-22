eca7c9b9ce50fc5b792a7945b03a28e47117fa17
Pol Amorós <info@polamoros.com>
2024-04-22 17:13:47 +0200

    fix(console): tsoa ui http client is not maintaining state (#6288)
    Resolves #6287 
    
    ## Checklist
    
    - [ ] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
    - [ ] Description explains motivation and solution
    - [ ] Tests added (always)
    - [ ] Docs updated (only required for features)
    - [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

--- apps/wing-console/console/ui/src/features/api-interaction-view.tsx
+++ apps/wing-console/console/ui/src/features/api-interaction-view.tsx
@@ -1,4 +1,5 @@
 import type { OpenApiSpec } from "@wingconsole/server/src/wingsdk";
+import { createPersistentState } from "@wingconsole/use-persistent-state";
 import { memo, useCallback, useContext, useState } from "react";
 
 import { AppContext } from "../AppContext.js";
@@ -13,11 +14,19 @@ export interface ApiViewProps {
 
 export const ApiInteractionView = memo(({ resourcePath }: ApiViewProps) => {
   const { appMode } = useContext(AppContext);
+  const { usePersistentState } = createPersistentState(resourcePath);
 
-  const [apiResponse, setApiResponse] = useState<ApiResponse>();
+  const [apiResponse, setApiResponse] = usePersistentState<
+    ApiResponse | undefined
+  >();
   const onFetchDataUpdate = useCallback(
-    (data: ApiResponse) => setApiResponse(data),
-    [],
+    (data: ApiResponse) => {
+      if (!data) {
+        return;
+      }
+      setApiResponse(data);
+    },
+    [setApiResponse],
   );
 
   const schema = trpc["api.schema"].useQuery({ resourcePath });
@@ -35,6 +44,7 @@ export const ApiInteractionView = memo(({ resourcePath }: ApiViewProps) => {
       callFetch={callFetch}
       isLoading={isLoading}
       apiResponse={apiResponse}
+      resetApiResponse={() => setApiResponse(undefined)}
     />
   );
 });
