f1043e39df67f14ad22f97b5a0c788b6136cf66a
Cristian Pallarés <cristianp@wing.cloud>
2024-04-22 11:24:23 +0200

    fix: conflicting `SIGINT` handlers
    There were two competing `SIGINT` handlers and the result was unreleased Console resources.
    
    This PR removes the `conf` package (which was registering a `SIGINT` handler) in favor of simple read and write methods for the config file.
    
    Fixes #6018.

--- apps/wing-console/console/server/src/utils/terms-and-conditions.ts
+++ apps/wing-console/console/server/src/utils/terms-and-conditions.ts
@@ -1,5 +1,6 @@
-// @ts-ignore-next-line
-import Conf from "conf";
+import * as fs from "node:fs";
+
+import envPaths from "env-paths";
 
 // @ts-ignore-next-line
 import License from "../../LICENSE.md?raw";
@@ -7,21 +8,40 @@ import License from "../../LICENSE.md?raw";
 const PROJECT_NAME = "@wingconsole/server";
 const CONFIG_KEY = "termsAndConditions";
 
-export const isTermsAccepted = (): boolean => {
-  const config = new Conf({
-    projectName: PROJECT_NAME,
-  });
+const paths = envPaths(PROJECT_NAME);
 
-  const accepted = config.get(CONFIG_KEY) as boolean;
+const getConfig = () => {
+  try {
+    return JSON.stringify(
+      fs.readFileSync(paths.config, "utf8"),
+    ) as unknown as Record<string, any>;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const saveConfig = (config: Record<string, any>) => {
+  try {
+    fs.writeFileSync(paths.config, JSON.stringify(config, undefined, 2));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const isTermsAccepted = (): boolean => {
+  const config = getConfig();
+
+  const accepted = config?.[CONFIG_KEY];
   return accepted === true;
 };
 
 export const acceptTerms = (value: boolean) => {
-  const config = new Conf({
-    projectName: PROJECT_NAME,
-  });
+  const config = getConfig();
 
-  config.set(CONFIG_KEY, value);
+  saveConfig({
+    ...config,
+    [CONFIG_KEY]: value,
+  });
 };
 
 export const getLicense = (): string => {
