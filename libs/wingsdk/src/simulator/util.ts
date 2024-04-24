d21d3ea0fd4195233a3669cdad93cee77343c2cb
Chris Rybicki <chrisr@monada.co>
2024-04-24 16:32:09 -0400

    try ruling this out maybe

--- libs/wingsdk/src/simulator/util.ts
+++ libs/wingsdk/src/simulator/util.ts
@@ -1,33 +1,33 @@
-import { access, constants } from "node:fs";
-import { promisify } from "node:util";
+// import { access, constants } from "node:fs";
+// import { promisify } from "node:util";
 
-/**
- * Check if a file exists for an specific path
- * @param filePath
- * @Returns Return `true` if the file exists, `false` otherwise.
- */
-export async function exists(filePath: string): Promise<boolean> {
-  try {
-    await promisify(access)(filePath, constants.F_OK);
-    return true;
-  } catch (er) {
-    return false;
-  }
-}
+// /**
+//  * Check if a file exists for an specific path
+//  * @param filePath
+//  * @Returns Return `true` if the file exists, `false` otherwise.
+//  */
+// export async function exists(filePath: string): Promise<boolean> {
+//   try {
+//     await promisify(access)(filePath, constants.F_OK);
+//     return true;
+//   } catch (er) {
+//     return false;
+//   }
+// }
 
-/**
- * Check if a file exists and if we have read and write permissions to it
- * @param filePath
- * @Returns Return `true` if the file exists, `false` otherwise.
- */
-export async function existsReadWrite(filePath: string): Promise<boolean> {
-  try {
-    await promisify(access)(
-      filePath,
-      constants.F_OK | constants.R_OK | constants.W_OK //eslint-disable-line no-bitwise
-    );
-    return true;
-  } catch (er) {
-    return false;
-  }
-}
+// /**
+//  * Check if a file exists and if we have read and write permissions to it
+//  * @param filePath
+//  * @Returns Return `true` if the file exists, `false` otherwise.
+//  */
+// export async function existsReadWrite(filePath: string): Promise<boolean> {
+//   try {
+//     await promisify(access)(
+//       filePath,
+//       constants.F_OK | constants.R_OK | constants.W_OK //eslint-disable-line no-bitwise
+//     );
+//     return true;
+//   } catch (er) {
+//     return false;
+//   }
+// }
