5927a85af0334b87bb5117e0a697cb689df6d7d0
Elad Cohen <eladc@monada.co>
2024-05-06 13:00:26 +0300

    wip

--- apps/wing-console/console/ui/src/ui/custom-resource-ui-field.tsx
+++ apps/wing-console/console/ui/src/ui/custom-resource-ui-field.tsx
@@ -1,15 +1,19 @@
+import { ShareIcon } from "@heroicons/react/24/outline";
 import { Attribute } from "@wingconsole/design-system";
+import { Link } from "@wingconsole/design-system";
 
 import { trpc } from "../services/trpc.js";
 
 export interface CustomResourceUiFieldItemProps {
   label: string;
   handlerPath: string;
+  link: boolean;
 }
 
 export const CustomResourceUiFieldItem = ({
   label,
   handlerPath,
+  link,
 }: CustomResourceUiFieldItemProps) => {
   const field = trpc["uiField.get"].useQuery(
     {
@@ -17,5 +21,14 @@ export const CustomResourceUiFieldItem = ({
     },
     { enabled: !!handlerPath },
   );
-  return <Attribute name={label} value={field.data?.value ?? ""} />;
+  const fieldValue = field.data?.value ?? "";
+  return (
+    <Attribute name={label} value={link ? undefined : fieldValue}>
+      {link && (
+        <Link id={label} href={fieldValue}>
+          <ShareIcon title={fieldValue} className="w-4 h-4 text-blue-500" />
+        </Link>
+      )}
+    </Attribute>
+  );
 };
