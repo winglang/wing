d63f05f13359fe612d7e9e6d82cdbedbdd156994
Mark McCulloh <Mark.McCulloh@gmail.com>
2024-05-07 08:53:41 -0400

    more unify

--- libs/wingsdk/src/shared-gcp/bucket.inflight.ts
+++ libs/wingsdk/src/shared-gcp/bucket.inflight.ts
@@ -119,10 +119,16 @@ export class BucketClient implements IBucketClient {
   }
 
   public async get(key: string, options?: BucketGetOptions): Promise<string> {
+    const body = await this.bucket
+      .file(key)
+      .download({ start: options?.startByte, end: options?.endByte })
+      .catch((e) => {
+        throw new Error(
+          `Failed to get object (key=${key}): ${(e as Error).stack})}`
+        );
+      });
+
     try {
-      const body = await this.bucket
-        .file(key)
-        .download({ start: options?.startByte, end: options?.endByte });
       return new TextDecoder("utf8", { fatal: true }).decode(body[0]);
     } catch (e) {
       throw new Error(
