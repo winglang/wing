b450581ed58a2ab048a6561011eaafa3155e460b
Ainvoner <shaia@monada.co>
2024-05-09 12:25:02 +0300

    fix(console): open link icon does not work
    resolves: https://github.com/winglang/wing/issues/6227

--- apps/wing-console/console/ui/src/ui/website-interaction.tsx
+++ apps/wing-console/console/ui/src/ui/website-interaction.tsx
@@ -1,20 +1,15 @@
-import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid";
-import { Attribute, useTheme } from "@wingconsole/design-system";
-import classNames from "classnames";
+import { Attribute } from "@wingconsole/design-system";
 
 import type { AppMode } from "../AppContext.js";
 
 export interface WebsiteInteractionProps {
   url: string;
   appMode: AppMode;
-  onUrlClick: (url: string) => void;
 }
 export const WebsiteInteraction = ({
   appMode,
   url,
-  onUrlClick,
 }: WebsiteInteractionProps) => {
-  const { theme } = useTheme();
   return (
     <div className="h-full flex-1 flex flex-col text-sm space-y-1">
       <div className="relative grow flex-row flex items-center">
@@ -22,17 +17,11 @@ export const WebsiteInteraction = ({
           <>
             <Attribute
               name="URL"
+              type={"url"}
               value={url}
+              url={url}
               noLeftPadding
-              dataTestId="cloud.website:url"
-            />
-            <ArrowTopRightOnSquareIcon
-              className={classNames(
-                theme.text2,
-                "text-sm flex ml-2 h-4 w-4 cursor-pointer",
-              )}
-              onClick={() => onUrlClick(url)}
-              data-testid="cloud.website:open-url"
+              dataTestId="cloud.website:open-url"
             />
           </>
         )}
