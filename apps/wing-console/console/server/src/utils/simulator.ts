a4fd22ebb7491418a909621d1747db5f3758554e
Chris Rybicki <chrisr@monada.co>
2024-05-21 16:16:18 -0700

    chore: update simulator updates in Wing Console by default

--- apps/wing-console/console/server/src/utils/simulator.ts
+++ apps/wing-console/console/server/src/utils/simulator.ts
@@ -25,7 +25,6 @@ export interface Simulator {
 
 export interface CreateSimulatorProps {
   stateDir?: string;
-  enableSimUpdates?: boolean;
 }
 
 const stopSilently = async (simulator: simulator.Simulator) => {
@@ -50,16 +49,10 @@ export const createSimulator = (props?: CreateSimulatorProps): Simulator => {
     if (!instance) {
       return true;
     }
-    if (props?.enableSimUpdates) {
-      await events.emit("starting", { instance });
-      await instance.update(simfile);
-      await events.emit("started");
-      return false;
-    } else {
-      await events.emit("stopping");
-      await stopSilently(instance);
-      return true;
-    }
+    await events.emit("starting", { instance });
+    await instance.update(simfile);
+    await events.emit("started");
+    return false;
   };
   const start = async (simfile: string) => {
     try {
