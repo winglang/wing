727f9f11513621eb876a95f951d9422079c13d2e
polamoros <info@polamoros.com>
2024-05-24 09:51:29 +0200

    wip

--- apps/wing-console/console/ui/src/ui/blue-screen-of-death.tsx
+++ apps/wing-console/console/ui/src/ui/blue-screen-of-death.tsx
@@ -1,5 +1,4 @@
-import { DocumentDuplicateIcon } from "@heroicons/react/24/outline";
-import { Button, useNotifications } from "@wingconsole/design-system";
+import { useNotifications } from "@wingconsole/design-system";
 import classNames from "classnames";
 import { memo, useCallback, useEffect, useState } from "react";
 
@@ -43,42 +42,32 @@ export const BlueScreenOfDeath = memo(
     return (
       <div
         className={classNames(
-          "absolute inset-0",
-          "px-10 pb-4 bg-[#004295] flex justify-center items-center",
+          "absolute h-full w-full z-50 px-10 py-20 bg-[#004295] overflow-auto flex justify-center items-center",
         )}
         data-testid="blue-screen-of-death"
       >
-        <div className="relative w-full h-full text-md font-share-tech text-white flex flex-col max-w-7xl break-words space-y-4">
-          <div className="flex-grow overflow-hidden flex flex-col">
-            <OpenFileInEditorButton className="cursor-text select-text flex-grow flex flex-col h-full">
-              <div className="py-10 overflow-auto h-full">
-                <div className="flex mb-4">
-                  <div className="bg-slate-400 px-4 text-[#004295]">
-                    {title}
-                  </div>
-                </div>
+        <div className="h-full w-full text-md font-share-tech text-white max-w-7xl break-words space-y-4">
+          <div className="space-y-4">
+            <div className="flex">
+              <button
+                onClick={copyError}
+                className={classNames(
+                  "px-4",
+                  "bg-slate-400 hover:bg-slate-450 text-[#004295]",
+                  "focus:ring-2 focus:ring-sky-500/50 focus:border-sky-500 outline-none",
+                )}
+              >
+                Click here to copy the error
+              </button>
+            </div>
+            <OpenFileInEditorButton className="cursor-text select-text">
+              <div className="py-4">
                 <span
                   className="outline-none whitespace-pre-wrap"
                   dangerouslySetInnerHTML={{ __html: formattedPathsError }}
                 />
               </div>
             </OpenFileInEditorButton>
-          </div>
-
-          <div className="flex flex-col items-center gap-y-4 flex-shrink-0">
-            <button
-              onClick={copyError}
-              className={classNames(
-                "inline-flex gap-2 items-center",
-                "font-sans px-2.5 py-1.5 text-xs rounded",
-                "bg-white hover:bg-slate-50 text-slate-900",
-                "focus:ring-2 focus:ring-sky-500/50 focus:border-sky-500 outline-none",
-                "border border-slate-300",
-              )}
-            >
-              <DocumentDuplicateIcon className="-ml-0.5 size-4" />
-              Copy error
-            </button>
             {displayLinks && (
               <div className="w-full text-center">
                 Click on any error reference to navigate to your IDE{" "}
