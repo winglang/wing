c6285e52a440330b7806f2eefed5a5d7574f969d
Chris Rybicki <chrisr@monada.co>
2024-05-29 16:42:10 -0400

    fix missing toInflightType

--- libs/wingsdk/src/target-sim/service.ts
+++ libs/wingsdk/src/target-sim/service.ts
@@ -3,7 +3,11 @@ import { Construct } from "constructs";
 import { ISimulatorInflightHost, ISimulatorResource } from "./resource";
 import { ServiceSchema } from "./schema-resources";
 import { simulatorHandleToken } from "./tokens";
-import { bindSimulatorResource, simulatorLiftedFieldsFor } from "./util";
+import {
+  bindSimulatorResource,
+  makeSimulatorJsClientType,
+  simulatorLiftedFieldsFor,
+} from "./util";
 import * as cloud from "../cloud";
 import { App, LiftMap } from "../core";
 import { PolicyStatement, ToSimulatorOutput } from "../simulator";
@@ -13,6 +17,11 @@ export class Service
   extends cloud.Service
   implements ISimulatorResource, ISimulatorInflightHost
 {
+  /** @internal */
+  public static _toInflightType(): string {
+    return makeSimulatorJsClientType("Service", cloud.Service._methods);
+  }
+
   private readonly permissions: Array<[IResource, string]> = [];
   private readonly autoStart: boolean;
 
