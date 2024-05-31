c2f5999efdbe6ee0188735c4cef6b16f9422e901
polamoros <info@polamoros.com>
2024-05-31 10:56:19 +0200

    wipg

--- apps/wing-console/console/app/test/login/login.test.ts
+++ apps/wing-console/console/app/test/login/login.test.ts
@@ -5,9 +5,33 @@ import { describe } from "../describe.js";
 describe(
   `${__dirname}/main.w`,
   () => {
-    test("Check login page", async ({ page }) => {
+    test("Sign in modal is visible when required", async ({ page }) => {
+      const signinModal = page.getByTestId("signin-modal");
+      await expect(signinModal).toBeVisible();
+    });
+  },
+  { requireSignIn: true },
+);
+
+describe(
+  `${__dirname}/main.w`,
+  () => {
+    test("GitHub button is clickable", async ({ page }) => {
       const githubLogin = page.getByTestId("signin-github-button");
       await expect(githubLogin).toBeVisible();
+      await githubLogin.click();
+    });
+  },
+  { requireSignIn: true },
+);
+
+describe(
+  `${__dirname}/main.w`,
+  () => {
+    test("Google button is clickable", async ({ page }) => {
+      const githubLogin = page.getByTestId("signin-google-button");
+      await expect(githubLogin).toBeVisible();
+      await githubLogin.click();
     });
   },
   { requireSignIn: true },
