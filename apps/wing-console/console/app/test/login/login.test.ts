213ff7875f1882e4fa700b81078037e70cd63b81
polamoros <info@polamoros.com>
2024-05-31 11:39:04 +0200

    wio

--- apps/wing-console/console/app/test/login/login.test.ts
+++ apps/wing-console/console/app/test/login/login.test.ts
@@ -17,9 +17,10 @@ describe(
   `${__dirname}/main.w`,
   () => {
     test("GitHub button is clickable", async ({ page }) => {
-      const githubLogin = page.getByTestId("signin-github-button");
-      await expect(githubLogin).toBeVisible();
-      await githubLogin.click();
+      const githubLoginButton = page.getByTestId("signin-github-button");
+      await expect(githubLoginButton).toBeVisible();
+      await expect(githubLoginButton).toBeEnabled();
+      await githubLoginButton.click();
     });
   },
   { requireSignIn: true },
@@ -29,9 +30,10 @@ describe(
   `${__dirname}/main.w`,
   () => {
     test("Google button is clickable", async ({ page }) => {
-      const githubLogin = page.getByTestId("signin-google-button");
-      await expect(githubLogin).toBeVisible();
-      await githubLogin.click();
+      const googleSignInButton = page.getByTestId("signin-google-button");
+      await expect(googleSignInButton).toBeVisible();
+      await expect(googleSignInButton).toBeEnabled();
+      await googleSignInButton.click();
     });
   },
   { requireSignIn: true },
