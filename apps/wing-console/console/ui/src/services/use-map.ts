winglang/wing

commit af69dda359e79bf2b49bacd4539d0683cf9cfe63

feat: show connections for token references (#6602)
Resolves #6433 by adding a connection edge when resources reference each other through tokens (e.g. a function references `api.url`). The fact that these are implicit references doesn't mean they are not references...

## Console UI Tweaks

* Remove the `()` from the connection operations because oftentimes they do not represent an actual method name. If we want to distinguish between operations and other types of connections, we should add this to the model and then display.
* Only show the operation when there's a `sourceOp` in the connection. Do not default to the connection name.
* In the navigation pane, change the label from "Access" to "Connections".
* Restore connections for `invokeAsync()`. Not sure why they were removed (resolves #6607)
* Change the node title of topics created for bucket notifications to `OnXxx` instead of `onxxx` (all lowercase).
* Change the lift-based connection name to `call`.
* Change the title for the queue consumer function to `Consumer` instead of `SetConsumer()`, also `Tick` (for schedules), `Subscriber` (for topics).

## Misc

* Make the `source` property of `nodeof(x).addConnection()` optional and default to `this`.

## Checklist

- [ ] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
- [ ] Description explains motivation and solution
- [ ] Tests added (always)
- [ ] Docs updated (only required for features)
- [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

apps/wing-console/console/ui/src/services/use-map.ts
@@ -229,12 +229,6 @@ export const useMap = ({ expandedItems }: UseMapOptions) => {
       connections:
         rawConnections
           .filter((connection) => {
-            return (
-              connection.sourceOp !== "invokeAsync" &&
-              connection.targetOp !== "invokeAsync"
-            );
-          })
-          .filter((connection) => {
             return connection.source !== connection.target;
           })
           .map((connection) => {

apps/wing-console/console/ui/src/features/map-view.tsx
@@ -352,7 +352,7 @@ const ConstructNode: FunctionComponent<PropsWithChildren<ConstructNodeProps>> =
                             "font-mono",
                           )}
                         >
-                          <span>{inflight.name}()</span>
+                          <span>{inflight.name}</span>
                         </div>
                       </div>
 

apps/wing-console/console/ui/src/ui/edge-metadata.tsx
@@ -144,7 +144,7 @@ export const EdgeMetadata = ({
           </Attribute>
 
           <div className="border-t border-transparent">
-            <Attribute name="Access" centerLabel={false}>
+            <Attribute name="Connections" centerLabel={false}>
               <div className="w-full">
                 <Tree
                   entries={entries}

examples/tests/sdk_tests/bucket/events.test.w
@@ -29,15 +29,15 @@ let logHistory = inflight (key: str, operation: str, source: Source) => {
 
 
 b.onDelete(inflight (key: str) => {
-  logHistory(key, "onDelete()", Source.anyEvent);
+  logHistory(key, "OnDelete()", Source.anyEvent);
 });
 
 b.onUpdate(inflight (key: str) => {
-  logHistory(key, "onUpdate()", Source.anyEvent);
+  logHistory(key, "OnUpdate()", Source.anyEvent);
 });
 
 b.onCreate(inflight (key: str) => {
-  logHistory(key, "onCreate()", Source.anyEvent);
+  logHistory(key, "OnCreate()", Source.anyEvent);
 });
 
 b.onEvent(inflight (key: str, event: cloud.BucketEventType) => { 
@@ -54,6 +54,7 @@ struct CheckHitCountOptions {
 let checkHitCount = inflight (opts: CheckHitCountOptions): void => {
   util.waitUntil(inflight () => {
     let var count = 0;
+
     for u in table.list() {
       if (u.get("key") == opts.key && u.get("operation") == opts.type && u.get("source") == "{opts.source}") {
         count = count + 1;
@@ -74,19 +75,19 @@ new std.Test(inflight () => {
 // https://github.com/winglang/wing/issues/2724
   if (util.env("WING_TARGET") != "tf-aws") {
     // assert that onCreate events about the "a", "b", and "c" objects were each produced exactly 1 time
-    checkHitCount(key: "a", type: "onCreate()", source: Source.anyEvent, count: 1);
-    checkHitCount(key: "b", type: "onCreate()", source: Source.anyEvent, count: 1);
-    checkHitCount(key: "c", type: "onCreate()", source: Source.anyEvent, count: 1);
+    checkHitCount(key: "a", type: "OnCreate()", source: Source.anyEvent, count: 1);
+    checkHitCount(key: "b", type: "OnCreate()", source: Source.anyEvent, count: 1);
+    checkHitCount(key: "c", type: "OnCreate()", source: Source.anyEvent, count: 1);
 
-    checkHitCount(key: "a", type: "onCreate()", source: Source.onEvent, count: 1);
-    checkHitCount(key: "b", type: "onCreate()", source:  Source.onEvent, count: 1);
-    checkHitCount(key: "c", type: "onCreate()", source:  Source.onEvent, count: 1);
+    checkHitCount(key: "a", type: "OnCreate()", source: Source.onEvent, count: 1);
+    checkHitCount(key: "b", type: "OnCreate()", source:  Source.onEvent, count: 1);
+    checkHitCount(key: "c", type: "OnCreate()", source:  Source.onEvent, count: 1);
 
-    checkHitCount(key: "b", type: "onUpdate()", source: Source.anyEvent, count: 1);
-    checkHitCount(key: "c", type: "onDelete()", source: Source.anyEvent, count: 1);
+    checkHitCount(key: "b", type: "OnUpdate()", source: Source.anyEvent, count: 1);
+    checkHitCount(key: "c", type: "OnDelete()", source: Source.anyEvent, count: 1);
 
-    checkHitCount(key: "b", type: "onUpdate()", source: Source.onEvent, count: 1);
-    checkHitCount(key: "c", type: "onDelete()", source: Source.onEvent, count: 1);
+    checkHitCount(key: "b", type: "OnUpdate()", source: Source.onEvent, count: 1);
+    checkHitCount(key: "c", type: "OnDelete()", source: Source.onEvent, count: 1);
   }
 
 }, timeout: 8m) as "hitCount is incremented according to the bucket event";

libs/awscdk/test/__snapshots__/bucket.test.ts.snap
@@ -270,12 +270,12 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::IAM::Policy",
     },
-    "mybucketAllowBucketNotificationsTomyprojectmybucketonCreate09D19557ECA49DF15": {
+    "mybucketAllowBucketNotificationsTomyprojectmybucketOnCreate0523E7CD109C74964": {
       "Properties": {
         "Action": "lambda:InvokeFunction",
         "FunctionName": {
           "Fn::GetAtt": [
-            "mybucketonCreate0503ECF25",
+            "mybucketOnCreate071865324",
             "Arn",
           ],
         },
@@ -316,7 +316,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
     },
     "mybucketNotificationsB76B6B79": {
       "DependsOn": [
-        "mybucketAllowBucketNotificationsTomyprojectmybucketonCreate09D19557ECA49DF15",
+        "mybucketAllowBucketNotificationsTomyprojectmybucketOnCreate0523E7CD109C74964",
       ],
       "Properties": {
         "BucketName": {
@@ -331,7 +331,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
               ],
               "LambdaFunctionArn": {
                 "Fn::GetAtt": [
-                  "mybucketonCreate0503ECF25",
+                  "mybucketOnCreate071865324",
                   "Arn",
                 ],
               },
@@ -347,9 +347,9 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "Custom::S3BucketNotifications",
     },
-    "mybucketonCreate0503ECF25": {
+    "mybucketOnCreate071865324": {
       "DependsOn": [
-        "mybucketonCreate0ServiceRole7D97CBD8",
+        "mybucketOnCreate0ServiceRole8EB9AE15",
       ],
       "Properties": {
         "Architectures": [
@@ -369,13 +369,13 @@ def submit_response(event: dict, context, response_status: str, error_message: s
         "Handler": "index.handler",
         "LoggingConfig": {
           "LogGroup": {
-            "Ref": "mybucketonCreate0LogGroup56018191",
+            "Ref": "mybucketOnCreate0LogGroup8E3C4A72",
           },
         },
         "MemorySize": 1024,
         "Role": {
           "Fn::GetAtt": [
-            "mybucketonCreate0ServiceRole7D97CBD8",
+            "mybucketOnCreate0ServiceRole8EB9AE15",
             "Arn",
           ],
         },
@@ -384,7 +384,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::Lambda::Function",
     },
-    "mybucketonCreate0LogGroup56018191": {
+    "mybucketOnCreate0LogGroup8E3C4A72": {
       "DeletionPolicy": "Retain",
       "Properties": {
         "RetentionInDays": 30,
@@ -392,7 +392,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       "Type": "AWS::Logs::LogGroup",
       "UpdateReplacePolicy": "Retain",
     },
-    "mybucketonCreate0ServiceRole7D97CBD8": {
+    "mybucketOnCreate0ServiceRole8EB9AE15": {
       "Properties": {
         "AssumeRolePolicyDocument": {
           "Statement": [
@@ -627,12 +627,12 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::IAM::Policy",
     },
-    "mybucketAllowBucketNotificationsTomyprojectmybucketonDelete057F4EEBE2F8EA524": {
+    "mybucketAllowBucketNotificationsTomyprojectmybucketOnDelete07E2A93E8688C7533": {
       "Properties": {
         "Action": "lambda:InvokeFunction",
         "FunctionName": {
           "Fn::GetAtt": [
-            "mybucketonDelete091EC820E",
+            "mybucketOnDelete0BC86CC27",
             "Arn",
           ],
         },
@@ -673,7 +673,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
     },
     "mybucketNotificationsB76B6B79": {
       "DependsOn": [
-        "mybucketAllowBucketNotificationsTomyprojectmybucketonDelete057F4EEBE2F8EA524",
+        "mybucketAllowBucketNotificationsTomyprojectmybucketOnDelete07E2A93E8688C7533",
       ],
       "Properties": {
         "BucketName": {
@@ -688,7 +688,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
               ],
               "LambdaFunctionArn": {
                 "Fn::GetAtt": [
-                  "mybucketonDelete091EC820E",
+                  "mybucketOnDelete0BC86CC27",
                   "Arn",
                 ],
               },
@@ -704,9 +704,9 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "Custom::S3BucketNotifications",
     },
-    "mybucketonDelete091EC820E": {
+    "mybucketOnDelete0BC86CC27": {
       "DependsOn": [
-        "mybucketonDelete0ServiceRole20B2A547",
+        "mybucketOnDelete0ServiceRole57A03DE1",
       ],
       "Properties": {
         "Architectures": [
@@ -726,13 +726,13 @@ def submit_response(event: dict, context, response_status: str, error_message: s
         "Handler": "index.handler",
         "LoggingConfig": {
           "LogGroup": {
-            "Ref": "mybucketonDelete0LogGroupD9E8D54C",
+            "Ref": "mybucketOnDelete0LogGroup44992347",
           },
         },
         "MemorySize": 1024,
         "Role": {
           "Fn::GetAtt": [
-            "mybucketonDelete0ServiceRole20B2A547",
+            "mybucketOnDelete0ServiceRole57A03DE1",
             "Arn",
           ],
         },
@@ -741,7 +741,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::Lambda::Function",
     },
-    "mybucketonDelete0LogGroupD9E8D54C": {
+    "mybucketOnDelete0LogGroup44992347": {
       "DeletionPolicy": "Retain",
       "Properties": {
         "RetentionInDays": 30,
@@ -749,7 +749,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       "Type": "AWS::Logs::LogGroup",
       "UpdateReplacePolicy": "Retain",
     },
-    "mybucketonDelete0ServiceRole20B2A547": {
+    "mybucketOnDelete0ServiceRole57A03DE1": {
       "Properties": {
         "AssumeRolePolicyDocument": {
           "Statement": [
@@ -984,12 +984,12 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::IAM::Policy",
     },
-    "mybucketAllowBucketNotificationsTomyprojectmybucketonCreate09D19557ECA49DF15": {
+    "mybucketAllowBucketNotificationsTomyprojectmybucketOnCreate0523E7CD109C74964": {
       "Properties": {
         "Action": "lambda:InvokeFunction",
         "FunctionName": {
           "Fn::GetAtt": [
-            "mybucketonCreate0503ECF25",
+            "mybucketOnCreate071865324",
             "Arn",
           ],
         },
@@ -1006,12 +1006,12 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::Lambda::Permission",
     },
-    "mybucketAllowBucketNotificationsTomyprojectmybucketonDelete057F4EEBE2F8EA524": {
+    "mybucketAllowBucketNotificationsTomyprojectmybucketOnDelete07E2A93E8688C7533": {
       "Properties": {
         "Action": "lambda:InvokeFunction",
         "FunctionName": {
           "Fn::GetAtt": [
-            "mybucketonDelete091EC820E",
+            "mybucketOnDelete0BC86CC27",
             "Arn",
           ],
         },
@@ -1028,12 +1028,12 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::Lambda::Permission",
     },
-    "mybucketAllowBucketNotificationsTomyprojectmybucketonUpdate0DA830360476798FF": {
+    "mybucketAllowBucketNotificationsTomyprojectmybucketOnUpdate036B5F47FED7981DE": {
       "Properties": {
         "Action": "lambda:InvokeFunction",
         "FunctionName": {
           "Fn::GetAtt": [
-            "mybucketonUpdate02A38D403",
+            "mybucketOnUpdate06503D6F0",
             "Arn",
           ],
         },
@@ -1074,9 +1074,9 @@ def submit_response(event: dict, context, response_status: str, error_message: s
     },
     "mybucketNotificationsB76B6B79": {
       "DependsOn": [
-        "mybucketAllowBucketNotificationsTomyprojectmybucketonCreate09D19557ECA49DF15",
-        "mybucketAllowBucketNotificationsTomyprojectmybucketonDelete057F4EEBE2F8EA524",
-        "mybucketAllowBucketNotificationsTomyprojectmybucketonUpdate0DA830360476798FF",
+        "mybucketAllowBucketNotificationsTomyprojectmybucketOnCreate0523E7CD109C74964",
+        "mybucketAllowBucketNotificationsTomyprojectmybucketOnDelete07E2A93E8688C7533",
+        "mybucketAllowBucketNotificationsTomyprojectmybucketOnUpdate036B5F47FED7981DE",
       ],
       "Properties": {
         "BucketName": {
@@ -1091,7 +1091,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
               ],
               "LambdaFunctionArn": {
                 "Fn::GetAtt": [
-                  "mybucketonCreate0503ECF25",
+                  "mybucketOnCreate071865324",
                   "Arn",
                 ],
               },
@@ -1102,7 +1102,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
               ],
               "LambdaFunctionArn": {
                 "Fn::GetAtt": [
-                  "mybucketonDelete091EC820E",
+                  "mybucketOnDelete0BC86CC27",
                   "Arn",
                 ],
               },
@@ -1113,7 +1113,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
               ],
               "LambdaFunctionArn": {
                 "Fn::GetAtt": [
-                  "mybucketonUpdate02A38D403",
+                  "mybucketOnUpdate06503D6F0",
                   "Arn",
                 ],
               },
@@ -1129,9 +1129,9 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "Custom::S3BucketNotifications",
     },
-    "mybucketonCreate0503ECF25": {
+    "mybucketOnCreate071865324": {
       "DependsOn": [
-        "mybucketonCreate0ServiceRole7D97CBD8",
+        "mybucketOnCreate0ServiceRole8EB9AE15",
       ],
       "Properties": {
         "Architectures": [
@@ -1151,13 +1151,13 @@ def submit_response(event: dict, context, response_status: str, error_message: s
         "Handler": "index.handler",
         "LoggingConfig": {
           "LogGroup": {
-            "Ref": "mybucketonCreate0LogGroup56018191",
+            "Ref": "mybucketOnCreate0LogGroup8E3C4A72",
           },
         },
         "MemorySize": 1024,
         "Role": {
           "Fn::GetAtt": [
-            "mybucketonCreate0ServiceRole7D97CBD8",
+            "mybucketOnCreate0ServiceRole8EB9AE15",
             "Arn",
           ],
         },
@@ -1166,7 +1166,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::Lambda::Function",
     },
-    "mybucketonCreate0LogGroup56018191": {
+    "mybucketOnCreate0LogGroup8E3C4A72": {
       "DeletionPolicy": "Retain",
       "Properties": {
         "RetentionInDays": 30,
@@ -1174,7 +1174,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       "Type": "AWS::Logs::LogGroup",
       "UpdateReplacePolicy": "Retain",
     },
-    "mybucketonCreate0ServiceRole7D97CBD8": {
+    "mybucketOnCreate0ServiceRole8EB9AE15": {
       "Properties": {
         "AssumeRolePolicyDocument": {
           "Statement": [
@@ -1205,9 +1205,9 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::IAM::Role",
     },
-    "mybucketonDelete091EC820E": {
+    "mybucketOnDelete0BC86CC27": {
       "DependsOn": [
-        "mybucketonDelete0ServiceRole20B2A547",
+        "mybucketOnDelete0ServiceRole57A03DE1",
       ],
       "Properties": {
         "Architectures": [
@@ -1227,13 +1227,13 @@ def submit_response(event: dict, context, response_status: str, error_message: s
         "Handler": "index.handler",
         "LoggingConfig": {
           "LogGroup": {
-            "Ref": "mybucketonDelete0LogGroupD9E8D54C",
+            "Ref": "mybucketOnDelete0LogGroup44992347",
           },
         },
         "MemorySize": 1024,
         "Role": {
           "Fn::GetAtt": [
-            "mybucketonDelete0ServiceRole20B2A547",
+            "mybucketOnDelete0ServiceRole57A03DE1",
             "Arn",
           ],
         },
@@ -1242,7 +1242,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::Lambda::Function",
     },
-    "mybucketonDelete0LogGroupD9E8D54C": {
+    "mybucketOnDelete0LogGroup44992347": {
       "DeletionPolicy": "Retain",
       "Properties": {
         "RetentionInDays": 30,
@@ -1250,7 +1250,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       "Type": "AWS::Logs::LogGroup",
       "UpdateReplacePolicy": "Retain",
     },
-    "mybucketonDelete0ServiceRole20B2A547": {
+    "mybucketOnDelete0ServiceRole57A03DE1": {
       "Properties": {
         "AssumeRolePolicyDocument": {
           "Statement": [
@@ -1281,9 +1281,9 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::IAM::Role",
     },
-    "mybucketonUpdate02A38D403": {
+    "mybucketOnUpdate06503D6F0": {
       "DependsOn": [
-        "mybucketonUpdate0ServiceRole6E92AC54",
+        "mybucketOnUpdate0ServiceRoleAF91E9C7",
       ],
       "Properties": {
         "Architectures": [
@@ -1303,13 +1303,13 @@ def submit_response(event: dict, context, response_status: str, error_message: s
         "Handler": "index.handler",
         "LoggingConfig": {
           "LogGroup": {
-            "Ref": "mybucketonUpdate0LogGroupDA44D636",
+            "Ref": "mybucketOnUpdate0LogGroupFDC75EA3",
           },
         },
         "MemorySize": 1024,
         "Role": {
           "Fn::GetAtt": [
-            "mybucketonUpdate0ServiceRole6E92AC54",
+            "mybucketOnUpdate0ServiceRoleAF91E9C7",
             "Arn",
           ],
         },
@@ -1318,7 +1318,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::Lambda::Function",
     },
-    "mybucketonUpdate0LogGroupDA44D636": {
+    "mybucketOnUpdate0LogGroupFDC75EA3": {
       "DeletionPolicy": "Retain",
       "Properties": {
         "RetentionInDays": 30,
@@ -1326,7 +1326,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       "Type": "AWS::Logs::LogGroup",
       "UpdateReplacePolicy": "Retain",
     },
-    "mybucketonUpdate0ServiceRole6E92AC54": {
+    "mybucketOnUpdate0ServiceRoleAF91E9C7": {
       "Properties": {
         "AssumeRolePolicyDocument": {
           "Statement": [
@@ -1561,12 +1561,12 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::IAM::Policy",
     },
-    "mybucketAllowBucketNotificationsTomyprojectmybucketonUpdate0DA830360476798FF": {
+    "mybucketAllowBucketNotificationsTomyprojectmybucketOnUpdate036B5F47FED7981DE": {
       "Properties": {
         "Action": "lambda:InvokeFunction",
         "FunctionName": {
           "Fn::GetAtt": [
-            "mybucketonUpdate02A38D403",
+            "mybucketOnUpdate06503D6F0",
             "Arn",
           ],
         },
@@ -1607,7 +1607,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
     },
     "mybucketNotificationsB76B6B79": {
       "DependsOn": [
-        "mybucketAllowBucketNotificationsTomyprojectmybucketonUpdate0DA830360476798FF",
+        "mybucketAllowBucketNotificationsTomyprojectmybucketOnUpdate036B5F47FED7981DE",
       ],
       "Properties": {
         "BucketName": {
@@ -1622,7 +1622,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
               ],
               "LambdaFunctionArn": {
                 "Fn::GetAtt": [
-                  "mybucketonUpdate02A38D403",
+                  "mybucketOnUpdate06503D6F0",
                   "Arn",
                 ],
               },
@@ -1638,9 +1638,9 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "Custom::S3BucketNotifications",
     },
-    "mybucketonUpdate02A38D403": {
+    "mybucketOnUpdate06503D6F0": {
       "DependsOn": [
-        "mybucketonUpdate0ServiceRole6E92AC54",
+        "mybucketOnUpdate0ServiceRoleAF91E9C7",
       ],
       "Properties": {
         "Architectures": [
@@ -1660,13 +1660,13 @@ def submit_response(event: dict, context, response_status: str, error_message: s
         "Handler": "index.handler",
         "LoggingConfig": {
           "LogGroup": {
-            "Ref": "mybucketonUpdate0LogGroupDA44D636",
+            "Ref": "mybucketOnUpdate0LogGroupFDC75EA3",
           },
         },
         "MemorySize": 1024,
         "Role": {
           "Fn::GetAtt": [
-            "mybucketonUpdate0ServiceRole6E92AC54",
+            "mybucketOnUpdate0ServiceRoleAF91E9C7",
             "Arn",
           ],
         },
@@ -1675,7 +1675,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       },
       "Type": "AWS::Lambda::Function",
     },
-    "mybucketonUpdate0LogGroupDA44D636": {
+    "mybucketOnUpdate0LogGroupFDC75EA3": {
       "DeletionPolicy": "Retain",
       "Properties": {
         "RetentionInDays": 30,
@@ -1683,7 +1683,7 @@ def submit_response(event: dict, context, response_status: str, error_message: s
       "Type": "AWS::Logs::LogGroup",
       "UpdateReplacePolicy": "Retain",
     },
-    "mybucketonUpdate0ServiceRole6E92AC54": {
+    "mybucketOnUpdate0ServiceRoleAF91E9C7": {
       "Properties": {
         "AssumeRolePolicyDocument": {
           "Statement": [

libs/wingsdk/src/cloud/bucket.ts
@@ -89,7 +89,7 @@ export class Bucket extends Resource {
    * @returns the created topic
    */
   protected createTopic(actionType: BucketEventType): Topic {
-    const topic = new Topic(this, actionType.toLowerCase());
+    const topic = new Topic(this, actionType);
     this.node.addDependency(topic);
     return topic;
   }
@@ -524,15 +524,15 @@ export enum BucketEventType {
   /**
    * Create
    */
-  CREATE = "onCreate",
+  CREATE = "OnCreate",
   /**
    * Delete
    */
-  DELETE = "onDelete",
+  DELETE = "OnDelete",
   /**
    * Update
    */
-  UPDATE = "onUpdate",
+  UPDATE = "OnUpdate",
 }
 
 /**

libs/wingsdk/src/std/node.ts
@@ -104,7 +104,10 @@ export class Node {
    * metadata describing how one construct is related to another construct.
    */
   public addConnection(props: AddConnectionProps) {
-    this._connections.add(props);
+    this._connections.add({
+      source: props.source ?? this.construct,
+      ...props,
+    });
   }
 
   // ---- constructs 10.x APIs ----
@@ -423,8 +426,9 @@ export class Node {
 export interface AddConnectionProps {
   /**
    * The source of the connection.
+   * @default this
    */
-  readonly source: IConstruct;
+  readonly source?: IConstruct;
 
   /**
    * An operation that the source construct supports.

libs/wingsdk/src/std/resource.ts
@@ -228,7 +228,7 @@ function addConnectionsFromLiftMap(
             sourceOp: baseOp ?? op,
             target: dep,
             targetOp: depOp,
-            name: depOp,
+            name: "call",
           });
         }
       } else if (hasLiftMap(dep)) {

commit 6031415d350bd459334a1dd8d5dbfa1bdef10a0b

fix(console): strange map connections (#6646)
Fixes a visual bug introduced by #6602 where strange connections would show floating around the map.

apps/wing-console/console/ui/src/services/use-map.ts
@@ -228,6 +228,21 @@ export const useMap = ({ expandedItems }: UseMapOptions) => {
     return bridgeConnections({
       connections:
         rawConnections
+          .map((connection) => {
+            // Convert invokeAsync to invoke, since they
+            // are the same to the map view.
+            return {
+              ...connection,
+              sourceOp:
+                connection.sourceOp === "invokeAsync"
+                  ? "invoke"
+                  : connection.sourceOp,
+              targetOp:
+                connection.targetOp === "invokeAsync"
+                  ? "invoke"
+                  : connection.targetOp,
+            };
+          })
           .filter((connection) => {
             return connection.source !== connection.target;
           })