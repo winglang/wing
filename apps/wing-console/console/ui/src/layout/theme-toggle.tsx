31bb7559fae5cf0bea09f41ac8840b3f11c18619
polamoros <info@polamoros.com>
2024-06-11 11:50:03 +0200

    wip

--- apps/wing-console/console/ui/src/layout/theme-toggle.tsx
+++ apps/wing-console/console/ui/src/layout/theme-toggle.tsx
@@ -6,9 +6,37 @@ import {
   MoonIcon as MoonIconSolid,
   SunIcon as SunIconSolid,
 } from "@heroicons/react/24/solid";
+import type { Mode } from "@wingconsole/design-system";
 import { useTheme } from "@wingconsole/design-system";
 import classNames from "classnames";
 import { useCallback, useMemo } from "react";
+
+const AutoIcon = ({ currentTheme }: { currentTheme?: Mode }) => {
+  return (
+    <div className="relative size-4 flex">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill={currentTheme === "light" ? "currentColor" : "none"}
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="absolute size-3.5 left-[1.5px] top-[0px] -rotate-[22deg]"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 1.591M5.25 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+        />
+      </svg>
+
+      {currentTheme === "light" ? (
+        <MoonIconOutline className="absolute bottom-0 left-0 size-3" />
+      ) : (
+        <MoonIconSolid className="absolute bottom-0 left-0 size-3" />
+      )}
+    </div>
+  );
+};
 
 export const ThemeToggle = () => {
   const { theme, setThemeMode, mode, mediaTheme } = useTheme();
@@ -41,45 +69,7 @@ export const ThemeToggle = () => {
     >
       {mode === "auto" && (
         <div className="relative size-4 flex">
-          {currentTheme === "light" && (
-            <>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="absolute size-3.5 left-[1.5px] top-[0px] -rotate-[22deg]"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 1.591M5.25 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                />
-              </svg>
-              <MoonIconOutline className="absolute bottom-0 left-0 size-3" />
-            </>
-          )}
-          {currentTheme === "dark" && (
-            <>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="absolute size-3.5 left-[1.5px] top-[0px] -rotate-[22deg]"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 1.591M5.25 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
-                />
-              </svg>
-
-              <MoonIconSolid className="absolute bottom-0 left-0 size-3" />
-            </>
-          )}
+          <AutoIcon currentTheme={currentTheme} />
         </div>
       )}
       {mode !== "auto" && (
