48abacebfae162e2df6ad22395021ad5b69a91d0
Chris Rybicki <chrisr@monada.co>
2024-06-14 18:00:03 -0400

    chore(repo): upgrade vitest (attempt 2) (#6686)
    https://github.com/winglang/wing/pull/6676 was reverted due to some unexpected build errors. This PR brings back the code with fixes to the benchmark script and to stack trace output on Windows machines.
    
    Huge thanks to @MarkMcCulloh for the help getting this PR over the finish line.
    
    Closes #6675
    
    ## Checklist
    
    - [x] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
    - [x] Description explains motivation and solution
    - [ ] Tests added (always)
    - [ ] Docs updated (only required for features)
    - [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

--- apps/wing/vitest.config.ts
+++ apps/wing/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     // Some tests use process.chdir which is not supported in vitest's multi-thread mode
-    threads: false,
+    pool: "forks",
     testTimeout: 200_000,
   },
 });
