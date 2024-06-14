0e541a3730978471cb02fa9648732b245d7889ae
Pol Amorós <info@polamoros.com>
2024-06-14 09:49:39 +0200

    Update apps/wing-console/console/ui/src/layout/status-bar.tsx
    Co-authored-by: Cristian Pallarés <cristianp@wing.cloud>

--- apps/wing-console/console/ui/src/layout/status-bar.tsx
+++ apps/wing-console/console/ui/src/layout/status-bar.tsx
@@ -85,7 +85,7 @@ export const StatusBar = ({
       {/*center*/}
       <div className="grow"></div>
       {/*right side*/}
-      <div className="w-full flex gap-2 items-center justify-end">
+      <div className="flex gap-2 items-center">
         <AutoUpdater />
         <DiscordButton />
         {showThemeToggle && <ThemeToggle />}
