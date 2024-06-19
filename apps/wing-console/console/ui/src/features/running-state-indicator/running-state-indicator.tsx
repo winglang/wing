fd2053737751bbbf06d920af6fd754a0929c4e52
Cristian Pallarés <cristianp@wing.cloud>
2024-06-19 13:22:46 +0300

    smaller indicator, yellow when loading

--- apps/wing-console/console/ui/src/features/running-state-indicator/running-state-indicator.tsx
+++ apps/wing-console/console/ui/src/features/running-state-indicator/running-state-indicator.tsx
@@ -9,18 +9,18 @@ export const RunningStateIndicator: FunctionComponent<
   RunningStateIndicatorProps
 > = ({ runningState }) => {
   if (runningState === "error") {
-    return <div className="size-2 rounded-full bg-red-500"></div>;
+    return <div className="size-1.5 rounded-full bg-red-500"></div>;
   }
   if (runningState === "starting" || runningState === "stopping") {
     return (
       <div className="relative">
-        <div className="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-400 opacity-75"></div>
-        <div className="size-2 rounded-full bg-gray-400"></div>
+        <div className="animate-ping absolute inline-flex h-full w-full rounded-full bg-yellow-500 opacity-75"></div>
+        <div className="size-1.5 rounded-full bg-yellow-500"></div>
       </div>
     );
   }
   if (runningState === "stopped") {
-    return <div className="size-2 rounded-full bg-gray-400"></div>;
+    return <div className="size-1.5 rounded-full bg-gray-400"></div>;
   }
-  return <div className="size-2"></div>;
+  return <div className="size-1.5"></div>;
 };
