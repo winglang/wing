f9dc0876b703cb293a762b0ec25c2d385f6e291c
polamoros <info@polamoros.com>
2024-06-19 14:18:12 +0300

    wip

--- apps/wing-console/console/ui/src/features/endpoints-pane/endpoint-tree.tsx
+++ apps/wing-console/console/ui/src/features/endpoints-pane/endpoint-tree.tsx
@@ -1,8 +1,6 @@
 import {
   GlobeAltIcon,
-  LinkIcon,
-  ShareIcon,
-  EyeSlashIcon,
+  ArrowTopRightOnSquareIcon,
 } from "@heroicons/react/24/outline";
 import {
   Button,
@@ -75,9 +73,27 @@ export const EndpointTree = ({
                     icon={
                       <>
                         {endpoint.exposeStatus === "disconnected" && (
-                          <EyeSlashIcon
-                            className={classNames("size-4", theme.text1)}
-                          />
+                          <div className={classNames("size-4", theme.text1)}>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              strokeWidth="1.5"
+                              stroke="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+                              />
+
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M3 3l18 18"
+                              />
+                            </svg>
+                          </div>
                         )}
                         {endpoint.exposeStatus === "connecting" && (
                           <SpinnerLoader size="xs" />
@@ -97,13 +113,16 @@ export const EndpointTree = ({
                         title={endpoint.url}
                         aria-disabled={endpoint.exposeStatus === "connecting"}
                         className={classNames(
+                          "flex gap-1 items-center",
+                          "justify-between group",
                           endpoint.exposeStatus !== "connecting" &&
                             "hover:underline text-sky-500 hover:text-sky-600",
                           endpoint.exposeStatus === "connecting" &&
                             "text-slate-400 cursor-not-allowed",
                         )}
                       >
-                        {endpoint.label}
+                        <span className="truncate">{endpoint.label}</span>
+                        <ArrowTopRightOnSquareIcon className="size-4 shrink-0 hidden group-hover:block" />
                       </a>
                     }
                     secondaryLabel={
