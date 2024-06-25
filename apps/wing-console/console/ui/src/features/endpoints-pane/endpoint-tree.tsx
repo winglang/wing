5ad3ae617b7a8754df0f35b7ee19639f10bc9af2
polamoros <info@polamoros.com>
2024-06-25 10:58:17 +0200

    wip

--- apps/wing-console/console/ui/src/features/endpoints-pane/endpoint-tree.tsx
+++ apps/wing-console/console/ui/src/features/endpoints-pane/endpoint-tree.tsx
@@ -5,7 +5,6 @@ import {
 import {
   Button,
   ScrollableArea,
-  SpinnerIcon,
   SpinnerLoader,
   Toolbar,
   TreeItem,
@@ -13,6 +12,7 @@ import {
   useTheme,
 } from "@wingconsole/design-system";
 import classNames from "classnames";
+import { useCallback, useEffect, useState } from "react";
 
 import type { EndpointItem } from "./endpoint-item.js";
 import { NoEndpoints } from "./no-endpoints.js";
@@ -44,6 +44,44 @@ export const EndpointTree = ({
 }: EndpointTreeProps) => {
   const { theme } = useTheme();
 
+  // list or map of endpoint ids that are currently being hidden
+  const [hidingEndpoint, setHidingEndpoint] = useState<string[]>();
+
+  const onHideEndpoint = useCallback(
+    (endpointId: string) => {
+      setHidingEndpoint((previousHidingEndpoint) => {
+        return previousHidingEndpoint?.includes(endpointId)
+          ? previousHidingEndpoint.filter((id) => id !== endpointId)
+          : [...(previousHidingEndpoint || []), endpointId];
+      });
+      hideEndpoint(endpointId);
+    },
+    [hideEndpoint],
+  );
+
+  useEffect(() => {
+    const isHidingEndpointDisconnected =
+      hidingEndpoint &&
+      endpointList.find(
+        (endpoint) =>
+          endpoint.exposeStatus === "disconnected" &&
+          hidingEndpoint.includes(endpoint.id),
+      );
+    if (isHidingEndpointDisconnected) {
+      setHidingEndpoint(undefined);
+    }
+  }, [endpointList, hidingEndpoint]);
+
+  const isLoading = useCallback(
+    (endpoint: EndpointItem) => {
+      return (
+        endpoint.exposeStatus === "connecting" ||
+        hidingEndpoint?.includes(endpoint.id)
+      );
+    },
+    [hidingEndpoint],
+  );
+
   return (
     <div
       className={classNames("w-full h-full flex flex-col", theme.bg3)}
@@ -72,36 +110,41 @@ export const EndpointTree = ({
                     title={getEndpointTitle(endpoint.exposeStatus)}
                     icon={
                       <>
-                        {endpoint.exposeStatus === "disconnected" && (
-                          <div className={classNames("size-4", theme.text1)}>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
-                              />
+                        {isLoading(endpoint) && <SpinnerLoader size="xs" />}
+                        {!isLoading(endpoint) && (
+                          <>
+                            {endpoint.exposeStatus === "disconnected" && (
+                              <div
+                                className={classNames("size-4", theme.text1)}
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  fill="none"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth="1.5"
+                                  stroke="currentColor"
+                                >
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+                                  />
 
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M3 3l18 18"
+                                  <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M3 3l18 18"
+                                  />
+                                </svg>
+                              </div>
+                            )}
+
+                            {endpoint.exposeStatus === "connected" && (
+                              <GlobeAltIcon
+                                className={classNames("size-4", theme.text1)}
                               />
-                            </svg>
-                          </div>
-                        )}
-                        {endpoint.exposeStatus === "connecting" && (
-                          <SpinnerLoader size="xs" />
-                        )}
-                        {endpoint.exposeStatus === "connected" && (
-                          <GlobeAltIcon
-                            className={classNames("size-4", theme.text1)}
-                          />
+                            )}
+                          </>
                         )}
                       </>
                     }
@@ -111,13 +154,13 @@ export const EndpointTree = ({
                         target="_blank"
                         rel="noreferrer"
                         title={endpoint.url}
-                        aria-disabled={endpoint.exposeStatus === "connecting"}
+                        aria-disabled={isLoading(endpoint)}
                         className={classNames(
                           "flex gap-1 items-center",
                           "justify-between group",
-                          endpoint.exposeStatus !== "connecting" &&
+                          !isLoading(endpoint) &&
                             "hover:underline text-sky-500 hover:text-sky-600",
-                          endpoint.exposeStatus === "connecting" &&
+                          isLoading(endpoint) &&
                             "text-slate-400 cursor-not-allowed",
                         )}
                       >
@@ -132,7 +175,7 @@ export const EndpointTree = ({
                           <Button
                             small
                             transparent
-                            disabled={endpoint.exposeStatus === "connecting"}
+                            disabled={isLoading(endpoint)}
                             onClick={() => {
                               exposeEndpoint(endpoint.id);
                             }}
@@ -140,11 +183,12 @@ export const EndpointTree = ({
                             Open
                           </Button>
                         )}
-                        {endpoint.exposeStatus == "connected" && (
+                        {endpoint.exposeStatus === "connected" && (
                           <Button
                             small
+                            disabled={isLoading(endpoint)}
                             onClick={() => {
-                              hideEndpoint(endpoint.id);
+                              onHideEndpoint(endpoint.id);
                             }}
                           >
                             Close
