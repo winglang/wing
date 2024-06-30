5d3f7699373416abad95f441b6d5788ce7d46ab7
tsuf239 <39455181+tsuf239@users.noreply.github.com>
2024-06-30 17:45:33 +0300

    refactor permissions not to use custom roles

--- libs/wingsdk/src/target-tf-gcp/function.ts
+++ libs/wingsdk/src/target-tf-gcp/function.ts
@@ -7,9 +7,9 @@ import { Bucket } from "./bucket";
 import { core } from "..";
 import { CloudfunctionsFunction } from "../.gen/providers/google/cloudfunctions-function";
 import { CloudfunctionsFunctionIamMember } from "../.gen/providers/google/cloudfunctions-function-iam-member";
-import { ProjectIamCustomRole } from "../.gen/providers/google/project-iam-custom-role";
 import { ProjectIamMember } from "../.gen/providers/google/project-iam-member";
 import { ServiceAccount } from "../.gen/providers/google/service-account";
+import { ServiceAccountIamMember } from "../.gen/providers/google/service-account-iam-member";
 import { StorageBucketObject } from "../.gen/providers/google/storage-bucket-object";
 import * as cloud from "../cloud";
 import { LiftMap } from "../core";
@@ -73,11 +73,8 @@ export class Function extends cloud.Function {
   }
 
   private readonly function: CloudfunctionsFunction;
-  private readonly functionServiceAccount: ServiceAccount;
-  private readonly functionCustomRole: ProjectIamCustomRole;
-  private readonly permissions: Set<string> = new Set([
-    "cloudfunctions.functions.get",
-  ]);
+  public readonly functionServiceAccount: ServiceAccount;
+  private roles?: Set<string> = new Set();
 
   private assetPath: string | undefined; // posix path
 
@@ -136,7 +133,7 @@ export class Function extends cloud.Function {
       }
     );
 
-    // Step 1: Create Custom Service Account
+    // Create Custom Service Account
     this.functionServiceAccount = new ServiceAccount(
       this,
       `ServiceAccount${this.node.addr.substring(-8)}`,
@@ -147,25 +144,7 @@ export class Function extends cloud.Function {
         )}`,
       }
     );
-    // Step 2: Create Custom Role
-    this.functionCustomRole = new ProjectIamCustomRole(
-      this,
-      `CustomRole${this.node.addr.substring(-8)}`,
-      {
-        roleId: `cloudfunctions.custom${this.node.addr.substring(-8)}`,
-        title: `Custom Role for Cloud Function ${this.node.addr.substring(-8)}`,
-        permissions: Lazy.listValue({
-          produce: () => Array.from(this.permissions),
-        }),
-      }
-    );
-    // Step 3: Grant Custom Role to Custom Service Account on the Project
-    new ProjectIamMember(this, "ProjectIamMember", {
-      project: app.projectId,
-      role: `projects/${app.projectId}/roles/${this.functionCustomRole.roleId}`,
-      member: `serviceAccount:${this.functionServiceAccount.email}`,
-    });
-    // Step 4: Create the Cloud Function with Custom Service Account
+    // Create the Cloud Function with Custom Service Account
     this.function = new CloudfunctionsFunction(this, "DefaultFunction", {
       name: ResourceNames.generateName(this, FUNCTION_NAME_OPTS),
       description: "This function was created by Wing",
@@ -290,12 +269,6 @@ export class Function extends cloud.Function {
     );
   }
 
-  public addPermissions(permissions: string[]): void {
-    permissions.forEach((permission) => {
-      this.permissions.add(permission);
-    });
-  }
-
   public onLift(host: IInflightHost, ops: string[]): void {
     if (!(host instanceof Function)) {
       throw new Error(
@@ -304,7 +277,7 @@ export class Function extends cloud.Function {
     }
 
     if (ops.includes(cloud.FunctionInflightMethods.INVOKE)) {
-      host.addPermissions(["cloudfunctions.functions.invoke"]);
+      this._addPermissionToInvoke(host.serviceAccountEmail);
     }
 
     const { region, projectId } = App.of(this) as App;
@@ -320,16 +293,43 @@ export class Function extends cloud.Function {
    * @param serviceAccount The service account to grant invoke permissions to.
    * @internal
    */
-  public _addPermissionToInvoke(serviceAccount: ServiceAccount): void {
-    const hash = Fn.sha256(serviceAccount.email).slice(-8);
+  public _addPermissionToInvoke(serviceAccountEmail: string): void {
+    const hash = Fn.sha256(serviceAccountEmail).slice(-8);
 
     new CloudfunctionsFunctionIamMember(this, `invoker-permission-${hash}`, {
       project: this.function.project,
       region: this.function.region,
       cloudFunction: this.function.name,
       role: "roles/cloudfunctions.invoker",
-      member: `serviceAccount:${serviceAccount.email}`,
+      member: `serviceAccount:${serviceAccountEmail}`,
     });
+  }
+
+  public _addTokenCreator() {
+    const tokenCreatorRole = "roles/iam.serviceAccountTokenCreator";
+    if (this.roles?.has(tokenCreatorRole)) {
+      return;
+    }
+    const hash = Fn.sha256(this.serviceAccountEmail).slice(-8);
+    new ServiceAccountIamMember(this, `service-iam-member-${hash}`, {
+      serviceAccountId: this.functionServiceAccount.name,
+      role: "roles/iam.serviceAccountTokenCreator",
+      member: `serviceAccount:${this.functionServiceAccount.email}`,
+    });
+    this.roles?.add(tokenCreatorRole);
+  }
+
+  public _addProjectIamMember(role: string) {
+    if (this.roles?.has(role)) {
+      return;
+    }
+    const hash = Fn.sha256(this.functionServiceAccount.email).slice(-8);
+    new ProjectIamMember(this, `project-iam-member-${hash}`, {
+      project: (App.of(this) as any).projectId,
+      member: `serviceAccount:${this.functionServiceAccount.email}`,
+      role,
+    });
+    this.roles?.add(role);
   }
 
   /** @internal */
