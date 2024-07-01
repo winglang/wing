11deab2e7934c7ed4b693822da0ef75c736fc92a
polamoros <info@polamoros.com>
2024-07-01 16:13:34 +0200

    wip

--- apps/wing-console/console/ui/src/features/endpoints-pane/endpoint-item.ts
+++ apps/wing-console/console/ui/src/features/endpoints-pane/endpoint-item.ts
@@ -2,9 +2,9 @@ export type EndpointExposeStatus = "connected" | "disconnected" | "connecting";
 
 export interface EndpointItem {
   id: string;
-  label: string;
-  url: string;
-  localUrl: string;
+  label?: string;
+  url?: string;
+  localUrl?: string;
   browserSupport: boolean;
   exposeStatus: EndpointExposeStatus;
 }
--- apps/wing-console/console/ui/src/features/endpoints-pane/endpoint-tree.tsx
+++ apps/wing-console/console/ui/src/features/endpoints-pane/endpoint-tree.tsx
@@ -9,19 +9,16 @@ import {
   Toolbar,
   TreeItem,
   TreeView,
+  useNotifications,
   useTheme,
 } from "@wingconsole/design-system";
 import classNames from "classnames";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+import { useEndpoints } from "../inspector-pane/resource-panes/use-endpoints.js";
 
 import type { EndpointItem } from "./endpoint-item.js";
 import { NoEndpoints } from "./no-endpoints.js";
-
-export interface EndpointTreeProps {
-  endpointList: EndpointItem[];
-  exposeEndpoint: (resourcePath: string) => void;
-  hideEndpoint: (resourcePath: string) => void;
-}
 
 const getEndpointTitle = (exposeStatus: EndpointItem["exposeStatus"]) => {
   switch (exposeStatus) {
@@ -37,50 +34,149 @@ const getEndpointTitle = (exposeStatus: EndpointItem["exposeStatus"]) => {
   }
 };
 
-export const EndpointTree = ({
-  endpointList,
-  exposeEndpoint,
-  hideEndpoint,
-}: EndpointTreeProps) => {
+const EndpointTreeViewItem = ({ endpoint }: { endpoint: EndpointItem }) => {
   const { theme } = useTheme();
+  const { exposeEndpoint, hideEndpoint } = useEndpoints();
 
-  // list or map of endpoint ids that are currently being hidden
-  const [hidingEndpoint, setHidingEndpoint] = useState<string[]>();
+  const { showNotification } = useNotifications();
 
-  const onHideEndpoint = useCallback(
-    (endpointId: string) => {
-      setHidingEndpoint((previousHidingEndpoint) => {
-        return previousHidingEndpoint?.includes(endpointId)
-          ? previousHidingEndpoint.filter((id) => id !== endpointId)
-          : [...(previousHidingEndpoint || []), endpointId];
-      });
-      hideEndpoint(endpointId);
-    },
-    [hideEndpoint],
+  const loading = useMemo(
+    () => exposeEndpoint.isLoading || hideEndpoint.isLoading,
+    [exposeEndpoint.isLoading, hideEndpoint.isLoading],
   );
+
+  const onExpose = useCallback(async () => {
+    await exposeEndpoint.mutateAsync({ resourcePath: endpoint.id });
+    showNotification(`Endpoint "${endpoint.label}" is exposed`);
+  }, [exposeEndpoint, endpoint, showNotification]);
 
   useEffect(() => {
-    const isHidingEndpointDisconnected =
-      hidingEndpoint &&
-      endpointList.find(
-        (endpoint) =>
-          endpoint.exposeStatus === "disconnected" &&
-          hidingEndpoint.includes(endpoint.id),
-      );
-    if (isHidingEndpointDisconnected) {
-      setHidingEndpoint(undefined);
+    if (exposeEndpoint.error) {
+      showNotification(exposeEndpoint.error.message, { type: "error" });
     }
-  }, [endpointList, hidingEndpoint]);
+  }, [exposeEndpoint.error, showNotification]);
 
-  const isLoading = useCallback(
-    (endpoint: EndpointItem) => {
-      return (
-        endpoint.exposeStatus === "connecting" ||
-        hidingEndpoint?.includes(endpoint.id)
-      );
-    },
-    [hidingEndpoint],
+  const buttonLabel = useMemo(() => {
+    if (endpoint.exposeStatus === "connected") {
+      return loading ? "Closing" : "Close";
+    }
+    if (endpoint.exposeStatus === "disconnected") {
+      return loading ? "Exposing" : "Expose";
+    }
+    if (endpoint.exposeStatus === "connecting") {
+      return "Exposing";
+    }
+  }, [endpoint.exposeStatus, loading]);
+
+  return (
+    <TreeItem
+      key={endpoint.id}
+      itemId={endpoint.id}
+      selectable={false}
+      title={getEndpointTitle(endpoint.exposeStatus)}
+      icon={
+        <>
+          {loading && <SpinnerLoader size="xs" />}
+          {!loading && (
+            <>
+              {endpoint.exposeStatus === "disconnected" && (
+                <div className={classNames("size-4", theme.text2)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+                    />
+
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 3l18 18"
+                    />
+                  </svg>
+                </div>
+              )}
+
+              {endpoint.exposeStatus === "connected" && (
+                <GlobeAltIcon className={classNames("size-4", theme.text1)} />
+              )}
+            </>
+          )}
+        </>
+      }
+      label={
+        <a
+          href={endpoint.url}
+          target="_blank"
+          rel="noreferrer"
+          title={endpoint.url}
+          aria-disabled={loading}
+          className={classNames(
+            "flex gap-1 items-center",
+            "justify-between group/endpoint-tree-item",
+            !loading && "hover:underline text-sky-500 hover:text-sky-600",
+            loading && "text-slate-400 cursor-not-allowed",
+          )}
+        >
+          <span className="truncate">{endpoint.label}</span>
+          <ArrowTopRightOnSquareIcon
+            className={classNames(
+              "size-4 shrink-0 hidden",
+              "group-hover/endpoint-tree-item:block",
+            )}
+          />
+        </a>
+      }
+      secondaryLabel={
+        <Button
+          small
+          disabled={loading}
+          className="min-w-[5rem] justify-center"
+          onClick={() => {
+            if (endpoint.exposeStatus === "connected") {
+              hideEndpoint.mutate({ resourcePath: endpoint.id });
+            } else {
+              onExpose();
+            }
+          }}
+        >
+          {buttonLabel}
+        </Button>
+      }
+    />
   );
+};
+
+export const EndpointTree = () => {
+  const { endpointList } = useEndpoints();
+
+  const { showNotification } = useNotifications();
+
+  const [initialNotification, setInitialNotification] = useState(true);
+
+  useEffect(() => {
+    if (!initialNotification) {
+      return false;
+    }
+    if (endpointList.data) {
+      const endpointIds: string[] = [];
+      for (const endpoint of endpointList.data) {
+        if (endpoint.exposeStatus === "connected") {
+          showNotification(`Endpoint "${endpoint.label}" is exposed`);
+          endpointIds.push(endpoint.id);
+        }
+      }
+      setInitialNotification(false);
+    }
+  }, [endpointList.data, showNotification]);
+
+  const { theme } = useTheme();
 
   return (
     <div
@@ -100,103 +196,11 @@ export const EndpointTree = ({
             )}
           >
             <div className="flex flex-col">
-              {endpointList.length === 0 && <NoEndpoints />}
+              {endpointList.data?.length === 0 && <NoEndpoints />}
+
               <TreeView>
-                {endpointList.map((endpoint) => (
-                  <TreeItem
-                    key={endpoint.id}
-                    itemId={endpoint.id}
-                    selectable={false}
-                    title={getEndpointTitle(endpoint.exposeStatus)}
-                    icon={
-                      <>
-                        {isLoading(endpoint) && <SpinnerLoader size="xs" />}
-                        {!isLoading(endpoint) && (
-                          <>
-                            {endpoint.exposeStatus === "disconnected" && (
-                              <div
-                                className={classNames("size-4", theme.text2)}
-                              >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth="1.5"
-                                  stroke="currentColor"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
-                                  />
-
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M3 3l18 18"
-                                  />
-                                </svg>
-                              </div>
-                            )}
-
-                            {endpoint.exposeStatus === "connected" && (
-                              <GlobeAltIcon
-                                className={classNames("size-4", theme.text1)}
-                              />
-                            )}
-                          </>
-                        )}
-                      </>
-                    }
-                    label={
-                      <a
-                        href={endpoint.url}
-                        target="_blank"
-                        rel="noreferrer"
-                        title={endpoint.url}
-                        aria-disabled={isLoading(endpoint)}
-                        className={classNames(
-                          "flex gap-1 items-center",
-                          "justify-between group/endpoint-tree-item",
-                          !isLoading(endpoint) &&
-                            "hover:underline text-sky-500 hover:text-sky-600",
-                          isLoading(endpoint) &&
-                            "text-slate-400 cursor-not-allowed",
-                        )}
-                      >
-                        <span className="truncate">{endpoint.label}</span>
-                        <ArrowTopRightOnSquareIcon
-                          className={classNames(
-                            "size-4 shrink-0 hidden",
-                            "group-hover/endpoint-tree-item:block",
-                          )}
-                        />
-                      </a>
-                    }
-                    secondaryLabel={
-                      <Button
-                        small
-                        disabled={isLoading(endpoint)}
-                        className="min-w-[5rem] justify-center"
-                        onClick={() => {
-                          if (endpoint.exposeStatus === "connected") {
-                            onHideEndpoint(endpoint.id);
-                          } else {
-                            exposeEndpoint(endpoint.id);
-                          }
-                        }}
-                      >
-                        {endpoint.exposeStatus === "connected" && (
-                          <>
-                            {isLoading(endpoint) && "Closing"}
-                            {!isLoading(endpoint) && "Close"}
-                          </>
-                        )}
-                        {endpoint.exposeStatus === "disconnected" && "Expose"}
-                        {endpoint.exposeStatus === "connecting" && "Exposing"}
-                      </Button>
-                    }
-                  />
+                {endpointList.data?.map((endpoint) => (
+                  <EndpointTreeViewItem key={endpoint.id} endpoint={endpoint} />
                 ))}
               </TreeView>
             </div>
