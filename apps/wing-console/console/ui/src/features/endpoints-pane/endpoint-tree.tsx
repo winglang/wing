8397a62b9c93260e7899459a536164d0fc6e3888
polamoros <info@polamoros.com>
2024-07-02 19:27:18 +0200

    wip

--- apps/wing-console/console/ui/src/features/endpoints-pane/endpoint-tree.tsx
+++ apps/wing-console/console/ui/src/features/endpoints-pane/endpoint-tree.tsx
@@ -22,42 +22,20 @@ import type { EndpointItem } from "./endpoint-item.js";
 import { EndpointsWarningModal } from "./endpoints-warning-modal.js";
 import { NoEndpoints } from "./no-endpoints.js";
 
-const EndpointTreeViewItem = ({ endpoint }: { endpoint: EndpointItem }) => {
+const EndpointTreeViewItem = ({
+  endpoint,
+  loading,
+  disabled,
+  onExposeEndpoint,
+  onHideEndpoint,
+}: {
+  endpoint: EndpointItem;
+  loading: boolean;
+  disabled: boolean;
+  onExposeEndpoint: () => void;
+  onHideEndpoint: () => void;
+}) => {
   const { theme } = useTheme();
-  const { exposeEndpoint, hideEndpoint } = useEndpoints();
-  const { requireAcceptWarning, notifyAcceptWarning } = useEndpointsWarning();
-
-  const { showNotification } = useNotifications();
-
-  const [showWarningModal, setShowWarningModal] = useState(false);
-
-  const loading = useMemo(
-    () => exposeEndpoint.isLoading || hideEndpoint.isLoading,
-    [exposeEndpoint.isLoading, hideEndpoint.isLoading],
-  );
-
-  const onExposeEndpoint = useCallback(async () => {
-    if (requireAcceptWarning.data?.requireAcceptWarning === true) {
-      setShowWarningModal(true);
-      return;
-    }
-    await exposeEndpoint.mutateAsync({ resourcePath: endpoint.id });
-    showNotification(`Endpoint "${endpoint.label}" is exposed`, {
-      type: "info",
-    });
-  }, [exposeEndpoint, endpoint, showNotification, requireAcceptWarning.data]);
-
-  const onAcceptWarning = useCallback(async () => {
-    notifyAcceptWarning.mutate();
-    setShowWarningModal(false);
-    onExposeEndpoint();
-  }, [onExposeEndpoint, setShowWarningModal, notifyAcceptWarning]);
-
-  useEffect(() => {
-    if (exposeEndpoint.error) {
-      showNotification(exposeEndpoint.error.message, { type: "error" });
-    }
-  }, [exposeEndpoint.error, showNotification]);
 
   const buttonLabel = useMemo(() => {
     if (endpoint.exposeStatus === "connected") {
@@ -86,103 +64,142 @@ const EndpointTreeViewItem = ({ endpoint }: { endpoint: EndpointItem }) => {
   }, [endpoint.exposeStatus]);
 
   return (
-    <>
-      <EndpointsWarningModal
-        visible={showWarningModal}
-        onContinue={() => onAcceptWarning()}
-        onCancel={() => setShowWarningModal(false)}
-      />
-      <TreeItem
-        key={endpoint.id}
-        itemId={endpoint.id}
-        selectable={false}
-        title={endpointTitle}
-        icon={
-          <>
-            {loading && <SpinnerLoader size="xs" />}
-            {!loading && (
-              <>
-                {endpoint.exposeStatus === "disconnected" && (
-                  <div className={classNames("size-4", theme.text2)}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
-                      />
-
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3 3l18 18"
-                      />
-                    </svg>
-                  </div>
-                )}
-
-                {endpoint.exposeStatus === "connected" && (
-                  <GlobeAltIcon className={classNames("size-4", theme.text1)} />
-                )}
-              </>
-            )}
-          </>
-        }
-        label={
-          <a
-            href={endpoint.url}
-            target="_blank"
-            rel="noreferrer"
-            title={endpoint.url}
-            aria-disabled={loading}
-            className={classNames(
-              "flex gap-1 items-center",
-              "justify-between group/endpoint-tree-item",
-              !loading && "hover:underline text-sky-500 hover:text-sky-600",
-              loading && "text-slate-400 cursor-not-allowed",
-            )}
-          >
-            <span className="truncate">{endpoint.label}</span>
-            <ArrowTopRightOnSquareIcon
-              className={classNames(
-                "size-4 shrink-0 hidden",
-                "group-hover/endpoint-tree-item:block",
+    <TreeItem
+      key={endpoint.id}
+      itemId={endpoint.id}
+      selectable={false}
+      title={endpointTitle}
+      icon={
+        <>
+          {loading && <SpinnerLoader size="xs" />}
+          {!loading && (
+            <>
+              {endpoint.exposeStatus === "disconnected" && (
+                <div className={classNames("size-4", theme.text2)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 3l18 18"
+                    />
+                  </svg>
+                </div>
               )}
-            />
-          </a>
-        }
-        secondaryLabel={
-          <Button
-            small
-            disabled={loading}
-            className="min-w-[5rem] justify-center"
-            onClick={() => {
-              if (endpoint.exposeStatus === "connected") {
-                hideEndpoint.mutate({ resourcePath: endpoint.id });
-              } else {
-                onExposeEndpoint();
-              }
-            }}
-          >
-            {buttonLabel}
-          </Button>
-        }
-      />
-    </>
+              {endpoint.exposeStatus === "connected" && (
+                <GlobeAltIcon className={classNames("size-4", theme.text1)} />
+              )}
+            </>
+          )}
+        </>
+      }
+      label={
+        <a
+          href={endpoint.url}
+          target="_blank"
+          rel="noreferrer"
+          title={endpoint.url}
+          aria-disabled={loading}
+          className={classNames(
+            "flex gap-1 items-center",
+            "justify-between group/endpoint-tree-item",
+            !disabled && "hover:underline text-sky-500 hover:text-sky-600",
+            disabled && "text-slate-400 cursor-not-allowed",
+          )}
+        >
+          <span className="truncate">{endpoint.label}</span>
+          <ArrowTopRightOnSquareIcon
+            className={classNames(
+              "size-4 shrink-0 hidden",
+              "group-hover/endpoint-tree-item:block",
+            )}
+          />
+        </a>
+      }
+      secondaryLabel={
+        <Button
+          small
+          disabled={loading || disabled}
+          className="min-w-[5rem] justify-center"
+          onClick={() => {
+            if (endpoint.exposeStatus === "connected") {
+              onHideEndpoint();
+            } else {
+              onExposeEndpoint();
+            }
+          }}
+        >
+          {buttonLabel}
+        </Button>
+      }
+    />
   );
 };
 
 export const EndpointTree = () => {
-  const { endpointList } = useEndpoints();
-
+  const { endpointList, exposeEndpoint, hideEndpoint } = useEndpoints();
+  const { requireAcceptWarning, notifyAcceptWarning } = useEndpointsWarning();
   const { showNotification } = useNotifications();
 
+  const [showWarningModal, setShowWarningModal] = useState(false);
+  const [selectedEndpoint, setSelectedEndpoint] = useState<EndpointItem>();
   const [initialNotification, setInitialNotification] = useState(true);
+
+  const loading = useMemo(
+    () => exposeEndpoint.isLoading || hideEndpoint.isLoading,
+    [exposeEndpoint.isLoading, hideEndpoint.isLoading],
+  );
+
+  const onExposeEndpoint = useCallback(
+    async (endpoint: EndpointItem) => {
+      setSelectedEndpoint(endpoint);
+
+      if (requireAcceptWarning.data?.requireAcceptWarning === true) {
+        setShowWarningModal(true);
+        return;
+      }
+      await exposeEndpoint.mutateAsync({ resourcePath: endpoint.id });
+      showNotification(`Endpoint "${endpoint.label}" is exposed`, {
+        type: "info",
+      });
+    },
+    [exposeEndpoint, showNotification, requireAcceptWarning.data],
+  );
+
+  const onHideEndpoint = useCallback(
+    async (endpoint: EndpointItem) => {
+      await hideEndpoint.mutateAsync({ resourcePath: endpoint.id });
+      showNotification(`Endpoint "${endpoint.label}" is hidden`, {
+        type: "info",
+      });
+    },
+    [hideEndpoint, showNotification],
+  );
+
+  const onAcceptWarning = useCallback(async () => {
+    if (selectedEndpoint) {
+      notifyAcceptWarning.mutate();
+      setShowWarningModal(false);
+      await onExposeEndpoint(selectedEndpoint);
+      setSelectedEndpoint(undefined);
+    }
+  }, [notifyAcceptWarning, onExposeEndpoint, selectedEndpoint]);
+
+  useEffect(() => {
+    if (exposeEndpoint.error) {
+      showNotification(exposeEndpoint.error.message, { type: "error" });
+    }
+  }, [exposeEndpoint.error, showNotification]);
 
   useEffect(() => {
     if (!initialNotification) {
@@ -229,13 +246,26 @@ export const EndpointTree = () => {
 
               <TreeView>
                 {endpointList.data?.map((endpoint) => (
-                  <EndpointTreeViewItem key={endpoint.id} endpoint={endpoint} />
+                  <EndpointTreeViewItem
+                    key={endpoint.id}
+                    endpoint={endpoint}
+                    loading={loading && selectedEndpoint?.id === endpoint.id}
+                    disabled={loading && selectedEndpoint?.id !== endpoint.id}
+                    onExposeEndpoint={() => onExposeEndpoint(endpoint)}
+                    onHideEndpoint={() => onHideEndpoint(endpoint)}
+                  />
                 ))}
               </TreeView>
             </div>
           </ScrollableArea>
         </div>
       </div>
+
+      <EndpointsWarningModal
+        visible={showWarningModal}
+        onContinue={onAcceptWarning}
+        onCancel={() => setShowWarningModal(false)}
+      />
     </div>
   );
 };
