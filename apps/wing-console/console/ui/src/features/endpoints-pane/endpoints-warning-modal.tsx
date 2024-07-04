7e89c0cb8718dc4a9315f33359275fb8c3559f15
polamoros <info@polamoros.com>
2024-07-04 14:38:42 +0200

    wip

--- apps/wing-console/console/ui/src/features/endpoints-pane/endpoints-warning-modal.tsx
+++ apps/wing-console/console/ui/src/features/endpoints-pane/endpoints-warning-modal.tsx
@@ -32,32 +32,21 @@ export const EndpointsWarningModal = ({
               "bg-yellow-100 dark:bg-yellow-800",
             )}
           >
-            <ExclamationTriangleIcon className="size-7" />
+            <ExclamationTriangleIcon className="size-6" aria-hidden="true" />
           </div>
-          <div
-            className={classNames(
-              "mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left",
-              "text-md",
-            )}
-          >
-            <h3
-              className={classNames(
-                "text-lg font-semibold leading-6",
-                "text-gray-900 dark:text-slate-100",
-              )}
-            >
-              Expose endpoint · Security warning
+          <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+            <h3 className="text-base font-semibold leading-6 text-slate-900 dark:text-slate-200">
+              Expose endpoint
             </h3>
-            <div
-              className={classNames(
-                "mt-2",
-                "text-gray-500 dark:text-slate-400 text-base",
-              )}
-            >
-              <p>
+            <div className="mt-2">
+              <p className="text-sm text-slate-500 dark:text-slate-400">
                 Exposing an endpoint will make it accessible from the internet.
               </p>
-              <p>Please, ensure you understand the risks before proceeding.</p>
+            </div>
+            <div className="mt-2">
+              <p className="text-sm text-slate-500 dark:text-slate-400">
+                Please, ensure you understand the risks before proceeding.
+              </p>
             </div>
           </div>
         </div>
