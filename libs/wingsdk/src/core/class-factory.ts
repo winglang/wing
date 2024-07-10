winglang/wing

commit a734f030cbb1a0c866f582c87e8138b33a79d5fa

fix!: refactor platforms to expose global class factory (#6099)
[Wing Platforms](https://www.winglang.io/docs/concepts/platforms) allow users to substitute in their own class definitions whenever a class is used in Wing code. To generate classes in JavaScript that extend a class defined by a Wing platform, the Wing compiler needs to generate code shaped something like this:

```
class Child extends getParentType() { ... }
```

where `getParentType()` is just some expression that obtains the parent class type from the Wing platform system. Surprisingly, JavaScript allows for putting expressions like function calls here (which is cool).

The compiler currently generates some code that no-ops, so classes defined by Wing platforms are never injected. We would like to instead generate code that accesses the current Wing platform, but it isn't possible because the only ways to obtain the platform are through the `App`.

To make a fix for this possible, this PR refactors our platform-related code so that we're now exposing the dependency injection factory through the [JavaScript global object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis). Wing generated code can obtain the target-specific version of any class it needs (if an implementation is available) by calling `globalThis.$ClassFactory.new(fqn, scope, id, ...args)`.

The unfortunate drawback of this change is that it pollutes the global namespace, and it means only a single Wing platform can be used for synthesizing apps in a given Node process. (If you want to compile apps to two different platforms, you'll likely need multiple node processes).

It's possible we could avoid this by making Wing-generated JavaScript exports parameterized -- however this may negatively affect the Wing TypeScript experience by introducing more steps for importing Wing libraries:

```
import { main, cloud } from "@wingcloud/framework";
import initPostgres from "@winglibs/postgres";

libs/wingsdk/src/core/class-factory.ts
@@ -1,0 +1,114 @@
+import { Construct, IConstruct } from "constructs";
+import { App } from "./app";
+import { Node } from "../std/node";
+
+const CLASS_FACTORY_SYMBOL = Symbol("@winglang/sdk.core.ClassFactory");
+
+/**
+ * A factory for instantiating new classes defined by platform(s).
+ */
+export class ClassFactory {
+  /**
+   * Returns the ClassFactory for the given scope.
+   * @param scope the scope to get the ClassFactory for
+   * @returns the ClassFactory for the given scope
+   */
+  public static of(scope: IConstruct): ClassFactory {
+    const app = Node.of(scope).app;
+    const factory = (app as any)[CLASS_FACTORY_SYMBOL];
+    if (!factory) {
+      throw new Error("ClassFactory not found");
+    }
+    return factory;
+  }
+
+  private readonly newInstanceOverrides: any[];
+  private readonly resolveTypeOverrides: any[];
+
+  public constructor(newInstanceOverrides: any[], resolveTypeOverrides: any[]) {
+    this.newInstanceOverrides = newInstanceOverrides;
+    this.resolveTypeOverrides = resolveTypeOverrides;
+  }
+
+  /**
+   * Instantiates a new object of the given type.
+   * Throws an error if the type is not supported by any of the registered platforms.
+   */
+  public new(
+    fqn: string,
+    ctor: any,
+    scope: Construct,
+    id: string,
+    ...args: any[]
+  ): any {
+    const instance = this.tryNewInstance(fqn, scope, id, ...args);
+    if (instance) {
+      return instance;
+    }
+
+    if (ctor) {
+      return new ctor(scope, id, ...args);
+    }
+
+    throw new Error(`Unsupported resource type: ${fqn}`);
+  }
+
+  /**
+   * Provides a new instance of an object if an override exists.
+   *
+   * @param fqn string fqn of the resource type
+   * @param scope construct scope
+   * @param id unique string id for resource
+   * @param args args to pass to the resource
+   */
+  public tryNewInstance(
+    fqn: string,
+    scope: Construct,
+    id: string,
+    ...args: any[]
+  ): any {
+    for (const override of this.newInstanceOverrides) {
+      const instance = override(fqn, scope, id, ...args);
+      if (instance) {
+        return instance;
+      }
+    }
+
+    const ctor = this.resolveType(fqn);
+    if (ctor) {
+      return new ctor(scope, id, ...args);
+    }
+
+    return undefined;
+  }
+
+  /**
+   * Provides the type for an fqn (fully qualified name) if an override exists.
+   *
+   * @param fqn string fqn of the resource type
+   */
+  public resolveType(fqn: string): any {
+    for (const override of this.resolveTypeOverrides) {
+      const type = override(fqn);
+      if (type) {
+        return type;
+      }
+    }
+
+    return undefined;
+  }
+
+  /**
+   * Registers the ClassFactory to an App, so that given any construct,
+   * its ClassFactory can be retrieved.
+   *
+   * @param app the App to register to
+   */
+  public register(app: App) {
+    const existing = (app as any)[CLASS_FACTORY_SYMBOL];
+    if (existing && existing !== this) {
+      throw new Error("ClassFactory already registered");
+    }
+    (app as any)[CLASS_FACTORY_SYMBOL] = this;
+  }
+}

commit 4a74587198edf500401f6aca27b49c63f46a8c54

chore: fix azure tests failing in CI (#6878)
Fixes a regression introduced by https://github.com/winglang/wing/pull/6099

## Checklist

- [ ] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
- [ ] Description explains motivation and solution
- [ ] Tests added (always)
- [ ] Docs updated (only required for features)
- [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

libs/wingsdk/src/core/class-factory.ts
@@ -1,5 +1,6 @@
 import { Construct, IConstruct } from "constructs";
 import { App } from "./app";
+import { NotImplementedError } from "./errors";
 import { Node } from "../std/node";
 
 const CLASS_FACTORY_SYMBOL = Symbol("@winglang/sdk.core.ClassFactory");
@@ -50,7 +51,7 @@ export class ClassFactory {
       return new ctor(scope, id, ...args);
     }
 
-    throw new Error(`Unsupported resource type: ${fqn}`);
+    throw new NotImplementedError(`Unsupported resource type: ${fqn}`);
   }
 
   /**