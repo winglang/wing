bf3ba313a1d5c4be3ab7092b659e6b2daceb37db
Chris Rybicki <chrisr@wing.cloud>
2024-09-09 14:08:49 -0400

    fix(compiler): regex is not bringable module

--- packages/@winglang/wingc/src/lib.rs
+++ packages/@winglang/wingc/src/lib.rs
@@ -84,21 +84,19 @@ const WINGSDK_HTTP_MODULE: &'static str = "http";
 const WINGSDK_MATH_MODULE: &'static str = "math";
 const WINGSDK_AWS_MODULE: &'static str = "aws";
 const WINGSDK_EXPECT_MODULE: &'static str = "expect";
-const WINGSDK_REGEX_MODULE: &'static str = "regex";
 const WINGSDK_FS_MODULE: &'static str = "fs";
 const WINGSDK_SIM_MODULE: &'static str = "sim";
 const WINGSDK_UI_MODULE: &'static str = "ui";
 
 pub const UTIL_CLASS_NAME: &'static str = "Util";
 
-const WINGSDK_BRINGABLE_MODULES: [&'static str; 10] = [
+const WINGSDK_BRINGABLE_MODULES: [&'static str; 9] = [
 	WINGSDK_CLOUD_MODULE,
 	WINGSDK_UTIL_MODULE,
 	WINGSDK_HTTP_MODULE,
 	WINGSDK_MATH_MODULE,
 	WINGSDK_AWS_MODULE,
 	WINGSDK_EXPECT_MODULE,
-	WINGSDK_REGEX_MODULE,
 	WINGSDK_FS_MODULE,
 	WINGSDK_SIM_MODULE,
 	WINGSDK_UI_MODULE,
