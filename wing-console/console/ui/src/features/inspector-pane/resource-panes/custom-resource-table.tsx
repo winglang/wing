7898ebffccab7c47021f68dbe6f827607ae09dd6
David Boyne <dboyne@Davids-MacBook-Pro-2.local>
2024-09-24 12:43:11 +0100

    feat(console): added ability to refresh data in table resources

--- wing-console/console/ui/src/features/inspector-pane/resource-panes/custom-resource-table.tsx
+++ wing-console/console/ui/src/features/inspector-pane/resource-panes/custom-resource-table.tsx
@@ -3,6 +3,8 @@ import { memo } from "react";
 import { trpc } from "../../../trpc.js";
 
 import { TableInteraction } from "./table-interaction.js";
+import { Button } from "@wingconsole/design-system";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
 
 export interface CustomResourceTableProps {
   scanHandler: string;
@@ -18,6 +20,15 @@ export const CustomResourceTable = memo(
       <div className="h-full flex-1 flex flex-col text-sm pl-4">
         <div className="flex flex-col gap-2">
           <div className="flex flex-col space-y-1">
+            <div className="flex items-center gap-1 justify-end">
+              <Button
+                label="Refresh"
+                icon={ArrowPathIcon}
+                onClick={() => {
+                  tableScan.refetch();
+                }}
+              />
+            </div>
             <div className="flex items-center gap-2 justify-end">
               {tableScan.data && (
                 <TableInteraction
