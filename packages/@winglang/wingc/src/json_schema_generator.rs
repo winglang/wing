winglang/wing

commit a9a35a6f3630e5e3c26f675e07a5fdc46d000715

feat(compiler): descriptions in generated JSON schemas (#7190)
Co-authored-by: monada-bot[bot] <monabot@monada.co>

packages/@winglang/wingc/src/json_schema_generator.rs
@@ -1,4 +1,5 @@
 use crate::{
+	docs::Docs,
 	jsify::{codemaker::CodeMaker, JSifier},
 	type_check::{symbol_env::SymbolEnv, Struct, Type, UnsafeRef},
 };
@@ -13,10 +14,12 @@ impl JsonSchemaGenerator {
 	fn get_struct_env_properties(&self, env: &SymbolEnv) -> CodeMaker {
 		let mut code = CodeMaker::default();
 		for (field_name, entry) in env.symbol_map.iter() {
-			code.line(format!(
-				"{}: {},",
+			let var_info = entry.kind.as_variable().unwrap();
+			let docs = var_info.docs.clone();
+			code.append(format!(
+				"{}:{},",
 				field_name,
-				self.get_struct_schema_field(&entry.kind.as_variable().unwrap().type_)
+				self.get_struct_schema_field(&entry.kind.as_variable().unwrap().type_, docs)
 			));
 		}
 		code
@@ -24,62 +27,86 @@ impl JsonSchemaGenerator {
 
 	fn get_struct_schema_required_fields(&self, env: &SymbolEnv) -> CodeMaker {
 		let mut code = CodeMaker::default();
-		code.open("required: [");
+		code.append("required:[");
 		for (field_name, entry) in env.symbol_map.iter() {
 			if !matches!(*entry.kind.as_variable().unwrap().type_, Type::Optional(_)) {
-				code.line(format!("\"{}\",", field_name));
+				code.append(format!("\"{}\",", field_name));
 			}
 		}
-		code.close("]");
+		code.append("]");
 		code
 	}
 
-	fn get_struct_schema_field(&self, typ: &UnsafeRef<Type>) -> String {
+	fn get_struct_schema_field(&self, typ: &UnsafeRef<Type>, docs: Option<Docs>) -> String {
 		match **typ {
 			Type::String | Type::Number | Type::Boolean => {
-				format!("{{ type: \"{}\" }}", JSifier::jsify_type(typ).unwrap())
+				let jsified_type = JSifier::jsify_type(typ).unwrap();
+				match docs {
+					Some(docs) => format!(
+						"{{type:\"{}\",description:\"{}\"}}",
+						jsified_type,
+						docs.to_escaped_string()
+					),
+					None => format!("{{type:\"{}\"}}", jsified_type),
+				}
 			}
 			Type::Struct(ref s) => {
 				let mut code = CodeMaker::default();
-				code.open("{");
-				code.line("type: \"object\",");
-				code.open("properties: {");
-				code.add_code(self.get_struct_env_properties(&s.env));
-				code.close("},");
-				code.add_code(self.get_struct_schema_required_fields(&s.env));
-				code.close("}");
+				code.append("{");
+				code.append("type:\"object\",");
+				code.append("properties:{");
+				code.append(self.get_struct_env_properties(&s.env));
+				code.append("},");
+				code.append(self.get_struct_schema_required_fields(&s.env));
+				let docs = docs.unwrap_or(s.docs.clone());
+				code.append(format!(",description:\"{}\"", docs.to_escaped_string()));
+				code.append("}");
 				code.to_string()
 			}
 			Type::Array(ref t) | Type::Set(ref t) => {
 				let mut code = CodeMaker::default();
-				code.open("{");
+				code.append("{");
 
-				code.line("type: \"array\",");
+				code.append("type:\"array\"");
 
 				if matches!(**typ, Type::Set(_)) {
-					code.line("uniqueItems: true,");
+					code.append(",uniqueItems:true");
 				}
 
-				code.line(format!("items: {}", self.get_struct_schema_field(t)));
+				code.append(format!(",items:{}", self.get_struct_schema_field(t, None)));
 
-				code.close("}");
+				if let Some(docs) = docs {
+					code.append(format!(",description:\"{}\"", docs.to_escaped_string()));
+				}
+
+				code.append("}");
 				code.to_string()
 			}
 			Type::Map(ref t) => {
 				let mut code = CodeMaker::default();
-				code.open("{");
+				code.append("{");
 
-				code.line("type: \"object\",");
-				code.line(format!(
-					"patternProperties: {{ \".*\": {} }}",
-					self.get_struct_schema_field(t)
+				code.append("type:\"object\",");
+				code.append(format!(
+					"patternProperties: {{\".*\":{}}}",
+					self.get_struct_schema_field(t, None)
 				));
 
-				code.close("}");
+				if let Some(docs) = docs {
+					code.append(format!("description:\"{}\",", docs.to_escaped_string()));
+				}
+
+				code.append("}");
 				code.to_string()
 			}
-			Type::Optional(t) => self.get_struct_schema_field(&t),
-			Type::Json(_) => "{ type: [\"object\", \"string\", \"boolean\", \"number\", \"array\"] }".to_string(),
+			Type::Optional(ref t) => self.get_struct_schema_field(t, docs),
+			Type::Json(_) => match docs {
+				Some(docs) => format!(
+					"{{type:[\"object\",\"string\",\"boolean\",\"number\",\"array\"],description:\"{}\"}}",
+					docs.to_escaped_string()
+				),
+				None => "{type:[\"object\",\"string\",\"boolean\",\"number\",\"array\"]}".to_string(),
+			},
 			Type::Enum(ref enu) => {
 				let choices = enu
 					.values
@@ -87,33 +114,41 @@ impl JsonSchemaGenerator {
 					.map(|(s, _)| format!("\"{}\"", s))
 					.collect::<Vec<String>>()
 					.join(", ");
-				format!("{{ type: \"string\", enum: [{}] }}", choices)
+				let docs = docs.unwrap_or(enu.docs.clone());
+				format!(
+					"{{type:\"string\",enum:[{}],description:\"{}\"}}",
+					choices,
+					docs.to_escaped_string()
+				)
 			}
-			_ => "{ type: \"null\" }".to_string(),
+			_ => match docs {
+				Some(docs) => format!("{{type:\"null\",description:\"{}\" }}", docs.to_escaped_string()),
+				None => "{type:\"null\"}".to_string(),
+			},
 		}
 	}
 
 	pub fn create_from_struct(&self, struct_: &Struct) -> CodeMaker {
 		let mut code = CodeMaker::default();
 
-		code.open("{");
-		code.line(format!("$id: \"/{}\",", struct_.name));
-		code.line("type: \"object\",".to_string());
+		code.append("{");
+		code.append(format!("$id:\"/{}\",", struct_.name));
+		code.append("type:\"object\",".to_string());
 
-		code.open("properties: {");
+		code.append("properties:{");
 
-		code.add_code(self.get_struct_env_properties(&struct_.env));
+		code.append(self.get_struct_env_properties(&struct_.env));
 
 		//close properties
-		code.close("},");
+		code.append("},");
 
-		code.add_code(self.get_struct_schema_required_fields(&struct_.env));
+		code.append(self.get_struct_schema_required_fields(&struct_.env));
+
+		code.append(format!(",description:\"{}\"", struct_.docs.to_escaped_string()));
 
 		// close schema
-		code.close("}");
+		code.append("}");
 
-		let cleaned = code.to_string().replace("\n", "").replace(" ", "");
-
-		CodeMaker::one_line(cleaned)
+		code
 	}
 }

tests/valid/struct_from_json.test.w
@@ -263,9 +263,15 @@ bring "./subdir/structs_2.w" as otherExternalStructs;
 
 enum Color { red, green, blue }
 
+/// MyStruct docs
+/// @foo bar
 struct MyStruct {
+  /// m1 docs
   m1: externalStructs.MyStruct;
+  /// m2 docs
   m2: otherExternalStructs.MyStruct;
+  /// color docs
+  /// @example Color.red
   color: Color;
 }
 
@@ -288,7 +294,31 @@ expect.equal(myStruct.color, Color.red);
 let schema = MyStruct.schema();
 schema.validate(jMyStruct); // Should not throw exception
 
-let expectedSchema = {"$id":"/MyStruct","type":"object","properties":{"color":{"type":"string","enum":["red","green","blue"]},"m1":{"type":"object","properties":{"val":{"type":"number"}},"required":["val"]},"m2":{"type":"object","properties":{"val":{"type":"string"}},"required":["val"]}},"required":["color","m1","m2"]};
+let expectedSchema = {
+  "$id": "/MyStruct",
+  "type": "object",
+  "properties": {
+    "color": {
+      "type": "string",
+      "enum": ["red", "green", "blue"],
+      "description": "color docs\n@example Color.red",
+    },
+    "m1": {
+      "type": "object",
+      "properties": { "val": { "type": "number", "description": "val docs" } },
+      "required": ["val"],
+      "description": "m1 docs",
+    },
+    "m2": {
+      "type": "object",
+      "properties": { "val": { "type": "string" } },
+      "required": ["val"],
+      "description": "m2 docs",
+    },
+  },
+  "required": ["color", "m1", "m2"],
+  "description": "MyStruct docs\n@foo bar",
+};
 
 expect.equal(schema.asStr(), Json.stringify(expectedSchema));
 

packages/@winglang/sdk/scripts/docgen.mts
@@ -1,4 +1,4 @@
-import { readdir, writeFile, readFile } from "node:fs/promises";
+import { readdir, writeFile, readFile, rm } from "node:fs/promises";
 import { join } from "node:path";
 import { fileURLToPath } from "node:url";
 import { Language, Documentation } from "@winglang/jsii-docgen";
@@ -183,6 +183,9 @@ await generateResourceApiDocs("target-sim", {
   filter: DocumentationFilter.ONLY_WITH_MD,
   jsiiModule: "sim",
 });
+
+// TODO: this file isn't generated correctly
+await rm(getStdlibDocsDir("std/reflect") + ".md");
 
 console.log(
   `${ANSI_LINE_CLEAR}${ANSI_LINE_CLEAR}${docCounter} Docs Generated!`

packages/@winglang/sdk/test/target-sim/__snapshots__/bucket.test.ts.snap
@@ -849,28 +849,3 @@ exports[`removing a key will call onDelete method 1`] = `
   "root/my_bucket/OnDelete/OnMessage0 stopped",
 ]
 `;
-
-exports[`update an object in bucket 1`] = `
-[
-  "root/my_bucket/OnCreate started",
-  "Server listening on http://127.0.0.1:<port>",
-  "root/my_bucket started",
-  "root/my_bucket/Policy started",
-  "root/my_bucket/OnCreate/OnMessage0 started",
-  "root/my_bucket/OnCreate/Policy started",
-  "root/my_bucket/OnCreate/TopicEventMapping0 started",
-  "Sending message (message=1.txt, subscriber=sim-3).",
-  "InvokeAsync (payload="1.txt").",
-  "Publish (messages=1.txt).",
-  "Put (key=1.txt).",
-  "Put (key=1.txt).",
-  "I am done",
-  "root/my_bucket/Policy stopped",
-  "Closing server on http://127.0.0.1:<port>",
-  "root/my_bucket stopped",
-  "root/my_bucket/OnCreate/Policy stopped",
-  "root/my_bucket/OnCreate/TopicEventMapping0 stopped",
-  "root/my_bucket/OnCreate stopped",
-  "root/my_bucket/OnCreate/OnMessage0 stopped",
-]
-`;

packages/@winglang/sdk/test/target-sim/bucket.test.ts
@@ -57,7 +57,6 @@ test("update an object in bucket", async () => {
 
   // THEN
   await s.stop();
-  expect(listMessages(s)).toMatchSnapshot();
   // The bucket notification topic should only publish one message, since the
   // second put() call counts as an update, not a create.
   expect(listMessages(s).filter((m) => m.includes(`Publish`))).toHaveLength(1);

packages/@winglang/wingc/src/docs.rs
@@ -6,7 +6,7 @@ use regex::Regex;
 use crate::{
 	ast::{AccessModifier, Phase},
 	closure_transform::CLOSURE_CLASS_PREFIX,
-	jsify::codemaker::CodeMaker,
+	jsify::{codemaker::CodeMaker, escape_javascript_string},
 	type_check::{
 		jsii_importer::is_construct_base, symbol_env::SymbolEnvKind, Class, ClassLike, Enum, FunctionSignature, Interface,
 		Namespace, Struct, SymbolKind, Type, TypeRef, VariableInfo, VariableKind, CLASS_INFLIGHT_INIT_NAME,
@@ -45,6 +45,50 @@ impl Docs {
 		render_docs(&mut markdown, self);
 
 		markdown.to_string().trim().to_string()
+	}
+
+	pub fn to_escaped_string(&self) -> String {
+		let mut contents = String::new();
+		if let Some(summary) = &self.summary {
+			contents.push_str(summary);
+		}
+		if let Some(remarks) = &self.remarks {
+			contents.push_str("\n\n");
+			contents.push_str(remarks);
+		}
+		if let Some(example) = &self.example {
+			contents.push_str("\n@example ");
+			contents.push_str(example);
+		}
+		if let Some(returns) = &self.returns {
+			contents.push_str("\n@returns ");
+			contents.push_str(returns);
+		}
+		if let Some(deprecated) = &self.deprecated {
+			contents.push_str("\n@deprecated ");
+			contents.push_str(deprecated);
+		}
+		if let Some(see) = &self.see {
+			contents.push_str("\n@see ");
+			contents.push_str(see);
+		}
+		if let Some(default) = &self.default {
+			contents.push_str("\n@default ");
+			contents.push_str(default);
+		}
+		if let Some(stability) = &self.stability {
+			contents.push_str("\n@stability ");
+			contents.push_str(stability);
+		}
+		if let Some(_) = &self.subclassable {
+			contents.push_str("\n@subclassable");
+		}
+		for (k, v) in self.custom.iter() {
+			contents.push_str(&format!("\n@{} ", k));
+			contents.push_str(v);
+		}
+
+		escape_javascript_string(&contents)
 	}
 
 	pub(crate) fn with_summary(summary: &str) -> Docs {

packages/@winglang/wingc/src/jsify.rs
@@ -380,7 +380,7 @@ impl<'a> JSifier<'a> {
 
 			code.line(format!(
 				"const {flat_name} = $stdlib.std.Struct._createJsonSchema({});",
-				schema_code.to_string().replace("\n", "").replace(" ", "")
+				schema_code.to_string()
 			));
 		}
 		code
@@ -2704,7 +2704,7 @@ fn lookup_span(span: &WingSpan, files: &Files) -> String {
 	result
 }
 
-fn escape_javascript_string(s: &str) -> String {
+pub fn escape_javascript_string(s: &str) -> String {
 	let mut result = String::new();
 
 	// escape all escapable characters -- see the section "Escape sequences" in

tests/doc_examples/invalid/32-testing.md_example_1/main.w
@@ -1,9 +1,12 @@
-// This file was auto generated from an example found in: 04-tests.md_example_5
+// This file was auto generated from an example found in: 32-testing.md_example_1
 // Example metadata: {"valid":false}
-// example.w
+bring math;
 bring cloud;
-
 let b = new cloud.Bucket();
+
+test "abs" {
+  assert(1 == math.abs(-1));
+}
 
 test "bucket list should include created file" {
   b.put("file", "lorem ipsum");
@@ -16,6 +19,6 @@ test "bucket starts empty" {
   assert(listOfFile.length == 0);
 }
 
-test "this test should fail" {
+test "this test fails" {
   throw "test throws an exception fails";
 }

commit 181738dd27eca5f373368296946bf551d0875586

fix(compiler): unexpected identifier 'description' (#7194)
Fixes a bug with how we generate properties, a regression from https://github.com/winglang/wing/pull/7190

## Checklist

- [x] Title matches [Winglang's style guide](https://www.winglang.io/contributing/start-here/pull_requests#how-are-pull-request-titles-formatted)
- [x] Description explains motivation and solution
- [x] Tests added (always)
- [ ] Docs updated (only required for features)
- [ ] Added `pr/e2e-full` label if this feature requires end-to-end testing

*By submitting this pull request, I confirm that my contribution is made under the terms of the [Wing Cloud Contribution License](https://github.com/winglang/wing/blob/main/CONTRIBUTION_LICENSE.md)*.

packages/@winglang/wingc/src/json_schema_generator.rs
@@ -88,7 +88,7 @@ impl JsonSchemaGenerator {
 
 				code.append("type:\"object\",");
 				code.append(format!(
-					"patternProperties: {{\".*\":{}}}",
+					"patternProperties: {{\".*\":{}}},",
 					self.get_struct_schema_field(t, None)
 				));
 

tests/valid/struct_from_json.test.w
@@ -345,3 +345,7 @@ test "unsafe flight" {
 
 // Check that imported files can use .fromJson on structs defined within them
 new otherExternalStructs.UsesStructInImportedFile();
+
+let resp = cloud.ApiResponse.fromJson({ status: 200, body: "ok"});
+expect.equal(resp.status, 200);
+expect.equal(resp.body, "ok");