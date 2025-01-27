77fc897e94bbd2b075bde3c046ccc6722f4e6e98
Cristian Pallarés <cristianp@wing.cloud>
2025-01-27 12:39:44 +0100

    wip

--- packages/@winglang/platform-awscdk/test/service.test.ts
+++ packages/@winglang/platform-awscdk/test/service.test.ts
@@ -3,12 +3,44 @@ import { test, expect } from "vitest";
 import { cloud } from "@winglang/sdk";
 import { AwsCdkApp } from "./util";
 import { inflight } from "@winglang/sdk/src/core";
+import { execSync } from "child_process";
 
 const INFLIGHT_CODE = inflight(async (_, name) => {
   console.log("Hello " + name);
 });
 
 test("multiple services", () => {
+  execSync("ls -al /home/runner/work/wing/wing/packages/@winglang/sdk/src", {
+    stdio: "inherit",
+  });
+  execSync("ls -al /home/runner/work/wing/wing/packages/@winglang/sdk/lib", {
+    stdio: "inherit",
+  });
+  execSync(
+    "ls -al /home/runner/work/wing/wing/packages/@winglang/sdk/src/std",
+    {
+      stdio: "inherit",
+    },
+  );
+  execSync(
+    "ls -al /home/runner/work/wing/wing/packages/@winglang/sdk/lib/std",
+    {
+      stdio: "inherit",
+    },
+  );
+  execSync(
+    "ls -al /home/runner/work/wing/wing/packages/@winglang/sdk/src/core",
+    {
+      stdio: "inherit",
+    },
+  );
+  execSync(
+    "ls -al /home/runner/work/wing/wing/packages/@winglang/sdk/lib/core",
+    {
+      stdio: "inherit",
+    },
+  );
+
   // GIVEN
   const app = new AwsCdkApp();
 
