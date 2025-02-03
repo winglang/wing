884077bf2e2ea6776c4ba01eae55bd9aad6628bf
Elad Ben-Israel <eladb@monada.co>
2025-02-03 18:18:39 +0200

    fix

--- packages/@winglang/wingc/src/json_schema_generator.rs
+++ packages/@winglang/wingc/src/json_schema_generator.rs
@@ -38,32 +38,93 @@ impl JsonSchemaGenerator {
 	}
 
 	fn get_struct_schema_field(&self, typ: &UnsafeRef<Type>, docs: Option<Docs>) -> String {
-		let field_type = self.get_struct_schema_field_type(typ, docs.clone());
-		let mut code = CodeMaker::default();
-		code.append("{");
-		code.append(format!("type:{}", field_type));
-		if let Some(docs) = docs {
-			code.append(format!(",description:\"{}\"", docs.to_escaped_string()));
-		}
-		code.append("}");
-		code.to_string()
-	}
-
-	fn get_struct_schema_field_type(&self, typ: &UnsafeRef<Type>, _docs: Option<Docs>) -> String {
 		match **typ {
 			Type::String | Type::Number | Type::Boolean => {
-				format!("\"{}\"", JSifier::jsify_type(typ).unwrap())
+				let jsified_type = JSifier::jsify_type(typ).unwrap();
+				match docs {
+					Some(docs) => format!(
+						"{{type:\"{}\",description:\"{}\"}}",
+						jsified_type,
+						docs.to_escaped_string()
+					),
+					None => format!("{{type:\"{}\"}}", jsified_type),
+				}
 			}
-			Type::Struct(_) => "\"object\"".to_string(),
-			Type::Array(_) | Type::Set(_) => "\"array\"".to_string(),
-			Type::Map(_) => "\"object\"".to_string(),
-			Type::Optional(ref t) => {
-				let inner_type = self.get_struct_schema_field_type(t, None);
-				format!("[\"null\",{}]", inner_type)
+			Type::Struct(ref s) => {
+				let mut code = CodeMaker::default();
+				code.append("{");
+				code.append("type:\"object\",");
+				code.append("properties:{");
+				code.append(self.get_struct_env_properties(&s.env));
+				code.append("},");
+				code.append(self.get_struct_schema_required_fields(&s.env));
+				let docs = docs.unwrap_or(s.docs.clone());
+				code.append(format!(",description:\"{}\"", docs.to_escaped_string()));
+				code.append("}");
+				code.to_string()
 			}
-			Type::Json(_) => "[\"object\",\"string\",\"boolean\",\"number\",\"array\"]".to_string(),
-			Type::Enum(_) => "\"string\"".to_string(),
-			_ => "\"null\"".to_string(),
+			Type::Array(ref t) | Type::Set(ref t) => {
+				let mut code = CodeMaker::default();
+				code.append("{");
+
+				code.append("type:\"array\"");
+
+				if matches!(**typ, Type::Set(_)) {
+					code.append(",uniqueItems:true");
+				}
+
+				code.append(format!(",items:{}", self.get_struct_schema_field(t, None)));
+
+				if let Some(docs) = docs {
+					code.append(format!(",description:\"{}\"", docs.to_escaped_string()));
+				}
+
+				code.append("}");
+				code.to_string()
+			}
+			Type::Map(ref t) => {
+				let mut code = CodeMaker::default();
+				code.append("{");
+
+				code.append("type:\"object\",");
+				code.append(format!(
+					"patternProperties: {{\".*\":{}}},",
+					self.get_struct_schema_field(t, None)
+				));
+
+				if let Some(docs) = docs {
+					code.append(format!("description:\"{}\",", docs.to_escaped_string()));
+				}
+
+				code.append("}");
+				code.to_string()
+			}
+			Type::Optional(ref t) => format!("{{oneOf:[{{type:\"null\"}},{}]}}", self.get_struct_schema_field(t, docs)),
+			Type::Json(_) => match docs {
+				Some(docs) => format!(
+					"{{type:[\"object\",\"string\",\"boolean\",\"number\",\"array\"],description:\"{}\"}}",
+					docs.to_escaped_string()
+				),
+				None => "{type:[\"object\",\"string\",\"boolean\",\"number\",\"array\"]}".to_string(),
+			},
+			Type::Enum(ref enu) => {
+				let choices = enu
+					.values
+					.iter()
+					.map(|(s, _)| format!("\"{}\"", s))
+					.collect::<Vec<String>>()
+					.join(", ");
+				let docs = docs.unwrap_or(enu.docs.clone());
+				format!(
+					"{{type:\"string\",enum:[{}],description:\"{}\"}}",
+					choices,
+					docs.to_escaped_string()
+				)
+			}
+			_ => match docs {
+				Some(docs) => format!("{{type:\"null\",description:\"{}\" }}", docs.to_escaped_string()),
+				None => "{type:\"null\"}".to_string(),
+			},
 		}
 	}
 
