a92daca4a5c5104298500d161720d9a4e9aa1222
eladb <elad.benisrael@gmail.com>
2026-09-02 12:34:22 +0000

    fix(compiler): use child process pool for vitest to support process.chdir()

--- packages/@winglang/compiler/vitest.config.ts
+++ packages/@winglang/compiler/vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     testTimeout: 200_000,
+    // Some tests use process.chdir which is not supported in vitest's multi-thread mode
+    pool: "forks",
     include: ["src/**/*.test.ts"],
     // https://vitest.dev/guide/features.html#chai-and-jest-expect-compatibility
     // Allows to use the matchers added by "aws-sdk-client-mock-jest"
